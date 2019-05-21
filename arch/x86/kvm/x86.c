366c4951e1c33dd25d2a10ffcb9dbaeeafd81755
Sean Christopherson <sean.j.christopherson@intel.com>
2019-05-21 15:06:49 -0700

    KVM: TDX: Handle TDX PV MMIO hypercall
    Export kvm_io_bus_read and kvm_mmio tracepoint and wire up TDX PV MMIO
    hypercall to the KVM backend functions.
    
    kvm_io_bus_read/write() searches KVM device emulated in kernel of the given
    MMIO address and emulates the MMIO.  As TDX PV MMIO also needs it, export
    kvm_io_bus_read().  kvm_io_bus_write() is already exported.  TDX PV MMIO
    emulates some of MMIO itself.  To add trace point consistently with x86
    kvm, export kvm_mmio tracepoint.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -13404,6 +13404,7 @@ bool kvm_arch_dirty_log_supported(struct kvm *kvm)
 
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_entry);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_exit);
+EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_mmio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_fast_mmio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_inj_virq);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_page_fault);

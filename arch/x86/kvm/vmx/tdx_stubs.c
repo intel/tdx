8762b385b129a14d1608e8f1d064a326457f775b
Sean Christopherson <sean.j.christopherson@intel.com>
2019-11-15 09:11:12 -0800

    KVM: TDX: Add methods to ignore accesses to CPU state
    TDX protects TDX guest state from VMM.  Implements to access methods for
    TDX guest state to ignore them or return zero.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/tdx_stubs.c
+++ arch/x86/kvm/vmx/tdx_stubs.c
@@ -10,6 +10,7 @@ int __init tdx_hardware_setup(struct kvm_x86_ops *x86_ops) { return -EOPNOTSUPP;
 void tdx_hardware_enable(void) {}
 void tdx_hardware_disable(void) {}
 
+void tdx_vm_teardown(struct kvm *kvm) {}
 int tdx_vcpu_create(struct kvm_vcpu *vcpu) { return -EOPNOTSUPP; }
 void tdx_vcpu_free(struct kvm_vcpu *vcpu) {}
 void tdx_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event) {}
@@ -17,13 +18,19 @@ void tdx_inject_nmi(struct kvm_vcpu *vcpu) {}
 fastpath_t tdx_vcpu_run(struct kvm_vcpu *vcpu) { return EXIT_FASTPATH_NONE; }
 void tdx_vcpu_load(struct kvm_vcpu *vcpu, int cpu) {}
 void tdx_vcpu_put(struct kvm_vcpu *vcpu) {}
+void tdx_set_virtual_apic_mode(struct kvm_vcpu *vcpu) {}
+
 void tdx_prepare_switch_to_guest(struct kvm_vcpu *vcpu) {}
 void tdx_handle_exit_irqoff(struct kvm_vcpu *vcpu) {}
 int tdx_handle_exit(struct kvm_vcpu *vcpu,
 		enum exit_fastpath_completion fastpath) { return 0; }
+int tdx_get_cpl(struct kvm_vcpu *vcpu) { return 0; }
+unsigned long tdx_get_rflags(struct kvm_vcpu *vcpu) { return 0; }
 bool tdx_is_emulated_msr(u32 index, bool write) { return false; }
 int tdx_get_msr(struct kvm_vcpu *vcpu, struct msr_data *msr) { return 1; }
 int tdx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr) { return 1; }
+u64 tdx_get_segment_base(struct kvm_vcpu *vcpu, int seg) { return 0; }
+void tdx_get_segment(struct kvm_vcpu *vcpu, struct kvm_segment *var, int seg) {}
 
 void tdx_apicv_post_state_restore(struct kvm_vcpu *vcpu) {}
 int tdx_deliver_posted_interrupt(struct kvm_vcpu *vcpu, int vector) { return 0; }

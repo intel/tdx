d81f8899482662e4d163787f5f0fc7e9326b219f
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2019-11-21 15:48:29 +0300

    x86/tdx: Add MSR support for TDX guest
    Operations on context-switched MSRs can be run natively. The rest of
    MSRs should be handled through TDVMCALLs.
    
    TDVMCALL[Instruction.RDMSR] and TDVMCALL[Instruction.WRMSR] provide
    MSR oprations.
    
    You can find RDMSR and WRMSR details in Guest-Host-Communication
    Interface (GHCI) for Intel Trust Domain Extensions (Intel TDX)
    specification, sec 3.10, 3.11.
    
    Also, since CSTAR MSR is not used on Intel CPUs as SYSCALL
    instruction, ignore accesses to CSTAR MSR. Ignore accesses to
    the MSR for compatibility: no need in wrap callers in
    !is_tdx_guest().
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/kernel/tdx.c
+++ arch/x86/kernel/tdx.c
@@ -94,6 +94,84 @@ static __cpuidle void tdx_safe_halt(void)
 	BUG_ON(ret || r10);
 }
 
+static bool tdx_is_context_switched_msr(unsigned int msr)
+{
+	/*  XXX: Update the list of context-switched MSRs */
+
+	switch (msr) {
+	case MSR_EFER:
+	case MSR_IA32_CR_PAT:
+	case MSR_FS_BASE:
+	case MSR_GS_BASE:
+	case MSR_KERNEL_GS_BASE:
+	case MSR_IA32_SYSENTER_CS:
+	case MSR_IA32_SYSENTER_EIP:
+	case MSR_IA32_SYSENTER_ESP:
+	case MSR_STAR:
+	case MSR_LSTAR:
+	case MSR_SYSCALL_MASK:
+	case MSR_IA32_XSS:
+	case MSR_TSC_AUX:
+	case MSR_IA32_BNDCFGS:
+		return true;
+	}
+	return false;
+}
+
+static u64 tdx_read_msr_safe(unsigned int msr, int *err)
+{
+	register long r10 asm("r10") = TDVMCALL_STANDARD;
+	register long r11 asm("r11") = EXIT_REASON_MSR_READ;
+	register long r12 asm("r12") = msr;
+	register long rcx asm("rcx");
+	long ret;
+
+	WARN_ON_ONCE(tdx_is_context_switched_msr(msr));
+
+	if (msr == MSR_CSTAR)
+		return 0;
+
+	/* Allow to pass R10, R11 and R12 down to the VMM */
+	rcx = BIT(10) | BIT(11) | BIT(12);
+
+	asm volatile(TDCALL
+			: "=a"(ret), "=r"(r10), "=r"(r11), "=r"(r12)
+			: "a"(TDVMCALL), "r"(rcx), "r"(r10), "r"(r11), "r"(r12)
+			: );
+
+	/* XXX: Better error handling needed? */
+	*err = (ret || r10) ? -EIO : 0;
+
+	return r11;
+}
+
+static int tdx_write_msr_safe(unsigned int msr, unsigned int low,
+			      unsigned int high)
+{
+	register long r10 asm("r10") = TDVMCALL_STANDARD;
+	register long r11 asm("r11") = EXIT_REASON_MSR_WRITE;
+	register long r12 asm("r12") = msr;
+	register long r13 asm("r13") = (u64)high << 32 | low;
+	register long rcx asm("rcx");
+	long ret;
+
+	WARN_ON_ONCE(tdx_is_context_switched_msr(msr));
+
+	if (msr == MSR_CSTAR)
+		return 0;
+
+	/* Allow to pass R10, R11, R12 and R13 down to the VMM */
+	rcx = BIT(10) | BIT(11) | BIT(12) | BIT(13);
+
+	asm volatile(TDCALL
+			: "=a"(ret), "=r"(r10), "=r"(r11), "=r"(r12), "=r"(r13)
+			: "a"(TDVMCALL), "r"(rcx), "r"(r10), "r"(r11), "r"(r12),
+			  "r"(r13)
+			: );
+
+	return ret || r10 ? -EIO : 0;
+}
+
 void __init tdx_early_init(void)
 {
 	if (!cpuid_has_tdx_guest())
@@ -132,9 +210,22 @@ unsigned long tdx_get_ve_info(struct ve_info *ve)
 int tdx_handle_virtualization_exception(struct pt_regs *regs,
 		struct ve_info *ve)
 {
+	unsigned long val;
+	int ret = 0;
+
 	switch (ve->exit_reason) {
 	case EXIT_REASON_HLT:
 		tdx_halt();
+		break;
+	case EXIT_REASON_MSR_READ:
+		val = tdx_read_msr_safe(regs->cx, (unsigned int *)&ret);
+		if (!ret) {
+			regs->ax = val & UINT_MAX;
+			regs->dx = val >> 32;
+		}
+		break;
+	case EXIT_REASON_MSR_WRITE:
+		ret = tdx_write_msr_safe(regs->cx, regs->ax, regs->dx);
 		break;
 	default:
 		pr_warn("Unexpected #VE: %d\n", ve->exit_reason);
@@ -142,7 +233,8 @@ int tdx_handle_virtualization_exception(struct pt_regs *regs,
 	}
 
 	/* After successful #VE handling, move the IP */
-	regs->ip += ve->instr_len;
+	if (!ret)
+		regs->ip += ve->instr_len;
 
 	return ret;
 }

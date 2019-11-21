cd61641d770853681fd55aad71f8f77705bc497a
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2019-11-21 17:17:39 +0300

    x86/tdx: Wire up KVM hypercalls
    KVM hypercalls use the VMCALL or VMMCALL instructions. Although the ABI
    is similar, those instructions no longer function for TDX guests. Make
    vendor-specific TDVMCALLs (hypercalls to communicate with VMM using
    TDCALL instruction) instead of VMCALL. This enables TDX guests to
    run with KVM acting as the hypervisor. TDX guests running under other
    hypervisors will continue to use those hypervisors' hypercalls.
    
    Since the KVM driver can be built as a kernel module, export
    tdx_kvm_hypercall() to make the symbols visible to kvm.ko.
    
    [Isaku Yamahata: proposed KVM VENDOR string]
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/kernel/tdx.c
+++ arch/x86/kernel/tdx.c
@@ -4,6 +4,8 @@
 #undef pr_fmt
 #define pr_fmt(fmt)     "tdx: " fmt
 
+#include <linux/export.h>
+
 #include <asm/tdx.h>
 #include <asm/vmx.h>
 
@@ -35,6 +37,22 @@ static inline u64 _tdx_hypercall(u64 fn, u64 r12, u64 r13, u64 r14,
 
 	return out->r10;
 }
+
+#ifdef CONFIG_KVM_GUEST
+/* Wrapper for KVM vendor specific hypercall */
+long tdx_kvm_hypercall(unsigned int nr, unsigned long p1, unsigned long p2,
+		       unsigned long p3, unsigned long p4)
+{
+	struct tdx_hypercall_output out;
+
+	/* Non zero return value indicates buggy TDX module, so panic */
+	if (__tdx_hypercall(nr, p1, p2, p3, p4, 0, &out))
+		panic("KVM hypercall failed (Buggy TDX module!))\n");
+
+	return out.r10;
+}
+EXPORT_SYMBOL_GPL(tdx_kvm_hypercall);
+#endif
 
 static __cpuidle void _tdx_halt(const bool irq_disabled, const bool do_sti)
 {

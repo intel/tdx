d3d25da86367430862ad48fc6f2b7b194b5abb02
Zhang Chen <chen.zhang@intel.com>
2019-12-13 16:51:55 +0800

    *** HACK *** KVM: selftests: Add a test case for Intel TDX-SEAM
    Add selftest for validating TDX-SEAM modules.  For now, implement basic
    functionality of probing KVM and (re)loading a SEAM module.
    
    Signed-off-by: Zhang Chen <chen.zhang@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

---
+++ tools/testing/selftests/kvm/x86_64/tdx_load_seam_test.c
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * TDX_load_SEAM_test
+ *
+ * Copyright (C) 2019, Intel.
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.
+ *
+ * Author:
+ *   Zhang Chen <chen.zhang@intel.com>
+ *
+ */
+
+#include <fcntl.h>
+#include <limits.h>
+#include <kvm_util.h>
+#include <linux/kvm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <test_util.h>
+#include <unistd.h>
+#include <processor.h>
+
+void intel_seam_load_test(char *path)
+{
+	int ret, fd;
+
+	fd = open("/dev/kvm", O_RDWR);
+	TEST_ASSERT(fd >= 0, "failed to open /dev/kvm fd: %i errno: %i",
+		    fd, errno);
+
+	ret = ioctl(fd, KVM_LOAD_SEAM, path);
+	TEST_ASSERT(!ret, "KVM_LOAD_SEAM failed ret: %i errno: %i",
+		    ret, errno);
+
+	close(fd);
+}
+
+int main(int argc, char **argv)
+{
+	char path[PATH_MAX];
+
+	TEST_ASSERT(argc == 2, "Must specific path to SEAM module");
+
+	strncpy(path, argv[1], PATH_MAX);
+
+	intel_seam_load_test(path);
+
+	return 0;
+}

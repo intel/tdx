129c3632ca422e81f6972eacb5cca97bc48a6aa9
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2020-01-04 00:24:55 -0800

    x86/cpufeatures: Add TDX Guest CPU feature
    Add CPU feature detection for Trusted Domain Extensions support.
    TDX feature adds capabilities to keep guest register state and
    memory isolated from hypervisor.
    
    For TDX guest platforms, executing CPUID(0x21, 0) will return
    following values in EAX, EBX, ECX and EDX.
    
    EAX:  Maximum sub-leaf number:  0
    EBX/EDX/ECX:  Vendor string:
    
    EBX =  "Inte"
    EDX =  "lTDX"
    ECX =  "    "
    
    So when above condition is true, set X86_FEATURE_TDX_GUEST
    feature cap bit
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>

---
+++ arch/x86/kernel/tdx.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (C) 2020 Intel Corporation */
+
+#include <asm/tdx.h>
+
+static inline bool cpuid_has_tdx_guest(void)
+{
+	u32 eax, signature[3];
+
+	if (cpuid_eax(0) < TDX_CPUID_LEAF_ID)
+		return false;
+
+	cpuid_count(TDX_CPUID_LEAF_ID, 0, &eax, &signature[0],
+			&signature[1], &signature[2]);
+
+	if (memcmp("IntelTDX    ", signature, 12))
+		return false;
+
+	return true;
+}
+
+void __init tdx_early_init(void)
+{
+	if (!cpuid_has_tdx_guest())
+		return;
+
+	setup_force_cpu_cap(X86_FEATURE_TDX_GUEST);
+
+	pr_info("TDX guest is initialized\n");
+}

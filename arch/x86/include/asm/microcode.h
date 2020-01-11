44880360a75b6ead1e49c5df9461daeac3343127
Zhang Chen <chen.zhang@intel.com>
2020-01-11 22:44:13 +1300

    x86/cpu: Move get_builtin_firmware() common code (from microcode only)
    Move get_builtin_firmware() to common.c so that it can be used to get
    non-ucode firmware, e.g. Intel's SEAM modules, even if MICROCODE=n.
    
    Require the consumers to select FW_LOADER, which is already true for
    MICROCODE, instead of having dead code that returns false at runtime.
    
    Signed-off-by: Zhang Chen <chen.zhang@intel.com>
    Co-developed-by: Kai Huang <kai.huang@linux.intel.com>
    Signed-off-by: Kai Huang <kai.huang@linux.intel.com>
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- arch/x86/include/asm/microcode.h
+++ arch/x86/include/asm/microcode.h
@@ -131,15 +131,12 @@ int __init microcode_init(void);
 extern void __init load_ucode_bsp(void);
 extern void load_ucode_ap(void);
 void reload_early_microcode(void);
-extern bool get_builtin_firmware(struct cpio_data *cd, const char *name);
 extern bool initrd_gone;
 #else
 static inline int __init microcode_init(void)			{ return 0; };
 static inline void __init load_ucode_bsp(void)			{ }
 static inline void load_ucode_ap(void)				{ }
 static inline void reload_early_microcode(void)			{ }
-static inline bool
-get_builtin_firmware(struct cpio_data *cd, const char *name)	{ return false; }
 #endif
 
 #endif /* _ASM_X86_MICROCODE_H */

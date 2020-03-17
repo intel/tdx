e6df0ed8893addfe342b0a04f570747c81dcca45
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2020-03-18 02:19:26 +0300

    x86/mm: Move force_dma_unencrypted() to common code
    Intel TDX doesn't allow VMM to access guest memory. Any memory
    that is required for communication with VMM must be shared
    explicitly by setting the bit in page table entry. And, after
    setting the shared bit, the conversion must be completed with
    MapGPA TDVMALL. The call informs VMM about the conversion and
    makes it remove the GPA from the S-EPT mapping. The shared
    memory is similar to unencrypted memory in AMD SME/SEV terminology
    but the underlying process of sharing/un-sharing the memory is
    different for Intel TDX guest platform.
    
    SEV assumes that I/O devices can only do DMA to "decrypted"
    physical addresses without the C-bit set.  In order for the CPU
    to interact with this memory, the CPU needs a decrypted mapping.
    To add this support, AMD SME code forces force_dma_unencrypted()
    to return true for platforms that support AMD SEV feature. It will
    be used for DMA memory allocation API to trigger
    set_memory_decrypted() for platforms that support AMD SEV feature.
    
    TDX is similar.  TDX architecturally prevents access to private
    guest memory by anything other than the guest itself. This means that
    any DMA buffers must be shared.
    
    So move force_dma_unencrypted() out of AMD specific code.
       
    It will be modified to return true for Intel TDX guest platform,
    similar to AMD SEV feature.
    
    Introduce new config option X86_MEM_ENCRYPT_COMMON that has to be
    selected by all x86 memory encryption features. This will be
    selected by both AMD SEV and Intel TDX guest config options.
    
    This is preparation for TDX changes in DMA code and it has not
    functional change.    
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

---
+++ arch/x86/mm/mem_encrypt_common.c
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * AMD Memory Encryption Support
+ *
+ * Copyright (C) 2016 Advanced Micro Devices, Inc.
+ *
+ * Author: Tom Lendacky <thomas.lendacky@amd.com>
+ */
+
+#include <linux/mm.h>
+#include <linux/mem_encrypt.h>
+#include <linux/dma-mapping.h>
+
+/* Override for DMA direct allocation check - ARCH_HAS_FORCE_DMA_UNENCRYPTED */
+bool force_dma_unencrypted(struct device *dev)
+{
+	/*
+	 * For SEV, all DMA must be to unencrypted/shared addresses.
+	 */
+	if (sev_active())
+		return true;
+
+	/*
+	 * For SME, all DMA must be to unencrypted addresses if the
+	 * device does not support DMA to addresses that include the
+	 * encryption mask.
+	 */
+	if (sme_active()) {
+		u64 dma_enc_mask = DMA_BIT_MASK(__ffs64(sme_me_mask));
+		u64 dma_dev_mask = min_not_zero(dev->coherent_dma_mask,
+						dev->bus_dma_limit);
+
+		if (dma_dev_mask <= dma_enc_mask)
+			return true;
+	}
+
+	return false;
+}

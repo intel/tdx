f01a87cce356cb6d07214d713ac4266c5f5fb227
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2020-03-27 15:19:32 -0700

    x86/topology: Disable CPU hotplug support for TDX platforms.
    As per Intel TDX Virtual Firmware Design Guide, sec 4.3.5 and
    sec 9.4, all unused CPUs are put in spinning state by
    TDVF until OS requests for CPU bring-up via mailbox address passed
    by ACPI MADT table. Since by default all unused CPUs are always in
    spinning state, there is no point in supporting dynamic CPU
    online/offline feature. So current generation of TDVF does not
    support CPU hotplug feature. It may be supported in next generation.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/tdx.c
+++ arch/x86/kernel/tdx.c
@@ -387,6 +387,17 @@ static int tdx_handle_mmio(struct pt_regs *regs, struct ve_info *ve)
 	return insn.length;
 }
 
+static int tdx_cpu_offline_prepare(unsigned int cpu)
+{
+	/*
+	 * Per Intel TDX Virtual Firmware Design Guide,
+	 * sec 4.3.5 and sec 9.4, Hotplug is not supported
+	 * in TDX platforms. So don't support CPU
+	 * offline feature once its turned on.
+	 */
+	return -EOPNOTSUPP;
+}
+
 void __init tdx_early_init(void)
 {
 	if (!cpuid_has_tdx_guest())
@@ -401,6 +412,9 @@ void __init tdx_early_init(void)
 
 	pv_ops.irq.safe_halt = tdx_safe_halt;
 	pv_ops.irq.halt = tdx_halt;
+
+	cpuhp_setup_state(CPUHP_AP_ONLINE_DYN, "tdx:cpu_hotplug",
+			  NULL, tdx_cpu_offline_prepare);
 
 	pr_info("TDX guest is initialized\n");
 }

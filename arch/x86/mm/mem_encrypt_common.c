48413c135db2c97546aaefa6b32753dea7b3ca27
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2020-03-30 18:42:56 +0300

    x86/tdx: Make DMA pages shared
    Just like MKTME, TDX reassigns bits of the physical address for
    metadata.  MKTME used several bits for an encryption KeyID. TDX
    uses a single bit in guests to communicate whether a physical page
    should be protected by TDX as private memory (bit set to 0) or
    unprotected and shared with the VMM (bit set to 1).
    
    __set_memory_enc_dec() is now aware about TDX and sets Shared bit
    accordingly following with relevant TDX hypercall.
    
    Also, Do TDACCEPTPAGE on every 4k page after mapping the GPA range
    when converting memory to private. Using 4k page size limit is due
    to current TDX spec restriction. Also, If the GPA (range) was
    already mapped as an active, private page, the host VMM may remove
    the private page from the TD by following the “Removing TD Private
    Pages” sequence in the Intel TDX-module specification [1] to safely
    block the mapping(s), flush the TLB and cache, and remove the
    mapping(s).
    
    BUG() if TDACCEPTPAGE fails (except "previously accepted page" case)
    , as the guest is completely hosed if it can't access memory. 
    
    [1] https://software.intel.com/content/dam/develop/external/us/en/documents/tdx-module-1eas-v0.85.039.pdf
    
    Tested-by: Kai Huang <kai.huang@linux.intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/mm/mem_encrypt_common.c
+++ arch/x86/mm/mem_encrypt_common.c
@@ -9,9 +9,16 @@
 
 #include <asm/mem_encrypt_common.h>
 #include <linux/dma-mapping.h>
+#include <linux/protected_guest.h>
 
 /* Override for DMA direct allocation check - ARCH_HAS_FORCE_DMA_UNENCRYPTED */
 bool force_dma_unencrypted(struct device *dev)
 {
-	return amd_force_dma_unencrypted(dev);
+	if (prot_guest_has(PATTR_SEV) || prot_guest_has(PATTR_SME))
+		return amd_force_dma_unencrypted(dev);
+
+	if (prot_guest_has(PATTR_GUEST_MEM_ENCRYPT))
+		return true;
+
+	return false;
 }

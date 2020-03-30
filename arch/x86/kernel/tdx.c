eef1f2dbd028fd89a05b0911450593f3caec98fe
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2020-03-30 18:42:53 +0300

    x86/tdx: Exclude Shared bit from physical_mask
    Just like MKTME, TDX reassigns bits of the physical address for
    metadata.  MKTME used several bits for an encryption KeyID. TDX
    uses a single bit in guests to communicate whether a physical page
    should be protected by TDX as private memory (bit set to 0) or
    unprotected and shared with the VMM (bit set to 1).
    
    Add a helper, tdx_shared_mask() to generate the mask.  The processor
    enumerates its physical address width to include the shared bit, which
    means it gets included in __PHYSICAL_MASK by default.
    
    Remove the shared mask from 'physical_mask' since any bits in
    tdx_shared_mask() are not used for physical addresses in page table
    entries.
    
    Also, note that shared mapping configuration cannot be clubbed between
    AMD SME and Intel TDX Guest platforms in common function. SME has
    to do it very early in __startup_64() as it sets the bit on all
    memory, except what is used for communication. TDX can postpone it,
    as it don't need any shared mapping in very early boot.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/kernel/tdx.c
+++ arch/x86/kernel/tdx.c
@@ -71,6 +71,12 @@ static inline u64 _tdx_hypercall(u64 fn, u64 r12, u64 r13, u64 r14,
 	return out->r10;
 }
 
+/* The highest bit of a guest physical address is the "sharing" bit */
+phys_addr_t tdx_shared_mask(void)
+{
+	return 1ULL << (td_info.gpa_width - 1);
+}
+
 static void tdx_get_info(void)
 {
 	struct tdx_module_output out;
@@ -94,6 +100,9 @@ static void tdx_get_info(void)
 
 	td_info.gpa_width = out.rcx & GENMASK(5, 0);
 	td_info.attributes = out.rdx;
+
+	/* Exclude Shared bit from the __PHYSICAL_MASK */
+	physical_mask &= ~tdx_shared_mask();
 }
 
 static __cpuidle void _tdx_halt(const bool irq_disabled, const bool do_sti)

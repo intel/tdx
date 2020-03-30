d9578ea636fdfc776afe2a172ef93fc83d5e48a1
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2020-03-30 18:42:53 +0300

    x86/tdx: Exclude Shared bit from physical_mask
    Just like MKTME, TDX reassigns bits of the physical address for
    metadata.  MKTME used several bits for an encryption KeyID. TDX
    uses a single bit in guests to communicate whether a physical page
    should be protected by TDX as private memory (bit set to 0) or
    unprotected and shared with the VMM (bit set to 1).
    
    Add a helper, tdg_shared_mask() to generate the mask.  The processor
    enumerates its physical address width to include the shared bit, which
    means it gets included in __PHYSICAL_MASK by default.
    
    Remove the shared mask from 'physical_mask' since any bits in
    tdg_shared_mask() are not used for physical addresses in page table
    entries.
    
    Also, note that shared mapping configuration cannot be clubbed between
    AMD SME and Intel TDX Guest platforms in common function. SME has
    to do it very early in __startup_64() as it sets the bit on all
    memory, except what is used for communication. TDX can postpone it,
    as it don't need any shared mapping in very early boot.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -77,6 +77,8 @@ int tdg_handle_virtualization_exception(struct pt_regs *regs,
 
 bool tdg_early_handle_ve(struct pt_regs *regs);
 
+extern phys_addr_t tdg_shared_mask(void);
+
 /*
  * To support I/O port access in decompressor or early kernel init
  * code, since #VE exception handler cannot be used, use paravirt
@@ -144,6 +146,8 @@ static inline void tdx_early_init(void) { };
 static inline bool tdx_prot_guest_has(unsigned long flag) { return false; }
 
 static inline bool tdg_early_handle_ve(struct pt_regs *regs) { return false; }
+
+static inline phys_addr_t tdg_shared_mask(void) { return 0; }
 
 #endif /* CONFIG_INTEL_TDX_GUEST */
 

6c0a50bd35a706ec6724bf35ee24fe8d33e89198
Sean Christopherson <sean.j.christopherson@intel.com>
2020-05-04 22:23:08 -0700

    KVM: x86: Add a switch_db_regs flag to handle TDX's auto-switched behavior
    Add a flag, KVM_DEBUGREG_AUTO_SWITCHED_GUEST, to skip saving/restoring DRs
    irrespective of any other flags.  TDX-SEAM unconditionally saves and
    restores guest DRs and reset to architectural INIT state on TD exit.
    So, KVM needs to save host DRs before TD enter without restoring guest DRs
    and restore host DRs after TD exit.
    
    Opportunistically convert the KVM_DEBUGREG_* definitions to use BIT().
    
    Reported-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Co-developed-by: Chao Gao <chao.gao@intel.com>
    Signed-off-by: Chao Gao <chao.gao@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -9704,7 +9704,7 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 	if (test_thread_flag(TIF_NEED_FPU_LOAD))
 		switch_fpu_return();
 
-	if (unlikely(vcpu->arch.switch_db_regs)) {
+	if (unlikely(vcpu->arch.switch_db_regs & ~KVM_DEBUGREG_AUTO_SWITCH_GUEST)) {
 		set_debugreg(0, 7);
 		set_debugreg(vcpu->arch.eff_db[0], 0);
 		set_debugreg(vcpu->arch.eff_db[1], 1);
@@ -9738,6 +9738,7 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 	 */
 	if (unlikely(vcpu->arch.switch_db_regs & KVM_DEBUGREG_WONT_EXIT)) {
 		WARN_ON(vcpu->guest_debug & KVM_GUESTDBG_USE_HW_BP);
+		WARN_ON(vcpu->arch.switch_db_regs & KVM_DEBUGREG_AUTO_SWITCH_GUEST);
 		static_call(kvm_x86_sync_dirty_debug_regs)(vcpu);
 		kvm_update_dr0123(vcpu);
 		kvm_update_dr7(vcpu);

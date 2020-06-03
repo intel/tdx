5f09e10964ea629c64eda0d7f72570fef1e5c753
Sean Christopherson <sean.j.christopherson@intel.com>
2020-06-02 21:48:09 -0700

    KVM: VMX: Move setting of EPT MMU masks to common VT-x code
    EPT MMU masks are used commonly for VMX and TDX.  The value needs to be
    initialized in common code before both VMX/TDX-specific initialization
    code.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/main.c
+++ arch/x86/kvm/vmx/main.c
@@ -5,6 +5,7 @@
 #include "mmu.h"
 #include "vmx.h"
 #include "nested.h"
+#include "mmu.h"
 #include "pmu.h"
 #include "tdx.h"
 #include "tdx_arch.h"
@@ -38,6 +39,14 @@ static __init int vt_hardware_setup(void)
 		return ret;
 
 	enable_tdx = enable_tdx && !tdx_hardware_setup(&vt_x86_ops);
+
+	/*
+	 * As kvm_mmu_set_ept_masks() updates enable_mmio_caching, call it
+	 * before checking enable_mmio_caching.
+	 */
+	if (enable_ept)
+		kvm_mmu_set_ept_masks(enable_ept_ad_bits,
+				      cpu_has_vmx_ept_execute_only());
 
 	/* TDX requires KVM TDP MMU and MMIO caching. */
 	if (enable_tdx && (!tdp_enabled || !enable_mmio_caching)) {

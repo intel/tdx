400bec4da4d102dc372ae911a7e438b01afde780
Kai Huang <kai.huang@linux.intel.com>
2020-06-08 23:00:45 +1200

    KVM: x86: Add per-VM flag to disable in-kernel I/O APIC and level routes
    Add a flag to let TDX disallow the in-kernel I/O APIC, level triggered
    routes for a userspace I/O APIC, and anything else that relies on being
    able to intercept EOIs.  TDX-SEAM does not allow intercepting EOI.
    
    Note, technically KVM could partially emulate the I/O APIC by allowing
    only edge triggered interrupts, but that adds a lot of complexity for
    basically zero benefit.  Ideally KVM wouldn't even allow I/O APIC route
    reservation, but disabling that is a train wreck for Qemu.
    
    Signed-off-by: Kai Huang <kai.huang@linux.intel.com>
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -6154,6 +6154,9 @@ set_identity_unlock:
 			goto create_irqchip_unlock;
 
 		r = -EINVAL;
+		if (kvm->arch.eoi_intercept_unsupported)
+			goto create_irqchip_unlock;
+
 		if (kvm->created_vcpus)
 			goto create_irqchip_unlock;
 
@@ -6185,6 +6188,9 @@ set_identity_unlock:
 		u.pit_config.flags = KVM_PIT_SPEAKER_DUMMY;
 		goto create_pit;
 	case KVM_CREATE_PIT2:
+		r = -EINVAL;
+		if (kvm->arch.eoi_intercept_unsupported)
+			goto out;
 		r = -EFAULT;
 		if (copy_from_user(&u.pit_config, argp,
 				   sizeof(struct kvm_pit_config)))

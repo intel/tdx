08e2360884bac8a184c2ec2535c95e431444a954
Sean Christopherson <sean.j.christopherson@intel.com>
2020-06-10 15:46:38 -0700

    KVM: x86/mmu: Track shadow MMIO value/mask on a per-VM basis
    Define the EPT Violation #VE control bit, #VE info VMCS fields, and the
    suppress #VE bit for EPT entries.
    
    TDX will use a different shadow PTE entry value for MMIO from VMX.  Add
    members to kvm_arch and track value for MMIO per-VM.  By using per-VM EPT
    entry value for MMIO, the existing VMX logic is kept working.
    
    In the case of VMX VM case, the EPT entry for MMIO is non-present PTE
    (present bit cleared) without backing guest physical address (on EPT
    violation, KVM seaching backing guest memory and it finds there is no
    backing guest page.) or the value to trigger EPT misconfiguration.  For
    fast path. Once MMIO is triggered on the EPT entry, the EPT entry is
    updated for the future MMIO.  It allows KVM to understand the memory access
    is for MMIO without searching backing guest pages.). And then KVM parses
    guest instruction to figure out address/value/width for MMIO.
    
    In the case of the guest TD, the guest memory is protected so that VMM
    can't parse guest instruction to trigger EPT violation.  Instead VMM sets
    up (Shared) EPT to trigger #VE.  When the guest TD issues MMIO, #VE is
    injected.  guest VE handler converts MMIO access into MMIO hypercall to
    pass address/value/width for MMIO to VMM. (or directly paravirtualize MMIO
    into hypercall.)  Then VMM can handle the MMIO hypercall without parsing
    guest instruction.
    
    When the guest accesses GPA if "the EPT Violation #VE" control bit is set
    and EPT SUPPRESS VE bit in EPT entry is cleared, #VE, virtualization
    exception, is injected into the guest.  Because the TDX guest vCPU state
    and memory are protected, a VMM can't emulate MMIO by the TDX guest on EPT
    violation by snooping vCPU state and parsing instruction to figure out MMIO
    address and value.  Instead, PV MMIO (MMIO hypercall) is adapted.  On EPT
    violation, CPU injects #VE to guest and the guest converts MMIO instruction
    into PV MMIO.  Or guest directly issues MMIO hypercall.
    
    The existing VMX code uses zero as an initial value for EPT entry.  TDX
    will enable EPT-violation #VE VM-execution control and requires suppress VE
    bit cleared in shared EPT entry to inject #VE into the TDX guest.  To keep
    the same behavior for VMX, suppress VE bit needs to be set.  Allow to
    specify an initial value for EPT entry and if TDX is enabled, set initial
    EPT entry value to suppress VE bit set.  EPT-violation #VE VM-execution
    control will be enabled, and For TDX shared EPT suppress VE bit will be
    cleared for TDX shared EPT entry.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/main.c
+++ arch/x86/kvm/vmx/main.c
@@ -23,9 +23,12 @@ static __init int vt_hardware_setup(void)
 
 	tdx_hardware_setup(&vt_x86_ops);
 
-	if (enable_ept)
+	if (enable_ept) {
+		const u64 init_value = enable_tdx ? VMX_EPT_SUPPRESS_VE_BIT : 0ull;
 		kvm_mmu_set_ept_masks(enable_ept_ad_bits,
-				      cpu_has_vmx_ept_execute_only());
+				      cpu_has_vmx_ept_execute_only(), init_value);
+		kvm_mmu_set_spte_init_value(init_value);
+	}
 
 	return 0;
 }

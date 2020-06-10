2930ba90d925771336241995adcc581cac8a9b7f
Sean Christopherson <sean.j.christopherson@intel.com>
2020-06-10 09:43:45 -0700

    KVM: x86/mmu: Allow per-VM override of the TDP max page level
    TDX requires special handling to support large private page.  For
    simplicity, only support 4K page for TD guest for now.  Add per-VM maximum
    page level support to support different maximum page sizes for TD guest and
    conventional VMX guest.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Acked-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/kvm/mmu/mmu_internal.h
+++ arch/x86/kvm/mmu/mmu_internal.h
@@ -299,7 +299,7 @@ static inline int kvm_mmu_do_page_fault(struct kvm_vcpu *vcpu, gpa_t cr2_or_gpa,
 		.nx_huge_page_workaround_enabled =
 			is_nx_huge_page_enabled(vcpu->kvm),
 
-		.max_level = KVM_MAX_HUGEPAGE_LEVEL,
+		.max_level = vcpu->kvm->arch.tdp_max_page_level,
 		.req_level = PG_LEVEL_4K,
 		.goal_level = PG_LEVEL_4K,
 	};

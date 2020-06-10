26be49ca4dd28a91d81946d0bfcaeb76afd3d847
Sean Christopherson <sean.j.christopherson@intel.com>
2020-06-10 09:43:45 -0700

    KVM: x86/mmu: Allow per-VM override of the TDP max page level
    In the existing x86 KVM MMU code, there is already max_level member in
    struct kvm_page_fault with KVM_MAX_HUGEPAGE_LEVEL initial value.  The KVM
    page fault handler denies page size larger than max_level.
    
    Add per-VM member to indicate the allowed maximum page size with
    KVM_MAX_HUGEPAGE_LEVEL as default value and initialize max_level in struct
    kvm_page_fault with it.
    
    For the guest TD, the set per-VM value for allows maximum page size to 4K
    page size.  Then only allowed page size is 4K.  It means large page is
    disabled.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -5782,6 +5782,8 @@ void kvm_mmu_init_vm(struct kvm *kvm)
 	node->track_write = kvm_mmu_pte_write;
 	node->track_flush_slot = kvm_mmu_invalidate_zap_pages_in_memslot;
 	kvm_page_track_register_notifier(kvm, node);
+
+	kvm->arch.tdp_max_page_level = KVM_MAX_HUGEPAGE_LEVEL;
 }
 
 void kvm_mmu_uninit_vm(struct kvm *kvm)

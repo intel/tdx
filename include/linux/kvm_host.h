45e4ff2eee58a9ccf0206cc123be30ac6bc3d6d8
Isaku Yamahata <isaku.yamahata@intel.com>
2020-06-19 13:52:39 -0700

    KVM: Add per-VM flag to mark read-only memory as unsupported
    Add a flag for TDX to flag RO memory as unsupported and propagate it to
    KVM_MEM_READONLY to allow reporting RO memory as unsupported on a per-VM
    basis.  TDX1 doesn't expose permission bits to the VMM in the SEPT
    tables, i.e. doesn't support read-only private memory.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -811,6 +811,9 @@ struct kvm {
 #ifdef CONFIG_HAVE_KVM_PRIVATE_MEM_ATTR
 	struct xarray mem_attr_array;
 #endif
+#ifdef __KVM_HAVE_READONLY_MEM
+	bool readonly_mem_unsupported;
+#endif
 };
 
 #ifdef CONFIG_HAVE_KVM_PRIVATE_MEM_ATTR

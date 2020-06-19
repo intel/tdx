79043b9a80ae36f738340138dffd23551263a2fb
Sean Christopherson <sean.j.christopherson@intel.com>
2020-06-18 22:00:22 -0700

    KVM: x86/tdp_mmu: Don't zap private pages for unsupported cases
    TDX supports only write-back(WB) memory type for private memory
    architecturally so that (virtualized) memory type change doesn't make sense
    for private memory.  Also currently, page migration isn't supported for TDX
    yet. (TDX architecturally supports page migration. it's KVM and kernel
    implementation issue.)
    
    Regarding memory type change (mtrr virtualization and lapic page mapping
    change), pages are zapped by kvm_zap_gfn_range().  On the next KVM page
    fault, the SPTE entry with a new memory type for the page is populated.
    Regarding page migration, pages are zapped by the mmu notifier. On the next
    KVM page fault, the new migrated page is populated.  Don't zap private
    pages on unmapping for those two cases.
    
    When deleting/moving a KVM memory slot, zap private pages. Typically
    tearing down VM.  Don't invalidate private page tables. i.e. zap only leaf
    SPTEs for KVM mmu that has a shared bit mask. The existing
    kvm_tdp_mmu_invalidate_all_roots() depends on role.invalid with read-lock
    of mmu_lock so that other vcpu can operate on KVM mmu concurrently.  It
    marks the root page table invalid and zaps SPTEs of the root page
    tables. The TDX module doesn't allow to unlink a protected root page table
    from the hardware and then allocate a new one for it. i.e. replacing a
    protected root page table.  Instead, zap only leaf SPTEs for KVM mmu with a
    shared bit mask set.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -966,13 +966,18 @@ bool kvm_tdp_mmu_zap_sp(struct kvm *kvm, struct kvm_mmu_page *sp)
  * operation can cause a soft lockup.
  */
 static bool tdp_mmu_zap_leafs(struct kvm *kvm, struct kvm_mmu_page *root,
-			      gfn_t start, gfn_t end, bool can_yield, bool flush)
+			      gfn_t start, gfn_t end, bool can_yield, bool flush,
+			      bool zap_private)
 {
 	struct tdp_iter iter;
 
 	end = min(end, tdp_mmu_max_gfn_exclusive());
 
 	lockdep_assert_held_write(&kvm->mmu_lock);
+
+	WARN_ON_ONCE(zap_private && !is_private_sp(root));
+	if (!zap_private && is_private_sp(root))
+		return false;
 
 	rcu_read_lock();
 
@@ -1006,12 +1011,13 @@ static bool tdp_mmu_zap_leafs(struct kvm *kvm, struct kvm_mmu_page *root,
  * more SPTEs were zapped since the MMU lock was last acquired.
  */
 bool kvm_tdp_mmu_zap_leafs(struct kvm *kvm, int as_id, gfn_t start, gfn_t end,
-			   bool can_yield, bool flush)
+			   bool can_yield, bool flush, bool zap_private)
 {
 	struct kvm_mmu_page *root;
 
 	for_each_tdp_mmu_root_yield_safe(kvm, root, as_id)
-		flush = tdp_mmu_zap_leafs(kvm, root, start, end, can_yield, flush);
+		flush = tdp_mmu_zap_leafs(kvm, root, start, end, can_yield, flush,
+					  zap_private && is_private_sp(root));
 
 	return flush;
 }
@@ -1071,6 +1077,12 @@ void kvm_tdp_mmu_invalidate_all_roots(struct kvm *kvm)
 
 	lockdep_assert_held_write(&kvm->mmu_lock);
 	list_for_each_entry(root, &kvm->arch.tdp_mmu_roots, link) {
+		/*
+		 * Skip private root since private page table
+		 * is only torn down when VM is destroyed.
+		 */
+		if (is_private_sp(root))
+			continue;
 		if (!root->role.invalid &&
 		    !WARN_ON_ONCE(!kvm_tdp_mmu_get_root(root))) {
 			root->role.invalid = true;
@@ -1255,11 +1267,13 @@ retry:
 	return ret;
 }
 
+/* Used by mmu notifier via kvm_unmap_gfn_range() */
 bool kvm_tdp_mmu_unmap_gfn_range(struct kvm *kvm, struct kvm_gfn_range *range,
-				 bool flush)
+				 bool flush, bool zap_private)
 {
 	return kvm_tdp_mmu_zap_leafs(kvm, range->slot->as_id, range->start,
-				     range->end, range->may_block, flush);
+				     range->end, range->may_block, flush,
+				     zap_private);
 }
 
 typedef bool (*tdp_handler_t)(struct kvm *kvm, struct tdp_iter *iter,

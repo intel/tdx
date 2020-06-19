edea9fc7107f4bf7f25435f7ac63ca908c8e498b
Sean Christopherson <sean.j.christopherson@intel.com>
2020-06-18 17:19:15 -0700

    KVM: VMX: Explicitly check for hv_remote_flush_tlb when loading pgd()
    Explicitly check that kvm_x86_ops.tlb_remote_flush() points at Hyper-V's
    implementation for PV flushing instead of assuming that a non-NULL
    implemenation means running on Hyper-V.  Wrap the related logic in
    ifdeffery as hv_remote_flush_tlb() is defined iff CONFIG_HYPERV!=n.
    
    Short term, the explicit check makes it more obvious why a non-NULL
    tlb_remote_flush() triggers EPTP shenanigans.  Long term, this will
    allow TDX to define its own implementation of tlb_remote_flush() without
    running afoul of Hyper-V.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -3115,14 +3115,15 @@ static void vmx_load_mmu_pgd(struct kvm_vcpu *vcpu, unsigned long pgd,
 		eptp = construct_eptp(vcpu, pgd, pgd_level);
 		vmcs_write64(EPT_POINTER, eptp);
 
-		if (kvm_x86_ops.tlb_remote_flush) {
+#if IS_ENABLED(CONFIG_HYPERV)
+		if (kvm_x86_ops.tlb_remote_flush == hv_remote_flush_tlb) {
 			spin_lock(&to_kvm_vmx(kvm)->ept_pointer_lock);
 			to_vmx(vcpu)->ept_pointer = eptp;
 			to_kvm_vmx(kvm)->ept_pointers_match
 				= EPT_POINTERS_CHECK;
 			spin_unlock(&to_kvm_vmx(kvm)->ept_pointer_lock);
 		}
-
+#endif
 		if (!enable_unrestricted_guest && !is_paging(vcpu))
 			guest_cr3 = to_kvm_vmx(kvm)->ept_identity_map_addr;
 		else if (test_bit(VCPU_EXREG_CR3, (ulong *)&vcpu->arch.regs_avail))
@@ -7012,7 +7013,9 @@ free_vpid:
 
 static int vmx_vm_init(struct kvm *kvm)
 {
+#if IS_ENABLED(CONFIG_HYPERV)
 	spin_lock_init(&to_kvm_vmx(kvm)->ept_pointer_lock);
+#endif
 
 	if (!ple_gap)
 		kvm->arch.pause_in_guest = true;

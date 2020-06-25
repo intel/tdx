40e317a2e71969083787e11a6fc7e12a8b174fd3
Sean Christopherson <sean.j.christopherson@intel.com>
2020-06-24 21:14:14 -0700

    KVM: TDX: Add load_mmu_pgd method for TDX
    For virtual IO, the guest TD shares guest pages with VMM without
    encryption.  Shared EPT is used to map guest pages in unprotected way.
    
    Add the VMCS field encoding for the shared EPTP, which will be used by
    TDX to have separate EPT walks for private GPAs (existing EPTP) versus
    shared GPAs (new shared EPTP).
    
    Set shared EPT pointer value for the TDX guest to initialize TDX MMU.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/tdx_stubs.c
+++ arch/x86/kvm/vmx/tdx_stubs.c
@@ -15,3 +15,5 @@ void tdx_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event) {}
 int tdx_dev_ioctl(void __user *argp) { return -EOPNOTSUPP; }
 int tdx_vm_ioctl(struct kvm *kvm, void __user *argp) { return -EOPNOTSUPP; }
 int tdx_vcpu_ioctl(struct kvm_vcpu *vcpu, void __user *argp) { return -EOPNOTSUPP; }
+
+void tdx_load_mmu_pgd(struct kvm_vcpu *vcpu, hpa_t root_hpa, int root_level) {}

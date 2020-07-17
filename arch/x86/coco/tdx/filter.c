6a2808d0938df72189b251a625eb5911c27e2334
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2020-07-17 11:12:09 +0300

    x86/tdx: Implement port I/O filtering
    In TDX guest, since host is an untrusted entity, interactions via
    IO ports had to be hardened to keep the VM guest secure. So by
    default block all IO ports and add support for allowlist of IO
    ports that can be used for communication with the host. Only RTC
    and PCI ranges are essential.
    
    There are a lot of other IO port interactions that are difficult to
    harden. So let's just block them by default to avoid any security
    risks from unsafe drivers. Except for the listed blocks a modern
    virtualized system shouldn't really need any IO ports.
    
    This is an extra safety net against drivers that don't use the normal
    driver registration infrastructure to make sure they cannot be loaded
    by a malicious hypervisor.
    
    The filtering is disabled for decompression code. It doesn't have
    significant attack surface to care about.
    
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

---
+++ arch/x86/coco/tdx/filter.c
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2020 Intel Corporation
+ */
+#define pr_fmt(fmt) "TDX: " fmt
+
+#include <asm/tdx.h>
+
+bool tdx_allowed_port(short int port)
+{
+	if (tdx_debug_enabled())
+		return true;
+
+	switch (port) {
+	/* MC146818 RTC */
+	case 0x70 ... 0x71:
+	/* i8237A DMA controller */
+	case 0x80 ... 0x8f:
+	/* PCI */
+	case 0xcd8 ... 0xcdf:
+	case 0xcf8 ... 0xcff:
+		return true;
+	/* PCIE hotplug device state for Q35 machine type */
+	case 0xcc4:
+	case 0xcc8:
+		return true;
+	/* ACPI ports list:
+	 * 0600-0603 : ACPI PM1a_EVT_BLK
+	 * 0604-0605 : ACPI PM1a_CNT_BLK
+	 * 0608-060b : ACPI PM_TMR
+	 * 0620-062f : ACPI GPE0_BLK
+	 */
+	case 0x600 ... 0x62f:
+		return true;
+	/* serial */
+	case 0x2e8 ... 0x2ef:
+	case 0x2f8 ... 0x2ff:
+	case 0x3e8 ... 0x3ef:
+	case 0x3f8 ... 0x3ff:
+		return tdx_debug_enabled();
+	default:
+		return false;
+	}
+}
+

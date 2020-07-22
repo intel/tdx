95e0f7230c7581c1b074ef060cd78c72b41039ed
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2020-07-21 21:49:29 -0700

    x86/TDX: Add command line option to update TDX filter allow-list
    Add a kernel command line option to update TDX PCI and ACPI device
    allow-list. This will help users to allow selected devices without
    updating the TDX device allow-list in kernel.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/kernel/tdx-filter.c
+++ arch/x86/kernel/tdx-filter.c
@@ -9,6 +9,8 @@
 #include <linux/acpi.h>
 #include <linux/pci.h>
 
+#define CMDLINE_FILTER_LIMIT 100
+
 static bool pci_bus_filter_cb(struct device *dev);
 static bool tdx_device_filter(struct device *dev);
 
@@ -44,6 +46,9 @@ static struct pci_filter_node pci_fnodes[] = {
 	{ PCI_VENDOR_ID_REDHAT_QUMRANET, 0x1049 }, /* Virtio 1.0 FS */
 };
 
+static struct pci_filter_node cmdline_pci_fnodes[CMDLINE_FILTER_LIMIT];
+static int cmdline_pci_len;
+
 /* List of ACPI HID allow-list */
 static const char *const acpi_flist[] = {
 	"PNP0A06",
@@ -56,6 +61,114 @@ static const char *const acpi_flist[] = {
 	"PNP0501",
 	"device"
 };
+
+static char *cmdline_acpi_fnodes[CMDLINE_FILTER_LIMIT];
+static int cmdline_acpi_len;
+
+static bool pci_fnode_decode(char *str,  struct pci_filter_node *node)
+{
+	char *fentry;
+	int ret;
+
+	/* get bus name */
+	fentry = strsep(&str, ":");
+	if (!strlen(fentry))
+		goto decode_failed;
+
+	/* get PCI vendor ID */
+	fentry = strsep(&str, ":");
+	if (!strlen(fentry))
+		goto decode_failed;
+
+	ret = kstrtou16(fentry, 16, &node->vendor);
+	if (ret < 0)
+		goto decode_failed;
+
+	/* get PCI device ID */
+	fentry = strsep(&str, ":");
+	if (!strlen(fentry))
+		goto decode_failed;
+
+	ret = kstrtou16(fentry, 16, &node->device);
+	if (ret < 0)
+		goto decode_failed;
+
+	return true;
+
+decode_failed:
+	node->vendor = 0;
+	node->device = 0;
+	return false;
+}
+
+static bool acpi_fnode_decode(char *str,  char **node)
+{
+	char *fentry;
+
+	/* get bus name */
+	fentry = strsep(&str, ":");
+	if (!strlen(fentry))
+		goto decode_failed;
+
+	/* get ACPI ID */
+	fentry = strsep(&str, ":");
+	if (!strlen(fentry))
+		goto decode_failed;
+
+	*node = fentry;
+	return true;
+
+decode_failed:
+	*node = NULL;
+	return false;
+}
+
+/*
+ * Command line parameter setup to update PCI and ACPI
+ * device filter allow-list.
+ */
+static int __init setup_tdx_filter_devids(char *str)
+{
+	char *p, *fentry;
+
+	/* check for empty string*/
+	if (!str || str[0] == '\0')
+		return 1;
+
+	p = str;
+	while ((fentry = strsep(&p, ","))) {
+		/* do nothing for empty entry */
+		if (!strlen(fentry))
+			continue;
+
+		if (!strncmp(fentry, "pci", 3)) {
+			if (cmdline_pci_len >= CMDLINE_FILTER_LIMIT) {
+				pr_err("PCI allowlist entries crosses limit\n");
+				continue;
+			}
+			if (!pci_fnode_decode(fentry,
+				&cmdline_pci_fnodes[cmdline_pci_len])) {
+				pr_err("Invalid PCI IDs: %s\n", fentry);
+				continue;
+			}
+			cmdline_pci_len++;
+		} else if (!strncmp(fentry, "acpi", 4)) {
+			if (cmdline_acpi_len >= CMDLINE_FILTER_LIMIT) {
+				pr_err("ACPI filter entries crosses limit\n");
+				continue;
+			}
+			if (!acpi_fnode_decode(fentry,
+				&cmdline_acpi_fnodes[cmdline_acpi_len])) {
+				pr_err("Invalid ACPI IDs: %s\n", fentry);
+				continue;
+			}
+			cmdline_acpi_len++;
+		}
+	}
+
+	return 0;
+}
+early_param("tdx_filter_devids", setup_tdx_filter_devids);
 
 /* get the filter node for the give bus name */
 struct filter_node *get_bus_fnode(const char *bus)
@@ -85,6 +198,16 @@ static bool pci_bus_filter_cb(struct device *dev)
 		}
 	}
 
+	for (i = 0; i < cmdline_pci_len; i++) {
+		if ((cmdline_pci_fnodes[i].vendor == pdev->vendor ||
+		     cmdline_pci_fnodes[i].vendor == PCI_ANY_ID) &&
+		    (cmdline_pci_fnodes[i].device == pdev->device ||
+		     cmdline_pci_fnodes[i].device == PCI_ANY_ID)) {
+			status = true;
+			break;
+		}
+	}
+
 	pr_info("PCI vendor:%x device:%x %s\n", pdev->vendor,
 		pdev->device, status ? "allowed" : "blocked");
 
@@ -102,6 +225,15 @@ static bool acpi_hid_verify(struct device *dev)
 	for (i = 0; i < ARRAY_SIZE(acpi_flist); i++) {
 		if (!strncmp(acpi_flist[i], acpi_device_hid(adev),
 			    strlen(acpi_flist[i]))) {
+			pr_debug("ACPI HID:%s allowed\n",
+				 acpi_device_hid(adev));
+			return true;
+		}
+	}
+
+	for (i = 0; i < cmdline_acpi_len; i++) {
+		if (!strncmp(cmdline_acpi_fnodes[i], acpi_device_hid(adev),
+			     strlen(cmdline_acpi_fnodes[i]))) {
 			pr_debug("ACPI HID:%s allowed\n",
 				 acpi_device_hid(adev));
 			return true;

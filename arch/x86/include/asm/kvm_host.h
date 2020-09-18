5612bf64fdfee29a75906aa54571369e25612896
Sean Christopherson <sean.j.christopherson@intel.com>
2020-09-17 23:54:17 -0700

    KVM: x86: Add flag to disallow #MC injection / KVM_X86_SETUP_MCE
    Add a flag to disallow MCE injection and reject KVM_X86_SETUP_MCE with
    -EINVAL when set.  TDX doesn't support injecting exceptions, including
    (virtual) #MCs.
    
    Signed-off-by: Kai Huang <kai.huang@linux.intel.com>
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -964,6 +964,7 @@ struct kvm_arch {
 
 	bool guest_state_protected;
 	bool irq_injection_disallowed;
+	bool mce_injection_disallowed;
 
 	struct kvm_pmu_event_filter *pmu_event_filter;
 	struct task_struct *nx_lpage_recovery_thread;

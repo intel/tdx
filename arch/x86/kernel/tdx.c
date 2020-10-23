56c149c7050d07d3390c0737221530cf987175e5
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2020-10-23 10:09:39 +0000

    [REVERTME] x86/tdx: Add command line option to force TDX mode
    In order to deal with faulty BIOS or TDX Module, allow user to
    force enable the TDX mode from kernel command line option.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/kernel/tdx.c
+++ arch/x86/kernel/tdx.c
@@ -8,6 +8,7 @@
 #include <linux/io.h>
 
 #include <asm/tdx.h>
+#include <asm/cmdline.h>
 #include <asm/i8259.h>
 #include <asm/apic.h>
 #include <asm/idtentry.h>
@@ -789,7 +790,14 @@ __init bool tdg_early_handle_ve(struct pt_regs *regs)
 
 void __init tdx_early_init(void)
 {
-	if (!cpuid_has_tdx_guest())
+	bool tdx_guest_forced;
+
+	tdx_guest_forced = cmdline_find_option_bool(boot_command_line,
+						    "force_tdx_guest");
+	if (tdx_guest_forced)
+		pr_info("Force enabling TDX Guest feature\n");
+
+	if (!cpuid_has_tdx_guest() && !tdx_guest_forced)
 		return;
 
 	setup_force_cpu_cap(X86_FEATURE_TDX_GUEST);

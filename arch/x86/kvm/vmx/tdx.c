9daab540f3d88a6e56c791df8a98d19834277540
Xiaoyao Li <xiaoyao.li@intel.com>
2020-10-26 16:36:29 +0800

    *** HACK *** KVM: TDX: Intercept triple fault exit reason from SEAM module
    When triple fault happens in TD vcpu, SEAM module sets vcpu to teardown
    state and disallow the TD vcpu to be associated anymore. This makes KVM
    hard to know the vcpu state for debugging purpose.
    
    Enable tdexit redirection on triple fault, which makes SEAM module
    redirect the triple fault to KVM without turning vcpu to teardown state.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -593,8 +593,12 @@ static void tdx_hardware_enable(void)
 {
 	INIT_LIST_HEAD(&per_cpu(associated_tdvcpus, raw_smp_processor_id()));
 
-	if (!cmpxchg(&trace_seamcalls_initialized, 0, 1))
+	if (!cmpxchg(&trace_seamcalls_initialized, 0, 1)) {
 		tdx_trace_seamcalls(trace_seamcalls);
+
+		/* Unconditionally intercept triple faults to aid debug. */
+		tdxmode(true, BIT_ULL(EXIT_REASON_TRIPLE_FAULT));
+	}
 }
 
 static void tdx_hardware_disable(void)
@@ -1252,6 +1256,8 @@ static int tdx_handle_exit(struct kvm_vcpu *vcpu,
 		 * needs to be done in KVM.
 		 */
 		return 1;
+	case EXIT_REASON_TRIPLE_FAULT:
+		return tdx_handle_triple_fault(vcpu);
 	default:
 		break;
 	}

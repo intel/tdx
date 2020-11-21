df3e58c5ec1c64a837fd086041e8d37f0e263467
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2020-11-21 05:24:39 +0000

    virt: tdx-guest: Add Quote generation support using TSM_REPORTS
    In TDX guest, the attestation process is used to verify the TDX guest
    trustworthiness to other entities before provisioning secrets to the
    guest. The First step in the attestation process is TDREPORT
    generation, which involves getting the guest measurement data in the
    format of TDREPORT, which is further used to validate the authenticity
    of the TDX guest. TDREPORT by design is integrity-protected and can
    only be verified on the local machine.
    
    To support remote verification of the TDREPORT (in a SGX-based
    attestation), the TDREPORT needs to be sent to the SGX Quoting Enclave
    (QE) to convert it to a remotely verifiable Quote. SGX QE by design can
    only run outside of the TDX guest (i.e. in a host process or in a
    normal VM) and guest can use communication channels like vsock or
    TCP/IP to send the TDREPORT to the QE. But for security concerns, the
    TDX guest may not support these communication channels. To handle such
    cases, TDX defines a GetQuote hypercall which can be used by the guest
    to request the host VMM to communicate with the SGX QE. More details
    about GetQuote hypercall can be found in TDX Guest-Host Communication
    Interface (GHCI) for Intel TDX 1.0, section titled
    "TDG.VP.VMCALL<GetQuote>".
    
    Trusted Security Module (TSM) [1] exposes a common ABI for Confidential
    Computing Guest platforms to get the measurement data via ConfigFS.
    Extend the TSM framework and add support to allow an attestation agent
    to get the TDX Quote data (included usage example below).
    
      report=/sys/kernel/config/tsm/report/report0
      mkdir $report
      dd if=/dev/urandom bs=64 count=1 > $report/inblob
      hexdump -C $report/outblob
      rmdir $report
    
    GetQuote TDVMCALL requires TD guest pass a 4K aligned shared buffer
    with TDREPORT data as input, which is further used by the VMM to copy
    the TD Quote result after successful Quote generation. To create the
    shared buffer, allocate a large enough memory and mark it shared using
    set_memory_decrypted() in tdx_guest_init(). This buffer will be re-used
    for GetQuote requests in the TDX TSM handler.
    
    Although this method reserves a fixed chunk of memory for GetQuote
    requests, such one time allocation can help avoid memory fragmentation
    related allocation failures later in the uptime of the guest.
    
    Since the Quote generation process is not time-critical or frequently
    used, the current version uses a polling model for Quote requests and
    it also does not support parallel GetQuote requests.
    
    Link: https://lore.kernel.org/lkml/169342399185.3934343.3035845348326944519.stgit@dwillia2-xfh.jf.intel.com/ [1]
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- drivers/virt/coco/tdx-guest/tdx-guest.c
+++ drivers/virt/coco/tdx-guest/tdx-guest.c
@@ -13,6 +13,10 @@
 #include <linux/string.h>
 #include <linux/uaccess.h>
 #include <linux/set_memory.h>
+#include <linux/io.h>
+#include <linux/delay.h>
+#include <linux/tsm.h>
+#include <linux/sizes.h>
 
 #include <uapi/linux/tdx-guest.h>
 
@@ -25,6 +29,18 @@
  * 3rd party implementations.
  */
 #define GET_QUOTE_MAX_SIZE		(4 * PAGE_SIZE)
+
+/*
+ * Intel's SGX QE implementation generally uses Quote size less
+ * than 8K (2K Quote data + ~5K of certificate blob).
+ */
+#define GET_QUOTE_BUF_SIZE		SZ_8K
+
+#define GET_QUOTE_CMD_VER		1
+
+/* TDX GetQuote status codes */
+#define GET_QUOTE_SUCCESS		0
+#define GET_QUOTE_IN_FLIGHT		0xffffffffffffffff
 
 /**
  * struct quote_entry - Quote request struct
@@ -43,8 +59,17 @@ struct quote_entry {
 /* Quote data entry */
 static struct quote_entry *qentry;
 
+/* Quote data buffer */
+static void *quote_data;
+
 /* Lock to streamline quote requests */
 static DEFINE_MUTEX(quote_lock);
+
+/*
+ * GetQuote request timeout in seconds. Expect that 30 seconds
+ * is enough time for QE to respond to any Quote requests.
+ */
+static u32 getquote_timeout = 30;
 
 static int quote_cb_handler(void *dev_id)
 {
@@ -201,6 +226,150 @@ quote_failed:
 	return ret;
 }
 
+static void free_quote_buf(void *buf)
+{
+	size_t len = PAGE_ALIGN(GET_QUOTE_BUF_SIZE);
+	unsigned int count = len >> PAGE_SHIFT;
+
+	if (set_memory_encrypted((unsigned long)buf, count)) {
+		pr_err("Failed to restore encryption mask for Quote buffer, leak it\n");
+		return;
+	}
+
+	free_pages_exact(buf, len);
+}
+
+static void *alloc_quote_buf(void)
+{
+	size_t len = PAGE_ALIGN(GET_QUOTE_BUF_SIZE);
+	unsigned int count = len >> PAGE_SHIFT;
+	void *addr;
+
+	addr = alloc_pages_exact(len, GFP_KERNEL | __GFP_ZERO);
+	if (!addr)
+		return NULL;
+
+	if (set_memory_decrypted((unsigned long)addr, count)) {
+		free_pages_exact(addr, len);
+		return NULL;
+	}
+
+	return addr;
+}
+
+/*
+ * wait_for_quote_completion() - Wait for Quote request completion
+ * @quote_buf: Address of Quote buffer.
+ * @timeout: Timeout in seconds to wait for the Quote generation.
+ *
+ * As per TDX GHCI v1.0 specification, sec titled "TDG.VP.VMCALL<GetQuote>",
+ * the status field in the Quote buffer will be set to GET_QUOTE_IN_FLIGHT
+ * while VMM processes the GetQuote request, and will change it to success
+ * or error code after processing is complete. So wait till the status
+ * changes from GET_QUOTE_IN_FLIGHT or the request being timed out.
+ */
+static int wait_for_quote_completion(struct tdx_quote_buf *quote_buf, u32 timeout)
+{
+	int i = 0;
+
+	/*
+	 * Quote requests usually take a few seconds to complete, so waking up
+	 * once per second to recheck the status is fine for this use case.
+	 */
+	while (quote_buf->status == GET_QUOTE_IN_FLIGHT && i++ < timeout) {
+		if (msleep_interruptible(MSEC_PER_SEC))
+			return -EINTR;
+	}
+
+	return (i == timeout) ? -ETIMEDOUT : 0;
+}
+
+static u8 *tdx_report_new(const struct tsm_desc *desc, void *data, size_t *outblob_len)
+{
+	struct tdx_quote_buf *quote_buf = quote_data;
+	int ret;
+	u8 *buf;
+	u64 err;
+
+	if (mutex_lock_interruptible(&quote_lock))
+		return ERR_PTR(-EINTR);
+
+	/*
+	 * If the previous request is timedout or interrupted, and the
+	 * Quote buf status is still in GET_QUOTE_IN_FLIGHT (owned by
+	 * VMM), don't permit any new request.
+	 */
+	if (quote_buf->status == GET_QUOTE_IN_FLIGHT) {
+		ret = -EBUSY;
+		goto done;
+	}
+
+	if (desc->inblob_len != TDX_REPORTDATA_LEN) {
+		ret = -EINVAL;
+		goto done;
+	}
+
+	/* TDX attestation only supports default format request */
+	if (desc->outblob_format != TSM_FORMAT_DEFAULT) {
+		ret = -EINVAL;
+		goto done;
+	}
+
+	u8 *reportdata __free(kfree) = kmalloc(TDX_REPORTDATA_LEN, GFP_KERNEL);
+	if (!reportdata) {
+		ret = -ENOMEM;
+		goto done;
+	}
+
+	u8 *tdreport __free(kfree) = kzalloc(TDX_REPORT_LEN, GFP_KERNEL);
+	if (!tdreport) {
+		ret = -ENOMEM;
+		goto done;
+	}
+
+	memcpy(reportdata, desc->inblob, desc->inblob_len);
+
+	/* Generate TDREPORT0 using "TDG.MR.REPORT" TDCALL */
+	ret = tdx_mcall_get_report0(reportdata, tdreport);
+	if (ret) {
+		pr_err("GetReport call failed\n");
+		goto done;
+	}
+
+	memset(quote_data, 0, GET_QUOTE_BUF_SIZE);
+
+	/* Update Quote buffer header */
+	quote_buf->version = GET_QUOTE_CMD_VER;
+	quote_buf->in_len = TDX_REPORT_LEN;
+
+	memcpy(quote_buf->data, tdreport, TDX_REPORT_LEN);
+
+	err = tdx_hcall_get_quote(quote_data, GET_QUOTE_BUF_SIZE);
+	if (err) {
+		pr_err("GetQuote hypercall failed, status:%llx\n", err);
+		ret = -EIO;
+		goto done;
+	}
+
+	ret = wait_for_quote_completion(quote_buf, getquote_timeout);
+	if (ret) {
+		pr_err("GetQuote request timedout\n");
+		goto done;
+	}
+
+	buf = kvmemdup(quote_buf->data, quote_buf->out_len, GFP_KERNEL);
+	if (!buf) {
+		ret = -ENOMEM;
+		goto done;
+	}
+
+	*outblob_len = quote_buf->out_len;
+
+done:
+	mutex_unlock(&quote_lock);
+	return ret ? ERR_PTR(ret) : buf;
+}
+
 static long tdx_guest_ioctl(struct file *file, unsigned int cmd,
 			    unsigned long arg)
 {
@@ -232,6 +401,11 @@ static const struct x86_cpu_id tdx_guest_ids[] = {
 };
 MODULE_DEVICE_TABLE(x86cpu, tdx_guest_ids);
 
+static const struct tsm_ops tdx_tsm_ops = {
+	.name = KBUILD_MODNAME,
+	.report_new = tdx_report_new,
+};
+
 static int __init tdx_guest_init(void)
 {
 	int ret;
@@ -245,18 +419,32 @@ static int __init tdx_guest_init(void)
 
 	qentry = alloc_quote_entry(GET_QUOTE_MAX_SIZE);
 	if (!qentry) {
-		pr_err("Failed to allocate Quote buffer\n");
 		ret = -ENOMEM;
 		goto free_misc;
 	}
 
+	quote_data = alloc_quote_buf();
+	if (!quote_data) {
+		pr_err("Failed to allocate Quote buffer\n");
+		ret = -ENOMEM;
+		goto free_quote_entry;
+	}
+
 	ret = tdx_register_event_irq_cb(quote_cb_handler, qentry);
 	if (ret)
-		goto free_quote;
+		goto free_quote_buf;
+
+	ret = register_tsm(&tdx_tsm_ops, NULL, NULL);
+	if (ret)
+		goto free_irq_cb;
 
 	return 0;
 
-free_quote:
+free_irq_cb:
+	tdx_unregister_event_irq_cb(quote_cb_handler, qentry);
+free_quote_buf:
+	free_quote_buf(quote_data);
+free_quote_entry:
 	free_quote_entry(qentry);
 free_misc:
 	misc_deregister(&tdx_misc_dev);
@@ -268,7 +456,9 @@ module_init(tdx_guest_init);
 static void __exit tdx_guest_exit(void)
 {
 	tdx_unregister_event_irq_cb(quote_cb_handler, qentry);
+	unregister_tsm(&tdx_tsm_ops);
 	free_quote_entry(qentry);
+	free_quote_buf(quote_data);
 	misc_deregister(&tdx_misc_dev);
 }
 module_exit(tdx_guest_exit);

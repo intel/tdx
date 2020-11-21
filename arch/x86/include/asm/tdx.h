0f0a0de9c492c2fe97016d1c7b2e1d5712eb14a2
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2020-11-21 05:04:37 +0000

    x86/tdx: Add GetQuote TDVMCALL support
    GetQuote TDVMCALL is a doorbell-like interface used to help
    send a message to the host VMM to queue operations that tend
    to be long-running operations. GetQuote is designed to invoke
    a request to generate a TD-Quote signing by a TD-Quoting Enclave
    operating in the host environment for a TD Report passed as a
    parameter by the TD. This is used by TD Guest in the runtime
    attestation process.
    
    You can find more details about the GetQuote TDVMCALL in Intel
    TDX Guest-Host Communication Interface (GHCI) Specification,
    sec 3.3.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -124,6 +124,7 @@ unsigned int tdg_inl(unsigned short port);
 phys_addr_t tdg_shared_mask(void);
 int tdg_map_gpa(phys_addr_t gpa, int numpages, enum tdx_map_type map_type);
 int tdg_get_tdreport(u64 data, u64 reportdata);
+int tdg_get_quote(u64 data);
 
 #else // !CONFIG_INTEL_TDX_GUEST
 

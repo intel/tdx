ba18e65e08bac0edd5688dd87ba66fee6ef326d3
Isaku Yamahata <isaku.yamahata@intel.com>
2020-11-24 14:23:27 -0800

    x86/mtrr: Mask out keyid bits from variable mtrr mask register
    MTRR calculates mask based on available physical address bits.  MKTME
    repurpose high bit of physical address to key id for encryption key.  CPUID
    MAX_PA remains the same and the bits stolen for key id is controlled by
    IA32_TME_ACTIVATE MSR bit 35:32.  Because the key ID bits shouldn't affect
    the type of caching, MTRR mask should exclude the bits repurposed for the
    Key ID.  It's the OS's responsibility to maintain cache coherency.
    detect_tme() @ arch/x86/kernel/cpu/intel.c detects TME and subtracts it
    from the total usable physical bits.  This patch adds the same logic needed
    for MTRR.
    
    Move out MKTME related constants/macro to msr-index.h to share the same
    constants.
    
    Co-developed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/include/asm/msr-index.h
+++ arch/x86/include/asm/msr-index.h
@@ -920,6 +920,22 @@
 /* Geode defined MSRs */
 #define MSR_GEODE_BUSCONT_CONF0		0x00001900
 
+/* MKTME MSRs */
+#define MSR_IA32_TME_ACTIVATE		0x00000982
+
+/* Helpers to access TME_ACTIVATE MSR */
+#define TME_ACTIVATE_LOCKED(x)		(x & 0x1)
+#define TME_ACTIVATE_ENABLED(x)		(x & 0x2)
+
+#define TME_ACTIVATE_POLICY(x)		((x >> 4) & 0xf)	/* Bits 7:4 */
+#define TME_ACTIVATE_KEYID_BITS(x)	((x >> 32) & 0xf)	/* Bits 35:32 */
+
+#define TME_ACTIVATE_POLICY_AES_XTS_128	0
+
+#define TME_ACTIVATE_CRYPTO_ALGS(x)	((x >> 48) & 0xffff)	/* Bits 63:48 */
+#define TME_ACTIVATE_CRYPTO_AES_XTS_128	1
+
+
 /* Intel VT MSRs */
 #define MSR_IA32_VMX_BASIC              0x00000480
 #define MSR_IA32_VMX_PINBASED_CTLS      0x00000481

bc2b778eff3d7f2ce202854fd59dc01bf591b384
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2020-12-07 14:18:48 +0000

    x86/tdx: Add tdx_debug_enabled() interface
    A guest TD is defined as debuggable if its ATTRIBUTES.DEBUG bit is 1.
    In this mode, the host VMM can use Intel TDX functions to access
    secret TD state that is not accessible for non-debuggable TDs. A
    debuggable TD is, by nature, untrusted.
    
    Since the TD’s ATTRIBUTES are included in the TDG.MR.REPORT, the TD’s
    debuggability state can be known to any third party to which the TD
    attests. TD Attributes are initialized during TD INIT call. For
    more details about debug features, check Intel Trust Domain Extensions
    (Intel TDX) Module Architecture specification, sec 13.3.
    
    Add a new interface to detect the TDX debug mode. This will be used by
    follow-on patches. Examples of its usage are, when adding command line
    debug options to disable TDX features like driver or port filter,
    tdx_debug_enabled() is used to make sure it is used only in debug
    mode.
    
    https://software.intel.com/content/dam/develop/external/us/en/documents/tdx-module-1eas-v0.85.039.pdf
    
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/coco/tdx/tdx.c
+++ arch/x86/coco/tdx/tdx.c
@@ -38,6 +38,9 @@
 
 #define TDREPORT_SUBTYPE_0	0
 
+/* Caches TD Attributes from TDG.VP.INFO TDCALL */
+static u64 td_attr;
+
 /* Called from __tdx_hypercall() for unrecoverable failure */
 noinstr void __noreturn __tdx_hypercall_failed(void)
 {
@@ -167,7 +170,6 @@ static void tdx_parse_tdinfo(u64 *cc_mask)
 {
 	struct tdx_module_args args = {};
 	unsigned int gpa_width;
-	u64 td_attr;
 
 	/*
 	 * TDINFO TDX module call is used to get the TD execution environment
@@ -248,6 +250,11 @@ static int ve_instr_len(struct ve_info *ve)
 		WARN_ONCE(1, "Unexpected #VE-type: %lld\n", ve->exit_reason);
 		return ve->instr_len;
 	}
+}
+
+bool tdx_debug_enabled(void)
+{
+	return !!(td_attr & ATTR_DEBUG);
 }
 
 static u64 __cpuidle __halt(const bool irq_disabled)

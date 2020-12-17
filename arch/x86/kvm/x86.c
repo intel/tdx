a5c3e4821c9d2fee3527239939483e74554fc938
Isaku Yamahata <isaku.yamahata@intel.com>
2020-12-16 16:10:05 -0800

    *** HACK *** KVM: TDX: warn on the use of backdoor ioctl
    Warn loudly on the use of KVM_SEAMCALL, KVM_TDENTER.
    They are backdoor api only for development purpose.
    To guard unintentional usage, introduce
    - CONFIG_KVM_TDX_SEAM_BACKDOOR with default n, and
    - tdx_seam_backdoor command line option with default off
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4253,6 +4253,7 @@ long kvm_arch_dev_ioctl(struct file *filp,
 			goto out;
 		r = kvm_x86_ops.mem_enc_op_dev(argp);
 		break;
+#ifdef CONFIG_KVM_TDX_SEAM_BACKDOOR
 	case KVM_SEAMCALL: {
 		struct kvm_seamcall __user *user_seamcall = argp;
 		struct kvm_seamcall seamcall;
@@ -4293,6 +4294,7 @@ long kvm_arch_dev_ioctl(struct file *filp,
 		r = 0;
 		break;
 	}
+#endif
 	default:
 		r = -EINVAL;
 		break;

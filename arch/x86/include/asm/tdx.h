78ee914ccc3ec6aba40ea3ffd044ea17b1aba9ce
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2021-02-21 22:24:44 -0800

    x86/tdx: Add __tdx_module_call() and __tdx_hypercall() helper functions
    Guests communicate with VMMs with hypercalls. Historically, these
    are implemented using instructions that are known to cause VMEXITs
    like VMCALL, VMLAUNCH, etc. However, with TDX, VMEXITs no longer
    expose the guest state to the host. This prevents the old hypercall
    mechanisms from working. So, to communicate with VMM, TDX
    specification defines a new instruction called TDCALL.
    
    In a TDX based VM, since the VMM is an untrusted entity, an intermediary
    layer -- TDX module -- facilitates secure communication between the host
    and the guest. TDX module is loaded like a firmware into a special CPU
    mode called SEAM. TDX guests communicate with the TDX module using the
    TDCALL instruction.
    
    A guest uses TDCALL to communicate with both the TDX module and VMM.
    The value of the RAX register when executing the TDCALL instruction is
    used to determine the TDCALL type. A variant of TDCALL used to communicate
    with the VMM is called TDVMCALL.
    
    Add generic interfaces to communicate with the TDX module and VMM
    (using the TDCALL instruction).
    
    __tdx_hypercall()    - Used by the guest to request services from the
    		       VMM (via TDVMCALL).
    __tdx_module_call()  - Used to communicate with the TDX module (via
    		       TDCALL).
    
    Also define an additional wrapper _tdx_hypercall(), which adds error
    handling support for the TDCALL failure.
    
    The __tdx_module_call() and __tdx_hypercall() helper functions are
    implemented in assembly in a .S file.  The TDCALL ABI requires
    shuffling arguments in and out of registers, which proved to be
    awkward with inline assembly.
    
    Just like syscalls, not all TDVMCALL use cases need to use the same
    number of argument registers. The implementation here picks the current
    worst-case scenario for TDCALL (4 registers). For TDCALLs with fewer
    than 4 arguments, there will end up being a few superfluous (cheap)
    instructions. But, this approach maximizes code reuse.
    
    For registers used by the TDCALL instruction, please check TDX GHCI
    specification, the section titled "TDCALL instruction" and "TDG.VP.VMCALL
    Interface".
    
    Based on previous patch by Sean Christopherson.
    
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -8,10 +8,50 @@
 #define TDX_CPUID_LEAF_ID	0x21
 #define TDX_IDENT		"IntelTDX    "
 
+#define TDX_HYPERCALL_STANDARD  0
+
+/*
+ * Used in __tdx_module_call() to gather the output registers'
+ * values of the TDCALL instruction when requesting services from
+ * the TDX module. This is a software only structure and not part
+ * of the TDX module/VMM ABI
+ */
+struct tdx_module_output {
+	u64 rcx;
+	u64 rdx;
+	u64 r8;
+	u64 r9;
+	u64 r10;
+	u64 r11;
+};
+
+/*
+ * Used in __tdx_hypercall() to gather the output registers' values
+ * of the TDCALL instruction when requesting services from the VMM.
+ * This is a software only structure and not part of the TDX
+ * module/VMM ABI.
+ */
+struct tdx_hypercall_output {
+	u64 r10;
+	u64 r11;
+	u64 r12;
+	u64 r13;
+	u64 r14;
+	u64 r15;
+};
+
 #ifdef CONFIG_INTEL_TDX_GUEST
 
 void __init tdx_early_init(void);
 bool is_tdx_guest(void);
+
+/* Used to communicate with the TDX module */
+u64 __tdx_module_call(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
+		      struct tdx_module_output *out);
+
+/* Used to request services from the VMM */
+u64 __tdx_hypercall(u64 type, u64 fn, u64 r12, u64 r13, u64 r14,
+		    u64 r15, struct tdx_hypercall_output *out);
 
 #else
 

e426795cad99a5f861e6e0dab3481ca5083c13e2
Chao Gao <chao.gao@intel.com>
2021-03-15 01:47:27 -0400

    KVM: x86: Restore host DRs after TD exit
    New TDX SEAM module won't restore DRs to original values on TD exit.
    Instead, DRs are reset to architectural INIT state. So, KVM needs to
    save host DRs before TD enter and restore them after TD exit.
    
    Current KVM_DEBUGREG_AUTO_SWITCHED instructs KVM to skip switching
    guest DRs and restoring host DRs. Remove this flag and introduce
    a new one, KVM_DEBUGREG_AUTO_SWITCH_GUEST, to instruct kvm to only
    skip switching guest DRs. And set this new flag for TD guests.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -480,7 +480,11 @@ enum {
 	KVM_DEBUGREG_BP_ENABLED 	= BIT(0),
 	KVM_DEBUGREG_WONT_EXIT		= BIT(1),
 	KVM_DEBUGREG_RELOAD		= BIT(2),
-	KVM_DEBUGREG_AUTO_SWITCHED	= BIT(3),
+	/*
+	 * Guest debug registers are saved/restored by hardware on exit from
+	 * or enter guest. KVM needn't switch them.
+	 */
+	KVM_DEBUGREG_AUTO_SWITCH_GUEST	= BIT(3),
 };
 
 struct kvm_mtrr_range {

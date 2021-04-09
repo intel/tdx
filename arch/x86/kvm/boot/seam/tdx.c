728cbef62c9eafdaa91a3ec9d619e66024f9585d
Xiaoyao Li <xiaoyao.li@intel.com>
2021-04-09 11:06:49 +0800

    KVM: TDX: Introduce pr_seamcall_ex_ret_info() to print more info when SEAMCALL fails
    Various SEAMCALLs may have additional info in RCX, RDX, R8, R9, R10
    when it completes. Introduce pr_seamcall_ex_ret_info() to parse those
    additional info based on returned SEAMCALL status code.
    
    It only processes some cases for reference. More processes for other
    SEAMCALL status code can be added in the future on demand.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- arch/x86/kvm/boot/seam/tdx.c
+++ arch/x86/kvm/boot/seam/tdx.c
@@ -1283,3 +1283,55 @@ const char *tdx_seamcall_error_name(u64 error_code){
 	return "Unkown SEAMCALL status code";
 }
 EXPORT_SYMBOL_GPL(tdx_seamcall_error_name);
+
+static const char *TDX_SEPT_ENTRY_STATES[] = {
+	"SEPT_FREE",
+	"SEPT_BLOCKED",
+	"SEPT_PENDING",
+	"SEPT_PENDING_BLOCKED",
+	"SEPT_PRESENT"
+};
+
+void pr_seamcall_ex_ret_info(u64 error_code, struct tdx_ex_ret *ex_ret)
+{
+	if (WARN_ON(!ex_ret))
+		return;
+
+	switch(error_code & TDX_SEAMCALL_STATUS_MASK) {
+	case TDX_INCORRECT_CPUID_VALUE:
+		pr_err("Expected CPUID [leaf 0x%x subleaf 0x%x]: "
+			"eax 0x%x check_mask 0x%x, ebx 0x%x check_mask 0x%x, "
+			"ecx 0x%x check_mask 0x%x, edx 0x%x check_mask 0x%x\n",
+			ex_ret->leaf, ex_ret->subleaf,
+			ex_ret->eax_val, ex_ret->eax_mask,
+			ex_ret->ebx_val, ex_ret->ebx_mask,
+			ex_ret->ecx_val, ex_ret->ecx_mask,
+			ex_ret->edx_val, ex_ret->edx_mask);
+		break;
+	case TDX_INCONSISTENT_CPUID_FIELD:
+		pr_err("Inconsistent CPUID [leaf 0x%x subleaf 0x%x]: "
+		       "eax_mask 0x%x, ebx_mask 0x%x, ecx_mask %x, edx_mask 0x%x\n",
+		       ex_ret->leaf, ex_ret->subleaf,
+		       ex_ret->eax_mask, ex_ret->ebx_mask,
+		       ex_ret->ecx_mask, ex_ret->edx_mask);
+		break;
+	case TDX_EPT_WALK_FAILED: {
+		const char *state;
+
+		if (ex_ret->state >= ARRAY_SIZE(TDX_SEPT_ENTRY_STATES))
+			state = "Invalid";
+		else
+			state = TDX_SEPT_ENTRY_STATES[ex_ret->state];
+
+		pr_err("Secure EPT walk error: SEPTE 0x%llx, level %d, %s\n",
+			ex_ret->septe, ex_ret->level, state);
+		break;
+	}
+	default:
+		pr_err("RCX 0x%llx, RDX 0x%llx, R8 0x%llx, R9 0x%llx, r10 0x%llx\n",
+		       ex_ret->rcx, ex_ret->rdx, ex_ret->r8, ex_ret->r9,
+		       ex_ret->r10);
+		break;
+	}
+}
+EXPORT_SYMBOL_GPL(pr_seamcall_ex_ret_info);

lantianyu/linux

hv/vmbus: Initialize VMbus ring buffer for Isolated VM
VMbus ring buffer are shared with Hypervisor/host and it's
need to be accessed via extra address space inside Isolated
VM with SNP support. This patch is to map the ring buffer
address in extra address space via ioremap(). HV host
visibility hvcall may smear data in the ring buffer and
so zero/initialize the ring buffer after calling the hvcall.

Signed-off-by: Tianyu Lan <Tianyu.Lan@microsoft.com>

lib/ioremap.c
@@ -231,3 +231,4 @@ int ioremap_page_range(unsigned long addr,
 
 	return err;
 }
+EXPORT_SYMBOL_GPL(ioremap_page_range);

intel/tdx

Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

mm/ioremap.c
@@ -248,6 +248,7 @@ int ioremap_page_range(unsigned long addr,
 
 	return err;
 }
+EXPORT_SYMBOL(ioremap_page_range);
 
 #ifdef CONFIG_GENERIC_IOREMAP
 void __iomem *ioremap_prot(phys_addr_t addr, size_t size, unsigned long prot)
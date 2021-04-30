95c03de681a2f05b18a9eebbc2a7c546d81f6288
Isaku Yamahata <isaku.yamahata@intel.com>
2021-04-30 11:52:02 -0700

    KVM: X86: move kvm_cpu_vmxon() from vmx.c to virtext.h
    This is preparatory for TDX support.
    
    Move out kvm_cpu_vmxon() from vmx.c to virtext.h with rename to
    vcpu_vmxon().  The function will be used outside of kvm.  P-SEAMLDR [1] is
    called earlier in the kernel's boot phase to load the TDX module [2].
    P-SEAMLDR requires to enable VMXON.  As bonus, this also increases the
    symmetry with cpu_vmxoff().
    
    Later patch will use vcpu_vmxon().
    
    [1] SEAM Loader interface specification
    https://software.intel.com/content/dam/develop/external/us/en/documents-tps/intel-tdx-seamldr-interface-specification.pdf
    
    [2] TDX Module spec
    https://software.intel.com/content/dam/develop/external/us/en/documents/tdx-module-1eas-v0.85.039.pdf
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -2290,26 +2290,6 @@ static __init int vmx_disabled_by_bios(void)
 	       !boot_cpu_has(X86_FEATURE_VMX);
 }
 
-static int kvm_cpu_vmxon(u64 vmxon_pointer)
-{
-	u64 msr;
-
-	cr4_set_bits(X86_CR4_VMXE);
-
-	asm_volatile_goto("1: vmxon %[vmxon_pointer]\n\t"
-			  _ASM_EXTABLE(1b, %l[fault])
-			  : : [vmxon_pointer] "m"(vmxon_pointer)
-			  : : fault);
-	return 0;
-
-fault:
-	WARN_ONCE(1, "VMXON faulted, MSR_IA32_FEAT_CTL (0x3a) = 0x%llx\n",
-		  rdmsrl_safe(MSR_IA32_FEAT_CTL, &msr) ? 0xdeadbeef : msr);
-	cr4_clear_bits(X86_CR4_VMXE);
-
-	return -EFAULT;
-}
-
 static int hardware_enable(void)
 {
 	int cpu = raw_smp_processor_id();
@@ -2329,7 +2309,7 @@ static int hardware_enable(void)
 
 	intel_pt_handle_vmx(1);
 
-	r = kvm_cpu_vmxon(phys_addr);
+	r = cpu_vmxon(phys_addr);
 	if (r) {
 		intel_pt_handle_vmx(0);
 		return r;

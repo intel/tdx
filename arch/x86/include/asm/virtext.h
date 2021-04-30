41bb27af8b221b936b9c9e6ecc6b18b9f2308c55
Isaku Yamahata <isaku.yamahata@intel.com>
2021-04-30 11:52:02 -0700

    KVM: X86: move kvm_cpu_vmxon() from vmx.c to virtext.h
    Trust Domain Extension (TDX) firmware must be loaded during kernel boot
    before TDX can be enabled.  Loading TDX firmware requires all CPUs to have
    VMX (Virtual Machine eXtensions) enabled, and in VMX operation via the
    VMXON instruction.
    
    Because the kvm_cpu_vmxon() function will be used outside of KVM, move it
    from vmx.c to virtext.h and rename to cpu_vmxon().  In the kernel's boot
    phase, invoke the TDX firmware loader (P-SEAMLDR) to load the TDX module
    firmware via a SEAMCALL instruction, which requires VMX enabled (VMXON).
    
    No functional change is intended.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/include/asm/virtext.h
+++ arch/x86/include/asm/virtext.h
@@ -29,6 +29,30 @@ static inline int cpu_has_vmx(void)
 	return test_bit(5, &ecx); /* CPUID.1:ECX.VMX[bit 5] -> VT */
 }
 
+/*
+ * cpu_vmxon() - Enable VMX on the current CPU
+ *
+ * Set CR4.VMXE and enable VMX
+ */
+static inline int cpu_vmxon(u64 vmxon_pointer)
+{
+	u64 msr;
+
+	cr4_set_bits(X86_CR4_VMXE);
+
+	asm_volatile_goto("1: vmxon %[vmxon_pointer]\n\t"
+			  _ASM_EXTABLE(1b, %l[fault])
+			  : : [vmxon_pointer] "m"(vmxon_pointer)
+			  : : fault);
+	return 0;
+
+fault:
+	WARN_ONCE(1, "VMXON faulted, MSR_IA32_FEAT_CTL (0x3a) = 0x%llx\n",
+		  rdmsrl_safe(MSR_IA32_FEAT_CTL, &msr) ? 0xdeadbeef : msr);
+	cr4_clear_bits(X86_CR4_VMXE);
+
+	return -EFAULT;
+}
 
 /**
  * cpu_vmxoff() - Disable VMX on the current CPU

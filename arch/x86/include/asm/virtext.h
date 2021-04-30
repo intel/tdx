33f288cd8c3f6f99eac071c68af60fda2fba67fc
Isaku Yamahata <isaku.yamahata@intel.com>
2021-04-30 11:52:02 -0700

    KVM: X86: move kvm_cpu_vmxon() from vmx.c to virtext.h
    This is preparatory clean up for TDX support.
    Move out kvm_cpu_vmxon() from vmx.c to virtext.h with rename to
    vcpu_vmxon(). Which will be used outside of kvm.
    SEAMLDER to load TDX module which occurs at kernel early boot phase.
    As bonus, this also increases the symetry with cpu_vmxoff().
    
    No functional change is intended.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/include/asm/virtext.h
+++ arch/x86/include/asm/virtext.h
@@ -31,6 +31,31 @@ static inline int cpu_has_vmx(void)
 
 
 /**
+ * cpu_vmxon() - Enable VMX on the current CPU
+ *
+ * Set CR4.VMXE and enable VMX
+ */
+static inline int cpu_vmxon(u64 vmxon_pointer)
+{
+	u64 msr;
+
+	cr4_set_bits(X86_CR4_VMXE);
+
+	asm_volatile_goto("1: vmxon %[vmxon_pointer]\n\t"
+			  _ASM_EXTABLE(1b, %l[fault])
+			  : : [vmxon_pointer] "m"(vmxon_pointer)
+			  : : fault);
+	return 0;
+
+fault:
+	WARN_ONCE(1, "VMXON faulted, MSR_IA32_FEAT_CTL (0x3a) = 0x%llx\n",
+		  rdmsrl_safe(MSR_IA32_FEAT_CTL, &msr) ? 0xdeadbeef : msr);
+	cr4_clear_bits(X86_CR4_VMXE);
+
+	return -EFAULT;
+}
+
+/**
  * cpu_vmxoff() - Disable VMX on the current CPU
  *
  * Disable VMX and clear CR4.VMXE (even if VMXOFF faults)

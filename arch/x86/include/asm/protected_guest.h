15e66cbfc5a009fafca4853625c581e84c85ef50
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2021-05-12 11:35:13 -0700

    x86/tdx: Add protected guest support for TDX guest
    TDX architecture provides a way for VM guests to be highly secure and
    isolated (from untrusted VMM). To achieve this requirement, any data
    coming from VMM cannot be completely trusted. TDX guest fixes this
    issue by hardening the IO drivers against the attack from the VMM.
    So, when adding hardening fixes to the generic drivers, to protect
    custom fixes use prot_guest_has() API.
    
    Also add TDX guest support to prot_guest_has() API to protect the
    TDX specific fixes.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/include/asm/protected_guest.h
+++ arch/x86/include/asm/protected_guest.h
@@ -11,13 +11,22 @@
 #define _X86_PROTECTED_GUEST_H
 
 #include <linux/mem_encrypt.h>
+#include <linux/processor.h>
 
 #ifndef __ASSEMBLY__
+
+#if defined(CONFIG_CPU_SUP_INTEL) && defined(CONFIG_ARCH_HAS_PROTECTED_GUEST)
+bool intel_prot_guest_has(unsigned int flag);
+#else
+static inline bool intel_prot_guest_has(unsigned int flag) { return false; }
+#endif
 
 static inline bool prot_guest_has(unsigned int attr)
 {
 	if (sme_me_mask)
 		return amd_prot_guest_has(attr);
+	else if (boot_cpu_data.x86_vendor == X86_VENDOR_INTEL)
+		return intel_prot_guest_has(attr);
 
 	return false;
 }

bc7245d056ced76d081050babb7b40386e10541f
Chao Gao <chao.gao@intel.com>
2021-06-08 16:35:48 +0800

    KVM: TDX: Let seam_load_module() accept a seamldr_params pointer
    seam_load_module() launches a ACM (seamloader) and passes
    seamldr_params as a parameter of the ACM to load TDX module.
    
    In the future, NP-seamloader (a new ACM) will be launched to load an
    embedded binary (P-seamloader) into SEAM mode and don't accept
    seamldr_params any longer. And the P-seamloader provides an interface
    to load TDX module and accept a seamldr_params.
    
    Two changes are made to support the new method of TDX module loading.
    1. split out seamldr_params initialization and free. And memory
    allocation/free involved may use memblock or page allocator depending
    on which one is available. Then, P-seamloader can reuse seamldr_params
    initialization and free when loading TDX module.
    
    2. make seam_load_module() accept a seamldr_params pointer. With the
    legacy seamloder, a seamldr_params is passed. With NP-seamloader, a
    NULL pointer can be passed.
    
    One subtle change is: previously seam module and sigstruct are
    relocated to be 4K-aligned if they are not. This relocation is done
    unconditionally to simplify the logic of freeing memory.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>

--- arch/x86/kvm/boot/seam/tdx.c
+++ arch/x86/kvm/boot/seam/tdx.c
@@ -929,6 +929,8 @@ void __init tdh_seam_init(void)
 	const char *seamldr_name = "intel-seam/seamldr.acm";
 	const char *module_name = "intel-seam/libtdx.so";
 	struct cpio_data module, sigstruct, seamldr;
+	struct seamldr_params *params;
+	int ret;
 
 	if (cmdline_find_option_bool(boot_command_line, "disable_tdx"))
 		return;
@@ -949,8 +951,22 @@ void __init tdh_seam_init(void)
 	if (!tdx_get_firmware(&seamldr, seamldr_name))
 		goto error;
 
-	if (seam_load_module(module.data, module.size, sigstruct.data,
-			     sigstruct.size, seamldr.data, seamldr.size))
+	/* Use the kernel's fake SEAMLDR when running as a VM. */
+	if (!tdx_get_firmware(&sigstruct, sigstruct_name))
+		goto error;
+
+	if (!tdx_get_firmware(&seamldr, seamldr_name))
+		goto error;
+
+	params = init_seamldr_params(module.data, module.size,
+				sigstruct.data, sigstruct.size);
+	if (IS_ERR(params))
+		goto error;
+
+	ret = seam_load_module(seamldr.data, seamldr.size, params);
+
+	free_seamldr_params(params);
+	if (ret)
 		goto error;
 
 	if (tdx_init_bsp() || construct_tdmrs())

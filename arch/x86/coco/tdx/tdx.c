14eaf314dd61a1ff82103ab8535cd7cf3625e4ab
Andi Kleen <ak@linux.intel.com>
2021-06-30 17:57:14 -0700

    x86/tdx: Disable early PCI in TDX
    There are various bug workarounds that scan the PCI bus early.
    These are not really needed in a VM and it would be a lot of work to
    properly audit and harden them for a confidential guest.
    There is already a flag to disable them using pci=noearly, just make
    that default when running TDX.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/coco/tdx/tdx.c
+++ arch/x86/coco/tdx/tdx.c
@@ -7,6 +7,7 @@
 #include <linux/cpufeature.h>
 #include <linux/export.h>
 #include <linux/io.h>
+#include <linux/pci.h>
 #include <asm/coco.h>
 #include <asm/tdx.h>
 #include <asm/i8259.h>
@@ -876,6 +877,8 @@ void __init tdx_early_init(void)
 	x86_platform.guest.enc_status_change_finish = tdx_enc_status_changed;
 
 	legacy_pic = &null_legacy_pic;
+
+	pci_disable_early();
 
 	pr_info("Guest detected\n");
 }

14ab0da9bb6d2170e150901b72d4128428eafb02
Andi Kleen <ak@linux.intel.com>
2021-06-30 17:57:17 -0700

    x86/tdx: Disable kvmclock in TDX
    TDX guests have a special secured TSC enforced by the TDX module to
    provide secure time to guests. This only works when only RDTSC is used
    to derive the time. So disable kvmclock in TDX because it uses host
    information too, which could be abused by a malicious hypervisor.
    
    Define a new SECURE_TIME prot flag for this.
    
    Note there are other time sources of course, but kvmclock has higher
    priority than TSC, so disabling it is most important.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/kernel/cpu/intel.c
+++ arch/x86/kernel/cpu/intel.c
@@ -87,6 +87,7 @@ bool intel_prot_guest_has(unsigned int flag)
 	case PATTR_GUEST_MEM_ENCRYPT:
 	case PATTR_GUEST_SHARED_MAPPING_INIT:
 	case PATTR_MEM_ENCRYPT:
+	case PATTR_GUEST_SECURE_TIME:
 		return tdx_guest_enabled;
 	case PATTR_GUEST_DEVICE_FILTER:
 		return tdx_filter_enabled() && tdx_guest_enabled;

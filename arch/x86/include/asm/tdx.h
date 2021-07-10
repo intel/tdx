7838bc37f8ad2f63b95dfcd968d330d19bebb065
Andi Kleen <ak@linux.intel.com>
2021-07-09 21:30:48 -0700

    [REVERTME] platform/x86: Add RTMR update interface
    Add an experimental interface to update the RTMR measurement registers
    in the TDX module from ring 3. Simply allow to pass in a update hash
    and pass it on to a TDCALL. The interface has the same rights
    as any other operation on the tdx-attest device node, so becomes
    controllable by udev rules.
    
    This is currently not for upstream, but mainly for internal
    testing.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -48,6 +48,8 @@ bool tdx_allowed_port(short int port);
 
 int tdx_mcall_tdreport(u64 data, u64 reportdata);
 
+int tdx_mcall_rtmr_extend(u64 data, u64 rmtr);
+
 int tdx_hcall_get_quote(u64 data);
 
 extern void (*tdx_event_notify_handler)(void);

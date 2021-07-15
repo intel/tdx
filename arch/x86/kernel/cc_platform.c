d5da2a0066bf5d0279d7ff1ebc920ec9f8cc6d86
Andi Kleen <ak@linux.intel.com>
2021-07-14 22:37:34 -0700

    x86/tdx: Add option to override prot values
    The kernel internally uses cc_platform_has to configure various behavior
    specific to confidential guests. Allow to clear a single value. This is
    mainly useful for debugging and not very user friendly because the hex
    values from the include file have to be used. But for debugging it's
    good enough and it matches the existing clear_cpuid option.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/kernel/cc_platform.c
+++ arch/x86/kernel/cc_platform.c
@@ -14,9 +14,26 @@
 #include <asm/tdx.h>
 #include <asm/processor.h>
 
+#ifdef CONFIG_INTEL_TDX_GUEST
+
+unsigned int x86_disable_cc = -1;
+
+static int __init x86_cc_clear_setup(char *arg)
+{
+	get_option(&arg, &x86_disable_cc);
+
+	return 1;
+}
+__setup("x86_cc_clear=", x86_cc_clear_setup);
+
+#endif
+
 static bool intel_cc_platform_has(enum cc_attr attr)
 {
 #ifdef CONFIG_INTEL_TDX_GUEST
+	if (attr == x86_disable_cc)
+		return false;
+
 	switch (attr) {
 	case CC_ATTR_GUEST_TDX:
 	case CC_ATTR_GUEST_UNROLL_STRING_IO:

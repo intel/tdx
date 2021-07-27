4a820b4f82a6a0e6ee874cdd0d7d715e96ae69a6
Tom Lendacky <thomas.lendacky@amd.com>
2021-07-27 17:26:07 -0500

    x86/sme: Replace occurrences of sme_active() with prot_guest_has()
    Replace occurrences of sme_active() with the more generic prot_guest_has()
    using PATTR_HOST_MEM_ENCRYPT, except for in arch/x86/mm/mem_encrypt*.c
    where PATTR_SME will be used. If future support is added for other memory
    encryption technologies, the use of PATTR_HOST_MEM_ENCRYPT can be
    updated, as required, to use PATTR_SME.
    
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Joerg Roedel <joro@8bytes.org>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- arch/x86/mm/mem_encrypt_identity.c
+++ arch/x86/mm/mem_encrypt_identity.c
@@ -30,6 +30,7 @@
 #include <linux/kernel.h>
 #include <linux/mm.h>
 #include <linux/mem_encrypt.h>
+#include <linux/protected_guest.h>
 
 #include <asm/setup.h>
 #include <asm/sections.h>
@@ -287,7 +288,7 @@ void __init sme_encrypt_kernel(struct boot_params *bp)
 	unsigned long pgtable_area_len;
 	unsigned long decrypted_base;
 
-	if (!sme_active())
+	if (!prot_guest_has(PATTR_SME))
 		return;
 
 	/*

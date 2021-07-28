2c8aa04e8e73d9595075e343dbb85429f7cc9c0d
Isaku Yamahata <isaku.yamahata@intel.com>
2021-07-28 13:27:49 -0700

    KVM: TDX: more pretty print for TDX error code
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kernel/cpu/tdx/tdx-error.c
+++ arch/x86/kernel/cpu/tdx/tdx-error.c
@@ -53,6 +53,14 @@ const char *tdx_seamcall_error_name(u64 error_code)
 }
 EXPORT_SYMBOL_GPL(tdx_seamcall_error_name);
 
+static const char * const TDX_SEPT_ENTRY_STATES[] = {
+	"SEPT_FREE",
+	"SEPT_BLOCKED",
+	"SEPT_PENDING",
+	"SEPT_PENDING_BLOCKED",
+	"SEPT_PRESENT"
+};
+
 void pr_seamcall_ex_ret_info(u64 op, u64 error_code,
 			     const struct tdx_ex_ret *ex_ret)
 {
@@ -60,8 +68,37 @@ void pr_seamcall_ex_ret_info(u64 op, u64 error_code,
 		return;
 
 	switch (error_code & TDX_SEAMCALL_STATUS_MASK) {
-	/* TODO: add API specific pretty print. */
+	case TDX_INCORRECT_CPUID_VALUE:
+		pr_err("Expected CPUID [leaf 0x%x subleaf 0x%x]: "
+			"eax 0x%x check_mask 0x%x, ebx 0x%x check_mask 0x%x, "
+			"ecx 0x%x check_mask 0x%x, edx 0x%x check_mask 0x%x\n",
+			ex_ret->leaf, ex_ret->subleaf,
+			ex_ret->eax_val, ex_ret->eax_mask,
+			ex_ret->ebx_val, ex_ret->ebx_mask,
+			ex_ret->ecx_val, ex_ret->ecx_mask,
+			ex_ret->edx_val, ex_ret->edx_mask);
+		break;
+	case TDX_INCONSISTENT_CPUID_FIELD:
+		pr_err("Inconsistent CPUID [leaf 0x%x subleaf 0x%x]: "
+		       "eax_mask 0x%x, ebx_mask 0x%x, ecx_mask %x, edx_mask 0x%x\n",
+		       ex_ret->leaf, ex_ret->subleaf,
+		       ex_ret->eax_mask, ex_ret->ebx_mask,
+		       ex_ret->ecx_mask, ex_ret->edx_mask);
+		break;
+	case TDX_EPT_WALK_FAILED: {
+		const char *state;
+
+		if (ex_ret->state >= ARRAY_SIZE(TDX_SEPT_ENTRY_STATES))
+			state = "Invalid";
+		else
+			state = TDX_SEPT_ENTRY_STATES[ex_ret->state];
+
+		pr_err("Secure EPT walk error: SEPTE 0x%llx, level %d, %s\n",
+			ex_ret->septe, ex_ret->level, state);
+		break;
+	}
 	default:
+		/* TODO: print only meaningful registers depending on op */
 		pr_err("RCX 0x%llx, RDX 0x%llx, R8 0x%llx, R9 0x%llx, "
 		       "R10 0x%llx, R11 0x%llx\n",
 		       ex_ret->rcx, ex_ret->rdx, ex_ret->r8, ex_ret->r9,

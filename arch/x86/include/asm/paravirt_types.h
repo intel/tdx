dce8b91e08f9dea18c0a619a966d90f79476aa4a
Andi Kleen <ak@linux.intel.com>
2021-07-30 20:45:21 -0700

    x86/tdx/paravirt: Move wrmsrl outside PARAVIRT_XXL
    For performance optimizations it is useful to directly replace wrmsrl
    with an paravirtual operation when running in TDX. Currently
    msr writes can be only patched when in full PARAVIRT_XXL mode,
    which includes many heavy changes and is only needed for old school
    Xen PV guests.
    
    Move wrmsrl out of PARAVIRT_XXL into the normal PARAVIRT support.
    Only do this for wrmsrl because that's the only case
    currently needed for performance optimization.
    
    It doesn't make a lot of difference for the code, since the msr
    write was already out of line for tracing, and the paravirt
    infrastructure takes care of directly patching in the alternative
    functions.
    
    One drawback is that these MSR writes will now go through patched
    code, which can confuse some debugging tools, but that's already
    fairly common in the kernel and shouldn't make a big difference.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/include/asm/paravirt_types.h
+++ arch/x86/include/asm/paravirt_types.h
@@ -122,7 +122,6 @@ struct pv_cpu_ops {
 
 	/* Unsafe MSR operations.  These will warn or panic on failure. */
 	u64 (*read_msr)(unsigned int msr);
-	void (*write_msr)(unsigned int msr, unsigned low, unsigned high);
 
 	/*
 	 * Safe MSR operations.
@@ -136,6 +135,8 @@ struct pv_cpu_ops {
 	void (*start_context_switch)(struct task_struct *prev);
 	void (*end_context_switch)(struct task_struct *next);
 #endif
+
+	void (*write_msr)(unsigned int msr, unsigned low, unsigned high);
 } __no_randomize_layout;
 
 struct pv_irq_ops {

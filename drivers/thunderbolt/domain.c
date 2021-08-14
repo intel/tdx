c8b36c0095479e51318dc2eb9324ad3c6316545b
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2021-08-14 04:27:20 +0000

    driver core: Move the "authorized" attribute from USB/Thunderbolt to core
    Currently bus drivers like "USB" or "Thunderbolt" implement a custom
    version of device authorization to selectively authorize the driver
    probes. Since there is a common requirement, move the "authorized"
    attribute support to the driver core in order to allow it to be used
    by other subsystems / buses.
    
    Similar requirements have been discussed in the PCI [1] community for
    PCI bus drivers as well.
    
    No functional changes are intended. It just converts authorized
    attribute from int to bool and moves it to the driver core. There
    should be no user-visible change in the location or semantics of
    attributes for USB devices.
    
    Regarding thunderbolt driver, although it declares sw->authorized as
    "int" and allows 0,1,2 as valid values for sw->authorized attribute,
    but within the driver, in all authorized attribute related checks,
    it is treated as bool value. So when converting the authorized
    attribute from int to bool value, there should be no functional
    changes other than value 2 being not visible to the user.
    
    [1]: https://lore.kernel.org/all/CACK8Z6E8pjVeC934oFgr=VB3pULx_GyT2NkzAogdRQJ9TKSX9A@mail.gmail.com/
    
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- drivers/thunderbolt/domain.c
+++ drivers/thunderbolt/domain.c
@@ -656,7 +656,7 @@ int tb_domain_approve_switch(struct tb *tb, struct tb_switch *sw)
 
 	/* The parent switch must be authorized before this one */
 	parent_sw = tb_to_switch(sw->dev.parent);
-	if (!parent_sw || !parent_sw->authorized)
+	if (!parent_sw || !parent_sw->dev.authorized)
 		return -EINVAL;
 
 	return tb->cm_ops->approve_switch(tb, sw);
@@ -683,7 +683,7 @@ int tb_domain_approve_switch_key(struct tb *tb, struct tb_switch *sw)
 
 	/* The parent switch must be authorized before this one */
 	parent_sw = tb_to_switch(sw->dev.parent);
-	if (!parent_sw || !parent_sw->authorized)
+	if (!parent_sw || !parent_sw->dev.authorized)
 		return -EINVAL;
 
 	ret = tb->cm_ops->add_switch_key(tb, sw);
@@ -720,7 +720,7 @@ int tb_domain_challenge_switch_key(struct tb *tb, struct tb_switch *sw)
 
 	/* The parent switch must be authorized before this one */
 	parent_sw = tb_to_switch(sw->dev.parent);
-	if (!parent_sw || !parent_sw->authorized)
+	if (!parent_sw || !parent_sw->dev.authorized)
 		return -EINVAL;
 
 	get_random_bytes(challenge, sizeof(challenge));

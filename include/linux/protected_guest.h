d20c7b8dcef3a0d25b4b37ef63444ef10dd9a704
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2021-08-23 16:30:12 -0700

    mm: Add prot_guest_authorized() interface
    Protected Guest platform has a requirement to allow only a
    trusted list of devices. So add an interface to allow
    the bus driver check with arch code to determine the default
    status of the authorized device.
    
    If ARCH code implements prot_guest_authorized(), it will
    return true or false based on arch specific device allow or
    deny list.
    
    This is a preparatory patch for adding x86 ARCH specific
    prot_guest_authorized() support.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- include/linux/protected_guest.h
+++ include/linux/protected_guest.h
@@ -38,6 +38,21 @@
 
 static inline bool prot_guest_has(unsigned int attr) { return false; }
 
+/*
+ * prot_guest_authorized() - Used to get ARCH specific authorized status of
+ *			     given device.
+ * @dev - device structure
+ * @dev_str - device search string (for PCI bus it is vendor:device, other
+ *	      bus device may use dev_name(dev))
+ *
+ * Return True to allow the device or False to deny it.
+ *
+ */
+static inline bool prot_guest_authorized(struct device *dev, char *dev_str)
+{
+	return dev->authorized;
+}
+
 #endif	/* CONFIG_ARCH_HAS_PROTECTED_GUEST */
 
 #endif	/* __ASSEMBLY__ */

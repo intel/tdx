279b0113bdf4553aad3d6e8b82a3b6cc11f9e9e1
Mike Rapoport <rppt@linux.ibm.com>
2021-08-25 21:42:47 +0300

    memblock: make memblock_find_in_range method private
    There are a lot of uses of memblock_find_in_range() along with
    memblock_reserve() from the times memblock allocation APIs did not exist.
    
    memblock_find_in_range() is the very core of memblock allocations, so any
    future changes to its internal behaviour would mandate updates of all the
    users outside memblock.
    
    Replace the calls to memblock_find_in_range() with an equivalent calls to
    memblock_phys_alloc() and memblock_phys_alloc_range() and make
    memblock_find_in_range() private method of memblock.
    
    This simplifies the callers, ensures that (unlikely) errors in
    memblock_reserve() are handled and improves maintainability of
    memblock_find_in_range().
    
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>
    Acked-by: Kirill A. Shutemov <kirill.shtuemov@linux.intel.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>	# ACPI
    Acked-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Acked-by: Nick Kossifidis <mick@ics.forth.gr>			# riscv
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>		# arm64
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Acked-by: Rob Herring <robh@kernel.org>

--- drivers/acpi/tables.c
+++ drivers/acpi/tables.c
@@ -594,8 +594,8 @@ void __init acpi_table_upgrade(void)
 	}
 
 	acpi_tables_addr =
-		memblock_find_in_range(0, ACPI_TABLE_UPGRADE_MAX_PHYS,
-				       all_tables_size, PAGE_SIZE);
+		memblock_phys_alloc_range(all_tables_size, PAGE_SIZE,
+					  0, ACPI_TABLE_UPGRADE_MAX_PHYS);
 	if (!acpi_tables_addr) {
 		WARN_ON(1);
 		return;
@@ -610,7 +610,6 @@ void __init acpi_table_upgrade(void)
 	 * Both memblock_reserve and e820__range_add (via arch_reserve_mem_area)
 	 * works fine.
 	 */
-	memblock_reserve(acpi_tables_addr, all_tables_size);
 	arch_reserve_mem_area(acpi_tables_addr, all_tables_size);
 
 	/*

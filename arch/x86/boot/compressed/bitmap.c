272095be44c9593e846533c21c5f9ede31fade2a
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2021-08-25 21:42:50 +0300

    efi/x86: Implement support for unaccepted memory
    UEFI Specification version 2.9 introduces concept of memory acceptance:
    Some Virtual Machine platforms, such as Intel TDX or AMD SEV-SNP,
    requiring memory to be accepted before it can be used by the guest.
    Accepting happens via a protocol specific for the Virtrual Machine
    platform.
    
    Accepting memory is costly and it makes VMM allocate memory for the
    accepted guest physical address range. It's better to postpone memory
    acceptation until memory is needed. It lowers boot time and reduces
    memory overhead.
    
    Kernel needs to know what memory has been accepted. Firmware
    communicates this information via memory map: a new memory type --
    EFI_UNACCEPTED_MEMORY -- indicates such memory.
    
    Range based tracking works fine for firmware, but it gets bulky for
    kernel: e820 has to be modified on every page acceptance. It leads to
    table fragmentation, but there's a limited number of entries in the e820
    table
    
    Other option is to mark such memory as usable in e820 and track if the
    range has been accepted in a bitmap. One bit in the bitmap represents
    2MiB in the address space: one 4k page is enough to track 64GiB or
    physical address space.
    
    In the worst case scenario -- a huge hole in the middle of the
    address space -- It needs 256MiB to handle 4PiB of the address
    space.
    
    Any unaccepted memory that is not aligned to 2M get accepted upfront.
    
    The bitmap allocated and constructed in EFI stub and passed down to
    kernel via boot_params. allocate_e820() allocates the bitmap if
    unaccepted memory present according to the maximum address in the memory
    map.
    
    The same boot_params.unaccepted_memory can be used to pass the bitmap
    between two kernel on kexec, but the use-case is not yet implemented.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

---
+++ arch/x86/boot/compressed/bitmap.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* Taken from lib/string.c */
+
+#include <linux/bitmap.h>
+
+void __bitmap_set(unsigned long *map, unsigned int start, int len)
+{
+	unsigned long *p = map + BIT_WORD(start);
+	const unsigned int size = start + len;
+	int bits_to_set = BITS_PER_LONG - (start % BITS_PER_LONG);
+	unsigned long mask_to_set = BITMAP_FIRST_WORD_MASK(start);
+
+	while (len - bits_to_set >= 0) {
+		*p |= mask_to_set;
+		len -= bits_to_set;
+		bits_to_set = BITS_PER_LONG;
+		mask_to_set = ~0UL;
+		p++;
+	}
+	if (len) {
+		mask_to_set &= BITMAP_LAST_WORD_MASK(size);
+		*p |= mask_to_set;
+	}
+}

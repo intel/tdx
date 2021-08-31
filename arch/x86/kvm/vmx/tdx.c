db25cba9615f5dcf3651fa4b2e63efae17458bb4
Xiaoyao Li <xiaoyao.li@intel.com>
2021-08-31 15:34:52 +0800

    KVM: TDX: Allow 2MB large page for TD GUEST
    Now that everything is there to support 2MB page for TD guest.  Because TDX
    module TDH.MEM.PAGE.AUG supports 4KB page and 2MB page, set struct
    kvm_arch.tdp_max_page_level to 2MB page level.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -487,8 +487,8 @@ int tdx_vm_init(struct kvm *kvm)
 	 */
 	kvm_mmu_set_mmio_spte_value(kvm, 0);
 
-	/* TODO: Enable 2mb and 1gb large page support. */
-	kvm->arch.tdp_max_page_level = PG_LEVEL_4K;
+	/* TDH.MEM.PAGE.AUG supports up to 2MB page. */
+	kvm->arch.tdp_max_page_level = PG_LEVEL_2M;
 
 	/*
 	 * This function initializes only KVM software construct.  It doesn't

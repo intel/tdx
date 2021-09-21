13a2ef69697e7bb52d7d136c262201c3f9ff5d04
Isaku Yamahata <isaku.yamahata@intel.com>
2021-09-21 16:17:15 -0700

    x86/cpu/tdx: Get the information about the TDX firmware loader
    Check if the TDX firmware loader (P-SEAMLDR) is available, get the
    information about the P-SEAMLDR and log them for the administrator of the
    machine or VM management software.
    
    Although the kernel doesn't use the TDX firmware loader at the moment, log
    its version for the admin because it's a part of TCB.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kernel/cpu/tdx/p-seamldr.h
+++ arch/x86/kernel/cpu/tdx/p-seamldr.h
@@ -4,6 +4,9 @@
 #ifndef _X86_TDX_P_SEAMLOADER_H
 #define _X86_TDX_P_SEAMLOADER_H
 
+#include <linux/types.h>
+
+#include <asm/page.h>
 #include <asm/tdx_errno.h>
 
 /*
@@ -18,5 +21,50 @@
 #define P_SEAMLDR_SEAMCALL_ERROR_CODE	0x8000000000000000ULL
 
 #define P_SEAMLDR_SUCCESS	0x0000000000000000ULL
+
+/*
+ * P-SEAMLDR function leaves
+ */
+#define SEAMCALL_SEAMLDR_BASE		BIT_ULL(63)
+#define SEAMCALL_SEAMLDR_INFO		SEAMCALL_SEAMLDR_BASE
+
+struct tee_tcb_svn {
+	u16 seam;
+	u8 reserved[14];
+} __packed;
+
+struct __tee_tcb_info {
+	u64 valid;
+	struct tee_tcb_svn tee_tcb_svn;
+	u64 mrseam[6];		/* SHA-384 */
+	u64 mrsignerseam[6];	/* SHA-384 */
+	u64 attributes;
+} __packed;
+
+struct tee_tcb_info {
+	struct __tee_tcb_info info;
+	u8 reserved[111];
+} __packed;
+
+#define P_SEAMLDR_INFO_ALIGNMENT	256
+struct p_seamldr_info {
+	u32 version;
+	u32 attributes;
+	u32 vendor_id;
+	u32 build_date;
+	u16 build_num;
+	u16 minor;
+	u16 major;
+	u8 reserved0[2];
+	u32 acm_x2apicid;
+	u8 reserved1[4];
+	struct __tee_tcb_info seaminfo;
+	u8 seam_ready;
+	u8 seam_debug;
+	u8 p_seamldr_ready;
+	u8 reserved2[88];
+} __packed __aligned(P_SEAMLDR_INFO_ALIGNMENT);
+
+int __init p_seamldr_get_info(void);
 
 #endif /* _X86_TDX_P_SEAMLOADER_H */

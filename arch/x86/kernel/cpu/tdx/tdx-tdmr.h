a92bae254557ab26c33501f04d8eb91a8e7f6cb0
Kai Huang <kai.huang@intel.com>
2021-09-28 09:55:49 +1300

    x86/cpu/tdx: Add helper to construct final TDMRs
    Add a helper to construct final TDMRs, using the final TDX memory instance
    built from system memory and legacy PMEMs.  A later patch in this patchset
    will use it.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kernel/cpu/tdx/tdx-tdmr.h
+++ arch/x86/kernel/cpu/tdx/tdx-tdmr.h
@@ -2,10 +2,20 @@
 #ifndef _X86_TDX_TDMR_H
 #define _X86_TDX_TDMR_H
 
+#include "tdmr-sysmem.h"
+
 /* Build TDX memory with all TDX capable memory blocks */
 int __init build_tdx_memory(void);
 
 /* Clean up TDX memory in case of any error before build_tdx_memory(). */
 void __init cleanup_subtype_tdx_memory(void);
+
+/*
+ * Construct final TDMRs based on CMR info and TDX module info, to cover
+ * final TDX memory @tmem_sysmem (built by tdx_sysmem_build()).
+ */
+int __init construct_tdx_tdmrs(struct cmr_info *cmr_array, int cmr_num,
+		struct tdx_module_descriptor *desc,
+		struct tdmr_info *tdmr_info_array, int *tdmr_num);
 
 #endif

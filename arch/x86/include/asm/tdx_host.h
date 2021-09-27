33d2f5cbcc785e4b4b022a515753ad63e423e0e1
Kai Huang <kai.huang@intel.com>
2021-09-28 09:55:53 +1300

    x86/mm: Prevent memory hotplug for TDX
    TDX architecturally doesn't support memory hotplug, so ACPI memory hotplug
    needs to be disabled.  Also, the kernel converts all convertible memory to
    TDX memory to avoid having to modify the page allocator to distinguish TDX
    and non-TDX memory allocation.  This means driver-managed memory hotplug
    (i.e. kmem-hot-added PMEM) needs to be disabled too.  Meanwhile,
    memremap_pages() must continue to work.  Although it extends the NUMA
    node's spanned pages, the newly added memory region will end up in
    ZONE_DEVICE, whose pages won't be managed by the page allocator.
    
    To cover all the above cases, implement an x86 replacement version of
    arch_check_hotplug_memory_range() to deny memory hotplug if memory the
    range is not fully covered by any TDX memory block because TDX memory
    blocks are converted to TDX memory during kernel boot.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/include/asm/tdx_host.h
+++ arch/x86/include/asm/tdx_host.h
@@ -15,6 +15,8 @@ void pr_seamcall_error(u64 op, u64 error_code, const struct tdx_ex_ret *ex_ret);
 struct tdsysinfo_struct;
 const struct tdsysinfo_struct *tdx_get_sysinfo(void);
 
+bool range_is_tdx_memory(phys_addr_t start, phys_addr_t end);
+
 #else
 struct tdx_ex_ret;
 static inline void pr_seamcall_ex_ret_info(u64 op, u64 error_code,
@@ -31,6 +33,11 @@ struct tdsysinfo_struct;
 static inline const struct tdsysinfo_struct *tdx_get_sysinfo(void)
 {
 	return NULL;
+}
+
+static inline bool range_is_tdx_memory(phys_addr_t start, phys_addr_t end)
+{
+	return false;
 }
 #endif
 

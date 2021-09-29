af4aa5c8cd9a4ecae951a5aad7f97563cca06a19
Andi Kleen <ak@linux.intel.com>
2021-09-28 19:04:56 -0700

    x86/tdx: Basic TDCALL fuzzing support
    In TDX the hypervisor is untrusted and can attack the kernel
    through the output of MMIO/MSR reads/port IO and some other options.
    Untrusted "hardware" is a need concept in Linux, so we need to make sure the
    code is hardened against bad responses.
    
    Add a simple fuzzer to allow hardening the interfaces. This is not
    sufficient for full hardening, but useful for basic testing.
    
    This hooks into the low level TDcalls and allows to fuzz all
    interactions with the host, except those through shared memory.
    
    The basic setup is through the fault injection framework, but
    in addition there is own random generation state that allows
    flipping random bits in the TDCALL responses.
    
    /sys/kernel/debug/tdx_fail/ Tunables
    (in addition to standard fault injection tunables like skip etc.):
    seed: Set the seed for the random generator per CPU.
    tdcall: Enable bit flipping for TDCALLs
    num_change_bits: How many bits to flip in a TDCALL response.
    		 Set to 64 to replace the complete value.
    
    The same can be also configure with a command line parameter
    for early  boot coverage
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/tdx.c
+++ arch/x86/kernel/tdx.c
@@ -309,6 +309,8 @@ static void tdx_get_info(void)
 	 */
 	BUG_ON(ret);
 
+	/* Not fuzzed because this comes from the trusted TDX module */
+
 	td_info.gpa_width = out.rcx & GENMASK(5, 0);
 	td_info.attributes = out.rdx;
 
@@ -453,7 +455,8 @@ static bool tdx_read_msr_safe(unsigned int msr, u64 *val)
 	if (_trace_tdx_hypercall(EXIT_REASON_MSR_READ, msr, 0, 0, 0, &out))
 		return false;
 
-	*val = out.r11;
+	/* Should filter the MSRs to only fuzz host controlled */
+	*val = tdx_fuzz(out.r11, TDX_FUZZ_MSR_READ);
 
 	return true;
 }
@@ -525,10 +528,10 @@ static bool tdx_handle_cpuid(struct pt_regs *regs)
 	 * EAX, EBX, ECX, EDX registers after CPUID instruction execution.
 	 * So copy the register contents back to pt_regs.
 	 */
-	regs->ax = out.r12;
-	regs->bx = out.r13;
-	regs->cx = out.r14;
-	regs->dx = out.r15;
+	regs->ax = tdx_fuzz(out.r12, TDX_FUZZ_CPUID1);
+	regs->bx = tdx_fuzz(out.r13, TDX_FUZZ_CPUID2);
+	regs->cx = tdx_fuzz(out.r14, TDX_FUZZ_CPUID3);
+	regs->dx = tdx_fuzz(out.r15, TDX_FUZZ_CPUID4);
 
 	return true;
 }
@@ -570,7 +573,9 @@ static bool tdx_handle_io(struct pt_regs *regs, u32 exit_qual)
 					   size, out, port, regs->ax,
 					   &outh);
 		regs->ax &= ~mask;
-		regs->ax |= (ret ? UINT_MAX : outh.r11) & mask;
+		regs->ax |= tdx_fuzz(ret ?
+				UINT_MAX : outh.r11, TDX_FUZZ_PORT_IN)
+			& mask;
 	} else {
 		ret = _tdx_hypercall(EXIT_REASON_IO_INSTRUCTION,
 				     size, out, port, regs->ax,
@@ -588,7 +593,8 @@ static unsigned long tdx_mmio(int size, bool write, unsigned long addr,
 
 	err = _trace_tdx_hypercall(EXIT_REASON_EPT_VIOLATION, size, write,
 				   addr, *val, &out);
-	*val = out.r11;
+
+	*val = tdx_fuzz(out.r11, TDX_FUZZ_MMIO_READ);
 
 	return err;
 }
@@ -805,6 +811,7 @@ bool tdx_get_ve_info(struct ve_info *ve)
 	ve->instr_len   = out.r10 & UINT_MAX;
 	ve->instr_info  = out.r10 >> 32;
 
+	/* Not fuzzed because it comes from the trusted TDX module */
 	return true;
 }
 

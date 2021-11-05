9a07dd035a28920fdd2b658d95f1a52618afe005
Andi Kleen <ak@linux.intel.com>
2021-11-05 13:19:12 -0700

    x86/tdx: Disable CQM in guest
    CQM has a lot of complex code that is difficult to harden. Currently
    there is no use case for CQM in a TDX guest, so just disable it.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/tdx.c
+++ arch/x86/kernel/tdx.c
@@ -16,6 +16,7 @@
 #include <asm/insn.h>
 #include <asm/insn-eval.h>
 #include <linux/pci.h>
+#include <linux/nmi.h>
 
 #define CREATE_TRACE_POINTS
 #include <asm/trace/tdx.h>
@@ -816,6 +817,7 @@ void __init tdx_early_init(void)
 	setup_clear_cpu_cap(X86_FEATURE_MTRR);
 	setup_clear_cpu_cap(X86_FEATURE_APERFMPERF);
 	setup_clear_cpu_cap(X86_FEATURE_TME);
+	setup_clear_cpu_cap(X86_FEATURE_CQM_LLC);
 
 	/*
 	 * The only secure (mononotonous) timer inside a TD guest
@@ -840,6 +842,14 @@ void __init tdx_early_init(void)
 	swiotlb_force = SWIOTLB_FORCE;
 
 	legacy_pic = &null_legacy_pic;
+
+	/*
+	 * Disable NMI watchdog because of the risk of false positives
+	 * and also can increase overhead in the TDX module.
+	 * This is already done for KVM, but covers other hypervisors
+	 * here.
+	 */
+	hardlockup_detector_disable();
 
 	/*
 	 * Make sure there is a panic if something goes wrong,

2d7fb26938f856987a649a40cb16b5ec7795cd73
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2021-11-08 17:55:50 +0300

    x86/swiotlb: Adjust SWIOTLB bounce buffer size for TDX guests
    As with SEV, TDX actively uses SWIOTLB and default 64MiB buffer might
    not be enough.
    
    SEV increases bounce buffer size to about 6% of RAM, but not exceeding
    1GiB. Use the same approach for TDX.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- arch/x86/mm/mem_encrypt.c
+++ arch/x86/mm/mem_encrypt.c
@@ -84,6 +84,32 @@ void __init mem_encrypt_init(void)
 	print_mem_encrypt_feature_info();
 }
 
+void __init mem_encrypt_init_swiotlb_size(void)
+{
+	unsigned long size;
+
+	/*
+	 * For SEV and TDX, all DMA has to occur via shared/unencrypted pages.
+	 * Kernel uses SWIOTLB to make this happen without changing device
+	 * drivers. However, depending on the workload being run, the
+	 * default 64MB of SWIOTLB may not be enough and SWIOTLB may
+	 * run out of buffers for DMA, resulting in I/O errors and/or
+	 * performance degradation especially with high I/O workloads.
+	 *
+	 * Adjust the default size of SWIOTLB using a percentage of guest
+	 * memory for SWIOTLB buffers. Also, as the SWIOTLB bounce buffer
+	 * memory is allocated from low memory, ensure that the adjusted size
+	 * is within the limits of low available memory.
+	 *
+	 * The percentage of guest memory used here for SWIOTLB buffers
+	 * is more of an approximation of the static adjustment which
+	 * 64MB for <1G, and ~128M to 256M for 1G-to-4G, i.e., the 6%
+	 */
+	size = memblock_phys_mem_size() * 6 / 100;
+	size = clamp_val(size, IO_TLB_DEFAULT_SIZE, SZ_1G);
+	swiotlb_adjust_size(size);
+}
+
 int arch_has_restricted_virtio_memory_access(void)
 {
 	return cc_platform_has(CC_ATTR_GUEST_MEM_ENCRYPT) ||

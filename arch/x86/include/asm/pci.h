a769495a8ed2750d06235a86f3a702fb4e12b6f2
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2021-11-10 19:23:10 -0800

    x86/tdx: Disable MMCONF for TDX guest
    Supporting MMCONF would require sharing the IO memory explicitly
    in individual drivers. But most of the drivers which use MMCONF
    is never used in TDX guest platforms. So disable its support
    by default.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/include/asm/pci.h
+++ arch/x86/include/asm/pci.h
@@ -95,8 +95,10 @@ int pcibios_set_irq_routing(struct pci_dev *dev, int pin, int irq);
 bool pci_dev_has_default_msi_parent_domain(struct pci_dev *dev);
 #ifdef CONFIG_PCI
 void pci_disable_early(void);
+void pci_disable_mmconf(void);
 #else
 static inline void pci_disable_early(void) { }
+static inline void pci_disable_mmconf(void) { }
 #endif
 
 #define HAVE_PCI_MMAP

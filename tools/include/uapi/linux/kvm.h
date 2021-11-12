5979d880dc074c7fa57e02da34a41a6905ebd89d
Sean Christopherson <sean.j.christopherson@intel.com>
2021-11-12 23:37:23 +0800

    KVM: x86: Introduce vm_type to differentiate default VMs from confidential VMs
    Unlike default VMs, confidential VMs (Intel TDX and AMD SEV-ES) don't allow
    some operations (e.g., memory read/write, register state access, etc).
    
    Introduce vm_type to track the type of the VM to x86 KVM.  Other arch KVMs
    already use vm_type, KVM_INIT_VM accepts vm_type, and x86 KVM callback
    vm_init accepts vm_type.  So follow them.  Further, a different policy can
    be made based on vm_type.  Define KVM_X86_DEFAULT_VM for default VM as
    default and define KVM_X86_TDX_VM for Intel TDX VM.  The wrapper function
    will be defined as "bool is_td(kvm) { return vm_type == VM_TYPE_TDX; }"
    
    Add a capability KVM_CAP_VM_TYPES to effectively allow device model,
    e.g. qemu, to query what VM types are supported by KVM.  This (introduce a
    new capability and add vm_type) is chosen to align with other arch KVMs
    that have VM types already.  Other arch KVMs uses different name to query
    supported vm types and there is no common name for it, so new name was
    chosen.
    
    Co-developed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>

--- tools/include/uapi/linux/kvm.h
+++ tools/include/uapi/linux/kvm.h
@@ -1152,6 +1152,7 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_DISABLE_QUIRKS2 213
 /* #define KVM_CAP_VM_TSC_CONTROL 214 */
 #define KVM_CAP_SYSTEM_EVENT_DATA 215
+#define KVM_CAP_VM_TYPES 220
 
 #ifdef KVM_CAP_IRQ_ROUTING
 

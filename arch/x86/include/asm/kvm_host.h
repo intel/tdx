5e38085b55410c5695ee876f39c8baf5066cd20a
Sean Christopherson <sean.j.christopherson@intel.com>
2021-11-12 23:37:23 +0800

    KVM: x86: Introduce vm_type to differentiate default VMs from confidential VMs
    Unlike default VMs, confidential VMs (Intel TDX and AMD SEV-ES) don't allow
    some operations (e.g., memory read/write, register state access, etc).
    
    Introduce vm_type to track the type of the VM. Further, a different policy
    can be made based on vm_type. Define KVM_X86_DEFAULT_VM for default VM as
    default and define KVM_X86_TDX_VM for Intel TDX VM.
    The helper function will be defined as
    "bool is_td(kvm) { return vm_type == VM_TYPE_TDX; }"
    
    Add a capability KVM_CAP_VM_TYPES to effectively allow userspace to query
    what VM types are supported by KVM.
    
    Co-developed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1048,6 +1048,7 @@ struct kvm_x86_msr_filter {
 #define APICV_INHIBIT_REASON_ABSENT	7
 
 struct kvm_arch {
+	unsigned long vm_type;
 	unsigned long n_used_mmu_pages;
 	unsigned long n_requested_mmu_pages;
 	unsigned long n_max_mmu_pages;
@@ -1322,6 +1323,7 @@ struct kvm_x86_ops {
 	bool (*has_emulated_msr)(struct kvm *kvm, u32 index);
 	void (*vcpu_after_set_cpuid)(struct kvm_vcpu *vcpu);
 
+	bool (*is_vm_type_supported)(unsigned long vm_type);
 	unsigned int vm_size;
 	int (*vm_init)(struct kvm *kvm);
 	void (*vm_destroy)(struct kvm *kvm);

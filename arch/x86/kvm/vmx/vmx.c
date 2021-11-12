e5face34864a25b16e70bc7798649e51e0712e9d
Isaku Yamahata <isaku.yamahata@intel.com>
2021-11-12 23:37:23 +0800

    KVM: x86: Add is_vm_type_supported callback
    For TDX, allow the backend can override the supported vm type.  Add
    KVM_X86_TDX_VM to reserve the bit.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    Changes v3 -> v4:
    - Added KVM_X86_SNP_VM
    
    Changes v2 -> v3:
    - no change
    - didn't bother to rename KVM_X86_PROTECTED_VM to KVM_X86_SW_PROTECTED_VM
    
    Changes v1 -> v2
    - no change

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -7540,6 +7540,12 @@ free_vpid:
 	return err;
 }
 
+static bool vmx_is_vm_type_supported(unsigned long type)
+{
+	/* TODO: Check if TDX is supported. */
+	return __kvm_is_vm_type_supported(type);
+}
+
 #define L1TF_MSG_SMT "L1TF CPU bug present and SMT on, data leak possible. See CVE-2018-3646 and https://www.kernel.org/doc/html/latest/admin-guide/hw-vuln/l1tf.html for details.\n"
 #define L1TF_MSG_L1D "L1TF CPU bug present and virtualization mitigation disabled, data leak possible. See CVE-2018-3646 and https://www.kernel.org/doc/html/latest/admin-guide/hw-vuln/l1tf.html for details.\n"
 
@@ -8209,6 +8215,7 @@ static struct kvm_x86_ops vmx_x86_ops __initdata = {
 	.hardware_disable = vmx_hardware_disable,
 	.has_emulated_msr = vmx_has_emulated_msr,
 
+	.is_vm_type_supported = vmx_is_vm_type_supported,
 	.vm_size = sizeof(struct kvm_vmx),
 	.vm_init = vmx_vm_init,
 	.vm_destroy = vmx_vm_destroy,

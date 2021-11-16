af82175a97002887d8565147902ec85dcc8b7872
Isaku Yamahata <isaku.yamahata@intel.com>
2021-11-15 22:31:15 -0800

    [REVERTME] x86/cpu/tsx: Add helper functions to clear IA32_TSX_CTRL MSR
    TODO: Drop this patch once TDX module 1.0.3.3/1.5.00.15 (or later) is
    widely used.
    
    The TDX SEAMCALL, TDH.SYS.ENTER, requires the RTM_DISALBE (bit 0) and
    TSX_CPUID_CLEAR (bit 1) to be 0 if IA32_TSX_CRTL MSR is supported by CPU
    (enumerated by IA32_ARCH_CAPABILITIES.TSX_CTRL).
    
    Add helper functions to clear RTM_DISABLE bit and TSX_CPUID_CLEAR
    bit of IA32_TSX_CTRL.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/include/asm/cpu.h
+++ arch/x86/include/asm/cpu.h
@@ -46,6 +46,7 @@ extern bool handle_user_split_lock(struct pt_regs *regs, long error_code);
 extern bool handle_guest_split_lock(unsigned long ip);
 extern void handle_bus_lock(struct pt_regs *regs);
 u8 get_this_hybrid_cpu_type(void);
+extern void tsx_ctrl_clear(void);
 #else
 static inline void __init sld_setup(struct cpuinfo_x86 *c) {}
 static inline bool handle_user_split_lock(struct pt_regs *regs, long error_code)
@@ -64,6 +65,9 @@ static inline u8 get_this_hybrid_cpu_type(void)
 {
 	return 0;
 }
+
+static inline void tsx_ctrl_clear(void) { }
+
 #endif
 #ifdef CONFIG_IA32_FEAT_CTL
 void init_ia32_feat_ctl(struct cpuinfo_x86 *c);

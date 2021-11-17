27e58c1676fe1c79eaf968b41c878db2937fab54
Yuan Yao <yuan.yao@intel.com>
2021-11-17 16:23:42 +0800

    KVM: X86: Introduce new kvm_x86_ops::load_guest_debug_regs interface
    This new interface focus on loading debug registers for
    normal/encrypted guests when before vmentry, because
    the encrypted guests (e.g INTEL TDX and AMD SEV) have their own
    interfaces to do such things from the normal legacy x86 guest.
    
    Tested for legacy guests on INTEL SPR, no function change.
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1217,6 +1217,7 @@ struct kvm_x86_ops {
 	void (*set_gdt)(struct kvm_vcpu *vcpu, struct desc_ptr *dt);
 	void (*sync_dirty_debug_regs)(struct kvm_vcpu *vcpu);
 	void (*set_dr7)(struct kvm_vcpu *vcpu, unsigned long value);
+	void (*load_guest_debug_regs)(struct kvm_vcpu *vcpu);
 	void (*cache_gprs)(struct kvm_vcpu *vcpu);
 	void (*flush_gprs)(struct kvm_vcpu *vcpu);
 	void (*cache_reg)(struct kvm_vcpu *vcpu, enum kvm_reg reg);
@@ -1868,5 +1869,8 @@ static inline int kvm_cpu_get_apicid(int mps_cpu)
 	(*(type *)((buf) + (offset) - 0x7e00))
 
 int kvm_cpu_dirty_log_size(void);
+
+/* The common function for normal x86 guest */
+void load_guest_debug_regs(struct kvm_vcpu *vcpu);
 
 #endif /* _ASM_X86_KVM_HOST_H */

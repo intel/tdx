38f6117d3b7e9fc3eedbcf79f5393675f516eeef
Isaku Yamahata <isaku.yamahata@intel.com>
2021-11-22 15:35:39 -0800

    KVM: TDX: Add helper functions to print TDX SEAMCALL error
    Add helper functions to print out errors from the TDX module in a uniform
    manner.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/seamcall.h
+++ arch/x86/kvm/vmx/seamcall.h
@@ -16,6 +16,8 @@ struct tdx_module_output;
 u64 kvm_seamcall(u64 op, u64 rcx, u64 rdx, u64 r8, u64 r9, u64 r10,
 		struct tdx_module_output *out);
 
+void pr_tdx_error(u64 op, u64 error_code, const struct tdx_module_output *out);
+
 #endif /* !__ASSEMBLY__ */
 
 #endif	/* CONFIG_INTEL_TDX_HOST */

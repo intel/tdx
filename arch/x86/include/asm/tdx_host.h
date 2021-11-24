368a0fdc7fb84b9227c8c21a63cb7af60a64243e
Isaku Yamahata <isaku.yamahata@intel.com>
2021-11-24 12:03:03 -0800

    x86/cpu/tdx: unexport struct tdx_ex_ret as KVM defines it itself
    To avoid type conflict between x86 host and kvm, unexport struct tdx_ex_ret
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/include/asm/tdx_host.h
+++ arch/x86/include/asm/tdx_host.h
@@ -10,42 +10,6 @@
 
 void tdx_early_init(void);
 
-/*
- * TDX extended return:
- * Some of The "TDX module" SEAMCALLs return extended values (which are function
- * leaf specific) in registers in addition to the completion status code in
- * %rax.  For example, in the error case of TDH.SYS.INIT, the registers hold
- * more detailed information about the error in addition to an error code.  Note
- * that some registers may be unused depending on SEAMCALL functions.
- */
-struct tdx_ex_ret {
-	union {
-		struct {
-			u64 rcx;
-			u64 rdx;
-			u64 r8;
-			u64 r9;
-			u64 r10;
-			u64 r11;
-		} regs;
-		/*
-		 * TDH_SYS_INFO returns the buffer address and its size, and the
-		 * CMR_INFO address and its number of entries.
-		 */
-		struct {
-			u64 buffer;
-			u64 nr_bytes;
-			u64 cmr_info;
-			u64 nr_cmr_entries;
-		} sys_info;
-		/* TDH_SYS_TDMR_INIT returns the input PA and next PA. */
-		struct {
-			u64 prev;
-			u64 next;
-		} sys_tdmr_init;
-	};
-};
-
 struct tdsysinfo_struct;
 const struct tdsysinfo_struct *tdx_get_sysinfo(void);
 

bb3de6f0bcbdfa6e4baae2682ec8d74e3628923f
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-08 12:04:34 -0800

    KVM: TDX: Silently discard SMI request
    TDX doesn't support system-management mode (SMM) and system-management
    interrupt (SMI) in guest TDs.  Because guest state (vcpu state, memory
    state) is protected, it must go through the TDX module APIs to change guest
    state, injecting SMI and changing vcpu mode into SMM.  The TDX module
    doesn't provide a way for VMM to inject SMI into guest TD and a way for VMM
    to switch guest vcpu mode into SMM.
    
    We have two options in KVM when handling SMM or SMI in the guest TD or the
    device model (e.g. QEMU): 1) silently ignore the request or 2) return a
    meaningful error.
    
    For simplicity, we implemented the option 1).
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4893,7 +4893,8 @@ static int kvm_vcpu_ioctl_nmi(struct kvm_vcpu *vcpu)
 
 static int kvm_vcpu_ioctl_smi(struct kvm_vcpu *vcpu)
 {
-	kvm_make_request(KVM_REQ_SMI, vcpu);
+	if (static_call(kvm_x86_has_emulated_msr)(vcpu->kvm, MSR_IA32_SMBASE))
+		kvm_make_request(KVM_REQ_SMI, vcpu);
 
 	return 0;
 }

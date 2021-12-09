a4d9e3e239d796d8527321433fe916f22b619a9b
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-08 17:40:09 -0800

    KVM: TDX: Silently ignore INIT/SIPI
    The TDX module API doesn't provide API for VMM to inject INIT IPI and SIPI.
    Instead it defines the different protocols to boot application processors.
    Ignore INIT and SIPI events for the TDX guest.
    
    There are two options. 1) (silently) ignore INIT/SIPI request or 2) return
    error to guest TDs somehow.  Given that TDX guest is paravirtualized to
    boot AP, the option 1 is chosen for simplicity.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/include/asm/kvm-x86-ops.h
+++ arch/x86/include/asm/kvm-x86-ops.h
@@ -145,6 +145,7 @@ KVM_X86_OP_OPTIONAL(migrate_timers)
 KVM_X86_OP(msr_filter_changed)
 KVM_X86_OP(complete_emulated_msr)
 KVM_X86_OP(vcpu_deliver_sipi_vector)
+KVM_X86_OP(vcpu_deliver_init)
 KVM_X86_OP_OPTIONAL_RET0(vcpu_get_apicv_inhibit_reasons);
 KVM_X86_OP_OPTIONAL_RET0(gmem_prepare)
 KVM_X86_OP_OPTIONAL(gmem_invalidate)

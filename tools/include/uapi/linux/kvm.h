7bbc47d24779454d52e749a557d6662a04e8e17a
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-13 14:59:16 -0800

    [REVERTME] workaround to avoid KVM_CAP conflict in the upstrewam
    Bump KVM_CAP_VM_TYPES to 1000 to avoid the conflict in the upstream.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- tools/include/uapi/linux/kvm.h
+++ tools/include/uapi/linux/kvm.h
@@ -1157,7 +1157,8 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_VM_TSC_CONTROL 214
 #define KVM_CAP_SYSTEM_EVENT_DATA 215
 #define KVM_CAP_ARM_SYSTEM_SUSPEND 216
-#define KVM_CAP_VM_TYPES 223
+/* TODO: remove this workaround to avoid CAP number conflict in the upstream. */
+#define KVM_CAP_VM_TYPES 1000
 
 #ifdef KVM_CAP_IRQ_ROUTING
 

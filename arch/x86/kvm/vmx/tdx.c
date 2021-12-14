85209122d5af1a3185ff58d13528284d91035100
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-13 17:13:49 -0800

    KVM: TDX: Detect CPU feature on kernel module initialization
    TDX requires several initialization steps for KVM to create guest TDs.
    Detect CPU feature, enable VMX (TDX is based on VMX), detect TDX module
    availability, and initialize TDX module.  This patch implements the first
    step to detect CPU feature.  Because VMX isn't enabled yet by VMXON
    instruction on KVM kernel module initialization, defer further
    initialization step until VMX is enabled by hardware_enable callback.
    
    Introduce a module parameter, enable_tdx, to explicitly enable TDX KVM
    support.  It's off by default to keep same behavior for those who don't use
    TDX.  Implement CPU feature detection at KVM kernel module initialization
    as hardware_setup callback to check if CPU feature is available and get
    some CPU parameters.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

---
+++ arch/x86/kvm/vmx/tdx.c
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/cpu.h>
+
+#include <asm/tdx.h>
+
+#include "capabilities.h"
+#include "x86_ops.h"
+
+#undef pr_fmt
+#define pr_fmt(fmt) "tdx: " fmt
+
+static u64 hkid_mask __ro_after_init;
+static u8 hkid_start_pos __ro_after_init;
+
+int __init tdx_hardware_setup(struct kvm_x86_ops *x86_ops)
+{
+	u32 max_pa;
+
+	if (!enable_ept) {
+		pr_warn("Cannot enable TDX with EPT disabled\n");
+		return -EINVAL;
+	}
+
+	if (!platform_tdx_enabled()) {
+		pr_warn("Cannot enable TDX on TDX disabled platform\n");
+		return -ENODEV;
+	}
+
+	/* Safe guard check because TDX overrides tlb_remote_flush callback. */
+	if (WARN_ON_ONCE(x86_ops->tlb_remote_flush))
+		return -EIO;
+
+	max_pa = cpuid_eax(0x80000008) & 0xff;
+	hkid_start_pos = boot_cpu_data.x86_phys_bits;
+	hkid_mask = GENMASK_ULL(max_pa - 1, hkid_start_pos);
+	pr_info("kvm: TDX is supported. hkid start pos %d mask 0x%llx\n",
+		hkid_start_pos, hkid_mask);
+
+	return 0;
+}

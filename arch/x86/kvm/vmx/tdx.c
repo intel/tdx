0e0234cecd5544cb819c8436a20c1b48a483cf8a
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-13 23:51:47 -0800

    KVM: TDX: Finalize VM initialization
    Add a new VM-scoped KVM_MEMORY_ENCRYPT_OP IOCTL subcommand,
    KVM_TDX_FINALIZE_VM, to perform TD Measurement Finalization.
    
    Documentation for the API is added in another patch:
    "Documentation/virt/kvm: Document on Trust Domain Extensions(TDX)"
    
    For the purpose of attestation, a measurement must be made of the TDX VM
    initial state. This is referred to as TD Measurement Finalization, and
    uses SEAMCALL TDH.MR.FINALIZE, after which:
    1. The VMM adding TD private pages with arbitrary content is no longer
       allowed
    2. The TDX VM is runnable
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Co-developed-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    ---
    v20:
    - Added premapped check.
    - Update for the wrapper functions for SEAMCALLs. (Sean)
    - Add check if nr_premapped is zero.  If not, return error.
    - Use KVM_BUG_ON() in tdx_td_finalizer() for consistency.
    - Change tdx_td_finalizemr() to take struct kvm_tdx_cmd *cmd and return error
      (Adrian)
    - Handle TDX_OPERAND_BUSY case (Adrian)
    - Updates from seamcall overhaul (Kai)
    - Rename error->hw_error
    
    v18:
    - Remove the change of tools/arch/x86/include/uapi/asm/kvm.h.
    
    v14 -> v15:
    - removed unconditional tdx_track() by tdx_flush_tlb_current() that
      does tdx_track().

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -1359,6 +1359,31 @@ void tdx_flush_tlb_current(struct kvm_vcpu *vcpu)
 	tdx_track(vcpu->kvm);
 }
 
+static int tdx_td_finalizemr(struct kvm *kvm, struct kvm_tdx_cmd *cmd)
+{
+	struct kvm_tdx *kvm_tdx = to_kvm_tdx(kvm);
+
+	if (!is_hkid_assigned(kvm_tdx) || is_td_finalized(kvm_tdx))
+		return -EINVAL;
+	/*
+	 * Pages are pending for KVM_TDX_INIT_MEM_REGION to issue
+	 * TDH.MEM.PAGE.ADD().
+	 */
+	if (atomic64_read(&kvm_tdx->nr_premapped))
+		return -EINVAL;
+
+	cmd->hw_error = tdh_mr_finalize(kvm_tdx);
+	if ((cmd->hw_error & TDX_SEAMCALL_STATUS_MASK) == TDX_OPERAND_BUSY)
+		return -EAGAIN;
+	if (KVM_BUG_ON(cmd->hw_error, kvm)) {
+		pr_tdx_error(TDH_MR_FINALIZE, cmd->hw_error);
+		return -EIO;
+	}
+
+	kvm_tdx->finalized = true;
+	return 0;
+}
+
 int tdx_vm_ioctl(struct kvm *kvm, void __user *argp)
 {
 	struct kvm_tdx_cmd tdx_cmd;
@@ -1382,6 +1407,9 @@ int tdx_vm_ioctl(struct kvm *kvm, void __user *argp)
 		break;
 	case KVM_TDX_INIT_VM:
 		r = tdx_td_init(kvm, &tdx_cmd);
+		break;
+	case KVM_TDX_FINALIZE_VM:
+		r = tdx_td_finalizemr(kvm, &tdx_cmd);
 		break;
 	default:
 		r = -EINVAL;

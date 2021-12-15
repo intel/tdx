14a5903bcab359142036fd14c148631ec3306a74
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-15 10:50:03 -0800

    KVM: x86: Refactor KVM VMX module init/exit functions
    Currently, KVM VMX module initialization/exit functions are a single
    function each.  Refactor KVM VMX module initialization functions into KVM
    common part and VMX part so that TDX specific part can be added cleanly.
    Opportunistically refactor module exit function as well.
    
    The current module initialization flow is, 1.) calculate the sizes of VMX
    kvm structure and VMX vcpu structure, 2.) hyper-v specific initialization
    3.) report those sizes to the KVM common layer and KVM common
    initialization, and 4.) VMX specific system-wide initialization.
    
    Refactor the KVM VMX module initialization function into functions with a
    wrapper function to separate VMX logic in vmx.c from a file, main.c, common
    among VMX and TDX.  We have a wrapper function, "vt_init() {vmx kvm/vcpu
    size calculation; hv_vp_assist_page_init(); kvm_init(); vmx_init(); }" in
    main.c, and hv_vp_assist_page_init() and vmx_init() in vmx.c.
    hv_vp_assist_page_init() initializes hyper-v specific assist pages,
    kvm_init() does system-wide initialization of the KVM common layer, and
    vmx_init() does system-wide VMX initialization.
    
    The KVM architecture common layer allocates struct kvm with reported size
    for architecture-specific code.  The KVM VMX module defines its structure
    as struct vmx_kvm { struct kvm; VMX specific members;} and uses it as
    struct vmx kvm.  Similar for vcpu structure. TDX KVM patches will define
    TDX specific kvm and vcpu structures, add tdx_pre_kvm_init() to report the
    sizes of them to the KVM common layer.
    
    The current module exit function is also a single function, a combination
    of VMX specific logic and common KVM logic.  Refactor it into VMX specific
    logic and KVM common logic.  This is just refactoring to keep the VMX
    specific logic in vmx.c from main.c.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -8242,15 +8242,45 @@ static void vmx_cleanup_l1d_flush(void)
 	l1tf_vmx_mitigation = VMENTER_L1D_FLUSH_AUTO;
 }
 
-static void vmx_exit(void)
+void __init hv_vp_assist_page_init(void)
 {
-#ifdef CONFIG_KEXEC_CORE
-	RCU_INIT_POINTER(crash_vmclear_loaded_vmcss, NULL);
-	synchronize_rcu();
+#if IS_ENABLED(CONFIG_HYPERV)
+	/*
+	 * Enlightened VMCS usage should be recommended and the host needs
+	 * to support eVMCS v1 or above. We can also disable eVMCS support
+	 * with module parameter.
+	 */
+	if (enlightened_vmcs &&
+	    ms_hyperv.hints & HV_X64_ENLIGHTENED_VMCS_RECOMMENDED &&
+	    (ms_hyperv.nested_features & HV_X64_ENLIGHTENED_VMCS_VERSION) >=
+	    KVM_EVMCS_VERSION) {
+		int cpu;
+
+		/* Check that we have assist pages on all online CPUs */
+		for_each_online_cpu(cpu) {
+			if (!hv_get_vp_assist_page(cpu)) {
+				enlightened_vmcs = false;
+				break;
+			}
+		}
+
+		if (enlightened_vmcs) {
+			pr_info("KVM: vmx: using Hyper-V Enlightened VMCS\n");
+			static_branch_enable(&enable_evmcs);
+		}
+
+		if (ms_hyperv.nested_features & HV_X64_NESTED_DIRECT_FLUSH)
+			vt_x86_ops.enable_direct_tlbflush
+				= hv_enable_direct_tlbflush;
+
+	} else {
+		enlightened_vmcs = false;
+	}
 #endif
+}
 
-	kvm_exit();
-
+void hv_vp_assist_page_exit(void)
+{
 #if IS_ENABLED(CONFIG_HYPERV)
 	if (static_branch_unlikely(&enable_evmcs)) {
 		int cpu;
@@ -8274,17 +8304,13 @@ static void vmx_exit(void)
 		static_branch_disable(&enable_evmcs);
 	}
 #endif
-	vmx_cleanup_l1d_flush();
-
-	allow_smaller_maxphyaddr = false;
 }
-module_exit(vmx_exit);
 
 /*
  * Early initialization before kvm_init() so that vmx_hardware_enable/disable()
  * can work.
  */
-static void __init vmx_init_early(void)
+void __init vmx_init_early(void)
 {
 	int cpu;
 
@@ -8297,48 +8323,9 @@ static void __init vmx_init_early(void)
 		INIT_LIST_HEAD(&per_cpu(loaded_vmcss_on_cpu, cpu));
 }
 
-static int __init vmx_init(void)
+int __init vmx_init(void)
 {
 	int r, cpu;
-
-#if IS_ENABLED(CONFIG_HYPERV)
-	/*
-	 * Enlightened VMCS usage should be recommended and the host needs
-	 * to support eVMCS v1 or above. We can also disable eVMCS support
-	 * with module parameter.
-	 */
-	if (enlightened_vmcs &&
-	    ms_hyperv.hints & HV_X64_ENLIGHTENED_VMCS_RECOMMENDED &&
-	    (ms_hyperv.nested_features & HV_X64_ENLIGHTENED_VMCS_VERSION) >=
-	    KVM_EVMCS_VERSION) {
-
-		/* Check that we have assist pages on all online CPUs */
-		for_each_online_cpu(cpu) {
-			if (!hv_get_vp_assist_page(cpu)) {
-				enlightened_vmcs = false;
-				break;
-			}
-		}
-
-		if (enlightened_vmcs) {
-			pr_info("KVM: vmx: using Hyper-V Enlightened VMCS\n");
-			static_branch_enable(&enable_evmcs);
-		}
-
-		if (ms_hyperv.nested_features & HV_X64_NESTED_DIRECT_FLUSH)
-			vt_x86_ops.enable_direct_tlbflush
-				= hv_enable_direct_tlbflush;
-
-	} else {
-		enlightened_vmcs = false;
-	}
-#endif
-
-	vmx_init_early();
-	r = kvm_init(&vt_init_ops, sizeof(struct vcpu_vmx),
-		__alignof__(struct vcpu_vmx), THIS_MODULE);
-	if (r)
-		return r;
 
 	/*
 	 * Must be called after kvm_init() so enable_ept is properly set
@@ -8348,10 +8335,8 @@ static int __init vmx_init(void)
 	 * mitigation mode.
 	 */
 	r = vmx_setup_l1d_flush(vmentry_l1d_flush_param);
-	if (r) {
-		vmx_exit();
+	if (r)
 		return r;
-	}
 
 	vmx_setup_fb_clear_ctrl();
 
@@ -8374,4 +8359,15 @@ static int __init vmx_init(void)
 
 	return 0;
 }
-module_init(vmx_init);
+
+void vmx_exit(void)
+{
+#ifdef CONFIG_KEXEC_CORE
+	RCU_INIT_POINTER(crash_vmclear_loaded_vmcss, NULL);
+	synchronize_rcu();
+#endif
+
+	vmx_cleanup_l1d_flush();
+
+	allow_smaller_maxphyaddr = false;
+}

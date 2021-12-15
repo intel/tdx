b445a2425212df896b5e448354d7c35a21e9bc84
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-14 23:43:44 -0800

    KVM: TDX: Implement TDX vcpu enter/exit path
    This patch implements running TDX vcpu.  Once vcpu runs on the logical
    processor (LP), the TDX vcpu is associated with it.  When the TDX vcpu
    moves to another LP, the TDX vcpu needs to flush its status on the LP.
    When destroying TDX vcpu, it needs to complete flush, and flush cpu memory
    cache.  Track which LP the TDX vcpu run and flush it as necessary.
    
    Compared to VMX, do nothing on sched_in event as TDX doesn't support pause
    loop.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    v20:
    - Make argument of tdx_vcpu_enter_exit() struct kvm_vcpu.
    - Update for the wrapper functions for SEAMCALLs. (Sean)
    - Remove noinstr (Sean)
    - Add a missing comma, clarify sched_in part, and update changelog to
      match code by dropping the PMU related paragraph (Binbin)
      https://lore.kernel.org/lkml/c0029d4d-3dee-4f11-a929-d64d2651bfb3@linux.intel.com/
    - Remove the union tdx_exit_reason. (Sean)
      https://lore.kernel.org/kvm/ZfSExlemFMKjBtZb@google.com/
    - Remove the code of special handling of vcpu->kvm->vm_bugged (Rick)
      https://lore.kernel.org/kvm/20240318234010.GD1645738@ls.amr.corp.intel.com/
    - For !tdx->initialized case, set tdx->vp_enter_ret to TDX_SW_ERROR to avoid
      collision with EXIT_REASON_EXCEPTION_NMI.
    
    v19:
    - Removed export_symbol_gpl(host_xcr0) to the patch that uses it
    
    Changes v15 -> v16:
    - use __seamcall_saved_ret()
    - As struct tdx_module_args doesn't match with vcpu.arch.regs, copy regs
      before/after calling __seamcall_saved_ret().

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -129,6 +129,7 @@ int tdx_vm_ioctl(struct kvm *kvm, void __user *argp);
 int tdx_vcpu_create(struct kvm_vcpu *vcpu);
 void tdx_vcpu_free(struct kvm_vcpu *vcpu);
 void tdx_vcpu_load(struct kvm_vcpu *vcpu, int cpu);
+fastpath_t tdx_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit);
 
 int tdx_vcpu_ioctl(struct kvm_vcpu *vcpu, void __user *argp);
 
@@ -156,6 +157,10 @@ static inline int tdx_vm_ioctl(struct kvm *kvm, void __user *argp) { return -EOP
 static inline int tdx_vcpu_create(struct kvm_vcpu *vcpu) { return -EOPNOTSUPP; }
 static inline void tdx_vcpu_free(struct kvm_vcpu *vcpu) {}
 static inline void tdx_vcpu_load(struct kvm_vcpu *vcpu, int cpu) {}
+static inline fastpath_t tdx_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit)
+{
+	return EXIT_FASTPATH_NONE;
+}
 
 static inline int tdx_vcpu_ioctl(struct kvm_vcpu *vcpu, void __user *argp) { return -EOPNOTSUPP; }
 

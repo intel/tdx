cea851f018d1d49163fb49e3010dfbcd2a490234
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-14 16:35:13 -0800

    KVM: TDX: handle EXIT_REASON_OTHER_SMI
    If the control reaches EXIT_REASON_OTHER_SMI, #SMI is delivered and
    handled right after returning from the TDX module to KVM nothing needs to
    be done in KVM.  Continue TDX vcpu execution.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -1351,6 +1351,13 @@ int tdx_handle_exit(struct kvm_vcpu *vcpu, fastpath_t fastpath)
 	}
 
 	switch (exit_reason.basic) {
+	case EXIT_REASON_OTHER_SMI:
+		/*
+		 * If reach here, it's not a Machine Check System Management
+		 * Interrupt(MSMI).  #SMI is delivered and handled right after
+		 * SEAMRET, nothing needs to be done in KVM.
+		 */
+		return 1;
 	default:
 		break;
 	}

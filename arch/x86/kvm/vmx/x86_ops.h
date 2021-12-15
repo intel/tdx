e7c74905d394990b4cd3489b761fa619eb805a69
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-14 23:43:44 -0800

    KVM: TDX: Implement TDX vcpu enter/exit path
    This patch implements running TDX vcpu.  Once vcpu runs on the logical
    processor (LP), the TDX vcpu is associated with it.  When the TDX vcpu
    moves to another LP, the TDX vcpu needs to flush its status on the LP.
    When destroying TDX vcpu, it needs to complete flush, and flush cpu memory
    cache.  Track which LP the TDX vcpu run and flush it as necessary.
    
    Compared to VMX, do nothing on sched_in event as TDX doesn't support pause
    loop.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    v20:
    - Make argument of tdx_vcpu_enter_exit() struct kvm_vcpu.
    - Update for the wrapper functions for SEAMCALLs. (Sean)
    - Remove noinstr (Sean)
    - Add a missing comma, clarify sched_in part, and update changelog to
      match code by dropping the PMU related paragraph (Binbin)
      https://lore.kernel.org/lkml/c0029d4d-3dee-4f11-a929-d64d2651bfb3@linux.intel.com/
    
    v19:
    - Removed export_symbol_gpl(host_xcr0) to the patch that uses it
    
    Changes v15 -> v16:
    - use __seamcall_saved_ret()
    - As struct tdx_module_args doesn't match with vcpu.arch.regs, copy regs
      before/after calling __seamcall_saved_ret().

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -129,6 +129,7 @@ int tdx_vm_ioctl(struct kvm *kvm, void __user *argp);
 int tdx_vcpu_create(struct kvm_vcpu *vcpu);
 void tdx_vcpu_free(struct kvm_vcpu *vcpu);
 void tdx_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event);
+fastpath_t tdx_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit);
 u8 tdx_get_mt_mask(struct kvm_vcpu *vcpu, gfn_t gfn, bool is_mmio);
 
 int tdx_vcpu_ioctl(struct kvm_vcpu *vcpu, void __user *argp);
@@ -163,6 +164,10 @@ static inline int tdx_vm_ioctl(struct kvm *kvm, void __user *argp) { return -EOP
 static inline int tdx_vcpu_create(struct kvm_vcpu *vcpu) { return -EOPNOTSUPP; }
 static inline void tdx_vcpu_free(struct kvm_vcpu *vcpu) {}
 static inline void tdx_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event) {}
+static inline fastpath_t tdx_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit)
+{
+	return EXIT_FASTPATH_NONE;
+}
 static inline u8 tdx_get_mt_mask(struct kvm_vcpu *vcpu, gfn_t gfn, bool is_mmio) { return 0; }
 
 static inline int tdx_vcpu_ioctl(struct kvm_vcpu *vcpu, void __user *argp) { return -EOPNOTSUPP; }

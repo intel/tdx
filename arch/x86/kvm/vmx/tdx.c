1cee80c80188aa0729ce768bb12218530e66a5b4
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-14 23:43:44 -0800

    KVM: TDX: Implement TDX vcpu enter/exit path
    This patch implements running TDX vcpu.  Once vcpu runs on the logical
    processor (LP), the TDX vcpu is associated with it.  When the TDX vcpu
    moves to another LP, the TDX vcpu needs to flush its status on the LP.
    When destroying TDX vcpu, it needs to complete flush, and flush cpu memory
    cache.  Track which LP the TDX vcpu run and flush it as necessary.
    
    Compared to VMX, do nothing on sched_in event as TDX doesn't support pause
    loop.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    v20:
    - Make argument of tdx_vcpu_enter_exit() struct kvm_vcpu.
    - Update for the wrapper functions for SEAMCALLs. (Sean)
    - Remove noinstr (Sean)
    - Add a missing comma, clarify sched_in part, and update changelog to
      match code by dropping the PMU related paragraph (Binbin)
      https://lore.kernel.org/lkml/c0029d4d-3dee-4f11-a929-d64d2651bfb3@linux.intel.com/
    
    v19:
    - Removed export_symbol_gpl(host_xcr0) to the patch that uses it
    
    Changes v15 -> v16:
    - use __seamcall_saved_ret()
    - As struct tdx_module_args doesn't match with vcpu.arch.regs, copy regs
      before/after calling __seamcall_saved_ret().

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -10,6 +10,9 @@
 #include "mmu/spte.h"
 #include "common.h"
 
+#include <trace/events/kvm.h>
+#include "trace.h"
+
 #undef pr_fmt
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
@@ -480,6 +483,80 @@ void tdx_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event)
 	 * Don't update mp_state to runnable because more initialization
 	 * is needed by TDX_VCPU_INIT.
 	 */
+}
+
+static void tdx_vcpu_enter_exit(struct kvm_vcpu *vcpu)
+{
+	struct vcpu_tdx *tdx = to_tdx(vcpu);
+	struct tdx_module_args args;
+
+	guest_state_enter_irqoff();
+
+	/*
+	 * TODO: optimization:
+	 * - Eliminate copy between args and vcpu->arch.regs.
+	 * - copyin/copyout registers only if (tdx->tdvmvall.regs_mask != 0)
+	 *   which means TDG.VP.VMCALL.
+	 */
+	args = (struct tdx_module_args) {
+		.rcx = tdx->tdvpr_pa,
+#define REG(reg, REG)	.reg = vcpu->arch.regs[VCPU_REGS_ ## REG]
+		REG(rdx, RDX),
+		REG(r8,  R8),
+		REG(r9,  R9),
+		REG(r10, R10),
+		REG(r11, R11),
+		REG(r12, R12),
+		REG(r13, R13),
+		REG(r14, R14),
+		REG(r15, R15),
+		REG(rbx, RBX),
+		REG(rdi, RDI),
+		REG(rsi, RSI),
+#undef REG
+	};
+
+	tdx->exit_reason.full = __seamcall_saved_ret(TDH_VP_ENTER, &args);
+
+#define REG(reg, REG)	vcpu->arch.regs[VCPU_REGS_ ## REG] = args.reg
+	REG(rcx, RCX);
+	REG(rdx, RDX);
+	REG(r8,  R8);
+	REG(r9,  R9);
+	REG(r10, R10);
+	REG(r11, R11);
+	REG(r12, R12);
+	REG(r13, R13);
+	REG(r14, R14);
+	REG(r15, R15);
+	REG(rbx, RBX);
+	REG(rdi, RDI);
+	REG(rsi, RSI);
+#undef REG
+
+	guest_state_exit_irqoff();
+}
+
+fastpath_t tdx_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit)
+{
+	struct vcpu_tdx *tdx = to_tdx(vcpu);
+
+	/* tdx exit handle takes care of this error case. */
+	if (unlikely(!tdx->initialized))
+		return EXIT_FASTPATH_EXIT_HANDLED;
+	if (unlikely(vcpu->kvm->vm_bugged)) {
+		tdx->exit_reason.full = TDX_NON_RECOVERABLE_VCPU;
+		return EXIT_FASTPATH_NONE;
+	}
+
+	trace_kvm_entry(vcpu, force_immediate_exit);
+
+	tdx_vcpu_enter_exit(vcpu);
+
+	vcpu->arch.regs_avail &= ~VMX_REGS_LAZY_LOAD_SET;
+	trace_kvm_exit(vcpu, KVM_ISA_VMX);
+
+	return EXIT_FASTPATH_NONE;
 }
 
 void tdx_load_mmu_pgd(struct kvm_vcpu *vcpu, hpa_t root_hpa, int pgd_level)

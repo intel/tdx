c562537a53bf0674a162334478b34b8ec6e14cae
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-15 17:42:53 -0800

    [REVERTME] workaround to avoid conflict with KVM_EXIT_TDX in the upstream
    Bump KVM_EXIT_TDX to 50 to avoid conflict with the upstream.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -325,7 +325,8 @@ struct kvm_tdx_exit {
 #define KVM_EXIT_XEN              34
 #define KVM_EXIT_RISCV_SBI        35
 #define KVM_EXIT_NOTIFY           36
-#define KVM_EXIT_TDX              37
+/* WORKAROUND to avoid conflict with upstream. */
+#define KVM_EXIT_TDX              50
 
 /* For KVM_EXIT_INTERNAL_ERROR */
 /* Emulate instruction failed. */

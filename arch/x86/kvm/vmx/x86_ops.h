e4dae43c1ef6ec8c56b20ff4d314987e5e13a320
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-20 12:37:12 -0800

    KVM: TDX: Make TDX VM type supported
    As first step TDX VM support, return that TDX VM type is supported.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -24,7 +24,6 @@ void vmx_hardware_unsetup(void);
 int vmx_hardware_enable(void);
 void vmx_hardware_disable(void);
 bool report_flexpriority(void);
-bool vmx_is_vm_type_supported(unsigned long type);
 int vmx_vm_init(struct kvm *kvm);
 int vmx_create_vcpu(struct kvm_vcpu *vcpu);
 fastpath_t vmx_vcpu_run(struct kvm_vcpu *vcpu);

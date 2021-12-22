4969c5dd00eaab9182078fe8b50154e0a5b1e33b
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-21 18:20:37 -0800

    KVM: TDX: restore user ret MSRs
    Several user ret MSRs are clobbered on TD exit.  Restore those values on
    TD exit and before returning to ring 3.  Because TSX_CTRL requires special
    treatment, this patch doesn't address it.
    
    Co-developed-by: Tony Lindgren <tony.lindgren@linux.intel.com>
    Signed-off-by: Tony Lindgren <tony.lindgren@linux.intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    ---
    TD vcpu enter/exit v1:
     - Rename tdx_user_return_update_cache() ->
         tdx_user_return_msr_update_cache() (extrapolated from Binbin)
     - Adjust to rename in previous patches (Binbin)
     - Simplify comment (Tony)
     - Move code change in tdx_hardware_setup() to __tdx_bringup().

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -2,7 +2,6 @@
 #include <linux/cleanup.h>
 #include <linux/cpu.h>
 #include <linux/mmu_context.h>
-
 #include <asm/fpu/xcr.h>
 #include <asm/tdx.h>
 #include "capabilities.h"
@@ -719,6 +718,28 @@ int tdx_vcpu_pre_run(struct kvm_vcpu *vcpu)
 	return 1;
 }
 
+struct tdx_uret_msr {
+	u32 msr;
+	unsigned int slot;
+	u64 defval;
+};
+
+static struct tdx_uret_msr tdx_uret_msrs[] = {
+	{.msr = MSR_SYSCALL_MASK, .defval = 0x20200 },
+	{.msr = MSR_STAR,},
+	{.msr = MSR_LSTAR,},
+	{.msr = MSR_TSC_AUX,},
+};
+
+static void tdx_user_return_msr_update_cache(void)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(tdx_uret_msrs); i++)
+		kvm_user_return_msr_update_cache(tdx_uret_msrs[i].slot,
+						 tdx_uret_msrs[i].defval);
+}
+
 static void tdx_restore_host_xsave_state(struct kvm_vcpu *vcpu)
 {
 	struct kvm_tdx *kvm_tdx = to_kvm_tdx(vcpu->kvm);
@@ -801,6 +822,7 @@ fastpath_t tdx_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit)
 
 	tdx_vcpu_enter_exit(vcpu);
 
+	tdx_user_return_msr_update_cache();
 	tdx_restore_host_xsave_state(vcpu);
 	tdx->prep_switch_state = TDX_PREP_SW_STATE_UNRESTORED;
 
@@ -2237,6 +2259,24 @@ static int __init __tdx_bringup(void)
 	/* tdx_disable_virtualization_cpu() uses associated_tdvcpus. */
 	for_each_possible_cpu(i)
 		INIT_LIST_HEAD(&per_cpu(associated_tdvcpus, i));
+
+	for (i = 0; i < ARRAY_SIZE(tdx_uret_msrs); i++) {
+		/*
+		 * Check if MSRs (tdx_uret_msrs) can be saved/restored
+		 * before returning to user space.
+		 *
+		 * this_cpu_ptr(user_return_msrs)->registered isn't checked
+		 * because the registration is done at vcpu runtime by
+		 * tdx_user_return_msr_update_cache().
+		 */
+		tdx_uret_msrs[i].slot = kvm_find_user_return_msr(tdx_uret_msrs[i].msr);
+		if (tdx_uret_msrs[i].slot == -1) {
+			/* If any MSR isn't supported, it is a KVM bug */
+			pr_err("MSR %x isn't included by kvm_find_user_return_msr\n",
+				tdx_uret_msrs[i].msr);
+			return -EIO;
+		}
+	}
 
 	/*
 	 * Enabling TDX requires enabling hardware virtualization first,

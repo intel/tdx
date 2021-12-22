962d28cfa717438ba63304ecaa5e81ba7ee1dcf9
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-21 19:08:17 -0800

    KVM: TDX: track LP tdx vcpu run and teardown vcpus on descroing the guest TD
    When shutting down the machine, (VMX or TDX) vcpus needs to be shutdown on
    each pcpu.  Do the similar for TDX with TDX SEAMCALL APIs.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -130,6 +130,8 @@ void __init tdx_pre_kvm_init(unsigned int *vcpu_size,
 			unsigned int *vcpu_align, unsigned int *vm_size);
 int __init tdx_hardware_setup(struct kvm_x86_ops *x86_ops);
 void tdx_hardware_unsetup(void);
+void tdx_hardware_enable(void);
+void tdx_hardware_disable(void);
 
 int tdx_vm_init(struct kvm *kvm);
 void tdx_mmu_prezap(struct kvm *kvm);

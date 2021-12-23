f5f0d65f1b5ca46cb93237b8ddf05b6d961825c7
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-23 14:36:45 -0800

    KVM: TDX: Implement non-NMI interrupt injection
    Implement non-NMI interrupt injection for TDX via posted interrupt.
    
    As CPU state is protected and APICv is enabled for the TDX guest, TDX
    supports non-NMI interrupt injection only by posted interrupt. Posted
    interrupt descriptors (PIDs) are allocated in shared memory, KVM can
    update them directly.  If target vCPU is in non-root mode, send posted
    interrupt notification to the vCPU and hardware will sync PIR to vIRR
    atomically.  Otherwise, kick it to pick up the interrupt from PID. To
    post pending interrupts in the PID, KVM can generate a self-IPI with
    notification vector prior to TD entry.
    
    Move the posted interrupt delivery code to common header so that TDX can
    leverage it to implement non-NMI interrupt injection.  Since the guest
    status of TD vCPU is protected, assume interrupt is always allowed.
    Ignore the code path for event injection mechanism or LAPIC emulation for
    TDX.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    ---
    TODO:
    - May need to remove the unnecessary wrappers like vt_set_interrupt_shadow().
      I remember I saw Sean's comment about the unnecessality of such wrapper,
      but can't find the link right now.
    
    v20:
    - Add non-NMI in shortlog.
    - Rewrite changelog.
    - Add a blank line. (Binbin)
    - Split kvm_wait_lapic_expire() out to a seperate patch. (Chao)
    - Use __pi_set_sn() to resolve upstream conflicts

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -7,6 +7,7 @@
 
 #include "capabilities.h"
 #include "x86_ops.h"
+#include "common.h"
 #include "mmu.h"
 #include "tdx.h"
 #include "vmx.h"
@@ -590,6 +591,9 @@ int tdx_vcpu_create(struct kvm_vcpu *vcpu)
 	tdx->host_state_need_save = true;
 	tdx->host_state_need_restore = false;
 
+	tdx->pi_desc.nv = POSTED_INTR_VECTOR;
+	__pi_set_sn(&tdx->pi_desc);
+
 	return 0;
 }
 
@@ -597,6 +601,7 @@ void tdx_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 {
 	struct vcpu_tdx *tdx = to_tdx(vcpu);
 
+	vmx_vcpu_pi_load(vcpu, cpu);
 	if (vcpu->cpu == cpu)
 		return;
 
@@ -828,6 +833,9 @@ fastpath_t tdx_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit)
 	}
 
 	trace_kvm_entry(vcpu, force_immediate_exit);
+
+	if (pi_test_on(&tdx->pi_desc))
+		apic->send_IPI_self(POSTED_INTR_VECTOR);
 
 	tdx_vcpu_enter_exit(vcpu);
 
@@ -1211,6 +1219,16 @@ static int tdx_sept_remove_private_spte(struct kvm *kvm, gfn_t gfn,
 		tdx_track(kvm);
 
 	return tdx_sept_drop_private_spte(kvm, gfn, level, pfn);
+}
+
+void tdx_deliver_interrupt(struct kvm_lapic *apic, int delivery_mode,
+			   int trig_mode, int vector)
+{
+	struct kvm_vcpu *vcpu = apic->vcpu;
+	struct vcpu_tdx *tdx = to_tdx(vcpu);
+
+	/* TDX supports only posted interrupt.  No lapic emulation. */
+	__vmx_deliver_posted_interrupt(vcpu, &tdx->pi_desc, vector);
 }
 
 static int tdx_get_capabilities(struct kvm_tdx_cmd *cmd)
@@ -1906,6 +1924,10 @@ static int tdx_vcpu_init(struct kvm_vcpu *vcpu, struct kvm_tdx_cmd *cmd)
 	ret = tdx_td_vcpu_init(vcpu, (u64)cmd->data);
 	if (ret)
 		return ret;
+
+	td_vmcs_write16(tdx, POSTED_INTR_NV, POSTED_INTR_VECTOR);
+	td_vmcs_write64(tdx, POSTED_INTR_DESC_ADDR, __pa(&tdx->pi_desc));
+	td_vmcs_setbit32(tdx, PIN_BASED_VM_EXEC_CONTROL, PIN_BASED_POSTED_INTR);
 
 	tdx->initialized = true;
 	return 0;

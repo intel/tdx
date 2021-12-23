ae314257b6821a1df422d6a72105422b7bb67374
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-23 13:50:48 -0800

    KVM: TDX: Handle TDX PV CPUID hypercall
    Handle TDX PV CPUID hypercall for the CPUIDs virtualized by VMM
    according to TDX Guest Host Communication Interface (GHCI).
    
    For TDX, most CPUID leaf/sub-leaf combinations are virtualized
    by the TDX module while some trigger #VE.  On #VE, TDX guest can
    issue a TDVMCALL with the leaf Instruction.CPUID (same value as
    EXIT_REASON_CPUID) to request VMM to emulate CPUID operation.
    
    Wire up TDX PV CPUID hypercall to the KVM backend function.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    [binbin: rewrite changelog]
    Signed-off-by: Binbin Wu <binbin.wu@linux.intel.com>
    ---
    v20:
     - Rewrite changelog.
     - Use TDVMCALL_STATUS prefix for TDX call status codes (Binbin)

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -1223,6 +1223,26 @@ static int tdx_report_fatal_error(struct kvm_vcpu *vcpu)
 	return 0;
 }
 
+static int tdx_emulate_cpuid(struct kvm_vcpu *vcpu)
+{
+	u32 eax, ebx, ecx, edx;
+
+	/* EAX and ECX for cpuid is stored in R12 and R13. */
+	eax = tdvmcall_a0_read(vcpu);
+	ecx = tdvmcall_a1_read(vcpu);
+
+	kvm_cpuid(vcpu, &eax, &ebx, &ecx, &edx, false);
+
+	tdvmcall_a0_write(vcpu, eax);
+	tdvmcall_a1_write(vcpu, ebx);
+	tdvmcall_a2_write(vcpu, ecx);
+	tdvmcall_a3_write(vcpu, edx);
+
+	tdvmcall_set_return_code(vcpu, TDVMCALL_STATUS_SUCCESS);
+
+	return 1;
+}
+
 static int handle_tdvmcall(struct kvm_vcpu *vcpu)
 {
 	if (tdvmcall_exit_type(vcpu))
@@ -1233,6 +1253,8 @@ static int handle_tdvmcall(struct kvm_vcpu *vcpu)
 		return tdx_map_gpa(vcpu);
 	case TDVMCALL_REPORT_FATAL_ERROR:
 		return tdx_report_fatal_error(vcpu);
+	case EXIT_REASON_CPUID:
+		return tdx_emulate_cpuid(vcpu);
 	default:
 		break;
 	}

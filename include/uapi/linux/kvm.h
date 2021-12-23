008a7ba805b81385550cb39cee8b08c4a8059f54
Isaku Yamahata <isaku.yamahata@intel.com>
2021-12-23 13:58:52 -0800

    KVM: TDX: Handle TDX PV report fatal error hypercall
    Wire up TDX PV report fatal error hypercall to KVM_SYSTEM_EVENT_CRASH KVM
    exit event.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -449,6 +449,7 @@ struct kvm_run {
 #define KVM_SYSTEM_EVENT_WAKEUP         4
 #define KVM_SYSTEM_EVENT_SUSPEND        5
 #define KVM_SYSTEM_EVENT_SEV_TERM       6
+#define KVM_SYSTEM_EVENT_TDX            7
 			__u32 type;
 			__u32 ndata;
 			union {

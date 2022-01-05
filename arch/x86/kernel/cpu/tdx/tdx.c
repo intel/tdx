bc017f27db3a2a46e7b63a0382bbacdc5d988422
Kai Huang <kai.huang@intel.com>
2022-01-05 13:06:12 +1300

    x86/cpu: Add placeholdes to detect and initialize TDX on demand
    Intel Trusted Domain Extensions (TDX) is essentially a CPU-attested
    trusted hypervisor running in an CPU isolated SEAM mode, allowing
    running crypto protected guest VMs (TDs).
    
    Before being able to use TDX to create TDs, the TDX module must be
    firstly loaded into SEAMRR and properly initialized, using SEAMCALLs
    defined by TDX architecture.  TDX module is expected to be loaded by
    BIOS before booting into kernel.  Kernel is expected to detect TDX
    module and initialize it.
    
    Detecting the TDX module could be done in two ways: 1) Issue the very
    first TDX SEAMCALL of initializing TDX module process to see whether
    it succeeds; 2) Detect the P-SEAMLDR by P-SEAMLDR's SEAMLDR.INFO
    SEAMCALL, which returns the information weather the TDX module has been
    loaded and ready for SEAMCALL.
    
    Use the latter way to detect the TDX module, as detecting P-SEAMLDR also
    provides the information of the P-SEAMLDR itself which could be useful
    to admin.  Also detecting P-SEAMLDR is also required anyway to support
    updating TDX module at runtime (not supported for now).
    
    Initializing the TDX module in general consists of below steps:
    
    1) Global initialization;
    2) Logical-CPU scope initialization;
    3) Enumerate the TDX module capabilities and platform configuration;
    4) Build the TDX usable memory ranges;
    5) Reserve one TDX private KeyID as global KeyID to protect TDX module
       global metadata;
    6) Configure the TDX module about TDX usable memory ranges and the
       global KeyID information;
    7) Package-scope configuration for the global KeyID;
    8) Initialize the TDX usable memory ranges based on 4).
    
    The initialization consumes additional memory as TDX module's global
    metadata (roughly 1/256th system RAM), and consumes additional time to
    initialize TDX usable memory (upto seconds for large memory in TBs).
    It can be done either during kernel boot, or during runtime when user of
    TDX truly wants to use TDX, for instance, when KVM wants to create a TD
    guest.  Choose the latter approach to defer initializing TDX, and allow
    to enable TDX on demand by user, as this is more flexible, and the
    overhead of initializing TDX can only take place when TDX is truly
    needed.  Another benefit is so far KVM is the only user of TDX, and
    letting KVM to initialize TDX avoids the duplicated code to handle
    entering/exiting VMX operation since KVM already handles it.
    
    Introduce detect_tdx() and init_tdx() to allow user to detect and
    initialize TDX on demand.  A state machine of the TDX module with mutex
    protection is introduced to orchestrate the detection and initialization
    process, so they can be called by multiple callers in parallel.
    
    As the first step, init_tdx() is a placeholder.  The TDX module will
    be initialized in multiple steps based on above initialization process.
    In case of fatal error during the initialization process, shut down the
    TDX module so no further SEAMCALLs can be made on any logical cpu.  It
    is pointless to leave TDX module in some intermediate status during the
    initialization.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/kernel/cpu/tdx/tdx.c
+++ arch/x86/kernel/cpu/tdx/tdx.c
@@ -8,11 +8,38 @@
 #define	pr_fmt(fmt)	"tdx: " fmt
 
 #include <linux/types.h>
+#include <linux/mutex.h>
+#include <linux/cpu.h>
+#include <linux/bug.h>
 #include <asm/cpufeatures.h>
 #include <asm/cpufeature.h>
 #include <asm/msr.h>
 #include <asm/msr-index.h>
 #include <asm/tdx_host.h>
+#include <asm/seam.h>
+#include "p-seamldr.h"
+#include "tdx_seamcall.h"
+
+/*
+ * TDX module status during initialization
+ */
+enum tdx_module_status_t {
+	/* TDX module status is unknown */
+	TDX_MODULE_UNKNOWN,
+	/* TDX module is not loaded */
+	TDX_MODULE_NONE,
+	/* TDX module is loaded, but not initialized */
+	TDX_MODULE_LOADED,
+	/* TDX module is fully initialized */
+	TDX_MODULE_INITIALIZED,
+	/* TDX module is shutdown due to error during initialization */
+	TDX_MODULE_SHUTDOWN,
+};
+
+static enum tdx_module_status_t tdx_module_status;
+
+/* Prevent concurrent attempts on TDX detection and initialization */
+static DEFINE_MUTEX(tdx_module_lock);
 
 /*
  * Intel Trusted Domain CPU Architecture Extension spec:
@@ -87,3 +114,202 @@ void detect_tdx_keyids(struct cpuinfo_x86 *c)
 	else
 		detect_tdx_keyids_ap(c);
 }
+
+/*
+ * Call the requested function on all online cpus concurrently.
+ * Any SEAMCALL invoked in the requested function must allow
+ * concurrent execution.
+ *
+ * Return error if requested function fails on any cpu.
+ */
+static int tdx_on_each_cpu(smp_call_func_t func)
+{
+	atomic_t err;
+
+	atomic_set(&err, 0);
+	on_each_cpu(func, &err, 1);
+
+	/* Don't care about exactly what error happened on which cpu */
+	return atomic_read(&err) ? -EFAULT : 0;
+}
+
+/* Initialize the TDX module. */
+static int init_tdx_module(void)
+{
+	return -EFAULT;
+}
+
+/* SMP call function to run TDH.SYS.LP.SHUTDOWN */
+static void smp_call_tdx_cpu_shutdown(void *data)
+{
+	atomic_t *err = (atomic_t *)data;
+
+	if (tdh_sys_lp_shutdown())
+		atomic_set(err, -1);
+}
+
+/* Shut down the TDX module */
+static void shutdown_tdx_module(void)
+{
+	tdx_on_each_cpu(smp_call_tdx_cpu_shutdown);
+
+	tdx_module_status = TDX_MODULE_SHUTDOWN;
+}
+
+static int __detect_tdx(void)
+{
+	/*
+	 * TDX requires at least two KeyIDs: one global KeyID to protect
+	 * the metadata of the TDX module and one or more KeyIDs to
+	 * protect VMs.  It's pointless to report TDX module as present
+	 * if this condition is not met.
+	 */
+	if (!(tdx_keyid_num > 1))
+		goto no_tdx_module;
+
+	/* P-SEAMLDR reports the presence of the TDX module */
+	if (detect_p_seamldr() || !tdx_module_ready())
+		goto no_tdx_module;
+
+	tdx_module_status = TDX_MODULE_LOADED;
+	return 0;
+
+no_tdx_module:
+	tdx_module_status = TDX_MODULE_NONE;
+	return -ENODEV;
+}
+
+static int __init_tdx(void)
+{
+	int ret;
+
+	/*
+	 * Logical-cpu scope initialization requires calling one SEAMCALL
+	 * on all logical cpus reported by BIOS, otherwise the SEAMCALL
+	 * of the next step of the initialization fails.  Shutting down
+	 * TDX module also has such requirement.  Further more,
+	 * configuring the key of the global KeyID requires calling one
+	 * SEAMCALL on at least one cpu for each package.
+	 *
+	 * For simplicity, instead of disabling CPU hotplug separately
+	 * for them to prevent any cpu going offline, disable CPU hotplug
+	 * during the entire TDX module initialization.
+	 *
+	 * Initializing TDX module could be time consuming, therefore use
+	 * cpu_hotplug_disable() instead of cpus_read_lock(), so that
+	 * another thread to run cpu hotplug could return -EBUSY
+	 * immediately rather than busy waiting.
+	 *
+	 * And assume all cpus reported by BIOS are online and depend on
+	 * the SEAMCALL failure to detect that this condition doesn't
+	 * meet.  Ideally a pre-check could be done before starting the
+	 * initialization, and return error early:
+	 *
+	 *	if (cpu_online_map != bios_enabled_cpu_map)
+	 *		return -EFAULT;
+	 *
+	 * However there is no existing variable (num_processors,
+	 * disabled_cpus, total_cpus, etc.) or bitmap (present, possbile,
+	 * online, etc.) which describes BIOS-enabled CPUs, while
+	 * cpu_online_maps can be easily changed due to various boot
+	 * options and cpu hotplug events.
+	 */
+	cpu_hotplug_disable();
+
+	ret = init_tdx_module();
+
+	/*
+	 * Turn off the TDX module in case of any error in the
+	 * initialization process.  It's pointless to leave the
+	 * module in an intermediate state.
+	 */
+	if (ret)
+		shutdown_tdx_module();
+
+	cpu_hotplug_enable();
+	return ret;
+}
+
+/**
+ * detect_tdx - Detect whether the TDX module has been loaded
+ *
+ * Detect whether the TDX module has been loaded and ready for
+ * initialization.  This function can be called in parallel
+ * by multiple callers.
+ *
+ * Call this function when the local cpu is already in VMX operation.
+ *
+ * Return: 0 if TDX module has been loaded, otherwise -ENODEV.
+ */
+int detect_tdx(void)
+{
+	int ret;
+
+	mutex_lock(&tdx_module_lock);
+
+	switch (tdx_module_status) {
+	case TDX_MODULE_UNKNOWN:
+		ret = __detect_tdx();
+		break;
+	case TDX_MODULE_NONE:
+	case TDX_MODULE_SHUTDOWN:
+		ret = -ENODEV;
+		break;
+	case TDX_MODULE_LOADED:
+	case TDX_MODULE_INITIALIZED:
+		ret = 0;
+		break;
+	default:
+		WARN_ON(1);
+		ret = -ENODEV;
+	}
+
+	mutex_unlock(&tdx_module_lock);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(detect_tdx);
+
+/**
+ * init_tdx - Initialize the TDX module
+ *
+ * Initialize the TDX module to make it ready to run TD guests.
+ * This function can be called in parallel by multiple callers.
+ *
+ * Call this function when all cpus are online and in VMX operation.
+ * CPU hotplug is also temporarily disabled in this function.
+ *
+ * Return:
+ *
+ * * -0: The TDX module has been successfully initialized.
+ * * -ENODEV: The TDX module is not loaded.
+ * * -EFAULT: Fatal error during TDX module initialization.
+ */
+int init_tdx(void)
+{
+	int ret;
+
+	mutex_lock(&tdx_module_lock);
+
+	/* Detect the TDX module if it's not done yet */
+	if (tdx_module_status == TDX_MODULE_UNKNOWN)
+		__detect_tdx();
+
+	switch (tdx_module_status) {
+	case TDX_MODULE_NONE:
+		ret = -ENODEV;
+		break;
+	case TDX_MODULE_LOADED:
+		ret = __init_tdx();
+		break;
+	case TDX_MODULE_INITIALIZED:
+		ret = 0;
+		break;
+	default:
+		ret = -EFAULT;
+		break;
+	}
+	mutex_unlock(&tdx_module_lock);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(init_tdx);

9a544476dafe0b4584b71c9c68b4c9ffa714698b
Kai Huang <kai.huang@intel.com>
2022-01-05 15:39:46 +1300

    x86/cpu: Get TDX module and TDX capable memory information
    TDX provides increased levels of memory confidentiality and integrity.
    This requires special hardware support for features like memory
    encryption and storage of memory integrity checksums.  Not all memory
    satisfies these requirements.
    
    As a result, TDX introduced the concept of a "Convertible Memory Region"
    (CMR).  During boot, the firmware builds a list of all of the memory
    ranges which can provide the TDX security guarantees.  The list of these
    ranges, along with TDX module information, is available to the kernel by
    querying the TDX module via TDH.SYS.INFO SEAMCALL on any cpu which has
    TDH.SYS.LP.INIT executed on it.
    
    Host kernel (VMM) can choose whether or not to use all convertible
    memory regions as TDX memory.  Before TDX module is ready to create any
    TD guests, all TDX memory regions that the VMM intends to use must be
    configured to the TDX module, using specific data structures defined by
    TDX architecture.  Constructing those structures requires both TDX
    module information and the CMR information.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>

---
+++ arch/x86/kernel/cpu/tdx/tdx_arch.h
@@ -0,0 +1,65 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright(c) 2021 Intel Corporation.
+ *
+ * Intel Trusted Domain Extensions (TDX) architectural data structures
+ */
+
+#ifndef _X86_TDX_ARCH_H
+#define _X86_TDX_ARCH_H
+
+#include <linux/types.h>
+#include <linux/compiler.h>
+
+struct cmr_info {
+	u64 base;
+	u64 size;
+} __packed;
+
+#define MAX_CMRS			32
+#define CMR_INFO_ARRAY_ALIGNMENT	512
+
+struct cpuid_config {
+	u32 leaf;
+	u32 sub_leaf;
+	u32 eax;
+	u32 ebx;
+	u32 ecx;
+	u32 edx;
+} __packed;
+
+#define TDSYSINFO_STRUCT_ALIGNMENT	1024
+struct tdsysinfo_struct {
+	/* TDX-SEAM Module Info */
+	u32 attributes;
+	u32 vendor_id;
+	u32 build_date;
+	u16 build_num;
+	u16 minor_version;
+	u16 major_version;
+	u8 reserved0[14];
+	/* Memory Info */
+	u16 max_tdmrs;
+	u16 max_reserved_per_tdmr;
+	u16 pamt_entry_size;
+	u8 reserved1[10];
+	/* Control Struct Info */
+	u16 tdcs_base_size;
+	u8 reserved2[2];
+	u16 tdvps_base_size;
+	u8 tdvps_xfam_dependent_size;
+	u8 reserved3[9];
+	/* TD Capabilities */
+	u64 attributes_fixed0;
+	u64 attributes_fixed1;
+	u64 xfam_fixed0;
+	u64 xfam_fixed1;
+	u8 reserved4[32];
+	u32 num_cpuid_config;
+	union {
+		struct cpuid_config cpuid_configs[0];
+		u8 reserved5[892];
+	};
+} __packed __aligned(TDSYSINFO_STRUCT_ALIGNMENT);
+
+#endif	/* _X86_TDX_ARCH_H */
--- arch/x86/kernel/cpu/tdx/tdx_seamcall.h
+++ arch/x86/kernel/cpu/tdx/tdx_seamcall.h
@@ -10,11 +10,42 @@
 
 #include <linux/types.h>
 #include <asm/seam.h>
+#include "tdx_arch.h"
 
 /* TDX module SEAMCALL leaf function numbers */
+#define TDH_SYS_INFO		32
 #define TDH_SYS_INIT		33
 #define TDH_SYS_LP_INIT		35
 #define TDH_SYS_LP_SHUTDOWN	44
+
+static inline int tdh_sys_info(struct tdsysinfo_struct *tdsysinfo,
+			       struct cmr_info *cmr_array,
+			       u64 *tdsysinfo_sz, u64 *cmr_num)
+{
+	struct seamcall_regs_in in;
+	struct seamcall_regs_out out;
+	int ret;
+
+	in.rcx = __pa(tdsysinfo);
+	in.rdx = sizeof(*tdsysinfo);
+	in.r8 = __pa(cmr_array);
+	in.r9 = MAX_CMRS;
+
+	ret = tdx_seamcall(TDH_SYS_INFO, &in, NULL, &out);
+	/* TDH.SYS.INFO should not fail.  WARN_ON() if it does. */
+	if (WARN_ON(ret))
+		return ret;
+
+	/*
+	 * If SEAMCALL succeeds, RDX contains the actual bytes written
+	 * to @tdsysinfo and R9 contains the actual written number of
+	 * CMR_INFO entries.  Otherwise both fields are 0.
+	 */
+	*tdsysinfo_sz = out.rdx;
+	*cmr_num = out.r9;
+
+	return 0;
+}
 
 static inline int tdh_sys_init(void)
 {

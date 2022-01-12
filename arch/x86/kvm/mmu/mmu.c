1239692abd07bf92db8f9bb4c767fcc006301555
Isaku Yamahata <isaku.yamahata@intel.com>
2022-01-12 11:02:37 -0800

    KVM: x86/legacy mmu: factor out __direct_map()
    Split the logic to populate non-leaf PTE into a function for code
    readability.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -3280,12 +3280,14 @@ void disallowed_hugepage_adjust(struct kvm_page_fault *fault, u64 spte, int cur_
 	}
 }
 
-static int direct_map(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
+static void direct_populate_nonleaf(struct kvm_vcpu *vcpu,
+				struct kvm_page_fault *fault,
+				struct kvm_shadow_walk_iterator *itp,
+				gfn_t *base_gfnp)
 {
 	struct kvm_shadow_walk_iterator it;
 	struct kvm_mmu_page *sp;
-	int ret;
-	gfn_t base_gfn = fault->gfn;
+	gfn_t base_gfn;
 
 	kvm_mmu_hugepage_adjust(vcpu, fault);
 
@@ -3312,6 +3314,18 @@ static int direct_map(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 					     fault->req_level >= it.level);
 	}
 
+	*itp = it;
+	if (base_gfnp)
+		*base_gfnp = base_gfn;
+}
+
+static int direct_map(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
+{
+	struct kvm_shadow_walk_iterator it;
+	int ret;
+	gfn_t base_gfn = fault->gfn;
+
+	direct_populate_nonleaf(vcpu, fault, &it, &base_gfn);
 	if (WARN_ON_ONCE(it.level != fault->goal_level))
 		return -EFAULT;
 

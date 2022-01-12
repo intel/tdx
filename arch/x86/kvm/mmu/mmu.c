aa95e23aa406260ce496b71dd4e6e145feb5d627
Isaku Yamahata <isaku.yamahata@intel.com>
2022-01-11 17:34:17 -0800

    KVM: x86/tdp_mmu: implement MapGPA hypercall for TDX
    The TDX Guest-Hypervisor communication interface(GHCI) specification
    defines MapGPA hypercall for guest TD to request the host VMM to map given
    GPA range as private or shared.
    
    It means the guest TD uses the GPA as shared(or private).  The GPA
    won't be used as private(or shared).  VMM should enforce GPA usage. VMM
    doesn't have to map the GPA on the hypercall request.
    
    - Zap the aliased region.
      If shared(or private) GPA is requested, zap private(or shared) GPA
      (modulo shared bit).
    
    - Record the request GPA is shared(or private) by SPTE_PRIVATE_PROHIBIT
      in SPTE in private EPT tables.
      - With SPTE_PRIVATE_PROHIBIT set, a shared GPA is allowed.
      - With SPTE_PRIVATE_PROHIBIT cleared, a private GPA is allowed.
    
      If the guest TD faults on not-allowed GPA (modulo shared bit), the KVM
      doesn't resolve EPT violation and let vcpu retry.  vcpu will keep
      faulting until other vcpu maps the region with MapGPA hypercall.  With
      the initial value of spte(initial shadow_init_value),
      SPTE_PRIVATE_PROHIBIT is cleared.  So the default behavior doesn't
      change.
    
    - don't map GPA.
      The GPA is mapped on the next EPT violation.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -6101,6 +6101,41 @@ void kvm_mmu_invalidate_mmio_sptes(struct kvm *kvm, u64 gen)
 	}
 }
 
+int kvm_mmu_map_gpa(struct kvm_vcpu *vcpu, gfn_t *startp, gfn_t end)
+{
+	struct kvm *kvm = vcpu->kvm;
+	gfn_t start = *startp;
+	bool allow_private;
+	int ret;
+
+	if (!kvm_gfn_stolen_mask(kvm))
+		return -EOPNOTSUPP;
+
+	ret = mmu_topup_memory_caches(vcpu, false);
+	if (ret)
+		return ret;
+
+	allow_private = kvm_is_private_gfn(kvm, start);
+	start = kvm_gfn_unalias(kvm, start);
+	end = kvm_gfn_unalias(kvm, end);
+
+	write_lock(&kvm->mmu_lock);
+	if (is_tdp_mmu_enabled(kvm))
+		ret = kvm_tdp_mmu_map_gpa(vcpu, &start, end, allow_private);
+	else
+		ret = -EOPNOTSUPP;
+	write_unlock(&kvm->mmu_lock);
+
+	if (ret == -EAGAIN) {
+		if (allow_private)
+			*startp = kvm_gfn_unalias(kvm, start);
+		else
+			*startp = kvm_gfn_shared(kvm, start);
+	}
+	return ret;
+}
+EXPORT_SYMBOL_GPL(kvm_mmu_map_gpa);
+
 static unsigned long
 mmu_shrink_scan(struct shrinker *shrink, struct shrink_control *sc)
 {

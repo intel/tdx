86c19c7d1c656cc8a2048cec0fb314aaa3645366
Isaku Yamahata <isaku.yamahata@intel.com>
2022-01-19 04:32:38 -0800

    KVM: x86/tdp_mmu: optimize remote tlb flush
    Implement batched TLB shootdown for optimization.  The current
    implementation to zap multiple the EPT entries, looks like as follows.
    
    Loop on GFNs
      1) Zap the EPT entry.  Zero the EPT entry.  PFN is saved on stack.
      2) TDX SEAMCALL TDH.MEM.RANGE.BLOCK with GFN. This corresponds to
         clearing the present bit.
      3) TDH.MEM.TRACK. This corresponds to local tlb flush.
      4) Send IPI to remote vcpus. This corresponds to remote tlb flush.
      5) When destructing VM, TDH.MEM.REMOVE with PFN. There is no
         corresponding to the VMX EPT operation.
      At the last of zapping
      6) kvm_flush_remote_tlbs_with_address(). This flushes shared EPT pointer.
         No operations on Secure EPT.
    
    The new implementation looks like as follows.  The number of TLB shootdown
    is reduced from the number of the EPT entries to zap to one.
    
    Loop on GFNs
      1) - Zap the EPT entry.
         - Clear present/dirty/access bits
         - Keep PFN
         - Set SPTE_PRIVATE_ZAPPED to indicate valid PFN to unlink from Secure
           EPT.
      2) TDX SEAMCALL TDH.MEM.RANGE.BLOCK with GFN.  This corresponds to
         clearing the present bit.
      KVM remote TLB flush callback does,
      3) TDH.MEM.TRACK. corresponds to local tlb flush
      4) Send IPI to remote vcpus. This corresponds to remote tlb flush.
      5) kvm_flush_remote_tlbs_with_address().  This flushes shared EPT
         pointer. No operations on Secure EPT.  When destructing VM, Check if
         SPTE_PRIVATE_ZAPPED and issue TDH.MEM.REMOVE with PFNs.
    
    For traversing private zapped EPT entries, introduce helper function in
    TDP MMU code.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -245,6 +245,7 @@ static void kvm_flush_remote_tlbs_with_range(struct kvm *kvm,
 	int ret = -ENOTSUPP;
 
 	if (range && kvm_available_flush_tlb_with_range())
+		/* Callback should flush both private GFN and shared GFN. */
 		ret = static_call(kvm_x86_tlb_remote_flush_with_range)(kvm, range);
 
 	if (ret)

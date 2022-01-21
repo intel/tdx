2bccc8d4a689d8156ecc3e53c62e3002f1d535d0
Isaku Yamahata <isaku.yamahata@intel.com>
2022-01-21 02:21:35 -0800

    KVM: TDX: Require TDP MMU and mmio caching for TDX
    Disable TDX support when TDP MMU or mmio caching aren't supported.
    
    As TDP MMU is becoming main stream than the legacy MMU, the legacy MMU
    support for TDX isn't implemented.
    
    TDX requires KVM mmio caching. Without mmio caching, KVM will go to MMIO
    emulation without installing SPTEs for MMIOs. However, TDX guest is
    protected and KVM would meet errors when trying to emulate MMIOs for TDX
    guest during instruction decoding. So, TDX guest relies on SPTEs being
    installed for MMIOs, which are with no RWX bits and with VE suppress bit
    unset, to inject VE to TDX guest. The TDX guest would then issue TDVMCALL
    in the VE handler to perform instruction decoding and have host do MMIO
    emulation.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Co-developed-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Co-developed-by: Yan Zhao <yan.y.zhao@intel.com>
    Signed-off-by: Yan Zhao <yan.y.zhao@intel.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    ---
    TDX MMU part 2 v2:
     - Added Paolo's rb.
    
    TDX MMU part 2 v1:
     - Addressed Binbin's comment by massaging Isaku's updated comments and
       adding more explanations about instroducing mmio caching.
     - Addressed Sean's comments of v19 according to Isaku's update but
       kept the warning for MOVDIR64B.
     - Move code change in tdx_hardware_setup() to __tdx_bringup() since the
       former has been removed.

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -1412,14 +1412,12 @@ static int __init __tdx_bringup(void)
 	const struct tdx_sys_info_td_conf *td_conf;
 	int r;
 
+	if (!tdp_mmu_enabled || !enable_mmio_caching)
+		return -EOPNOTSUPP;
+
 	if (!cpu_feature_enabled(X86_FEATURE_MOVDIR64B)) {
 		pr_warn("MOVDIR64B is reqiured for TDX\n");
 		return -EOPNOTSUPP;
-	}
-
-	if (!enable_ept) {
-		pr_err("Cannot enable TDX with EPT disabled.\n");
-		return -EINVAL;
 	}
 
 	/*

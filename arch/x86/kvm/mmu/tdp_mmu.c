5eef52821e971132a995c32908e3d9d2b98d360d
Isaku Yamahata <isaku.yamahata@intel.com>
2022-01-27 17:05:40 -0800

    KVM: x86/mmu: Allow TDX VM for legacy MMU
    Now legacy MMU suports TDX, allow TDX VM for legacy VM.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -18,12 +18,7 @@ int kvm_mmu_init_tdp_mmu(struct kvm *kvm)
 {
 	struct workqueue_struct *wq;
 
-	/*
-	 *  Because TDX supports only TDP MMU, forcibly use TDP MMU in the case
-	 *  of TDX.
-	 */
-	if (kvm->arch.vm_type != KVM_X86_TDX_VM &&
-		(!tdp_enabled || !READ_ONCE(tdp_mmu_enabled)))
+	if (!tdp_enabled || !READ_ONCE(tdp_mmu_enabled))
 		return false;
 
 	wq = alloc_workqueue("kvm", WQ_UNBOUND|WQ_MEM_RECLAIM|WQ_CPU_INTENSIVE, 0);

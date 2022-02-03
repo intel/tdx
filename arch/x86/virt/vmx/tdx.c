bb5ee6268e62fba992f14ee5f40b41cfb5b9d488
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-03 11:17:12 -0800

    [REVERTME] TDX: Add kernel params to disable SEAM tracing
    Add a kernel command line option, trace_boot_seamcalls to enable TDX module
    tracing.  tdx_trace_level to set trace severity for TDX module.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/virt/vmx/tdx.c
+++ arch/x86/virt/vmx/tdx.c
@@ -132,6 +132,53 @@ static int __init tdx_host_setup(char *s)
 }
 __setup("tdx_host=", tdx_host_setup);
 
+/* REVERTME: tdx module debug */
+/* Non-architectural debug configuration SEAMCALLs. */
+#define SEAMCALL_TDDEBUGCONFIG		0xFE
+
+#define DEBUGCONFIG_SET_TRACE_LEVEL	3
+#define DEBUGCONFIG_TRACE_ALL		0
+#define DEBUGCONFIG_TRACE_WARN		1
+#define DEBUGCONFIG_TRACE_ERROR		2
+#define DEBUGCONFIG_TRACE_CUSTOM	1000
+#define DEBUGCONFIG_TRACE_NONE		-1ULL
+
+static bool trace_boot_seamcalls;
+
+static int __init trace_seamcalls(char *s)
+{
+	trace_boot_seamcalls = true;
+	return 1;
+}
+__setup("trace_boot_seamcalls", trace_seamcalls);
+
+static u64 tdx_trace_level = DEBUGCONFIG_TRACE_CUSTOM;
+
+static void tdx_trace_seamcalls(u64 level);
+static int trace_level_set(const char *val, const struct kernel_param *kp)
+{
+	int r;
+
+	r = param_set_ulong(val, kp);
+	if (tdx_trace_level == DEBUGCONFIG_TRACE_ALL ||
+		tdx_trace_level == DEBUGCONFIG_TRACE_WARN ||
+		tdx_trace_level == DEBUGCONFIG_TRACE_ERROR ||
+		tdx_trace_level == DEBUGCONFIG_TRACE_CUSTOM ||
+		tdx_trace_level == DEBUGCONFIG_TRACE_NONE) {
+		tdx_trace_seamcalls(tdx_trace_level);
+	}
+
+	return r;
+}
+
+static const struct kernel_param_ops tdx_trace_ops = {
+	.set = trace_level_set,
+	.get = param_get_ulong,
+};
+
+module_param_cb(tdx_trace_level, &tdx_trace_ops, &tdx_trace_level, 0644);
+MODULE_PARM_DESC(tdx_trace_level, "TDX module trace level");
+
 bool __seamrr_enabled(void)
 {
 	return (seamrr_mask & SEAMRR_ENABLED_BITS) == SEAMRR_ENABLED_BITS;
@@ -1487,6 +1534,15 @@ static int init_tdmrs(struct tdmr_info **tdmr_array, int tdmr_num)
 	return 0;
 }
 
+static void tdx_trace_seamcalls(u64 level)
+{
+	int ret;
+
+	ret = seamcall(SEAMCALL_TDDEBUGCONFIG, DEBUGCONFIG_SET_TRACE_LEVEL, level,
+		0, 0, NULL, NULL);
+	WARN_ON_ONCE(ret);
+}
+
 static int init_tdx_module(void)
 {
 	struct tdmr_info **tdmr_array;
@@ -1497,6 +1553,11 @@ static int init_tdx_module(void)
 	ret = seamcall(TDH_SYS_INIT, 0, 0, 0, 0, NULL, NULL);
 	if (ret)
 		goto out;
+
+	if (trace_boot_seamcalls)
+		tdx_trace_seamcalls(DEBUGCONFIG_TRACE_ALL);
+	else
+		tdx_trace_seamcalls(tdx_trace_level);
 
 	/* Logical-cpu scope initialization */
 	ret = tdx_module_init_cpus();

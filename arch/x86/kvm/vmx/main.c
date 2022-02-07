2d65bd56a7ab8c0776f5c6b7c8481dd45ad96794
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-07 13:42:44 -0800

    KVM: TDX: Add place holder for TDX VM specific mem_enc_op ioctl
    Add a place holder function for TDX specific VM-scoped ioctl as mem_enc_op.
    TDX specific sub-commands will be added to retrieve/pass TDX specific
    parameters.
    
    KVM_MEMORY_ENCRYPT_OP was introduced for VM-scoped operations specific for
    guest state-protected VM.  It defined subcommands for technology-specific
    operations under KVM_MEMORY_ENCRYPT_OP.  Despite its name, the subcommands
    are not limited to memory encryption, but various technology-specific
    operations are defined.  It's natural to repurpose KVM_MEMORY_ENCRYPT_OP
    for TDX specific operations and define subcommands.
    
    TDX requires VM-scoped TDX-specific operations for device model, for
    example, qemu.  Getting system-wide parameters, TDX-specific VM
    initialization.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/main.c
+++ arch/x86/kvm/vmx/main.c
@@ -73,6 +73,14 @@ static void vt_vm_free(struct kvm *kvm)
 		return tdx_vm_free(kvm);
 }
 
+static int vt_mem_enc_ioctl(struct kvm *kvm, void __user *argp)
+{
+	if (!is_td(kvm))
+		return -ENOTTY;
+
+	return tdx_vm_ioctl(kvm, argp);
+}
+
 struct kvm_x86_ops vt_x86_ops __initdata = {
 	.name = "kvm_intel",
 
@@ -214,6 +222,7 @@ struct kvm_x86_ops vt_x86_ops __initdata = {
 	.vcpu_deliver_sipi_vector = kvm_vcpu_deliver_sipi_vector,
 
 	.dev_mem_enc_ioctl = tdx_dev_ioctl,
+	.mem_enc_ioctl = vt_mem_enc_ioctl,
 };
 
 struct kvm_x86_init_ops vt_init_ops __initdata = {

c43b76e9392794cfb0015e0446eb2f805b91d698
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-07 13:42:44 -0800

    KVM: TDX: Add place holder for TDX VM specific mem_enc_op ioctl
    KVM_MEMORY_ENCRYPT_OP was introduced for VM-scoped operations specific for
    guest state-protected VM.  It defined subcommands for technology-specific
    operations under KVM_MEMORY_ENCRYPT_OP.  Despite its name, the subcommands
    are not limited to memory encryption, but various technology-specific
    operations are defined.  It's natural to repurpose KVM_MEMORY_ENCRYPT_OP
    for TDX specific operations and define subcommands.
    
    TDX requires VM-scoped TDX-specific operations for device model, for
    example, qemu.  Getting system-wide parameters, TDX-specific VM
    initialization.
    
    Add a place holder function for TDX specific VM-scoped ioctl as mem_enc_op.
    TDX specific sub-commands will be added to retrieve/pass TDX specific
    parameters.  Make mem_enc_ioctl non-optional as it's always filled.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    ---
    v15:
    - change struct kvm_tdx_cmd to drop unused member.

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -41,6 +41,32 @@ struct tdx_info {
 /* Info about the TDX module. */
 static struct tdx_info *tdx_info;
 
+int tdx_vm_ioctl(struct kvm *kvm, void __user *argp)
+{
+	struct kvm_tdx_cmd tdx_cmd;
+	int r;
+
+	if (copy_from_user(&tdx_cmd, argp, sizeof(struct kvm_tdx_cmd)))
+		return -EFAULT;
+	if (tdx_cmd.error)
+		return -EINVAL;
+
+	mutex_lock(&kvm->lock);
+
+	switch (tdx_cmd.id) {
+	default:
+		r = -EINVAL;
+		goto out;
+	}
+
+	if (copy_to_user(argp, &tdx_cmd, sizeof(struct kvm_tdx_cmd)))
+		r = -EFAULT;
+
+out:
+	mutex_unlock(&kvm->lock);
+	return r;
+}
+
 static int __init tdx_module_setup(void)
 {
 	struct st {

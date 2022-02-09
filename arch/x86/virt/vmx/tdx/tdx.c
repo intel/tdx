69281f4f2e4d2c3c906518d83bc6ec9c0debda16
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-09 11:23:49 -0800

    x86/cpu: Add helper functions to allocate/free TDX private host key id
    Add helper functions to allocate/free TDX private host key id (HKID), and
    export the global TDX HKID.
    
    The memory controller encrypts TDX memory with the assigned TDX HKIDs.  The
    global TDX HKID is to encrypt the TDX module, its memory, and some dynamic
    data (TDR).  The private TDX HKID is assigned to guest TD to encrypt guest
    memory and the related data.  When VMM releases an encrypted page for
    reuse, the page needs a cache flush with the used HKID.  VMM needs the
    global TDX HKID and the private TDX HKIDs to flush encrypted pages.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -37,7 +37,8 @@
 #include <asm/tdx.h>
 #include "tdx.h"
 
-static u32 tdx_global_keyid __ro_after_init;
+u32 tdx_global_keyid __ro_after_init;
+EXPORT_SYMBOL_GPL(tdx_global_keyid);
 static u32 tdx_guest_keyid_start __ro_after_init;
 static u32 tdx_nr_guest_keyids __ro_after_init;
 
@@ -104,6 +105,31 @@ static inline int sc_retry_prerr(sc_func_t func, sc_err_func_t err_func,
 
 #define seamcall_prerr_ret(__fn, __args)					\
 	sc_retry_prerr(__seamcall_ret, seamcall_err_ret, (__fn), (__args))
+
+/* TDX KeyID pool */
+static DEFINE_IDA(tdx_guest_keyid_pool);
+
+int tdx_guest_keyid_alloc(void)
+{
+	if (WARN_ON_ONCE(!tdx_guest_keyid_start || !tdx_nr_guest_keyids))
+		return -EINVAL;
+
+	/* The first keyID is reserved for the global key. */
+	return ida_alloc_range(&tdx_guest_keyid_pool, tdx_guest_keyid_start + 1,
+			       tdx_guest_keyid_start + tdx_nr_guest_keyids - 1,
+			       GFP_KERNEL);
+}
+EXPORT_SYMBOL_GPL(tdx_guest_keyid_alloc);
+
+void tdx_guest_keyid_free(int keyid)
+{
+	/* keyid = 0 is reserved. */
+	if (WARN_ON_ONCE(keyid <= 0))
+		return;
+
+	ida_free(&tdx_guest_keyid_pool, keyid);
+}
+EXPORT_SYMBOL_GPL(tdx_guest_keyid_free);
 
 /*
  * Do the module global initialization once and return its result.

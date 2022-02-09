c3920e2887ce051e4874eeb6bcd12691855207c2
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-09 15:38:31 -0800

    KVM: TDX: Add placeholders for TDX VM/vcpu structure
    Add placeholders TDX VM/vcpu structure that overlays with VMX VM/vcpu
    structures.  Initialize VM structure size and vcpu size/align so that x86
    KVM common code knows those size irrespective of VMX or TDX.  Those
    structures will be populated as guest creation logic develops.
    
    Add helper functions to check if the VM is guest TD and add conversion
    functions between KVM VM/VCPU and TDX VM/VCPU.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    v19:
    - correctly update ops.vm_size, vcpu_size and, vcpu_align by Xiaoyao
    
    v14 -> v15:
    - use KVM_X86_TDX_VM

--- arch/x86/kvm/vmx/main.c
+++ arch/x86/kvm/vmx/main.c
@@ -5,6 +5,7 @@
 #include "vmx.h"
 #include "nested.h"
 #include "pmu.h"
+#include "tdx.h"
 
 static bool enable_tdx __ro_after_init;
 module_param_named(tdx, enable_tdx, bool, 0444);
@@ -18,6 +19,9 @@ static __init int vt_hardware_setup(void)
 		return ret;
 
 	enable_tdx = enable_tdx && !tdx_hardware_setup(&vt_x86_ops);
+	if (enable_tdx)
+		vt_x86_ops.vm_size = max_t(unsigned int, vt_x86_ops.vm_size,
+					   sizeof(struct kvm_tdx));
 
 	return 0;
 }
@@ -215,8 +219,18 @@ static int __init vt_init(void)
 	 * Common KVM initialization _must_ come last, after this, /dev/kvm is
 	 * exposed to userspace!
 	 */
+	/*
+	 * kvm_x86_ops is updated with vt_x86_ops.  vt_x86_ops.vm_size must
+	 * be set before kvm_x86_vendor_init().
+	 */
 	vcpu_size = sizeof(struct vcpu_vmx);
 	vcpu_align = __alignof__(struct vcpu_vmx);
+	if (enable_tdx) {
+		vcpu_size = max_t(unsigned int, vcpu_size,
+				  sizeof(struct vcpu_tdx));
+		vcpu_align = max_t(unsigned int, vcpu_align,
+				   __alignof__(struct vcpu_tdx));
+	}
 	r = kvm_init(vcpu_size, vcpu_align, THIS_MODULE);
 	if (r)
 		goto err_kvm_init;

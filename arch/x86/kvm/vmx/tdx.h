a7e10afd72c593231dca982bf8ff4bc7dc4afe70
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-09 15:38:31 -0800

    KVM: TDX: Add placeholders for TDX VM/vCPU structures
    Add TDX's own VM and vCPU structures as placeholder to manage and run
    TDX guests.  Also add helper functions to check whether a VM/vCPU is
    TDX or normal VMX one, and add helpers to convert between TDX VM/vCPU
    and KVM VM/vCPU.
    
    TDX protects guest VMs from malicious host.  Unlike VMX guests, TDX
    guests are crypto-protected.  KVM cannot access TDX guests' memory and
    vCPU states directly.  Instead, TDX requires KVM to use a set of TDX
    architecture-defined firmware APIs (a.k.a TDX module SEAMCALLs) to
    manage and run TDX guests.
    
    In fact, the way to manage and run TDX guests and normal VMX guests are
    quite different.  Because of that, the current structures
    ('struct kvm_vmx' and 'struct vcpu_vmx') to manage VMX guests are not
    quite suitable for TDX guests.  E.g., the majority of the members of
    'struct vcpu_vmx' don't apply to TDX guests.
    
    Introduce TDX's own VM and vCPU structures ('struct kvm_tdx' and 'struct
    vcpu_tdx' respectively) for KVM to manage and run TDX guests.  And
    instead of building TDX's VM and vCPU structures based on VMX's, build
    them directly based on 'struct kvm'.
    
    As a result, TDX and VMX guests will have different VM size and vCPU
    size/alignment.
    
    Currently, kvm_arch_alloc_vm() uses 'kvm_x86_ops::vm_size' to allocate
    enough space for the VM structure when creating guest.  With TDX guests,
    ideally, KVM should allocate the VM structure based on the VM type so
    that the precise size can be allocated for VMX and TDX guests.  But this
    requires more extensive code change.  For now, simply choose the maximum
    size of 'struct kvm_tdx' and 'struct kvm_vmx' for VM structure
    allocation for both VMX and TDX guests.  This would result in small
    memory waste for each VM which has smaller VM structure size but this is
    acceptable.
    
    For simplicity, use the same way for vCPU allocation too.  Otherwise KVM
    would need to maintain a separate 'kvm_vcpu_cache' for each VM type.
    
    Note, updating the 'vt_x86_ops::vm_size' needs to be done before calling
    kvm_ops_update(), which copies vt_x86_ops to kvm_x86_ops.  However this
    happens before TDX module initialization.  Therefore theoretically it is
    possible that 'kvm_x86_ops::vm_size' is set to size of 'struct kvm_tdx'
    (when it's larger) but TDX actually fails to initialize at a later time.
    
    Again the worst case of this is wasting couple of bytes memory for each
    VM.  KVM could choose to update 'kvm_x86_ops::vm_size' at a later time
    depending on TDX's status but that would require base KVM module to
    export either kvm_x86_ops or kvm_ops_update().
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    ---
    uAPI breakout v2:
     - Correct typo for update (Tony)
    
    uAPI breakout v1:
     - Re-add __always_inline to to_kvm_tdx(), to_tdx(). (Sean)
     - Fix bisectability issues in headers (Kai)
     - Add a comment around updating vt_x86_ops.vm_size.
     - Update the comment around updating vcpu_size/align:
       https://lore.kernel.org/kvm/25d2bf93854ae7410d82119227be3cb2ce47c4f2.camel@intel.com/
     - Refine changelog:
       https://lore.kernel.org/kvm/9c592801471a137c51f583065764fbfc3081c016.camel@intel.com/
    
    v19:
     - correctly update ops.vm_size, vcpu_size and, vcpu_align by Xiaoyao
    
    v14 -> v15:
     - use KVM_X86_TDX_VM

--- arch/x86/kvm/vmx/tdx.h
+++ arch/x86/kvm/vmx/tdx.h
@@ -4,9 +4,58 @@
 #ifdef CONFIG_INTEL_TDX_HOST
 void tdx_bringup(void);
 void tdx_cleanup(void);
+
+extern bool enable_tdx;
+
+struct kvm_tdx {
+	struct kvm kvm;
+	/* TDX specific members follow. */
+};
+
+struct vcpu_tdx {
+	struct kvm_vcpu	vcpu;
+	/* TDX specific members follow. */
+};
+
+static inline bool is_td(struct kvm *kvm)
+{
+	return kvm->arch.vm_type == KVM_X86_TDX_VM;
+}
+
+static inline bool is_td_vcpu(struct kvm_vcpu *vcpu)
+{
+	return is_td(vcpu->kvm);
+}
+
+static __always_inline struct kvm_tdx *to_kvm_tdx(struct kvm *kvm)
+{
+	return container_of(kvm, struct kvm_tdx, kvm);
+}
+
+static __always_inline struct vcpu_tdx *to_tdx(struct kvm_vcpu *vcpu)
+{
+	return container_of(vcpu, struct vcpu_tdx, vcpu);
+}
+
 #else
 static inline void tdx_bringup(void) {}
 static inline void tdx_cleanup(void) {}
+
+#define enable_tdx	0
+
+struct kvm_tdx {
+	struct kvm kvm;
+};
+
+struct vcpu_tdx {
+	struct kvm_vcpu	vcpu;
+};
+
+static inline bool is_td(struct kvm *kvm) { return false; }
+static inline bool is_td_vcpu(struct kvm_vcpu *vcpu) { return false; }
+static inline struct kvm_tdx *to_kvm_tdx(struct kvm *kvm) { return NULL; }
+static inline struct vcpu_tdx *to_tdx(struct kvm_vcpu *vcpu) { return NULL; }
+
 #endif
 
 #endif

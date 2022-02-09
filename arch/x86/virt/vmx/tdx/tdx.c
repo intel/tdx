86a3e7e9c3e8a4f3dd72d72ec370239d8e96306f
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-09 11:23:49 -0800

    x86/cpu: Add helper functions to allocate/free TDX private host key id
    TDX private host key id is assigned to guest TD.  The memory controller
    encrypts guest TD memory with the assigned TDX private host key id (HIKD).
    Add helper functions to allocate/free TDX private host key id so that TDX
    KVM manage it.
    
    Also export the global TDX private host key id that is used to encrypt TDX
    module, its memory and some dynamic data (TDR).  When VMM releasing
    encrypted page to reuse it, the page needs to be flushed with the used host
    key id.  VMM needs the global TDX private host key id to flush such pages
    TDX module accesses with the global TDX private host key id.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -57,7 +57,13 @@ static struct cmr_info tdx_cmr_array[MAX_CMRS] __aligned(CMR_INFO_ARRAY_ALIGNMEN
 static int tdx_cmr_num;
 
 /* TDX module global KeyID.  Used in TDH.SYS.CONFIG ABI. */
-static u32 tdx_global_keyid;
+static u32 __read_mostly tdx_global_keyid;
+
+u32 tdx_get_global_keyid(void)
+{
+	return tdx_global_keyid;
+}
+EXPORT_SYMBOL_GPL(tdx_get_global_keyid);
 
 /* Detect whether CPU supports SEAM */
 static int detect_seam(void)
@@ -80,6 +86,31 @@ static int detect_seam(void)
 	pr_info("SEAMRR enabled.\n");
 	return 0;
 }
+
+/* TDX KeyID pool */
+static DEFINE_IDA(tdx_keyid_pool);
+
+int tdx_keyid_alloc(void)
+{
+	if (WARN_ON_ONCE(!tdx_keyid_start || !tdx_keyid_num))
+		return -EINVAL;
+
+	/* The first keyID is reserved for the global key. */
+	return ida_alloc_range(&tdx_keyid_pool, tdx_keyid_start + 1,
+			       tdx_keyid_start + tdx_keyid_num - 1,
+			       GFP_KERNEL);
+}
+EXPORT_SYMBOL_GPL(tdx_keyid_alloc);
+
+void tdx_keyid_free(int keyid)
+{
+	/* keyid = 0 is reserved. */
+	if (!keyid || keyid <= 0)
+		return;
+
+	ida_free(&tdx_keyid_pool, keyid);
+}
+EXPORT_SYMBOL_GPL(tdx_keyid_free);
 
 static int detect_tdx_keyids(void)
 {

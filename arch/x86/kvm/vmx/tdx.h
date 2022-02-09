14964cbfa09131c0fab7f46eb51dc230b55bd29c
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-09 15:38:31 -0800

    KVM: TDX: Add placeholders for TDX VM/vcpu structure
    Add placeholders TDX VM/vcpu structure that overlays with VMX VM/vcpu
    structures.  Initialize VM structure size and vcpu size/align so that x86
    KVM common code knows those size irrespective of VMX or TDX.  Those
    structures will be populated as guest creation logic develops.
    
    Add helper functions to check if the VM is guest TD and add conversion
    functions between KVM VM/VCPU and TDX VM/VCPU.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    v20:
    - Re-add __always_inline to to_kvm_tdx(), to_tdx(). (Sean)
    - Fix bisectability issues in headers (Kai)
    
    v19:
    - correctly update ops.vm_size, vcpu_size and, vcpu_align by Xiaoyao
    
    v14 -> v15:
    - use KVM_X86_TDX_VM

---
+++ arch/x86/kvm/vmx/tdx.h
@@ -0,0 +1,55 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __KVM_X86_TDX_H
+#define __KVM_X86_TDX_H
+
+#include <linux/container_of.h>
+#include <linux/compiler_attributes.h>
+#include <linux/kvm_host.h>
+#include <uapi/asm/kvm.h>
+
+#ifdef CONFIG_INTEL_TDX_HOST
+struct kvm_tdx {
+	struct kvm kvm;
+	/* TDX specific members follow. */
+};
+
+struct vcpu_tdx {
+	struct kvm_vcpu	vcpu;
+	/* TDX specific members follow. */
+};
+
+static inline bool is_td(struct kvm *kvm)
+{
+	return kvm->arch.vm_type == KVM_X86_TDX_VM;
+}
+
+static inline bool is_td_vcpu(struct kvm_vcpu *vcpu)
+{
+	return is_td(vcpu->kvm);
+}
+
+static __always_inline struct kvm_tdx *to_kvm_tdx(struct kvm *kvm)
+{
+	return container_of(kvm, struct kvm_tdx, kvm);
+}
+
+static __always_inline struct vcpu_tdx *to_tdx(struct kvm_vcpu *vcpu)
+{
+	return container_of(vcpu, struct vcpu_tdx, vcpu);
+}
+#else
+struct kvm_tdx {
+	struct kvm kvm;
+};
+
+struct vcpu_tdx {
+	struct kvm_vcpu	vcpu;
+};
+
+static inline bool is_td(struct kvm *kvm) { return false; }
+static inline bool is_td_vcpu(struct kvm_vcpu *vcpu) { return false; }
+static inline struct kvm_tdx *to_kvm_tdx(struct kvm *kvm) { return NULL; }
+static inline struct vcpu_tdx *to_tdx(struct kvm_vcpu *vcpu) { return NULL; }
+#endif /* CONFIG_INTEL_TDX_HOST */
+
+#endif /* __KVM_X86_TDX_H */

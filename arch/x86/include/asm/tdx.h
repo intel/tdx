1a4a16b8e5fce7b4ae7cfe8ab4f4e6b60de84ab5
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-09 11:23:49 -0800

    x86/cpu: Add helper functions to allocate/free TDX private host key id
    TDX private host key id is assigned to guest TD.  The memory controller
    encrypts guest TD memory with the assigned TDX private host key id (HIKD).
    Add helper functions to allocate/free TDX private host key id so that TDX
    KVM manage it.
    
    Also export the global TDX private host key id that is used to encrypt TDX
    module, its memory and some dynamic data (TDR).  When VMM releasing
    encrypted page to reuse it, the page needs to be flushed with the used host
    key id.  VMM needs the global TDX private host key id to flush such pages
    TDX module accesses with the global TDX private host key id.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -133,11 +133,24 @@ static inline long tdx_kvm_hypercall(unsigned int nr, unsigned long p1,
 #ifdef CONFIG_INTEL_TDX_HOST
 bool platform_tdx_enabled(void);
 int tdx_enable(void);
+
+/*
+ * Key id globally used by TDX module: TDX module maps TDR with this TDX global
+ * key id.  TDR includes key id assigned to the TD.  Then TDX module maps other
+ * TD-related pages with the assigned key id.  TDR requires this TDX global key
+ * id for cache flush unlike other TD-related pages.
+ */
+extern u32 tdx_global_keyid __read_mostly;
+int tdx_keyid_alloc(void);
+void tdx_keyid_free(int keyid);
+
 u64 __seamcall(u64 op, u64 rcx, u64 rdx, u64 r8, u64 r9,
 	       struct tdx_module_output *out);
 #else	/* !CONFIG_INTEL_TDX_HOST */
 static inline bool platform_tdx_enabled(void) { return false; }
 static inline int tdx_enable(void)  { return -ENODEV; }
+static inline int tdx_keyid_alloc(void) { return -EOPNOTSUPP; }
+static inline void tdx_keyid_free(int keyid) { }
 #endif	/* CONFIG_INTEL_TDX_HOST */
 
 #endif /* !__ASSEMBLY__ */

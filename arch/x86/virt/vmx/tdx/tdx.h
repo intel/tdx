8f940298ba0a7eb093eb3826840de0d8cd583a13
Kai Huang <kai.huang@intel.com>
2022-02-15 00:14:42 +1300

    x86/virt/tdx: Implement the SEAMCALL base function
    Secure Arbitration Mode (SEAM) is an extension of VMX architecture.  It
    defines a new VMX root operation (SEAM VMX root) and a new VMX non-root
    operation (SEAM VMX non-root) which isolate from legacy VMX root and VMX
    non-root mode.
    
    A CPU-attested software module (called the 'TDX module') runs in SEAM
    VMX root to manage the crypto protected VMs running in SEAM VMX non-root.
    SEAM VMX root is also used to host another CPU-attested software module
    (called the 'P-SEAMLDR') to load and update the TDX module.
    
    Host kernel transits to either the P-SEAMLDR or the TDX module via the
    new SEAMCALL instruction.  SEAMCALL leaf functions are host-side
    interface functions defined by the P-SEAMLDR and the TDX module around
    the new SEAMCALL instruction.  They are similar to a hypercall, except
    they are made by host kernel to the SEAM software.
    
    SEAMCALL leaf functions use an ABI different from the x86-64 system-v
    ABI.  Instead, they share the same ABI with the TDCALL leaf functions.
    %rax is used to carry both the SEAMCALL leaf function number (input) and
    the completion status code (output).  Additional GPRs (%rcx, %rdx,
    %r8->%r11) may be further used as both input and output operands in
    individual leaf functions.
    
    Implement a C function __seamcall() to do SEAMCALL leaf functions using
    the assembly macro used by __tdx_module_call() (the implementation of
    TDCALL leaf functions).  The only exception not covered here is TDENTER
    leaf function which takes all GPRs and XMM0-XMM15 as both input and
    output.  The caller of TDENTER should implement its own logic to call
    TDENTER directly instead of using this function.
    
    SEAMCALL instruction is essentially a VMExit from VMX root to SEAM VMX
    root, and it can fail with VMfailInvalid, for instance, when the SEAM
    software module is not loaded.  The C function __seamcall() returns
    TDX_SEAMCALL_VMFAILINVALID, which doesn't conflict with any actual error
    code of SEAMCALLs, to uniquely represent this case.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>

---
+++ arch/x86/virt/vmx/tdx/tdx.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _X86_VIRT_TDX_H
+#define _X86_VIRT_TDX_H
+
+#include <linux/types.h>
+
+struct tdx_module_output;
+u64 __seamcall(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
+	       struct tdx_module_output *out);
+
+#endif

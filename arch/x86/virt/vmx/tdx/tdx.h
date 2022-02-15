16b7a28fc1ae0da4d5c21d028e958aad7cf70db6
Kai Huang <kai.huang@intel.com>
2022-02-15 13:13:25 +1300

    x86/virt/tdx: Detect P-SEAMLDR and TDX module
    The P-SEAMLDR (persistent SEAM loader) is the first software module that
    runs in SEAM VMX root, responsible for loading and updating the TDX
    module.  Both the P-SEAMLDR and the TDX module are expected to be loaded
    before host kernel boots.
    
    There is no CPUID or MSR to detect whether the P-SEAMLDR or the TDX
    module has been loaded.  SEAMCALL instruction fails with VMfailInvalid
    when the target SEAM software module is not loaded, so SEAMCALL can be
    used to detect whether the P-SEAMLDR and the TDX module are loaded.
    
    Detect the P-SEAMLDR and the TDX module by calling SEAMLDR.INFO SEAMCALL
    to get the P-SEAMLDR information.  If the SEAMCALL succeeds, the
    P-SEAMLDR information further tells whether the TDX module is loaded or
    not.
    
    Also add a wrapper of __seamcall() to make SEAMCALL to the P-SEAMLDR and
    the TDX module with additional defensive check on SEAMRR and CR4.VMXE,
    since both detecting and initializing TDX module require the caller of
    TDX to handle VMXON.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/virt/vmx/tdx/tdx.h
+++ arch/x86/virt/vmx/tdx/tdx.h
@@ -3,6 +3,37 @@
 #define _X86_VIRT_TDX_H
 
 #include <linux/types.h>
+#include <linux/compiler.h>
+
+/*
+ * TDX architectural data structures
+ */
+
+#define P_SEAMLDR_INFO_ALIGNMENT	256
+
+struct p_seamldr_info {
+	u32	version;
+	u32	attributes;
+	u32	vendor_id;
+	u32	build_date;
+	u16	build_num;
+	u16	minor;
+	u16	major;
+	u8	reserved0[2];
+	u32	acm_x2apicid;
+	u8	reserved1[4];
+	u8	seaminfo[128];
+	u8	seam_ready;
+	u8	seam_debug;
+	u8	p_seamldr_ready;
+	u8	reserved2[88];
+} __packed __aligned(P_SEAMLDR_INFO_ALIGNMENT);
+
+/*
+ * P-SEAMLDR SEAMCALL leaf function
+ */
+#define P_SEAMLDR_SEAMCALL_BASE		BIT_ULL(63)
+#define P_SEAMCALL_SEAMLDR_INFO		(P_SEAMLDR_SEAMCALL_BASE | 0x0)
 
 struct tdx_module_output;
 u64 __seamcall(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,

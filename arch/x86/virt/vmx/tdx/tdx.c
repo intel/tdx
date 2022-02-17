343b709eabbd98a82db83c3eec352a7b1e1168d0
Kai Huang <kai.huang@intel.com>
2022-02-17 22:53:06 +1300

    x86/virt/tdx: Reserve TDX module global KeyID
    TDX module initialization requires to use one TDX private KeyID as the
    global KeyID to protect the TDX module metadata.  The global KeyID is
    configured to the TDX module along with TDMRs.
    
    Just reserve the first TDX private KeyID as the global KeyID.  Keep the
    global KeyID as a static variable as KVM will need to use it too.
    
    Reviewed-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -62,6 +62,9 @@ static struct tdsysinfo_struct tdx_sysinfo;
 static struct cmr_info tdx_cmr_array[MAX_CMRS] __aligned(CMR_INFO_ARRAY_ALIGNMENT);
 static int tdx_cmr_num;
 
+/* TDX module global KeyID.  Used in TDH.SYS.CONFIG ABI. */
+static u32 tdx_global_keyid;
+
 /*
  * Detect TDX private KeyIDs to see whether TDX has been enabled by the
  * BIOS.  Both initializing the TDX module and running TDX guest require
@@ -1112,6 +1115,12 @@ static int init_tdx_module(void)
 	ret = construct_tdmrs(tdmr_array, &tdmr_num);
 	if (ret)
 		goto out_free_tdmrs;
+
+	/*
+	 * Reserve the first TDX KeyID as global KeyID to protect
+	 * TDX module metadata.
+	 */
+	tdx_global_keyid = tdx_keyid_start;
 
 	/*
 	 * Return -EINVAL until all steps of TDX module initialization

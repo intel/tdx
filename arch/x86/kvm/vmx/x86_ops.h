9b5afc53b5ba35d04a666eaa31939bdb2dc3ec66
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-22 14:44:15 -0800

    KVM: TDX: Initialize the TDX module when loading the KVM intel kernel module
    TDX requires several initialization steps for KVM to create guest TDs.
    Detect CPU feature, enable VMX (TDX is based on VMX), detect the TDX module
    availability, and initialize it.  This patch implements those steps.
    
    There are several options on when to initialize the TDX module.  A.) kernel
    module loading time, B.) the first guest TD creation time.  A.) was chosen.
    With B.), a user may hit an error of the TDX initialization when trying to
    create the first guest TD.  The machine that fails to initialize the TDX
    module can't boot any guest TD further.  Such failure is undesirable and a
    surprise because the user expects that the machine can accommodate guest
    TD, but actually not.  So A.) is better than B.).
    
    Introduce a module parameter, enable_tdx, to explicitly enable TDX KVM
    support.  It's off by default to keep same behavior for those who don't use
    TDX.  Implement hardware_setup method to detect TDX feature of CPU.
    Because TDX requires all present CPUs to enable VMX (VMXON).  The x86
    specific kvm_arch_post_hardware_enable_setup overrides the existing weak
    symbol of kvm_arch_post_hardware_enable_setup which is called at the KVM
    module initialization.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -17,6 +17,9 @@ __init int vmx_cpu_has_kvm_support(void);
 __init int vmx_disabled_by_bios(void);
 __init int vmx_hardware_setup(void);
 
+int __init vmxon_all(void);
+void __init vmxoff_all(void);
+
 extern struct kvm_x86_ops vt_x86_ops __initdata;
 extern struct kvm_x86_init_ops vt_init_ops __initdata;
 
@@ -126,5 +129,11 @@ int vmx_set_hv_timer(struct kvm_vcpu *vcpu, u64 guest_deadline_tsc,
 void vmx_cancel_hv_timer(struct kvm_vcpu *vcpu);
 #endif
 void vmx_setup_mce(struct kvm_vcpu *vcpu);
+
+#ifdef CONFIG_INTEL_TDX_HOST
+int __init tdx_hardware_setup(struct kvm_x86_ops *x86_ops);
+#else
+static inline int tdx_hardware_setup(struct kvm_x86_ops *x86_ops) { return 0; }
+#endif
 
 #endif /* __KVM_X86_VMX_X86_OPS_H */

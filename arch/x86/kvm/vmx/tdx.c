5185c59bd4b3626835bfd02083b3af680471bcf0
Isaku Yamahata <isaku.yamahata@intel.com>
2022-02-22 14:44:15 -0800

    KVM: TDX: Initialize the TDX module when loading the KVM intel kernel module
    TDX requires several initialization steps for KVM to create guest TDs.
    Detect CPU feature, enable VMX (TDX is based on VMX) on all online CPUs,
    detect the TDX module availability, initialize it and disable VMX.
    
    To enable/disable VMX on all online CPUs, utilize
    vmx_hardware_enable/disable().  The method also initializes each CPU for
    TDX.  TDX requires calling a TDX initialization function per logical
    processor (LP) before the LP uses TDX.  When the CPU is becoming online,
    call the TDX LP initialization API.  If it fails to initialize TDX, refuse
    CPU online for simplicity instead of TDX avoiding the failed LP.
    
    There are several options on when to initialize the TDX module.  A.) kernel
    module loading time, B.) the first guest TD creation time.  A.) was chosen.
    With B.), a user may hit an error of the TDX initialization when trying to
    create the first guest TD.  The machine that fails to initialize the TDX
    module can't boot any guest TD further.  Such failure is undesirable and a
    surprise because the user expects that the machine can accommodate guest
    TD, but not.  So A.) is better than B.).
    
    Introduce a module parameter, kvm_intel.tdx, to explicitly enable TDX KVM
    support.  It's off by default to keep the same behavior for those who don't
    use TDX.  Implement hardware_setup method to detect TDX feature of CPU and
    initialize TDX module.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

---
+++ arch/x86/kvm/vmx/tdx.c
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/cpu.h>
+
+#include <asm/tdx.h>
+
+#include "capabilities.h"
+#include "x86_ops.h"
+#include "x86.h"
+
+#undef pr_fmt
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+static int __init tdx_module_setup(void)
+{
+	int ret;
+
+	ret = tdx_enable();
+	if (ret) {
+		pr_info("Failed to initialize TDX module.\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static void __init vmx_tdx_on(void *info)
+{
+	atomic_t *err = info;
+	int r;
+
+	r = vmx_hardware_enable();
+	if (!r)
+		r = tdx_cpu_enable();
+	if (r)
+		atomic_set(err, r);
+}
+
+static void __init vmx_off(void *unused)
+{
+	vmx_hardware_disable();
+}
+
+int __init tdx_hardware_setup(struct kvm_x86_ops *x86_ops)
+{
+	atomic_t err = ATOMIC_INIT(0);
+	int r = 0;
+
+	if (!enable_ept) {
+		pr_warn("Cannot enable TDX with EPT disabled\n");
+		return -EINVAL;
+	}
+
+	/* tdx_enable() in tdx_module_setup() requires cpus lock. */
+	cpus_read_lock();
+	on_each_cpu(vmx_tdx_on, &err, true);	/* TDX requires vmxon. */
+	r = atomic_read(&err);
+	if (!r)
+		r = tdx_module_setup();
+	on_each_cpu(vmx_off, NULL, true);
+	cpus_read_unlock();
+
+	return r;
+}

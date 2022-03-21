ff6e21e8d10b3e9d9b8a00fb77deb011b29fc040
Isaku Yamahata <isaku.yamahata@intel.com>
2022-03-21 14:28:28 -0700

    [REVERTME] X86: KVM: fix print format errors with MMU_DEBUG with 64bit
    gpa is 64bit and it requires "%llx" instead of "%lx".  Fix format error.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -4471,7 +4471,7 @@ out_unlock:
 static int nonpaging_page_fault(struct kvm_vcpu *vcpu,
 				struct kvm_page_fault *fault)
 {
-	pgprintk("%s: gva %lx error %x\n", __func__, fault->addr, fault->error_code);
+	pgprintk("%s: gva %llx error %x\n", __func__, fault->addr, fault->error_code);
 
 	/* This path builds a PAE pagetable, we can map 2mb pages at maximum. */
 	fault->max_level = PG_LEVEL_2M;

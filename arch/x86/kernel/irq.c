c706521024dadca08de7f9e1481c415c4e0bb9cc
Yuan Yao <yuan.yao@intel.com>
2022-03-22 17:52:59 +0800

    x86: Introduce TDX_GUEST_PMI_VECTOR to handle PMI of TDX guest
    Unlike legacy VM guest, INTEL TDX guest can use PMU hardware
    but not rely on software emulation, except we need do guest
    PMI injection in EXIT_REASON_EXCEPTION_NMI(NMI).
    
    But there's another case which we also need to do guest
    PMI injection:
    
    TDX module has security feature called 0/1 stepping defense,
    any NMI happened during the feature is active will be delay
    delivered at end phase of this feature, by injecting NMI to
    CPU from loacl APIC directly. This means it's very possible
    that A NMI happened immediately after TDENER is PMI for TDX
    guest.
    
    We need installing a new NMI handler for this, but meanwhile
    we still need to handle normal EXIT_REASON_EXCEPTION_NMI(NMI)
    to inject PMI to TDX guest, we don't do this all in the new
    installed NMI handler because it's too late to restore host
    PMU context there, we want to restore host PMU context as
    early as possible to not reduce the accuracy.
    
    We also need to call host's NMI handler from KVM in
    EXIT_REASON_EXCEPTION_NMI(NMI) no matter we injected PMI
    to TDX guest or not, because we can't assume that it's
    the only reason of the NMI. Let's call this "case VMEXIT NMI".
    
    The host's LOCAL_NMI handlers are chained so every handler
    is called unconditionally. So our new installed handler need
    to distinguish the caller:
    
    if caller is from "case VMEXIT NMI" && we already injected PMI
      return NMI_HANDLED
    
    We meet "unknown NMI" issue if we don't return NMI_HANDLED here,
    because no one handle this Guest PMI.
    
    but it's hard to indentify the caller, the only workable way is
    return RIP checking but that's still not 100% safe.
    
    We decide to introduce a new dedicate vector as TDX guest PMI,
    this decouples Guest PMI from Host NMI, which eliminates the
    issue above.
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>

--- arch/x86/kernel/irq.c
+++ arch/x86/kernel/irq.c
@@ -182,6 +182,12 @@ int arch_show_interrupts(struct seq_file *p, int prec)
 			   irq_stats(j)->kvm_posted_intr_wakeup_ipis);
 	seq_puts(p, "  Posted-interrupt wakeup event\n");
 #endif
+#if defined(CONFIG_HAVE_KVM) && defined(CONFIG_INTEL_TDX_HOST)
+	seq_printf(p, "%*s", prec, "TGP");
+	for_each_online_cpu(j)
+		seq_printf(p, "%10u ", irq_stats(j)->kvm_tdx_guest_pmis);
+	seq_puts(p, "  TDX Guest performance monitoring interrupts\n");
+#endif
 	return 0;
 }
 
@@ -305,6 +311,18 @@ void kvm_set_posted_intr_wakeup_handler(void (*handler)(void))
 }
 EXPORT_SYMBOL_GPL(kvm_set_posted_intr_wakeup_handler);
 
+#ifdef CONFIG_INTEL_TDX_HOST
+static void (*kvm_tdx_guest_pmi_handler)(void) = dummy_handler;
+
+void kvm_set_tdx_guest_pmi_handler(void (*handler)(void))
+{
+	if (handler)
+		kvm_tdx_guest_pmi_handler = handler;
+	else
+		kvm_tdx_guest_pmi_handler = dummy_handler;
+}
+EXPORT_SYMBOL_GPL(kvm_set_tdx_guest_pmi_handler);
+#endif
 /*
  * Handler for POSTED_INTERRUPT_VECTOR.
  */
@@ -332,6 +350,15 @@ DEFINE_IDTENTRY_SYSVEC_SIMPLE(sysvec_kvm_posted_intr_nested_ipi)
 	ack_APIC_irq();
 	inc_irq_stat(kvm_posted_intr_nested_ipis);
 }
+
+#ifdef CONFIG_INTEL_TDX_HOST
+DEFINE_IDTENTRY_SYSVEC(sysvec_kvm_tdx_guest_pmi)
+{
+	ack_APIC_irq();
+	inc_irq_stat(kvm_tdx_guest_pmis);
+	kvm_tdx_guest_pmi_handler();
+}
+#endif
 #endif
 
 

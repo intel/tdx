intel/tdx

commit 217e503098c4435ae6f29020d17b809749749162

KVM: TDX: Split a large page when 4KB page within it converted to shared
When mapping the shared page for TDX, it needs to zap private alias.

In the case that private page is mapped as large page (2MB), it can be
removed directly only when the whole 2MB is converted to shared.
Otherwise, it has to split 2MB page into 512 4KB page, and only remove
the pages that convertd to shared.

Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

arch/x86/kvm/mmu/mmu.c
@@ -3341,6 +3341,56 @@ static void kvm_mmu_link_private_sp(struct kvm_vcpu *vcpu,
 		free_page((unsigned long)p);
 }
 
+static void split_private_spte(struct kvm_vcpu *vcpu, u64 *sptep, u64 old_spte)
+{
+	bool host_writable = !!(old_spte & EPT_SPTE_HOST_WRITABLE);
+	struct kvm_mmu_page *sp = sptep_to_sp(sptep);
+	kvm_pfn_t pfn = spte_to_pfn(old_spte);
+	struct kvm_rmap_head *rmap_head;
+	struct kvm_memory_slot *slot;
+	int level = sp->role.level;
+	gfn_t gfn;
+	void *p;
+	int i;
+
+	if (!is_zapped_private_pte(*sptep))
+		return;
+
+	if (WARN_ON_ONCE(level < PG_LEVEL_2M || !is_private_spte(sptep)))
+		return;
+
+	gfn = kvm_mmu_page_get_gfn(sp, sptep - sp->spt);
+	slot = kvm_vcpu_gfn_to_memslot(vcpu, gfn);
+	rmap_head = gfn_to_rmap(gfn, sp->role.level, slot);
+	__pte_list_remove(sptep, rmap_head);
+
+	level--;
+	sp = kvm_mmu_get_page(vcpu, gfn, gfn >> PAGE_SHIFT, level, true,
+			ACC_ALL, true);
+
+	link_shadow_page(vcpu, sptep, sp);
+	kvm_update_page_stats(vcpu->kvm, PG_LEVEL_4K, -1);
+
+	for (i = 0; i < PT64_ENT_PER_PAGE; i++) {
+		struct kvm_page_fault fault = (struct kvm_page_fault) {
+			.map_writable = host_writable,
+			.prefetch = false,
+			.write = false,
+		};
+		mmu_set_spte(vcpu, slot, sp->spt + i, ACC_ALL, gfn, pfn, &fault);
+		gfn += KVM_PAGES_PER_HPAGE(level);
+		pfn += KVM_PAGES_PER_HPAGE(level);
+	}
+	if (level > PG_LEVEL_4K)
+		kvm_update_page_stats(vcpu->kvm, level, 1);
+
+	p = kvm_mmu_memory_cache_alloc(&vcpu->arch.mmu_private_sp_cache);
+	if (!static_call(kvm_x86_split_private_spte)(vcpu->kvm, sp->gfn, level + 1, p))
+		sp->private_sp = p;
+	else
+		free_page((unsigned long)p);
+}
+
 static void kvm_mmu_zap_alias_spte(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 {
 	struct kvm_shadow_walk_iterator it;
@@ -3355,6 +3405,7 @@ static void kvm_mmu_zap_alias_spte(struct kvm_vcpu *vcpu, struct kvm_page_fault 
 	u64 *sptep;
 	u64 spte;
 
+re_start:
 	for_each_shadow_entry(vcpu, gpa_alias, it) {
 		if (!is_shadow_present_pte(*it.sptep))
 			break;
@@ -3398,6 +3449,12 @@ static void kvm_mmu_zap_alias_spte(struct kvm_vcpu *vcpu, struct kvm_page_fault 
 
 	if (!is_private_sp(sp))
 		return;
+
+	if (is_large_pte(spte) &&
+	    !kvm_page_type_valid_on_level(fault->gfn, slot, it.level)) {
+		split_private_spte(vcpu, it.sptep, spte);
+		goto re_start;
+	}
 
 	for_each_rmap_spte(rmap_head, &iter, sptep) {
 		if (!is_zapped_private_pte(*sptep))

arch/x86/include/asm/kvm-x86-ops.h
@@ -97,6 +97,7 @@ KVM_X86_OP(zap_private_spte)
 KVM_X86_OP(unzap_private_spte)
 KVM_X86_OP(link_private_sp)
 KVM_X86_OP(free_private_sp)
+KVM_X86_OP(split_private_spte)
 KVM_X86_OP_NULL(has_wbinvd_exit)
 KVM_X86_OP(get_l2_tsc_offset)
 KVM_X86_OP(get_l2_tsc_multiplier)

arch/x86/include/asm/kvm_host.h
@@ -1463,6 +1463,8 @@ struct kvm_x86_ops {
 			       void *private_sp);
 	int (*free_private_sp)(struct kvm *kvm, gfn_t gfn, enum pg_level level,
 			       void *private_sp);
+	int (*split_private_spte)(struct kvm *kvm, gfn_t gfn, enum pg_level level,
+				  void *private_sp);
 
 	bool (*has_wbinvd_exit)(void);
 

arch/x86/kvm/vmx/tdx.c
@@ -1748,6 +1748,41 @@ static int tdx_sept_link_private_sp(struct kvm *kvm, gfn_t gfn,
 	return ret;
 }
 
+static int __tdx_sept_split_private_spte(struct kvm *kvm, gfn_t gfn,
+					enum pg_level level, void *sept_page)
+{
+	int tdx_level = pg_level_to_tdx_sept_level(level);
+	struct kvm_tdx *kvm_tdx = to_kvm_tdx(kvm);
+	gpa_t gpa = gfn << PAGE_SHIFT;
+	hpa_t hpa = __pa(sept_page);
+	struct tdx_ex_ret ex_ret;
+	u64 err;
+
+	trace_kvm_sept_seamcall(TDH_MEM_PAGE_DEMOTE, gpa, hpa, tdx_level);
+
+	err = tdh_mem_page_demote(kvm_tdx->tdr.pa, gpa, tdx_level, hpa, &ex_ret);
+	if (KVM_BUG_ON(err, kvm)) {
+		pr_tdx_error(TDH_MEM_PAGE_DEMOTE, err, &ex_ret);
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int tdx_sept_split_private_spte(struct kvm *kvm, gfn_t gfn,
+				enum pg_level level, void *sept_page)
+{
+	struct kvm_tdx *kvm_tdx = to_kvm_tdx(kvm);
+	int ret;
+
+	/* See comment in tdx_sept_set_private_spte() */
+	spin_lock(&kvm_tdx->seamcall_lock);
+	ret = __tdx_sept_split_private_spte(kvm, gfn, level, sept_page);
+	spin_unlock(&kvm_tdx->seamcall_lock);
+
+	return ret;
+}
+
 static void __tdx_sept_zap_private_spte(struct kvm *kvm, gfn_t gfn,
 					enum pg_level level)
 {
@@ -3263,6 +3298,7 @@ int __init tdx_hardware_setup(struct kvm_x86_ops *x86_ops)
 	x86_ops->unzap_private_spte = tdx_sept_unzap_private_spte;
 	x86_ops->link_private_sp = tdx_sept_link_private_sp;
 	x86_ops->free_private_sp = tdx_sept_free_private_sp;
+	x86_ops->split_private_spte = tdx_sept_split_private_spte;
 	x86_ops->mem_enc_read_memory = tdx_read_guest_memory;
 	x86_ops->mem_enc_write_memory = tdx_write_guest_memory;
 

commit 81ddbaa0da7b16ba7648cf818b2dff99481cad03

KVM: TDX: use sp->role.level instead of shadow page iterator
Shadow page iterator has level=0 when it reaches last pte even if it's
large page.  It results in out-of-bounds array access as follows.  It.level
isn't equal to the level of shadow page table.  Use role.level instead of
iterator's.

>  ================================================================================
>  UBSAN: array-index-out-of-bounds in /linux/arch/x86/kvm/mmu/mmu.c:917:30
>  index -1 is out of range for type 'kvm_page_attr *[3]'
>  CPU: 139 PD: 4064 Comm: qemu-system-x86 Tainted: G        W         5.16.0-rc5
>  Hardware name: Intel Corporation EAGLESTREAM/EAGLESTREAM, BIOS EGSDCRB1.SYS.0066.D24.2110072326 10/07/2021
>
>  Call Trace: 984fee009b7f
>   <TASK>
>   dump_stack_lvl+0x5b/0x82
>   dump_stack+0x10/0x12
>   ubsan_epilogue+0x9/0x45
>   __ubsan_handle_out_of_bounds+0x70/0x80
>   ? kvm_flush_remote_tlbs+0x4f/0x70 [kvm]
>   kvm_mmu_zap_alias_spte.isra.0+0x91e/0x930 [kvm]
>   ? drop_large_spte+0x290/0x290 [kvm]
>   ? try_merge_page_type+0x117/0x1d0 [kvm]
>   __direct_map+0xa06/0xe10 [kvm]
>   ? ept_page_fault+0x610/0x610 [kvm]
>   ? kvm_mmu_topup_memory_cache+0x24/0xf0 [kvm]
>   ? is_obsolete_sp.part.0+0x4e/0x60 [kvm]
>   ? is_page_fault_stale.isra.0+0xea/0x160 [kvm]
>   direct_page_fault+0x442/0x460 [kvm]
>   ? __direct_map+0xe10/0xe10 [kvm]
>   ? perf_event_upate_userpage+0x35b/0x4f0
>   kvm_tdp_page_fault+0xf1/0x100 [kvm]
>   kvm_mmu_page_fault+0x3c2/0x420 [kvm]
>   ? kvm_tdp_page_fault+0x100/0x100 [kvm]
>   ? rcu_read_lock_bh_held+0xc0/0xc0
>   ? rcu_read_lock_sched_held+0x51/0xe0
>   tdx_handle_exit+0x641/0xc51 [kvm_intel]
>   ? tdx_skip_emulated_instruction+0x3a0/0x3a0 [kvm_intel]
>   ? rcu_read_unlock+0x50/0x50
>   ? lock_downgrade+0xd0/0xd0
>   ? rcu_read_unlock+0x50/0x50
>   ? rcu_read_lock_sched_held+0x60/0xe0
>   ? rcu_read_lock_held_common+x1e/0x60
>   vt_handle_exit+0x4a/0x60 [kvm_intel]
>   vcpu_enter_guest+0x147d/0x2220 [kvm]
>   ? lock_acquire+0xdf/0x450
>   ? kvm_vcpu_reload_apic_access_page+0x50/0x50 [kvm]
>   ? xfd_validate_state+0x11/0xd0
>   ? rcu_read_lock_held_common+0x1e/0x60
>   ? rcu_read_lock_sched_held+0x60/0xe0
>   ? __kasan_check_read+0x11/0x20
>   kvm_arch_vcpu_ioctl_run+0x278/0xcf0 [kvm]
>   kvm_vcpu_ioctl+0x414/0xa30 [kvm]
>   ? kvm_set_memory_region+0x50/0x50 [kvm]
>   ? lock_release+0xcb/0x450
>   ? __fget_files+0x49/0x240
>   ? lock_downgrade+0xd0/0xd0
>   ? __fget_files+0x161/0x240
>   ? __fget_light+0xec/0x100
>  __x64_sys_ioctl+0xc0/0x100
>   do_syscall_64+0x39/0xc0
>   entry_SYSCALL_64_after_hwframe+0x44/0xae
>  RIP: 0033:0x7fa137db43db
>  Code: 0f 1e fa 48 8b 05 b5 7a 0d 00 64 c7 00 26 00 00 00 48 c7 c0 ff ff ff ff c3 66 0f 1f 44 00 00 f3 0f 1e fa b8 10 00 00 00 0f 05 <48> 3d 01 f0 ff ff 73 01 c3 48 8b 0d 85 7a 0d 00 f7 d8 64 89 01 48
>  RSP: 002b:00007fa1275ba0e8 EFLAGS: 00000246 ORIG_RAX: 0000000000000010
>  RAX: ffffffffffffffda RBX: 0000000000000000 RCX: 00007fa137db43db
>  RDX: 0000000000000000 RSI: 000000000000ae80 RDI: 0000000000000012
>  RBP: 00007fa1275ba1e0 R08: 0000000000000000 R09: 000000000000ffff
> R10: 000055d18137c41a R11: 0000000000000246 R12: 00007fff938f419e
>  R13: 00007fff938f419f R14: 00007fff938f4260 R15: 00007fa1275ba400
>   </TASK>
>  ================================================================================

Fixes: 217e503098c4 ("KVM: TDX: Split a large page when 4KB page within it converted to shared")

arch/x86/kvm/mmu/mmu.c
@@ -3488,7 +3488,7 @@ re_start:
 		return;
 
 	if (is_large_pte(spte) &&
-	    !kvm_page_type_valid_on_level(fault->gfn, slot, it.level)) {
+	    !kvm_page_type_valid_on_level(fault->gfn, slot, sp->role.level)) {
 		split_private_spte(vcpu, it.sptep, spte);
 		goto re_start;
 	}
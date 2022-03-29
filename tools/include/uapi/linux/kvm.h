ee207c479c9af881e8e44edfe7beed3ee07c5520
Isaku Yamahata <isaku.yamahata@intel.com>
2022-03-29 08:11:32 -0700

    [REVERTME] KVM: bump KVM_CAP_ENCRYPT_MEMORY_DEBUG to 300
    To avoid near future conflict with the upstream, bump
    KVM_CAP_ENCRYPT_MEMORY_DEBUG to 300 instead of the next unused value.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- tools/include/uapi/linux/kvm.h
+++ tools/include/uapi/linux/kvm.h
@@ -1144,9 +1144,9 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_S390_MEM_OP_EXTENSION 211
 #define KVM_CAP_PMU_CAPABILITY 212
 #define KVM_CAP_DISABLE_QUIRKS2 213
-#define KVM_CAP_ENCRYPT_MEMORY_DEBUG 216
 
 /* TODO: remove this workaround to avoid CAP number conflict in the upstream. */
+#define KVM_CAP_ENCRYPT_MEMORY_DEBUG 300
 #define KVM_CAP_VM_TYPES 1000
 
 #ifdef KVM_CAP_IRQ_ROUTING

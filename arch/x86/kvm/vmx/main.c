22329f873c4af86628af2c40d003defc10186006
Sean Christopherson <seanjc@google.com>
2022-04-08 08:56:27 -0700

    KVM: TDX: Add support for find pending IRQ in a protected local APIC
    Add flag and hook to KVM's local APIC management to support determining
    whether or not a TDX guest as a pending IRQ.  For TDX vCPUs, the virtual
    APIC page is owned by the TDX module and cannot be accessed by KVM.  As a
    result, registers that are virtualized by the CPU, e.g. PPR, cannot be
    read or written by KVM.  To deliver interrupts for TDX guests, KVM must
    send an IRQ to the CPU on the posted interrupt notification vector.  And
    to determine if TDX vCPU has a pending interrupt, KVM must check if there
    is an outstanding notification.
    
    Return "no interrupt" in kvm_apic_has_interrupt() if the guest APIC is
    protected to short-circuit the various other flows that try to pull an
    IRQ out of the vAPIC, the only valid operation is querying _if_ an IRQ is
    pending, KVM can't do anything based on _which_ IRQ is pending.
    
    Intentionally omit sanity checks from other flows, e.g. PPR update, so as
    not to degrade non-TDX guests with unnecessary checks.  A well-behaved KVM
    and userspace will never reach those flows for TDX guests, but reaching
    them is not fatal if something does go awry.
    
    Note, this doesn't handle interrupts that have been delivered to the vCPU
    but not yet recognized by the core, i.e. interrupts that are sitting in
    vmcs.GUEST_INTR_STATUS.  Querying that state requires a SEAMCALL and will
    be supported in a future patch.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/main.c
+++ arch/x86/kvm/vmx/main.c
@@ -78,6 +78,8 @@ static __init int vt_hardware_setup(void)
 	if (enable_tdx)
 		vt_x86_ops.vm_size = max_t(unsigned int, vt_x86_ops.vm_size,
 					   sizeof(struct kvm_tdx));
+	else
+		vt_x86_ops.protected_apic_has_interrupt = NULL;
 
 #if IS_ENABLED(CONFIG_HYPERV)
 	if (enable_tdx)
@@ -217,6 +219,13 @@ static void vt_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 	}
 
 	vmx_vcpu_load(vcpu, cpu);
+}
+
+static bool vt_protected_apic_has_interrupt(struct kvm_vcpu *vcpu)
+{
+	KVM_BUG_ON(!is_td_vcpu(vcpu), vcpu->kvm);
+
+	return tdx_protected_apic_has_interrupt(vcpu);
 }
 
 static void vt_flush_tlb_all(struct kvm_vcpu *vcpu)
@@ -443,6 +452,7 @@ struct kvm_x86_ops vt_x86_ops __initdata = {
 	.sync_pir_to_irr = vmx_sync_pir_to_irr,
 	.deliver_interrupt = vmx_deliver_interrupt,
 	.dy_apicv_has_pending_interrupt = pi_has_pending_interrupt,
+	.protected_apic_has_interrupt = vt_protected_apic_has_interrupt,
 
 	.set_tss_addr = vmx_set_tss_addr,
 	.set_identity_map_addr = vmx_set_identity_map_addr,

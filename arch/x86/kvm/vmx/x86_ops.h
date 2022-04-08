2bbc34e20a78a9352004f676b5cf98ce6d673864
Sean Christopherson <seanjc@google.com>
2022-04-08 08:56:27 -0700

    KVM: TDX: Add support for find pending IRQ in a protected local APIC
    Add flag and hook to KVM's local APIC management to support determining
    whether or not a TDX guest as a pending IRQ.  For TDX vCPUs, the virtual
    APIC page is owned by the TDX module and cannot be accessed by KVM.  As a
    result, registers that are virtualized by the CPU, e.g. PPR, cannot be
    read or written by KVM.  To deliver interrupts for TDX guests, KVM must
    send an IRQ to the CPU on the posted interrupt notification vector.  And
    to determine if TDX vCPU has a pending interrupt, KVM must check if there
    is an outstanding notification.
    
    Return "no interrupt" in kvm_apic_has_interrupt() if the guest APIC is
    protected to short-circuit the various other flows that try to pull an
    IRQ out of the vAPIC, the only valid operation is querying _if_ an IRQ is
    pending, KVM can't do anything based on _which_ IRQ is pending.
    
    Intentionally omit sanity checks from other flows, e.g. PPR update, so as
    not to degrade non-TDX guests with unnecessary checks.  A well-behaved KVM
    and userspace will never reach those flows for TDX guests, but reaching
    them is not fatal if something does go awry.
    
    Note, this doesn't handle interrupts that have been delivered to the vCPU
    but not yet recognized by the core, i.e. interrupts that are sitting in
    vmcs.GUEST_INTR_STATUS.  Querying that state requires a SEAMCALL and will
    be supported in a future patch.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    ---
    
    Kai rebase:
    - Resolve conflict, no code change.
    
    v20:
    - Dropped vt_protected_apic_has_interrupt() with KVM_BUG_ON(). (Rick)

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -138,6 +138,7 @@ fastpath_t tdx_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediate_exit);
 void tdx_prepare_switch_to_guest(struct kvm_vcpu *vcpu);
 void tdx_vcpu_put(struct kvm_vcpu *vcpu);
 void tdx_vcpu_load(struct kvm_vcpu *vcpu, int cpu);
+bool tdx_protected_apic_has_interrupt(struct kvm_vcpu *vcpu);
 u8 tdx_get_mt_mask(struct kvm_vcpu *vcpu, gfn_t gfn, bool is_mmio);
 
 int tdx_vcpu_ioctl(struct kvm_vcpu *vcpu, void __user *argp);
@@ -180,6 +181,7 @@ static inline fastpath_t tdx_vcpu_run(struct kvm_vcpu *vcpu, bool force_immediat
 static inline void tdx_prepare_switch_to_guest(struct kvm_vcpu *vcpu) {}
 static inline void tdx_vcpu_put(struct kvm_vcpu *vcpu) {}
 static inline void tdx_vcpu_load(struct kvm_vcpu *vcpu, int cpu) {}
+static inline bool tdx_protected_apic_has_interrupt(struct kvm_vcpu *vcpu) { return false; }
 static inline u8 tdx_get_mt_mask(struct kvm_vcpu *vcpu, gfn_t gfn, bool is_mmio) { return 0; }
 
 static inline int tdx_vcpu_ioctl(struct kvm_vcpu *vcpu, void __user *argp) { return -EOPNOTSUPP; }

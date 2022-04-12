5ea560e456aa2b6e29da6888e5642afe1be195dd
Isaku Yamahata <isaku.yamahata@intel.com>
2022-04-12 14:18:32 -0700

    KVM: x86: simplfy page fault for private slot
    This is a simplification of "KVM: Handle page fault for private memory" by
    introducing is_private_pfn member to struct kvm_page_fault.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -4241,16 +4241,20 @@ static bool kvm_vcpu_is_private_gfn(struct kvm_vcpu *vcpu, gfn_t gfn)
 }
 
 static bool kvm_faultin_pfn_private(struct kvm_vcpu *vcpu,
-				    struct kvm_page_fault *fault,
-				    bool *is_private_pfn, int *r)
+				    struct kvm_page_fault *fault, int *r)
 {
 	int order;
 	unsigned int flags = 0;
 	struct kvm_memory_slot *slot = fault->slot;
-	long pfn = kvm_memfile_get_pfn(slot, fault->gfn, &order);
+	kvm_pfn_t pfn;
 
+	fault->is_private_pfn = false;
+	if (!kvm_slot_is_private(slot))
+		return false;
+
+	pfn = kvm_memfile_get_pfn(slot, fault->gfn, &order);
 	if (kvm_vcpu_is_private_gfn(vcpu, fault->addr >> PAGE_SHIFT)) {
-		if (pfn < 0)
+		if (!VALID_PAGE(pfn))
 			flags |= KVM_MEMORY_EXIT_FLAG_PRIVATE;
 		else {
 			fault->pfn = pfn;
@@ -4261,12 +4265,12 @@ static bool kvm_faultin_pfn_private(struct kvm_vcpu *vcpu,
 
 			if (order == 0)
 				fault->max_level = PG_LEVEL_4K;
-			*is_private_pfn = true;
+			fault->is_private_pfn = true;
 			*r = RET_PF_FIXED;
 			return true;
 		}
 	} else {
-		if (pfn < 0)
+		if (!VALID_PAGE(pfn))
 			return false;
 
 		kvm_memfile_put_pfn(slot, pfn);
@@ -4283,7 +4287,7 @@ static bool kvm_faultin_pfn_private(struct kvm_vcpu *vcpu,
 }
 
 static bool kvm_faultin_pfn(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault,
-			    bool *is_private_pfn, int *r)
+			    int *r)
 {
 	struct kvm_memory_slot *slot = fault->slot;
 	bool async;
@@ -4317,9 +4321,8 @@ static bool kvm_faultin_pfn(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault,
 		}
 	}
 
-	if (kvm_slot_is_private(slot) &&
-	    kvm_faultin_pfn_private(vcpu, fault, is_private_pfn, r))
-		return *r == RET_PF_FIXED ? false : true;
+	if (kvm_faultin_pfn_private(vcpu, fault, r))
+		return *r != RET_PF_FIXED;
 
 	async = false;
 	fault->pfn = __gfn_to_pfn_memslot(slot, fault->gfn, false, &async,
@@ -4357,6 +4360,9 @@ static bool is_page_fault_stale(struct kvm_vcpu *vcpu,
 {
 	struct kvm_mmu_page *sp = to_shadow_page(vcpu->arch.mmu->root.hpa);
 
+	if (fault->is_private_pfn)
+		return false;
+
 	/* Special roots, e.g. pae_root, are not backed by shadow pages. */
 	if (sp && is_obsolete_sp(vcpu->kvm, sp))
 		return true;
@@ -4376,12 +4382,22 @@ static bool is_page_fault_stale(struct kvm_vcpu *vcpu,
 	       mmu_notifier_retry_hva(vcpu->kvm, mmu_seq, fault->hva);
 }
 
+void kvm_mmu_release_fault(struct kvm_page_fault *fault)
+{
+	if (is_error_noslot_pfn(fault->pfn) || kvm_is_reserved_pfn(fault->pfn))
+		return;
+
+	if (fault->is_private_pfn)
+		kvm_memfile_put_pfn(fault->slot, fault->pfn);
+	else
+		kvm_release_pfn_clean(fault->pfn);
+}
+
 static int direct_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 {
 	bool is_tdp_mmu_fault = is_tdp_mmu(vcpu->arch.mmu);
 
 	unsigned long mmu_seq;
-	bool is_private_pfn = false;
 	int r;
 
 	fault->gfn = kvm_gfn_unalias(vcpu->kvm, gpa_to_gfn(fault->addr));
@@ -4401,7 +4417,7 @@ static int direct_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault
 	mmu_seq = vcpu->kvm->mmu_notifier_seq;
 	smp_rmb();
 
-	if (kvm_faultin_pfn(vcpu, fault, &is_private_pfn, &r))
+	if (kvm_faultin_pfn(vcpu, fault, &r))
 		return r;
 
 	if (handle_abnormal_pfn(vcpu, fault, ACC_ALL, &r))
@@ -4414,7 +4430,7 @@ static int direct_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault
 	else
 		write_lock(&vcpu->kvm->mmu_lock);
 
-	if (!is_private_pfn && is_page_fault_stale(vcpu, fault, mmu_seq))
+	if (is_page_fault_stale(vcpu, fault, mmu_seq))
 		goto out_unlock;
 
 	r = make_mmu_pages_available(vcpu);
@@ -4432,11 +4448,7 @@ out_unlock:
 	else
 		write_unlock(&vcpu->kvm->mmu_lock);
 
-	if (is_private_pfn)
-		kvm_memfile_put_pfn(fault->slot, fault->pfn);
-	else
-		kvm_release_pfn_clean(fault->pfn);
-
+	kvm_mmu_release_fault(fault);
 	return r;
 }
 

b18bc33767f99ddc556afe5d179be858f5baf244
Isaku Yamahata <isaku.yamahata@intel.com>
2022-04-19 07:01:37 -0700

    KVM: TDX: mmu: use kvm_mmu_zap_all_fast for default VM in kvm_mmu_invalidate_zap_pages_in_memslot
    In v5.18, kvm_mmu_zap_all_fast() was optimized with read lock of
    mmu_lock and work queue to allow concurrent operation during zapping.
    kvm_mmu_zap_memslot() has to use write lock of mmu_lock because Secure-EPT
    requires to unlink leaf level EPT pages first before parent EPT page.
    
    Because kvm_mmu_zap_all_fast() is better for default VM, the existing EPT
    case, use it.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -6323,7 +6323,7 @@ static void kvm_mmu_invalidate_zap_pages_in_memslot(struct kvm *kvm,
 			struct kvm_memory_slot *slot,
 			struct kvm_page_track_notifier_node *node)
 {
-	if (memslot_update_zap_all && !kvm_mmu_supports_shared_bit(kvm))
+	if (memslot_update_zap_all || !kvm_mmu_supports_shared_bit(kvm))
 		kvm_mmu_zap_all_fast(kvm);
 	else
 		kvm_mmu_zap_memslot(kvm, slot);

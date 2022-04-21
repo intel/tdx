8e8aad8bab6e27b509f2e4a78351fa373ef2d04a
Yuan Yao <yuan.yao@intel.com>
2022-04-21 16:50:24 +0800

    KVM: X86: Add struct kvm for kvm_x86_ops::check_apicv_inhibit_reasons
    This is preparation patch to introduce check_apicv_inhibit_reasons
    implementation for INTEL TDX guest, struct kvm* parameter is used
    to identify the running VM type:
    
    if (is_td(kvm))
       return tdx_check_apicv_inhibit_reasons
    return vmx_check_apicv_inhibit_reasons
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -10200,7 +10200,7 @@ void __kvm_set_or_clear_apicv_inhibit(struct kvm *kvm,
 
 	lockdep_assert_held_write(&kvm->arch.apicv_update_lock);
 
-	if (!static_call(kvm_x86_check_apicv_inhibit_reasons)(reason))
+	if (!static_call(kvm_x86_check_apicv_inhibit_reasons)(kvm, reason))
 		return;
 
 	old = new = kvm->arch.apicv_inhibit_reasons;

d5116a2096742d347c7ceb839f3124f84242f913
Yuan Yao <yuan.yao@intel.com>
2022-04-21 16:49:49 +0800

    KVM: TDX: Introduce KVM_MEMORY_ENCRYPT_READ_MEMORY handler for INTEL TDX guest
    A basic handler frame of KVM_MEMORY_ENCRYPT_READ_MEMORY, no
    full implementation of reading memory here because most of
    the code can be shared between reading and writing memory,
    so other reading related code will be added after the sharing
    part of reading/writing is introduced.
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -3001,6 +3001,39 @@ bool tdx_is_vm_type_supported(unsigned long type)
 	return type == KVM_X86_TDX_VM;
 }
 
+static int tdx_guest_memory_access_check(struct kvm *kvm, struct kvm_rw_memory *rw_memory)
+{
+	if (!is_td(kvm))
+		return -EINVAL;
+
+	if (!(to_kvm_tdx(kvm)->attributes & TDX_TD_ATTRIBUTE_DEBUG))
+		return -EINVAL;
+
+	if (!is_hkid_assigned(to_kvm_tdx(kvm)))
+		return -EINVAL;
+
+	if (rw_memory->len == 0 || !rw_memory->ubuf)
+		return -EINVAL;
+
+	if (rw_memory->addr + rw_memory->len < rw_memory->addr)
+		return -EINVAL;
+
+	return 0;
+}
+
+static int tdx_read_guest_memory(struct kvm *kvm, struct kvm_rw_memory *rw_memory)
+{
+	int ret;
+
+	rw_memory->addr = rw_memory->addr & ~gfn_to_gpa(kvm_gfn_shared_mask(kvm));
+
+	/*TODO: actual reading function */
+	ret = tdx_guest_memory_access_check(kvm, rw_memory);
+	rw_memory->len = 0;
+
+	return ret;
+}
+
 int __init tdx_hardware_setup(struct kvm_x86_ops *x86_ops)
 {
 	int max_pkgs;
@@ -3062,6 +3095,7 @@ int __init tdx_hardware_setup(struct kvm_x86_ops *x86_ops)
 	x86_ops->zap_private_spte = tdx_sept_zap_private_spte;
 	x86_ops->unzap_private_spte = tdx_sept_unzap_private_spte;
 	x86_ops->drop_private_spte = tdx_sept_drop_private_spte;
+	x86_ops->mem_enc_read_memory = tdx_read_guest_memory;
 
 	return 0;
 }

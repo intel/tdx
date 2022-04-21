d961bd294ae4e7cb6ccc4e34050822b43cd16e85
Yuan Yao <yuan.yao@intel.com>
2022-04-21 16:50:06 +0800

    KVM: TDX: Support get/set IDT for DEBUG TDX guest
    Set IDT may lead TDX guest crash, it's caller's duty
    to avoid this happen. Getting IDT is useful for
    guest issue debugging in early stage.
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -184,6 +184,8 @@ void tdx_set_rflags(struct kvm_vcpu *vcpu, unsigned long rflags);
 u64 tdx_get_segment_base(struct kvm_vcpu *vcpu, int seg);
 void tdx_get_segment(struct kvm_vcpu *vcpu, struct kvm_segment *var, int seg);
 void tdx_get_cs_db_l_bits(struct kvm_vcpu *vcpu, int *db, int *l);
+void tdx_get_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt);
+void tdx_set_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt);
 
 int tdx_vm_ioctl(struct kvm *kvm, void __user *argp);
 int tdx_vcpu_ioctl(struct kvm_vcpu *vcpu, void __user *argp);
@@ -240,6 +242,8 @@ static inline u64 tdx_get_segment_base(struct kvm_vcpu *vcpu, int seg) { return 
 static inline void tdx_get_segment(struct kvm_vcpu *vcpu, struct kvm_segment *var,
 				   int seg) {}
 static inline void tdx_get_cs_db_l_bits(struct kvm_vcpu *vcpu, int *db, int *l) {}
+static inline void tdx_get_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt) {}
+static inline void tdx_set_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt) {}
 
 static inline int tdx_vm_ioctl(struct kvm *kvm, void __user *argp) { return -EOPNOTSUPP; }
 static inline int tdx_vcpu_ioctl(struct kvm_vcpu *vcpu, void __user *argp) { return -EOPNOTSUPP; }

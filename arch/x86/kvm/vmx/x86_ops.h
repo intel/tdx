b5654ffdc2e61d7ec4fbd29235705ce22343377a
Yuan Yao <yuan.yao@intel.com>
2022-04-21 16:50:13 +0800

    KVM: TDX: Basic exception injection support for DEBUG TDX guest
    Add exception injection because we need following the common
    kvm_queue_exception() for exception injection to DEBUG TDX
    guest in instruction emulation and exception interception.
    
    Eeven the real injection is not supported by TDX 1.0, but
    fortunately TDX 1.0 spec defines SEAMCALL for this already.
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -190,6 +190,7 @@ void tdx_get_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt);
 void tdx_set_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt);
 void tdx_get_gdt(struct kvm_vcpu *vcpu, struct desc_ptr *dt);
 void tdx_set_gdt(struct kvm_vcpu *vcpu, struct desc_ptr *dt);
+void tdx_inject_exception(struct kvm_vcpu *vcpu);
 
 int tdx_vm_ioctl(struct kvm *kvm, void __user *argp);
 int tdx_vcpu_ioctl(struct kvm_vcpu *vcpu, void __user *argp);
@@ -252,6 +253,7 @@ static inline void tdx_get_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt) {}
 static inline void tdx_set_idt(struct kvm_vcpu *vcpu, struct desc_ptr *dt) {}
 static inline void tdx_get_gdt(struct kvm_vcpu *vcpu, struct desc_ptr *dt) {}
 static inline void tdx_set_gdt(struct kvm_vcpu *vcpu, struct desc_ptr *dt) {}
+static inline void tdx_inject_exception(struct kvm_vcpu *vcpu) {}
 
 static inline int tdx_vm_ioctl(struct kvm *kvm, void __user *argp) { return -EOPNOTSUPP; }
 static inline int tdx_vcpu_ioctl(struct kvm_vcpu *vcpu, void __user *argp) { return -EOPNOTSUPP; }

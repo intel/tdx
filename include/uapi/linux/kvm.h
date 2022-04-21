b69c36c4103119eff0ad472e4a6a42d650d687a4
Yuan Yao <yuan.yao@intel.com>
2022-04-21 16:49:48 +0800

    KVM: Introduce the KVM_MEMORY_ENCRYPT_{READ,WRITE}_MEMORY VM ioctl
    These 2 new VM level ioctls focus on memory read/write from/to
    encrypted guest like INTEL TDX guest or AMD SEV guest for
    debugging purpose. Userspace VMM uses these interface to get
    content of guest memory, e.g. supporing "x"/"xp" commands
    in QEMU monitor or supporting QEMU's guest debugging feature.
    
    Also introduce 2 new interfaces in kvm_x86_ops to handle the
    new ioctls.
    
    int (*mem_enc_read_memory)(struct kvm *kvm,
                               struct kvm_rw_memory *argp);
    int (*mem_enc_write_memory)(struct kvm *kvm,
                                struct kvm_rw_memory *argp);
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -1270,6 +1270,8 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_DIRTY_LOG_RING_ACQ_REL 223
 #define KVM_CAP_S390_PROTECTED_ASYNC_DISABLE 224
 #define KVM_CAP_DIRTY_LOG_RING_WITH_BITMAP 225
+#define KVM_CAP_ENCRYPT_MEMORY_DEBUG 226
+
 /* TODO: remove this workaround to avoid CAP number conflict in the upstream. */
 #define KVM_CAP_MEMORY_ATTRIBUTES 500
 #define KVM_CAP_VM_TYPES 1000
@@ -1954,6 +1956,10 @@ struct kvm_xen_vcpu_attr {
 #define KVM_XEN_VCPU_ATTR_TYPE_VCPU_ID		0x6
 #define KVM_XEN_VCPU_ATTR_TYPE_TIMER		0x7
 #define KVM_XEN_VCPU_ATTR_TYPE_UPCALL_VECTOR	0x8
+
+/* Read/write encrypted guest memory, for guest debugging support in QEMU*/
+#define KVM_MEMORY_ENCRYPT_READ_MEMORY   _IOWR(KVMIO, 0xcc, struct kvm_rw_memory)
+#define KVM_MEMORY_ENCRYPT_WRITE_MEMORY  _IOWR(KVMIO, 0xcd, struct kvm_rw_memory)
 
 /* Secure Encrypted Virtualization command */
 enum sev_cmd_id {

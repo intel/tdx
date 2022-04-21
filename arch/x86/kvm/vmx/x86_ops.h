f87f10d3111b07cd0361f242039b4ba5469c8f7c
Yuan Yao <yuan.yao@intel.com>
2022-04-21 16:50:18 +0800

    KVM: TDX: Enable DR context switch for DEBUG TDX guest
    Do DR context switch following frame in x86.c, excepts
    the DR6. DR6 loading happens only when we had correct
    DR6 in KVM, by #DB interception or set via IOCTL or other
    ways. A new variable vcpu_tdx::dr6 is used to save the
    original DR6 after retrieved from TDX guest, DR6 loading
    happens only when:
    
      vcpu_tdx.dr6 != vcpu->arch.dr6
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -204,6 +204,7 @@ int tdx_sept_tlb_remote_flush_with_range(struct kvm *kvm,
 void tdx_load_mmu_pgd(struct kvm_vcpu *vcpu, hpa_t root_hpa, int root_level);
 
 void tdx_load_guest_debug_regs(struct kvm_vcpu *vcpu);
+void tdx_sync_dirty_debug_regs(struct kvm_vcpu *vcpu);
 #else
 static inline int tdx_init(void) { return 0; };
 static inline int tdx_hardware_setup(struct kvm_x86_ops *x86_ops) { return 0; }
@@ -274,6 +275,7 @@ static inline int tdx_sept_tlb_remote_flush_with_range(struct kvm *kvm,
 static inline void tdx_load_mmu_pgd(struct kvm_vcpu *vcpu, hpa_t root_hpa, int root_level) {}
 
 static inline void tdx_load_guest_debug_regs(struct kvm_vcpu *vcpu) {}
+static inline void tdx_sync_dirty_debug_regs(struct kvm_vcpu *vcpu) {}
 #endif
 
 #if defined(CONFIG_INTEL_TDX_HOST) && defined(CONFIG_KVM_SMM)

726ad5f390f07e40b7e9d08518ac1d334a0fb387
Yuan Yao <yuan.yao@intel.com>
2022-04-21 16:50:10 +0800

    KVM: X86: Introduce kvm_x86_ops::get_xcr
    Encrypted guest's XCR is not accessible as normal x86 guest, so
    introduces new callback to allow KVM get it from vendor specific
    implementation of encrypted guest. This also allows userspace VMM
    like QEMU to get correct guest XCR value.
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -4838,6 +4838,7 @@ static struct kvm_x86_ops svm_x86_ops __initdata = {
 	.set_rflags = svm_set_rflags,
 	.get_if_flag = svm_get_if_flag,
 	.get_cr2 = kvm_get_cr2,
+	.get_xcr = kvm_get_xcr,
 
 	.flush_tlb_all = svm_flush_tlb_all,
 	.flush_tlb_current = svm_flush_tlb_current,

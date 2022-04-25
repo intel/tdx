18df3f6e6b7fbf384bbf09c7acd2e87e08fcb92c
Sean Christopherson <sean.j.christopherson@intel.com>
2022-04-25 14:56:08 -0700

    KVM: TDX: x86: Add ioctl to get TDX systemwide parameters
    Implement an ioctl to get system-wide parameters for TDX.  Although the
    function is systemwide, vm scoped mem_enc ioctl works for userspace VMM
    like qemu and device scoped version is not define, re-use vm scoped
    mem_enc.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    ---
    v18:
    - drop the use of tdhsysinfo_struct and TDH.SYS.INFO, use TDH.SYS.RD().
      For that, dynamically allocate/free tdx_info.
    - drop the change of tools/arch/x86/include/uapi/asm/kvm.h.
    
    v14 -> v15:
    - ABI change: added supported_gpaw and reserved area.

--- arch/x86/include/uapi/asm/kvm.h
+++ arch/x86/include/uapi/asm/kvm.h
@@ -604,4 +604,21 @@ struct kvm_tdx_cpuid_config {
 	__u32 edx;
 };
 
+/* supported_gpaw */
+#define TDX_CAP_GPAW_48	(1 << 0)
+#define TDX_CAP_GPAW_52	(1 << 1)
+
+struct kvm_tdx_capabilities {
+	__u64 attrs_fixed0;
+	__u64 attrs_fixed1;
+	__u64 xfam_fixed0;
+	__u64 xfam_fixed1;
+	__u32 supported_gpaw;
+	__u32 padding;
+	__u64 reserved[251];
+
+	__u32 nr_cpuid_configs;
+	struct kvm_tdx_cpuid_config cpuid_configs[];
+};
+
 #endif /* _ASM_X86_KVM_H */

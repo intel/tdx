7649ca429400c22bb04fad7bca15703b134f451f
Sean Christopherson <sean.j.christopherson@intel.com>
2022-04-25 14:56:08 -0700

    [REVERTME] KVM: TDX: x86: Add ioctl to get TDX systemwide parameters
    Implement a system-scoped ioctl to get system-wide parameters for TDX.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/main.c
+++ arch/x86/kvm/vmx/main.c
@@ -1137,6 +1137,7 @@ struct kvm_x86_ops vt_x86_ops __initdata = {
 	.vcpu_deliver_sipi_vector = vt_vcpu_deliver_sipi_vector,
 	.vcpu_deliver_init = vt_vcpu_deliver_init,
 
+	.dev_mem_enc_ioctl = tdx_dev_ioctl,
 	.mem_enc_ioctl = vt_mem_enc_ioctl,
 	.vcpu_mem_enc_ioctl = vt_vcpu_mem_enc_ioctl,
 };

e544cf3ff0371e940b45e9e57e4568a41b0b4c87
Isaku Yamahata <isaku.yamahata@intel.com>
2022-05-04 11:39:53 -0700

    KVM: TDX: Make pmu_intel.c ignore guest TD case
    Because TDX KVM doesn't support PMU yet (it's future work of TDX KVM
    support as another patch series) and pmu_intel.c touches vmx specific
    structure in vcpu initialization, as workaround add dummy structure to
    struct vcpu_tdx and pmu_intel.c can ignore TDX case.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    ---
    v18:
    - Removed unnecessary change to vmx.c which caused kernel warning.

--- arch/x86/kvm/vmx/vmx.h
+++ arch/x86/kvm/vmx/vmx.h
@@ -11,6 +11,7 @@
 #include "capabilities.h"
 #include "../kvm_cache_regs.h"
 #include "posted_intr.h"
+#include "pmu_intel.h"
 #include "vmcs.h"
 #include "vmx_ops.h"
 #include "../cpuid.h"
@@ -91,22 +92,6 @@ union vmx_exit_reason {
 		u32	failed_vmentry		: 1;
 	};
 	u32 full;
-};
-
-struct lbr_desc {
-	/* Basic info about guest LBR records. */
-	struct x86_pmu_lbr records;
-
-	/*
-	 * Emulate LBR feature via passthrough LBR registers when the
-	 * per-vcpu guest LBR event is scheduled on the current pcpu.
-	 *
-	 * The records may be inaccurate if the host reclaims the LBR.
-	 */
-	struct perf_event *event;
-
-	/* True if LBRs are marked as not intercepted in the MSR bitmap */
-	bool msr_passthrough;
 };
 
 /*
@@ -657,21 +642,6 @@ static __always_inline struct kvm_vmx *to_kvm_vmx(struct kvm *kvm)
 static __always_inline struct vcpu_vmx *to_vmx(struct kvm_vcpu *vcpu)
 {
 	return container_of(vcpu, struct vcpu_vmx, vcpu);
-}
-
-static inline struct lbr_desc *vcpu_to_lbr_desc(struct kvm_vcpu *vcpu)
-{
-	return &to_vmx(vcpu)->lbr_desc;
-}
-
-static inline struct x86_pmu_lbr *vcpu_to_lbr_records(struct kvm_vcpu *vcpu)
-{
-	return &vcpu_to_lbr_desc(vcpu)->records;
-}
-
-static inline bool intel_pmu_lbr_is_enabled(struct kvm_vcpu *vcpu)
-{
-	return !!vcpu_to_lbr_records(vcpu)->nr;
 }
 
 void intel_pmu_cross_mapped_check(struct kvm_pmu *pmu);

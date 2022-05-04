b029210eb3de0cee8c7b245cb6a53786312954a4
Isaku Yamahata <isaku.yamahata@intel.com>
2022-05-04 12:42:11 -0700

    KVM: TDX: Remove usage of struct vcpu_vmx from posted_intr.c
    Unify the layout for vcpu_vmx and vcpu_tdx at the top of the structure
    with members needed in posted-intr.c, and introduce a struct vcpu_pi to
    have the common part.  Use vcpu_pi instead of vcpu_vmx to enable TDX
    compatibility with posted_intr.c.
    
    To minimize the diff size, code conversion such as changing vmx->pi_desc
    to vmx->common->pi_desc is avoided.  Instead, compile-time checks are
    added to ensure the layout is as expected.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    ---
    v20:
    - Use vcpu_pi instead vcpu_vmx in pi_wakeup_handler().
    - Update for the wrapper functions for SEAMCALLs. (Sean)
    - Capitalize the first word of the short log; "use" -> "usage";
      posted_interrupt.c should be posted_intr.c
    - Update change log.
    - Remove functional change from the code refactor patch, move into its
      own separate patch. (Chao)

--- arch/x86/kvm/vmx/tdx.h
+++ arch/x86/kvm/vmx/tdx.h
@@ -10,6 +10,8 @@ void tdx_cleanup(void);
 
 extern bool enable_tdx;
 
+#include "irq.h"
+#include "posted_intr.h"
 #include "pmu_intel.h"
 
 struct kvm_tdx {
@@ -74,6 +76,13 @@ union tdx_exit_reason {
 
 struct vcpu_tdx {
 	struct kvm_vcpu	vcpu;
+
+	/* Posted interrupt descriptor */
+	struct pi_desc pi_desc;
+
+	/* Used if this vCPU is waiting for PI notification wakeup. */
+	struct list_head pi_wakeup_list;
+	/* Until here same layout to struct vcpu_pi. */
 
 	unsigned long tdvpr_pa;
 	unsigned long *tdcx_pa;

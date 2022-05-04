6e61fd198e610831c037b3e956bf570c6efafa6f
Isaku Yamahata <isaku.yamahata@intel.com>
2022-05-04 11:39:53 -0700

    KVM: TDX: Make pmu_intel.c ignore guest TD case
    Because TDX KVM doesn't support PMU yet (it's future work of TDX KVM
    support as another patch series) and pmu_intel.c touches vmx specific
    structure in vcpu initialization, as workaround add dummy structure to
    struct vcpu_tdx and pmu_intel.c can ignore TDX case.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/tdx.h
+++ arch/x86/kvm/vmx/tdx.h
@@ -2,9 +2,10 @@
 #ifndef __KVM_X86_TDX_H
 #define __KVM_X86_TDX_H
 
-#include "tdx_ops.h"
-
 #ifdef CONFIG_INTEL_TDX_HOST
+
+#include "pmu_intel.h"
+#include "tdx_ops.h"
 
 struct tdx_td_page {
 	unsigned long pa;
@@ -26,7 +27,12 @@ struct kvm_tdx {
 
 struct vcpu_tdx {
 	struct kvm_vcpu	vcpu;
-	/* TDX specific members follow. */
+
+	/*
+	 * Dummy to make pmu_intel not corrupt memory.
+	 * TODO: Support PMU for TDX.  Future work.
+	 */
+	struct lbr_desc lbr_desc;
 };
 
 static inline bool is_td(struct kvm *kvm)

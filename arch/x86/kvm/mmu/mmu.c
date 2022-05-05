intel/tdx

commit 9775bace165ddfc08792b2dd7f75f7c2db571543

KVM: tdp_mmu, TDX: pin pages when faultin
So far page is pinned down inside tdp_mmu callback for TDX by
pin_user_page_fast_only() which can fail.  But the callback can't return
error.  Pin pages when faultin before calling tdp_mmu fault handler for
private GFN.  If kvm page fault is successfully, don't unpin the PFN because
KVM TDX doesn't support page migration yet.

Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

arch/x86/kvm/mmu/mmu.c
@@ -4246,8 +4246,8 @@ static bool kvm_arch_setup_async_pf(struct kvm_vcpu *vcpu, gpa_t cr2_or_gpa,
  *          path
  *        - *r is invalid
  */
-static bool kvm_faultin_pfn_private(struct kvm_vcpu *vcpu,
-				    struct kvm_page_fault *fault, int *r)
+static bool kvm_faultin_pfn_private_slot(struct kvm_vcpu *vcpu,
+					 struct kvm_page_fault *fault, int *r)
 {
 	int order;
 	unsigned int flags = 0;
@@ -4290,6 +4290,33 @@ static bool kvm_faultin_pfn_private(struct kvm_vcpu *vcpu,
 	fault->pfn = -1;
 	*r = -1;
 	return true;
+}
+
+static bool kvm_faultin_pfn_private_gfn(struct kvm_vcpu *vcpu,
+					struct kvm_page_fault *fault, int *r)
+{
+	hva_t hva = gfn_to_hva_memslot(fault->slot, fault->gfn);
+	struct page *page[1];
+	unsigned int flags;
+	int npages;
+
+	fault->map_writable = false;
+	fault->pfn = KVM_PFN_ERR_FAULT;
+	*r = -1;
+	if (hva == KVM_HVA_ERR_RO_BAD || hva == KVM_HVA_ERR_BAD)
+		return true;
+
+	/* TDX allows only RWX.  Read-only isn't supported. */
+	WARN_ON_ONCE(!fault->write);
+	flags = FOLL_WRITE | FOLL_LONGTERM;
+
+	npages = pin_user_pages_fast(hva, 1, flags, page);
+	if (npages != 1)
+		return true;
+
+	fault->map_writable = true;
+	fault->pfn = page_to_pfn(page[0]);
+	return false;
 }
 
 /*
@@ -4338,8 +4365,11 @@ static bool kvm_faultin_pfn(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault,
 		}
 	}
 
-	if (kvm_faultin_pfn_private(vcpu, fault, r))
+	if (kvm_faultin_pfn_private_slot(vcpu, fault, r))
 		return *r != RET_PF_FIXED;
+
+	if (kvm_is_private_gpa(vcpu->kvm, fault->addr))
+		return kvm_faultin_pfn_private_gfn(vcpu, fault, r);
 
 	async = false;
 	fault->pfn = __gfn_to_pfn_memslot(slot, fault->gfn, false, &async,
@@ -4399,15 +4429,20 @@ static bool is_page_fault_stale(struct kvm_vcpu *vcpu,
 	       mmu_notifier_retry_hva(vcpu->kvm, mmu_seq, fault->hva);
 }
 
-void kvm_mmu_release_fault(struct kvm_page_fault *fault)
+void kvm_mmu_release_fault(struct kvm *kvm, struct kvm_page_fault *fault, int r)
 {
 	if (is_error_noslot_pfn(fault->pfn) || kvm_is_reserved_pfn(fault->pfn))
 		return;
 
 	if (fault->is_private_pfn)
 		kvm_memfile_put_pfn(fault->slot, fault->pfn);
-	else
-		kvm_release_pfn_clean(fault->pfn);
+	else {
+		if (kvm_is_private_gpa(kvm, fault->addr)) {
+			if (r != RET_PF_FIXED)
+				unpin_user_page(pfn_to_page(fault->pfn));
+		} else
+			kvm_release_pfn_clean(fault->pfn);
+	}
 }
 
 static int direct_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
@@ -4465,7 +4500,7 @@ out_unlock:
 	else
 		write_unlock(&vcpu->kvm->mmu_lock);
 
-	kvm_mmu_release_fault(fault);
+	kvm_mmu_release_fault(vcpu->kvm, fault, r);
 	return r;
 }
 

arch/x86/kvm/mmu/mmu_internal.h
@@ -229,7 +229,7 @@ static inline int kvm_mmu_link_private_sp(struct kvm *kvm,
 }
 #endif
 
-void kvm_mmu_release_fault(struct kvm_page_fault *fault);
+void kvm_mmu_release_fault(struct kvm *kvm, struct kvm_page_fault *fault, int r);
 
 static inline bool kvm_mmu_page_ad_need_write_protect(struct kvm_mmu_page *sp)
 {

arch/x86/kvm/mmu/paging_tmpl.h
@@ -906,7 +906,7 @@ static int FNAME(page_fault)(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault
 
 out_unlock:
 	write_unlock(&vcpu->kvm->mmu_lock);
-	kvm_mmu_release_fault(fault);
+	kvm_mmu_release_fault(vcpu->kvm, fault, r);
 	return r;
 }
 

arch/x86/kvm/vmx/tdx.c
@@ -1585,8 +1585,14 @@ static void tdx_pin_gfn(struct kvm *kvm, gfn_t gfn, kvm_pfn_t pfn)
 	hva = gfn_to_hva(kvm, gfn);
 	WARN_ON(hva == KVM_HVA_ERR_BAD);
 	WARN_ON(hva == KVM_HVA_ERR_RO_BAD);
-	npinned = pin_user_pages_fast_only(
-		hva, 1, FOLL_WRITE | FOLL_LONGTERM, &page);
+
+	/*
+	 * This function is called via mmu_notifier whose caller locked
+	 * mm.mmap_sem.
+	 */
+	lockdep_assert_held(&current->mm->mmap_lock);
+	npinned = pin_user_pages(
+		hva, 1, FOLL_WRITE | FOLL_LONGTERM, &page, NULL);
 	WARN_ON(npinned != 1);
 	WARN_ON(page_to_pfn(page) != pfn);
 }
@@ -1624,8 +1630,10 @@ static void __tdx_sept_set_private_spte(struct kvm *kvm, gfn_t gfn,
 	if (KVM_BUG_ON(level != PG_LEVEL_4K, kvm))
 		return;
 
-	/* Pin the page, TDX KVM doesn't yet support page migration. */
-	tdx_pin_gfn(kvm, gfn, pfn);
+	/*
+	 * The caller pinned this pfn for us and don't unpin when success.
+	 * See kvm_faultin_pfn_private_gfn() and kvm_mmu_release_fault().
+	 */
 
 	/* Build-time faults are induced and handled via TDH_MEM_PAGE_ADD. */
 	if (likely(is_td_finalized(kvm_tdx))) {

commit 402cfffa29d485ada5a679ccb6573dfbd78c7978

KVM: mmu: UMP: keep pin for private slot when fault is resolved
TDX doesn't page migration yet, keep pinning of page from KVM private
memslot when kvm page fault is successfully resolved.

Fixes: 9775bace165d ("KVM: tdp_mmu, TDX: pin pages when faultin")
Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

arch/x86/kvm/mmu/mmu.c
@@ -4514,7 +4514,8 @@ void kvm_mmu_release_fault(struct kvm *kvm, struct kvm_page_fault *fault, int r)
 		return;
 
 	if (fault->is_private_pfn) {
-		kvm_memfile_put_pfn(fault->slot, fault->pfn);
+		if (r != RET_PF_FIXED)
+			kvm_memfile_put_pfn(fault->slot, fault->pfn);
 	} else {
 		if (fault->is_private) {
 			if (r != RET_PF_FIXED)
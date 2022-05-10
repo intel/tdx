4e82bdb6ea06561d7633359a58ddda8e1e8b937b
Zhiming Hu <zhiming.hu@intel.com>
2022-05-10 09:15:30 -0400

    x86/tdx: Register TDX host key IDs to cgroup misc controller
    TDX host key IDs (HKID) are limit resources in a machine, there is a
    requirement to limit end-user to abuse TD HKIDs by launching TD VM.  This
    feature provides machine owner to limit end-user to apply HKIDs under
    machine owner's control.
    
    The cgroup v2 miscellaneous subsystem was introduced to control the
    resource of AMD SEV & SEV-ES ASIDs.  By following it, introduce tdx hkid
    resource and adapt to TDX KVM.
    
    Signed-off-by: Zhiming Hu <zhiming.hu@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -157,6 +157,7 @@ bool tdx_is_private_mem(unsigned long phys);
  * id for cache flush unlike other TD-related pages.
  */
 extern u32 tdx_global_keyid __ro_after_init;
+u32 tdx_get_nr_guest_keyids(void);
 int tdx_guest_keyid_alloc(void);
 void tdx_guest_keyid_free(int keyid);
 
@@ -179,6 +180,7 @@ static inline void tdx_reset_memory(void) { }
 static inline bool tdx_is_private_mem(unsigned long phys) { return false; }
 static inline u64 __seamcall(u64 op, u64 rcx, u64 rdx, u64 r8, u64 r9,
 			     struct tdx_module_output *out) { return TDX_SEAMCALL_UD; };
+static inline u32 tdx_get_nr_guest_keyids(void) { return 0; }
 static inline int tdx_guest_keyid_alloc(void) { return -EOPNOTSUPP; }
 static inline void tdx_guest_keyid_free(int keyid) { }
 #endif	/* CONFIG_INTEL_TDX_HOST */

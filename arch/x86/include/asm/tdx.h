114dcf9b97f0874e39e073db6fdf399e6d6b83a7
Kai Huang <kai.huang@intel.com>
2022-05-19 14:39:40 +1200

    x86/virt/tdx: Implement functions to make SEAMCALL
    TDX introduces a new CPU mode: Secure Arbitration Mode (SEAM).  This
    mode runs only the TDX module itself or other code to load the TDX
    module.
    
    The host kernel communicates with SEAM software via a new SEAMCALL
    instruction.  This is conceptually similar to a guest->host hypercall,
    except it is made from the host to SEAM software instead.
    
    The TDX module defines a set of SEAMCALL leaf functions to allow the
    host to initialize it, and to create and run protected VMs.  SEAMCALL
    leaf functions use an ABI different from the x86-64 system-v ABI.
    Instead, they share the same ABI with the TDCALL leaf functions.
    
    Implement a function __seamcall() to allow the host to make SEAMCALL
    to SEAM software using the TDX_MODULE_CALL macro which is the common
    assembly for both SEAMCALL and TDCALL.
    
    SEAMCALL instruction causes #GP when SEAMRR isn't enabled, and #UD when
    CPU is not in VMX operation.  The current TDX_MODULE_CALL macro doesn't
    handle any of them.  There's no way to check whether the CPU is in VMX
    operation or not.
    
    Initializing the TDX module is done at runtime on demand, and it depends
    on the caller to ensure CPU is in VMX operation before making SEAMCALL.
    To avoid getting Oops when the caller mistakenly tries to initialize the
    TDX module when CPU is not in VMX operation, extend the TDX_MODULE_CALL
    macro to handle #UD (and also #GP, which can theoretically still happen
    when TDX isn't actually enabled by the BIOS, i.e. due to BIOS bug).
    
    Introduce two new TDX error codes for #UD and #GP respectively so the
    caller can distinguish.  Also, Opportunistically put the new TDX error
    codes and the existing TDX_SEAMCALL_VMFAILINVALID into INTEL_TDX_HOST
    Kconfig option as they are only used when it is on.
    
    As __seamcall() can potentially return multiple error codes, besides the
    actual SEAMCALL leaf function return code, also introduce a wrapper
    function seamcall() to convert the __seamcall() error code to the kernel
    error code, so the caller doesn't need to duplicate the code to check
    return value of __seamcall() and return kernel error code accordingly.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -8,6 +8,10 @@
 #include <asm/ptrace.h>
 #include <asm/shared/tdx.h>
 
+#ifdef CONFIG_INTEL_TDX_HOST
+
+#include <asm/trapnr.h>
+
 /*
  * SW-defined error codes.
  *
@@ -17,6 +21,11 @@
 #define TDX_ERROR			_BITUL(63)
 #define TDX_SW_ERROR			(TDX_ERROR | GENMASK_ULL(47, 40))
 #define TDX_SEAMCALL_VMFAILINVALID	(TDX_SW_ERROR | _UL(0xFFFF0000))
+
+#define TDX_SEAMCALL_GP			(TDX_SW_ERROR | X86_TRAP_GP)
+#define TDX_SEAMCALL_UD			(TDX_SW_ERROR | X86_TRAP_UD)
+
+#endif
 
 #ifndef __ASSEMBLY__
 

add8593d9acfaccbe0bdf4d102b0d3c6b601969d
Kai Huang <kai.huang@intel.com>
2022-05-19 14:39:40 +1200

    x86/virt/tdx: Implement functions to make SEAMCALL
    TDX introduces a new CPU mode: Secure Arbitration Mode (SEAM).  This
    mode runs only the TDX module itself or other code to load the TDX
    module.
    
    The host kernel communicates with SEAM software via a new SEAMCALL
    instruction.  This is conceptually similar to a guest->host hypercall,
    except it is made from the host to SEAM software instead.  The TDX
    module establishes a new SEAMCALL ABI which allows the host to
    initialize the module and to manage VMs.
    
    Add infrastructure to make SEAMCALLs.  The SEAMCALL ABI is very similar
    to the TDCALL ABI and leverages much TDCALL infrastructure.
    
    SEAMCALL instruction causes #GP when TDX isn't BIOS enabled, and #UD
    when CPU is not in VMX operation.  The current TDX_MODULE_CALL macro
    doesn't handle any of them.  There's no way to check whether the CPU is
    in VMX operation or not.
    
    Initializing the TDX module is done at runtime on demand, and it depends
    on the caller to ensure CPU is in VMX operation before making SEAMCALL.
    To avoid getting Oops when the caller mistakenly tries to initialize the
    TDX module when CPU is not in VMX operation, extend the TDX_MODULE_CALL
    macro to handle #UD (and opportunistically #GP since they share the same
    assembly).
    
    Introduce two new TDX error codes for #UD and #GP respectively so the
    caller can distinguish.  Also, Opportunistically put the new TDX error
    codes and the existing TDX_SEAMCALL_VMFAILINVALID into INTEL_TDX_HOST
    Kconfig option as they are only used when it is on.
    
    Any failure during the module initialization is not recoverable for now.
    Print out error message when SEAMCALL failed depending on the error code
    to help the user to understand what went wrong.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/virt/vmx/tdx/tdx.h
+++ arch/x86/virt/vmx/tdx/tdx.h
@@ -2,6 +2,8 @@
 #ifndef _X86_VIRT_TDX_H
 #define _X86_VIRT_TDX_H
 
+#include <linux/types.h>
+
 /*
  * This file contains both macros and data structures defined by the TDX
  * architecture and Linux defined software data structures and functions.
@@ -12,4 +14,12 @@
 /* MSR to report KeyID partitioning between MKTME and TDX */
 #define MSR_IA32_MKTME_KEYID_PARTITIONING	0x00000087
 
+/*
+ * Do not put any hardware-defined TDX structure representations below
+ * this comment!
+ */
+
+struct tdx_module_output;
+u64 __seamcall(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
+	       struct tdx_module_output *out);
 #endif

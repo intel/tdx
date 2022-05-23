c99fe94fccd99c522061ec4679fb3c5f90e1ad20
Kai Huang <kai.huang@intel.com>
2022-05-23 15:14:16 +1200

    x86/virt/tdx: Add skeleton to initialize TDX on demand
    Before the TDX module can be used to create and run TDX guests, it must
    be loaded and properly initialized.  The TDX module is expected to be
    loaded by the BIOS, and to be initialized by the kernel.
    
    TDX introduces a new CPU mode: Secure Arbitration Mode (SEAM).  The host
    kernel communicates with the TDX module via a new SEAMCALL instruction.
    The TDX module implements a set of SEAMCALL leaf functions to allow the
    host kernel to initialize it.
    
    The TDX module can be initialized only once in its lifetime.  Instead
    of always initializing it at boot time, this implementation chooses an
    "on demand" approach to initialize TDX until there is a real need (e.g
    when requested by KVM).  This approach has below pros:
    
    1) It avoids consuming the memory that must be allocated by kernel and
    given to the TDX module as metadata (~1/256th of the TDX-usable memory),
    and also saves the CPU cycles of initializing the TDX module (and the
    metadata) when TDX is not used at all.
    
    2) The TDX module design allows it to be updated while the system is
    running.  The update procedure shares quite a few steps with this "on
    demand" loading mechanism.  The hope is that much of "on demand"
    mechanism can be shared with a future "update" mechanism.  A boot-time
    TDX module implementation would not be able to share much code with the
    update mechanism.
    
    3) Loading the TDX module requires VMX to be enabled.  Currently, only
    the kernel KVM code mucks with VMX enabling.  If the TDX module were to
    be initialized separately from KVM (like at boot), the boot code would
    need to be taught how to muck with VMX enabling and KVM would need to be
    taught how to cope with that.  Making KVM itself responsible for TDX
    initialization lets the rest of the kernel stay blissfully unaware of
    VMX.
    
    Add a placeholder tdx_enable() to initialize the TDX module on demand.
    Use a state machine protected by mutex to make sure the initialization
    will only be done once, as it can be called multiple times (i.e. KVM
    module can be reloaded) and be called concurrently by other kernel
    components in the future.
    
    The TDX module will be initialized in multi-steps defined by the TDX
    module and most of those steps involve a specific SEAMCALL:
    
     1) Get the TDX module information and TDX-capable memory regions
        (TDH.SYS.INFO).
     2) Build the list of TDX-usable memory regions.
     3) Construct a list of "TD Memory Regions" (TDMRs) to cover all
        TDX-usable memory regions.
     4) Pick up one TDX private KeyID as the global KeyID.
     5) Configure the TDMRs and the global KeyID to the TDX module
        (TDH.SYS.CONFIG).
     6) Configure the global KeyID on all packages (TDH.SYS.KEY.CONFIG).
     7) Initialize all TDMRs (TDH.SYS.TDMR.INIT).
    
    Reviewed-by: Chao Gao <chao.gao@intel.com>
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -12,12 +12,24 @@
 #include <linux/init.h>
 #include <linux/errno.h>
 #include <linux/printk.h>
+#include <linux/mutex.h>
 #include <asm/msr.h>
 #include <asm/tdx.h>
 #include "tdx.h"
 
+/* Kernel defined TDX module status during module initialization. */
+enum tdx_module_status_t {
+	TDX_MODULE_UNKNOWN,
+	TDX_MODULE_INITIALIZED,
+	TDX_MODULE_ERROR
+};
+
 static u32 tdx_keyid_start __ro_after_init;
 static u32 nr_tdx_keyids __ro_after_init;
+
+static enum tdx_module_status_t tdx_module_status;
+/* Prevent concurrent attempts on TDX detection and initialization */
+static DEFINE_MUTEX(tdx_module_lock);
 
 /*
  * tdx_keyid_start and nr_tdx_keyids indicate that TDX is uninitialized.
@@ -88,3 +100,84 @@ bool platform_tdx_enabled(void)
 {
 	return !!nr_tdx_keyids;
 }
+
+static int init_tdx_module(void)
+{
+	/*
+	 * TODO:
+	 *
+	 *  - Get TDX module information and TDX-capable memory regions.
+	 *  - Build the list of TDX-usable memory regions.
+	 *  - Construct a list of TDMRs to cover all TDX-usable memory
+	 *    regions.
+	 *  - Pick up one TDX private KeyID as the global KeyID.
+	 *  - Configure the TDMRs and the global KeyID to the TDX module.
+	 *  - Configure the global KeyID on all packages.
+	 *  - Initialize all TDMRs.
+	 *
+	 *  Return error before all steps are done.
+	 */
+	return -EINVAL;
+}
+
+static int __tdx_enable(void)
+{
+	int ret;
+
+	ret = init_tdx_module();
+	if (ret) {
+		pr_err_once("initialization failed (%d)\n", ret);
+		tdx_module_status = TDX_MODULE_ERROR;
+		/*
+		 * Just return one universal error code.
+		 * For now the caller cannot recover anyway.
+		 */
+		return -EINVAL;
+	}
+
+	pr_info_once("TDX module initialized.\n");
+	tdx_module_status = TDX_MODULE_INITIALIZED;
+
+	return 0;
+}
+
+/**
+ * tdx_enable - Enable TDX by initializing the TDX module
+ *
+ * The caller must make sure all online cpus are in VMX operation before
+ * calling this function.
+ *
+ * This function can be called in parallel by multiple callers.
+ *
+ * Return 0 if TDX is enabled successfully, otherwise error.
+ */
+int tdx_enable(void)
+{
+	int ret;
+
+	if (!platform_tdx_enabled()) {
+		pr_err_once("initialization failed: TDX is disabled.\n");
+		return -EINVAL;
+	}
+
+	mutex_lock(&tdx_module_lock);
+
+	switch (tdx_module_status) {
+	case TDX_MODULE_UNKNOWN:
+		ret = __tdx_enable();
+		break;
+	case TDX_MODULE_INITIALIZED:
+		/* Already initialized, great, tell the caller. */
+		ret = 0;
+		break;
+	default:
+		/* Failed to initialize in the previous attempts */
+		ret = -EINVAL;
+		break;
+	}
+
+	mutex_unlock(&tdx_module_lock);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(tdx_enable);

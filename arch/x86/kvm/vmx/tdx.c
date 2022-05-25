6ca9d87bc46143397daf1114ec353500a44bd308
Xiaoyao Li <xiaoyao.li@intel.com>
2022-05-25 14:41:55 +0800

    KVM: TDX: Pass desired page level in err code for page fault handler
    For TDX, EPT violation can happen when TDG.MEM.PAGE.ACCEPT.
    And TDG.MEM.PAGE.ACCEPT contains the desired accept page level of TD guest.
    
    1. KVM can map it with 4KB page while TD guest wants to accept 2MB page.
    
      TD geust will get TDX_PAGE_SIZE_MISMATCH and it should try to accept
      4KB size.
    
    2. KVM can map it with 2MB page while TD guest wants to accept 4KB page.
    
      KVM needs to honor it because
      a) there is no way to tell guest KVM maps it as 2MB size. And
      b) guest accepts it in 4KB size since guest knows some other 4KB page
         in the same 2MB range will be used as shared page.
    
    For case 2, it need to pass desired page level to MMU's
    page_fault_handler. Use bit 29:31 of kvm PF error code for this purpose.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -2360,6 +2360,7 @@ static int tdx_init_mem_region(struct kvm *kvm, struct kvm_tdx_cmd *cmd)
 	struct kvm_tdx_init_mem_region region;
 	struct kvm_vcpu *vcpu;
 	struct page *page;
+	u64 error_code;
 	kvm_pfn_t pfn;
 	int idx, ret = 0;
 
@@ -2412,7 +2413,11 @@ static int tdx_init_mem_region(struct kvm *kvm, struct kvm_tdx_cmd *cmd)
 		kvm_tdx->source_pa = pfn_to_hpa(page_to_pfn(page)) |
 				     (cmd->flags & KVM_TDX_MEASURE_MEMORY_REGION);
 
-		pfn = kvm_mmu_map_tdp_page(vcpu, region.gpa, TDX_SEPT_PFERR,
+		/* TODO: large page support. */
+		error_code = TDX_SEPT_PFERR;
+		error_code |= (PG_LEVEL_4K << PFERR_LEVEL_START_BIT) &
+			PFERR_LEVEL_MASK;
+		pfn = kvm_mmu_map_tdp_page(vcpu, region.gpa, error_code,
 					   PG_LEVEL_4K);
 		if (is_error_noslot_pfn(pfn) || kvm->vm_bugged)
 			ret = -EFAULT;

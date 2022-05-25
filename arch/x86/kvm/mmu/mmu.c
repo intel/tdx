11eb3e8af3feecb959d8d262a89dac54d04f2bf2
Xiaoyao Li <xiaoyao.li@intel.com>
2022-05-25 14:41:57 +0800

    KVM: x86/tdp_mmu: Split the large page when zap leaf
    When TDX enabled, a large page cannot be zapped if it contains mixed
    pages. In this case, it has to split the large page.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -7334,6 +7334,15 @@ static bool linfo_is_mixed(struct kvm_lpage_info *linfo)
 	return linfo->disallow_lpage & KVM_LPAGE_MIXED_FLAG;
 }
 
+bool kvm_mem_attr_is_mixed(struct kvm_memory_slot *slot, gfn_t gfn, int level)
+{
+       struct kvm_lpage_info *linfo = lpage_info_slot(gfn & KVM_HPAGE_MASK(level),
+						      slot, level);
+
+       WARN_ON_ONCE(level == PG_LEVEL_4K);
+       return linfo_is_mixed(linfo);
+}
+
 static void linfo_update_mixed(gfn_t gfn, struct kvm_memory_slot *slot,
 			       int level, bool mixed)
 {

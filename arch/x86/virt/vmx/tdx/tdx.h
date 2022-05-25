f76ab994bb4b70a7361b0572af49b338624086c4
Kai Huang <kai.huang@intel.com>
2022-05-25 17:01:32 +1200

    x86/virt/tdx: Get information about TDX module and TDX-capable memory
    Start to transit out the "multi-steps" of initializing the TDX module as
    listed in the skeleton infrastructure.  Do the first step to get the TDX
    module information and the TDX-capable memory regions.
    
    TDX provides increased levels of memory confidentiality and integrity.
    This requires special hardware support for features like memory
    encryption and storage of memory integrity checksums.  Not all memory
    satisfies these requirements.
    
    As a result, TDX introduced the concept of a "Convertible Memory Region"
    (CMR).  During boot, the firmware builds a list of all of the memory
    ranges which can provide the TDX security guarantees.
    
    CMRs tell the kernel which memory is TDX compatible.  The kernel takes
    CMRs (plus a little more metadata) and constructs "TD Memory Regions"
    (TDMRs).  TDMRs let the kernel grant TDX protections to some or all of
    the CMR areas.
    
    The TDX module information tells the kernel TDX module properties such
    as metadata entry size, the maximum number of TDMRs, and the maximum
    number of reserved areas per TDMR that the module allows, etc.
    
    The list of CMRs, along with the TDX module information, is available to
    the kernel by querying the TDX module.
    
    For now, both the TDX module information and CMRs are only used during
    the module initialization, so declare them as local.  However, they are
    1024 bytes and 512 bytes respectively.  Putting them to the stack
    exceeds the default "stack frame size" that the kernel assumes as safe,
    and the compiler yields a warning about this.  Add a kernel build flag
    to extend the safe stack size to 4K for tdx.c to silence the warning --
    the initialization function is only called once so it's safe to have a
    4K stack.
    
    Note not all members in the 1024 bytes TDX module information are used
    (even by the KVM).
    
    Reviewed-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/virt/vmx/tdx/tdx.h
+++ arch/x86/virt/vmx/tdx/tdx.h
@@ -3,6 +3,8 @@
 #define _X86_VIRT_TDX_H
 
 #include <linux/types.h>
+#include <linux/stddef.h>
+#include <linux/compiler_attributes.h>
 
 /*
  * This file contains both macros and data structures defined by the TDX
@@ -13,6 +15,80 @@
 
 /* MSR to report KeyID partitioning between MKTME and TDX */
 #define MSR_IA32_MKTME_KEYID_PARTITIONING	0x00000087
+
+/*
+ * TDX module SEAMCALL leaf functions
+ */
+#define TDH_SYS_INFO		32
+
+struct cmr_info {
+	u64	base;
+	u64	size;
+} __packed;
+
+#define MAX_CMRS			32
+#define CMR_INFO_ARRAY_ALIGNMENT	512
+
+struct cpuid_config {
+	u32	leaf;
+	u32	sub_leaf;
+	u32	eax;
+	u32	ebx;
+	u32	ecx;
+	u32	edx;
+} __packed;
+
+#define DECLARE_PADDED_STRUCT(type, name, size, alignment)	\
+	struct type##_padded {					\
+		union {						\
+			struct type name;			\
+			u8 padding[size];			\
+		};						\
+	} name##_padded __aligned(alignment)
+
+#define PADDED_STRUCT(name)	(name##_padded.name)
+
+#define TDSYSINFO_STRUCT_SIZE		1024
+#define TDSYSINFO_STRUCT_ALIGNMENT	1024
+
+/*
+ * The size of this structure itself is flexible.  The actual structure
+ * passed to TDH.SYS.INFO must be padded to TDSYSINFO_STRUCT_SIZE and be
+ * aligned to TDSYSINFO_STRUCT_ALIGNMENT using DECLARE_PADDED_STRUCT().
+ */
+struct tdsysinfo_struct {
+	/* TDX-SEAM Module Info */
+	u32	attributes;
+	u32	vendor_id;
+	u32	build_date;
+	u16	build_num;
+	u16	minor_version;
+	u16	major_version;
+	u8	reserved0[14];
+	/* Memory Info */
+	u16	max_tdmrs;
+	u16	max_reserved_per_tdmr;
+	u16	pamt_entry_size;
+	u8	reserved1[10];
+	/* Control Struct Info */
+	u16	tdcs_base_size;
+	u8	reserved2[2];
+	u16	tdvps_base_size;
+	u8	tdvps_xfam_dependent_size;
+	u8	reserved3[9];
+	/* TD Capabilities */
+	u64	attributes_fixed0;
+	u64	attributes_fixed1;
+	u64	xfam_fixed0;
+	u64	xfam_fixed1;
+	u8	reserved4[32];
+	u32	num_cpuid_config;
+	/*
+	 * The actual number of CPUID_CONFIG depends on above
+	 * 'num_cpuid_config'.
+	 */
+	DECLARE_FLEX_ARRAY(struct cpuid_config, cpuid_configs);
+} __packed;
 
 /*
  * Do not put any hardware-defined TDX structure representations below

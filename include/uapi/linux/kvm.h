7f93021073e2f35490f7d0d0ac1e2fc7539c9da7
Isaku Yamahata <isaku.yamahata@intel.com>
2022-05-25 13:24:36 -0700

    [REVERTME] bump KVM_EXIT_MEMORY_FAULT to avoid merge conflict
    To avoid merge conflict with the upstream, bump KVM_EXIT_MEMORY_FAULT
    instead of using the next unused value.
    TODO: once the patch is upstreamed, remove this patch and correct
    KVM_EXIT_MEMORY_FAULT.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -348,9 +348,9 @@ struct kvm_tdx_exit {
 #define KVM_EXIT_X86_BUS_LOCK     33
 #define KVM_EXIT_XEN              34
 #define KVM_EXIT_RISCV_SBI        35
-#define KVM_EXIT_MEMORY_FAULT     36
 /* WORKAROUND to avoid conflict with upstream. */
 #define KVM_EXIT_TDX              50
+#define KVM_EXIT_MEMORY_FAULT     100
 
 /* For KVM_EXIT_INTERNAL_ERROR */
 /* Emulate instruction failed. */

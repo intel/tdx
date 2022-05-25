395684b125e12839ba916f7ac7cd39a599ee487e
Kai Huang <kai.huang@intel.com>
2022-05-25 17:01:32 +1200

    x86/virt/tdx: Get information about TDX module and TDX-capable memory
    Start to transit out the "multi-steps" to initialize the TDX module.
    
    TDX provides increased levels of memory confidentiality and integrity.
    This requires special hardware support for features like memory
    encryption and storage of memory integrity checksums.  Not all memory
    satisfies these requirements.
    
    As a result, TDX introduced the concept of a "Convertible Memory Region"
    (CMR).  During boot, the firmware builds a list of all of the memory
    ranges which can provide the TDX security guarantees.
    
    CMRs tell the kernel which memory is TDX compatible.  The kernel takes
    CMRs (plus a little more metadata) and constructs "TD Memory Regions"
    (TDMRs).  TDMRs let the kernel grant TDX protections to some or all of
    the CMR areas.
    
    The TDX module also reports necessary information to let the kernel
    build TDMRs and run TDX guests in structure 'tdsysinfo_struct'.  The
    list of CMRs, along with the TDX module information, is available to
    the kernel by querying the TDX module.
    
    As a preparation to construct TDMRs, get the TDX module information and
    the list of CMRs.  Print out CMRs to help user to decode which memory
    regions are TDX convertible.
    
    The 'tdsysinfo_struct' is fairly large (1024 bytes) and contains a lot
    of info about the TDX module.  Fully define the entire structure, but
    only use the fields necessary to build the TDMRs and pr_info() some
    basics about the module.  The rest of the fields will get used by KVM.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Reviewed-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/virt/vmx/tdx/tdx.h
+++ arch/x86/virt/vmx/tdx/tdx.h
@@ -3,6 +3,8 @@
 #define _X86_VIRT_TDX_H
 
 #include <linux/types.h>
+#include <linux/stddef.h>
+#include <linux/compiler_attributes.h>
 
 /*
  * This file contains both macros and data structures defined by the TDX
@@ -19,8 +21,66 @@
 /*
  * TDX module SEAMCALL leaf functions
  */
+#define TDH_SYS_INFO		32
 #define TDH_SYS_INIT		33
 #define TDH_SYS_LP_INIT		35
+
+struct cmr_info {
+	u64	base;
+	u64	size;
+} __packed;
+
+#define MAX_CMRS	32
+
+struct cpuid_config {
+	u32	leaf;
+	u32	sub_leaf;
+	u32	eax;
+	u32	ebx;
+	u32	ecx;
+	u32	edx;
+} __packed;
+
+#define TDSYSINFO_STRUCT_SIZE		1024
+
+/*
+ * The size of this structure itself is flexible.  The actual structure
+ * passed to TDH.SYS.INFO must be padded to 1024 bytes and be 1204-bytes
+ * aligned.
+ */
+struct tdsysinfo_struct {
+	/* TDX-SEAM Module Info */
+	u32	attributes;
+	u32	vendor_id;
+	u32	build_date;
+	u16	build_num;
+	u16	minor_version;
+	u16	major_version;
+	u8	reserved0[14];
+	/* Memory Info */
+	u16	max_tdmrs;
+	u16	max_reserved_per_tdmr;
+	u16	pamt_entry_size;
+	u8	reserved1[10];
+	/* Control Struct Info */
+	u16	tdcs_base_size;
+	u8	reserved2[2];
+	u16	tdvps_base_size;
+	u8	tdvps_xfam_dependent_size;
+	u8	reserved3[9];
+	/* TD Capabilities */
+	u64	attributes_fixed0;
+	u64	attributes_fixed1;
+	u64	xfam_fixed0;
+	u64	xfam_fixed1;
+	u8	reserved4[32];
+	u32	num_cpuid_config;
+	/*
+	 * The actual number of CPUID_CONFIG depends on above
+	 * 'num_cpuid_config'.
+	 */
+	DECLARE_FLEX_ARRAY(struct cpuid_config, cpuid_configs);
+} __packed;
 
 /*
  * Do not put any hardware-defined TDX structure representations below

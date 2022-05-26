d0123963bce2a8e7a302f20262fc8c6b68a05a45
Yuan Yao <yuan.yao@intel.com>
2022-05-26 11:08:01 +0800

    X86: FPU: Fix unexpected #PF while executing XRSTORS
    The #PF can be triggered by XRSTORS when do component
    initialization with component buffer which not map by page
    table but the corresponding bit is 1 in its XSAVE buffer's
    xcomp_bv, for example:
    
    init_fpstate's xsave.header.xfeature = 0
    init_fpstate's xsave.header.xcomp_bv = 0x80000000000602e7
    XCR0: 0x602e7
    
    Do XRSTORS with init_fpstate causes #PF if the 2 pages
    following it's xsave buffer are not mapped. There're other
    scenarios also have same issue in kernel:
    
    1. The new created (fork then exec) process first time do
       XRSTORS when it returns to user space.
    2. Do XRSTORS with init_fpstate directly.
    3. Some functions uses component buffer pointer from
       init_fpstate. e.g. __raw_xsave_addr(&init_fpstate, AMX
       TILE DATA feature) from __copy_xstate_to_uabi_buf().
    
    The #PF described above won't happen if the corresponding
    component bit in it's xcomp_bv is 0, this patch fixs them
    based on this theory.
    
    For scenario 1, Reset the new created thread's xcomp_bv to
    it's enable feature set after inherited XSAVE buffer from
    init_fpstate, in fpu_reset_fpregs(). Same change also
    applied to fpu_clone() for kernel threads, to make sure
    xcomp_bv aligns with allocated XSAVE buffer size for even
    kernel threads.
    
    For scenario 2 and 3, Enlarge the init_fpstate to support
    all components buffer includes the AMX TILE DATA component,
    this allows us to use init_fpstate with XRSTORS or use
    component buffer pointer from it without potential risk of
    triggering the #PF. I introduces a macro to do this to
    reduce the change size since this is RFC.
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>

--- arch/x86/kernel/fpu/core.c
+++ arch/x86/kernel/fpu/core.c
@@ -41,7 +41,7 @@ struct fpu_state_config fpu_user_cfg __ro_after_init;
  * Represents the initial FPU state. It's mostly (but not completely) zeroes,
  * depending on the FPU hardware format:
  */
-struct fpstate init_fpstate __ro_after_init;
+union fpstate_full init_fpstate_full __ro_after_init;
 
 /* Track in-kernel FPU usage */
 static DEFINE_PER_CPU(bool, in_kernel_fpu);
@@ -552,6 +552,13 @@ static inline void fpu_inherit_perms(struct fpu *dst_fpu)
 	}
 }
 
+static void fpu_reset_fpstate(struct fpstate *fpstate)
+{
+	memcpy(&fpstate->regs, &init_fpstate.regs, init_fpstate_copy_size());
+	if (use_xsave())
+		xstate_init_xcomp_bv(&fpstate->regs.xsave, fpstate->xfeatures);
+}
+
 /* Clone current's FPU state on fork */
 int fpu_clone(struct task_struct *dst, unsigned long clone_flags, bool minimal)
 {
@@ -578,8 +585,7 @@ int fpu_clone(struct task_struct *dst, unsigned long clone_flags, bool minimal)
 	 */
 	if (minimal) {
 		/* Clear out the minimal state */
-		memcpy(&dst_fpu->fpstate->regs, &init_fpstate.regs,
-		       init_fpstate_copy_size());
+		fpu_reset_fpstate(dst_fpu->fpstate);
 		return 0;
 	}
 
@@ -693,7 +699,7 @@ static void fpu_reset_fpregs(void)
 	 * user space as PKRU is eagerly written in switch_to() and
 	 * flush_thread().
 	 */
-	memcpy(&fpu->fpstate->regs, &init_fpstate.regs, init_fpstate_copy_size());
+	fpu_reset_fpstate(fpu->fpstate);
 	set_thread_flag(TIF_NEED_FPU_LOAD);
 	fpregs_unlock();
 }

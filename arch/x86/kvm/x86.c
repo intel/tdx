chao-p/linux

TDX: Use private memory for TDX
The patch is only for internal private memory test and will not be
sent to community together with private memory patchset, instead it
should be sent with TDX patchset if private memory is merged ahead.

Turn on KVM_MEM_PRIVATE for TDX VM and exit to userspace for
tdx_map_gpa for private/shared conversion.

Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>

arch/x86/kvm/x86.c
@@ -12714,6 +12714,11 @@ bool kvm_arch_dirty_log_supported(struct kvm *kvm)
 	return kvm->arch.vm_type != KVM_X86_TDX_VM;
 }
 
+bool kvm_arch_private_memory_supported(struct kvm *kvm)
+{
+	return kvm->arch.vm_type == KVM_X86_TDX_VM;
+}
+
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_entry);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_mmio);

intel/tdx

arch/x86/kvm/x86.c
@@ -13161,6 +13161,11 @@ bool kvm_arch_dirty_log_supported(struct kvm *kvm)
 	return kvm->arch.vm_type != KVM_X86_TDX_VM;
 }
 
+bool kvm_arch_private_mem_supported(struct kvm *kvm)
+{
+	return kvm->arch.vm_type == KVM_X86_TDX_VM;
+}
+
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_entry);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_mmio);
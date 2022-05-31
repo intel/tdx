d06befc63d254a2e0070a6a9f7f1b1bef385e19e
Kai Huang <kai.huang@intel.com>
2022-05-31 22:37:54 +1200

    x86/virt/tdx: Prevent ACPI CPU hotplug and ACPI memory hotplug
    Intel Trust Domain Extensions (TDX) protects guest VMs from malicious
    host and certain physical attacks.  To guarantee the security, TDX
    imposes additional requirements on both CPU and memory.
    
    TDX doesn't work with ACPI CPU hotplug.  During platform boot, MCHECK
    verifies all logical CPUs on all packages are TDX compatible.  Any
    hot-added CPU at runtime is not verified thus cannot support TDX.  And
    TDX requires all boot-time verified CPUs being present during machine's
    runtime, so TDX doesn't support ACPI CPU hot-removal either.
    
    TDX doesn't work with ACPI memory hotplug either.  TDX also provides
    increased levels of memory confidentiality and integrity.  During
    platform boot, MCHECK also verifies all TDX-capable memory regions are
    physically present and meet TDX's security requirements.  Any hot-added
    memory is not verified thus cannot work with TDX.  TDX also assumes all
    TDX-capable memory regions are present during machine's runtime thus it
    doesn't support ACPI memory removal either.
    
    Select ARCH_HAS_CC_PLATFORM when CONFIG_INTEL_TDX_HOST is on.  Set CC
    vendor to CC_VENDOR_INTEL if TDX is enabled by BIOS, and report ACPI CPU
    hotplug and ACPI memory hotplug attributes as disabled to prevent them.
    
    Note TDX does allow CPU to go offline and then to be brought up again, so
    software CPU hotplug attribute is not reported.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/coco/core.c
+++ arch/x86/coco/core.c
@@ -12,11 +12,14 @@
 
 #include <asm/coco.h>
 #include <asm/processor.h>
+#include <asm/cpufeature.h>
+#include <asm/tdx.h>
 
 static enum cc_vendor vendor __ro_after_init;
 static u64 cc_mask __ro_after_init;
 
-static bool intel_cc_platform_has(enum cc_attr attr)
+#ifdef CONFIG_INTEL_TDX_GUEST
+static bool intel_tdx_guest_has(enum cc_attr attr)
 {
 	switch (attr) {
 	case CC_ATTR_GUEST_UNROLL_STRING_IO:
@@ -27,6 +30,33 @@ static bool intel_cc_platform_has(enum cc_attr attr)
 	default:
 		return false;
 	}
+}
+#endif
+
+#ifdef CONFIG_INTEL_TDX_HOST
+static bool intel_tdx_host_has(enum cc_attr attr)
+{
+	switch (attr) {
+	case CC_ATTR_ACPI_CPU_HOTPLUG_DISABLED:
+	case CC_ATTR_ACPI_MEMORY_HOTPLUG_DISABLED:
+		return true;
+	default:
+		return false;
+	}
+}
+#endif
+
+static bool intel_cc_platform_has(enum cc_attr attr)
+{
+#ifdef CONFIG_INTEL_TDX_GUEST
+	if (boot_cpu_has(X86_FEATURE_TDX_GUEST))
+		return intel_tdx_guest_has(attr);
+#endif
+#ifdef CONFIG_INTEL_TDX_HOST
+	if (platform_tdx_enabled())
+		return intel_tdx_host_has(attr);
+#endif
+	return false;
 }
 
 /*

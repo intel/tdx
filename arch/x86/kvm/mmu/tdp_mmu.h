c14b1a2b9d728e9673f912ac5ae871382fca927b
Isaku Yamahata <isaku.yamahata@intel.com>
2022-06-02 14:33:21 -0700

    KVM: x86/tdp_mmu: Fix MapGpa for tdp_mmu
    When zapping GPA range on MapGPA, only zap the range of opposite sharedness.
    When mapping private(or shared) GPA range => zap shared(or private) GPA
    range.  Otherwise with mapgpa(private) when private GPA is already mapped,
    the content of private page is lost.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/mmu/tdp_mmu.h
+++ arch/x86/kvm/mmu/tdp_mmu.h
@@ -15,6 +15,9 @@ __must_check static inline bool kvm_tdp_mmu_get_root(struct kvm_mmu_page *root)
 void kvm_tdp_mmu_put_root(struct kvm *kvm, struct kvm_mmu_page *root,
 			  bool shared);
 
+bool __kvm_tdp_mmu_zap_leafs(struct kvm *kvm, int as_id, gfn_t start,
+			     gfn_t end, bool can_yield, bool flush,
+			     bool drop_private, bool is_private);
 bool kvm_tdp_mmu_zap_leafs(struct kvm *kvm, int as_id, gfn_t start,
 				gfn_t end, bool can_yield, bool flush,
 				bool drop_private);

31c89f5761cb245f9ff75881ab524db1bcd8b5aa
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2022-06-02 21:55:55 +0000

    virt: tdx-guest: Add VERIFYREPORT support
    During the TDX Guest bring-up, the TDX Module records various details
    about the guest (like configuration data, kernel image, TDVF, etc) in
    measurement registers (RTMR or MTMR). This measurement data can be
    used to verify the validity of TDX guest in applications like guest
    attestation, trusted boot implementation, etc. The TDG.MR.TDREPORT
    TDCALL is used to get these measurement data in the format of TDREPORT.
    More details about the attestation process and TDREPORT can be found in
    Documentation/x86/tdx.rst.
    
    To facilitate the TDREPORT verification, the TDX Module provides
    support for TDG.MR.VERIFYREPORT TDCALL, which can be used to verify
    whether the given TDREPORT is generated in the current platform. It
    takes MAC protected REPORTMACSTRUCT data (from TDREPORT) as input and
    verifies whether the MAC ID matches with the current platform. More
    details about the TDG.MR.VERIFYREPORT TDCALL can be found in TDX
    Module 1.5 Specification ,section titled TDG.MR.VERIFYREPORT leaf.
    
    Add IOCTL interface to allow user space use VERIFYREPORT support.
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/include/asm/shared/tdx.h
+++ arch/x86/include/asm/shared/tdx.h
@@ -17,6 +17,7 @@
 #define TDG_MR_REPORT			4
 #define TDG_MEM_PAGE_ACCEPT		6
 #define TDG_VM_WR			8
+#define TDX_VERIFYREPORT		22
 
 /* TDCS fields. To be used by TDG.VM.WR and TDG.VM.RD module calls */
 #define TDCS_NOTIFY_ENABLES		0x9100000000000010

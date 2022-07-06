4ea791277a539f88b6ba7340c42ea1ae9627f85b
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2022-07-06 16:20:06 +0800

    mm/shmem: Support memfile_notifier
    Implement shmem as a memfile_notifier backing store. Essentially it
    interacts with the memfile_notifier feature flags for userspace
    access/page migration/page reclaiming and implements the necessary
    memfile_backing_store callbacks.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>
    Link: https://lore.kernel.org/r/20220706082016.2603916-5-chao.p.peng@linux.intel.com

--- include/linux/shmem_fs.h
+++ include/linux/shmem_fs.h
@@ -9,6 +9,7 @@
 #include <linux/percpu_counter.h>
 #include <linux/xattr.h>
 #include <linux/fs_parser.h>
+#include <linux/memfile_notifier.h>
 
 /* inode in-kernel data */
 
@@ -25,6 +26,7 @@ struct shmem_inode_info {
 	struct simple_xattrs	xattrs;		/* list of xattrs */
 	atomic_t		stop_eviction;	/* hold when working on inode */
 	struct timespec64	i_crtime;	/* file creation time */
+	struct memfile_node	memfile_node;	/* memfile node */
 	struct inode		vfs_inode;
 };
 

33be54a19f72dc8e610315cb9341b6a29ea803dd
Kai Huang <kai.huang@intel.com>
2022-07-25 16:08:49 +1200

    Revert "cc_platform: Add new attribute to prevent ACPI CPU hotplug"
    This reverts commit f4bff93d83814ea1f54494f51ce3e5d954cf0f5b.
    
    This patch is wrong, leading to below error message and incorrect
    handling in acpi_processor_add() during kernel boot on TDX (BIOS)
    enabled platform.  Revert it.
    
    [   30.067725] acpi ACPI0007:00: [BIOS bug]: Platform doesn't support ACPI CPU hotplug.  New CPU ignored.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/coco/core.c
+++ arch/x86/coco/core.c
@@ -20,7 +20,7 @@ static bool intel_cc_platform_has(enum cc_attr attr)
 {
 	switch (attr) {
 	case CC_ATTR_GUEST_UNROLL_STRING_IO:
-	case CC_ATTR_CPU_HOTPLUG_DISABLED:
+	case CC_ATTR_HOTPLUG_DISABLED:
 	case CC_ATTR_GUEST_MEM_ENCRYPT:
 	case CC_ATTR_MEM_ENCRYPT:
 		return true;

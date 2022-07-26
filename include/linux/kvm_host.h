d6709fd26a2ff97f2617b928f8b14ec42b42ab0d
Isaku Yamahata <isaku.yamahata@intel.com>
2022-07-26 09:37:52 -0700

    KVM: x86/mmu: Disallow dirty logging for x86 TDX
    TDX doesn't support dirty logging.  Report dirty logging isn't supported so
    that device model, for example qemu, can properly handle it.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -1476,6 +1476,7 @@ int kvm_arch_post_init_vm(struct kvm *kvm);
 void kvm_arch_pre_destroy_vm(struct kvm *kvm);
 int kvm_arch_create_vm_debugfs(struct kvm *kvm);
 bool kvm_arch_has_private_mem(struct kvm *kvm);
+bool kvm_arch_dirty_log_supported(struct kvm *kvm);
 
 #ifndef __KVM_HAVE_ARCH_VM_ALLOC
 /*

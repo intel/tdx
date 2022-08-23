f95a079a2efbe0c73fcea963b58136d117c21707
Isaku Yamahata <isaku.yamahata@intel.com>
2022-08-23 08:31:52 -0700

    [REVERTME] Make some TDX pages non-present for debug
    To debug the TDX module shutdown, Make some pages passed to TDX module,
    non-present in kernel. pamt, tdr, tdcx, tdvr, tdvpx. Secure EPT page and
    pages for TD aren't made non-present for now.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -26,6 +26,7 @@
 #include <asm/apic.h>
 #include <asm/cpu.h>
 #include <asm/tdx.h>
+#include <asm/set_memory.h>
 #include "tdx.h"
 
 struct tdx_memblock {
@@ -913,6 +914,9 @@ static int tdmr_set_up_pamt(struct tdmr_info *tdmr)
 			nid, &node_online_map);
 	if (!pamt)
 		return -ENOMEM;
+	if (IS_ENABLED(CONFIG_INTEL_TDX_HOST_DEBUG_MEMORY_CORRUPT))
+		set_memory_np((unsigned long)page_to_virt(pamt),
+			      tdmr_pamt_size >> PAGE_SHIFT);
 
 	/*
 	 * Break the contiguous allocation back up into the
@@ -963,6 +967,9 @@ static void tdmr_free_pamt(struct tdmr_info *tdmr)
 	if (WARN_ON_ONCE(!pamt_pfn))
 		return;
 
+	if (IS_ENABLED(CONFIG_INTEL_TDX_HOST_DEBUG_MEMORY_CORRUPT))
+		__set_memory_prot((unsigned long)pfn_to_kaddr(pamt_pfn), pamt_npages,
+				  __pgprot(_PAGE_PRESENT | _PAGE_RW));
 	free_contig_range(pamt_pfn, pamt_npages);
 }
 

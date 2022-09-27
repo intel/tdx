cfc1d142fc37a7ec39e0c31a12b7f27322e64cea
Yuan Yao <yuan.yao@intel.com>
2022-09-27 11:15:46 +0800

    Partly Revert "KVM: TDX: Support private memfd for DEBUG TDX guest"
    This reverts part of
    commit 043661a698fb ("KVM: TDX: Support private memfd for DEBUG TDX guest")
    Keep calling kvm_tdp_mmu_is_page_private from
    kvm_mmu_is_page_private() to simplify tdx_access_guest_memory().
    
    commit 043661a698fb ("KVM: TDX: Support private memfd for DEBUG TDX guest")
    works with the initial version of UPM, but not with the current
    version. The reason is pages in private_fd are fully allocated at
    beginning, but most of them have not been mapped in TDX modules'
    Secure-EPT. So we can't rely on a page is allocated or not in the
    backend to decide its page type(private or shared).
    
    Let's roll back to the more basic way to decide the page type:
    A page is private page if it exists in private page table, the mirror
    of TDX's secure EPT in KVM. This also keeps us out of the changes from
    UPM side.
    
    Signed-off-by: Yuan Yao <yuan.yao@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -3838,7 +3838,6 @@ static int tdx_access_guest_memory(struct kvm *kvm,
 	u32 offset = offset_in_page(gpa);
 	u32 done_len;
 	bool is_private;
-	kvm_pfn_t pfn;
 	int idx;
 	int ret;
 
@@ -3859,9 +3858,8 @@ static int tdx_access_guest_memory(struct kvm *kvm,
 
 	write_lock(&kvm->mmu_lock);
 	ret = kvm_mmu_is_page_private(kvm, memslot, gpa_to_gfn(gpa),
-				      &is_private, &pfn);
+				      &is_private);
 	if (ret) {
-		is_private = false;
 		done_len = 0;
 		goto exit_unlock;
 	}
@@ -3889,8 +3887,6 @@ exit_unlock_srcu:
 
 	if (completed_len)
 		*completed_len = done_len;
-	if (is_private && kvm_slot_can_be_private(memslot))
-		kvm_private_mem_put_pfn(memslot, pfn);
 	return ret;
 }
 

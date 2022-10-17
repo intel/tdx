intel/tdx

commit 592ef1869c09fc4404920d11b5d8f0a21a8879cd

KVM: x86/mmu: Assume guest MMIOs are shared
Current TD guest doesn't invoke MAP_GPA to convert MMIO range to shared
before accessing it. It implies that current TD guest assumes MMIOs are
shared.

When TD tries to access assigned device's MMIO as shared, an EPT violation
is raised first. kvm_mem_is_private() checks the page shared or private
attribute against the access type (shared bit in GPA). Then since no
MAP_GPA is called for the MMIO, KVM thinks the MMIO is private and refuses
shared access and doesn't set up shared EPT. Then KVM returns to TD and TD
just retries and this causes an infinite loop.

Instead of requiring guest to invoke MAP_GPA for MMIOs, assume guest MMIOs
are shared in KVM as well and don't expect explicit calls of MAP_GAP for
guest MMIOs (i.e., GPAs either have no kvm_memory_slot or are backed by
host MMIOs). So, allow shared access to guest MMIOs and move the page type
check after the corresponding pfn is available.

Signed-off-by: Chao Gao <chao.gao@intel.com>

arch/x86/kvm/mmu/mmu.c
@@ -4604,13 +4604,11 @@ static int kvm_faultin_pfn(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 			return RET_PF_EMULATE;
 	}
 
-	if (kvm_gfn_shared_mask(vcpu->kvm) &&
-	    (kvm_mem_is_private(vcpu->kvm, fault->gfn) != fault->is_private)) {
-		kvm_exit_memory_fault(vcpu, fault);
-		return RET_PF_USER;
-	}
-
-	if (kvm_slot_can_be_private(slot) &&
+	/*
+	 * !fault->slot means MMIO.  Don't require explicit GPA conversion for
+	 * MMIO because MMIO is assigned at the boot time.
+	 */
+	if (fault->slot &&
 	    fault->is_private != kvm_mem_is_private(vcpu->kvm, fault->gfn)) {
 		kvm_exit_memory_fault(vcpu, fault);
 		return RET_PF_USER;
@@ -4698,6 +4696,19 @@ static int direct_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault
 	r = kvm_faultin_pfn(vcpu, fault);
 	if (r != RET_PF_CONTINUE)
 		return r;
+
+	if (kvm_gfn_shared_mask(vcpu->kvm) &&
+	    /*
+	     * Don't require explicit GPA conversion for MMIO because MMIO is
+	     * assigned as shared at the boot time.
+	     */
+	    (fault->slot && !kvm_is_mmio_pfn(fault->pfn)) &&
+	    /* private slot case is handled by kvm_faultin_pfn_private() */
+	    !kvm_slot_can_be_private(fault->slot) &&
+	    (kvm_mem_is_private(vcpu->kvm, fault->gfn) != fault->is_private)) {
+		kvm_exit_memory_fault(vcpu, fault);
+		return RET_PF_USER;
+	}
 
 	r = handle_abnormal_pfn(vcpu, fault, ACC_ALL);
 	if (r != RET_PF_CONTINUE)

arch/x86/kvm/mmu/spte.c
@@ -86,7 +86,7 @@ u64 make_mmio_spte(struct kvm_vcpu *vcpu, u64 gfn, unsigned int access)
 	return spte;
 }
 
-static bool kvm_is_mmio_pfn(kvm_pfn_t pfn)
+bool kvm_is_mmio_pfn(kvm_pfn_t pfn)
 {
 	if (pfn_valid(pfn))
 		return !is_zero_pfn(pfn) && PageReserved(pfn_to_page(pfn)) &&

arch/x86/kvm/mmu/spte.h
@@ -505,4 +505,5 @@ u64 kvm_mmu_changed_pte_notifier_make_spte(u64 old_spte, kvm_pfn_t new_pfn);
 void __init kvm_mmu_spte_module_init(void);
 void kvm_mmu_reset_all_pte_masks(void);
 
+bool kvm_is_mmio_pfn(kvm_pfn_t pfn);
 #endif

commit 90d59662bef15401ef3d8dbcf3f031f196323d60

KVM: x86y/tdp_mmu: Check gfn shared mask for KVM_EXIT_MEMORY_FAULT
KVM_EXIT_MEMORY_FAULT is only for TDX case.  Require gfn_shared_mask is
non-zero.

Fixes: 592ef1869c09 ("KVM: x86/mmu: Assume guest MMIOs are shared")
Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

arch/x86/kvm/mmu/mmu.c
@@ -4608,7 +4608,8 @@ static int kvm_faultin_pfn(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 	 * !fault->slot means MMIO.  Don't require explicit GPA conversion for
 	 * MMIO because MMIO is assigned at the boot time.
 	 */
-	if (fault->slot &&
+	if (kvm_gfn_shared_mask(vcpu->kvm) &&
+	    fault->slot &&
 	    fault->is_private != kvm_mem_is_private(vcpu->kvm, fault->gfn)) {
 		kvm_exit_memory_fault(vcpu, fault);
 		return RET_PF_USER;
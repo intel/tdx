c0fb68cbca9ad83d1a9eccf76a1cab415d9fa9dd
Isaku Yamahata <isaku.yamahata@intel.com>
2022-10-27 02:13:40 -0700

    KVM: Add new members to struct kvm_gfn_range to operate on
    Add new members to strut kvm_gfn_range to indicate which mapping
    (private-vs-shared) to operate on.  only_private and only_shared.  Update
    mmu notifier, set memory attributes ioctl or KVM gmem callback to
    initialize them.
    
    It was premature for set_memory_attributes ioctl to call
    kvm_unmap_gfn_range().  Instead, let kvm_arch_ste_memory_attributes()
    handle it and add a new x86 vendor callback to react to memory attribute
    change.  [1]
    
    - If it's from the mmu notifier, zap shared pages only
    - If it's from the KVM gmem, zap private pages only
    - If setting memory attributes, vendor callback checks new attributes
      and make decisions.
      SNP would do nothing and handle it later with gmem callback
      TDX callback would do as follows.
      When it converts pages to shared, zap private pages only.
      When it converts pages to private, zap shared pages only.
    
    TDX needs to know which mapping to operate on.  Shared-EPT vs. Secure-EPT.
    The following sequence to convert the GPA to private doesn't work for TDX
    because the page can already be private.
    
    1) Update memory attributes to private in memory attributes xarray
    2) Zap the GPA range irrespective of private-or-shared.
       Even if the page is already private, zap the entry.
    3) EPT violation on the GPA
    4) Populate the GPA as private
       The page is zeroed, and the guest has to accept the page again.
    
    In step 2, TDX wants to zap only shared pages and skip private ones.
    
    [1] https://lore.kernel.org/all/ZJX0hk+KpQP0KUyB@google.com/
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    Changes v2 -> v3:
    - Drop the KVM_GFN_RANGE flags
    - Updated struct kvm_gfn_range
    - Change kvm_arch_set_memory_attributes() to return bool for flush
    - Added set_memory_attributes x86 op for vendor backends
    - Refined commit message to describe TDX care concretely
    
    Changes v1 -> v2:
    - consolidate KVM_GFN_RANGE_FLAGS_GMEM_{PUNCH_HOLE, RELEASE} into
      KVM_GFN_RANGE_FLAGS_GMEM.
    - Update the commit message to describe TDX more.  Drop SEV_SNP.

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -604,6 +604,8 @@ static __always_inline int __kvm_handle_hva_range(struct kvm *kvm,
 			 */
 			gfn_range.arg.raw = range->arg.raw;
 			gfn_range.may_block = range->may_block;
+			gfn_range.only_private = false;
+			gfn_range.only_shared = true;
 
 			/*
 			 * {gfn(page) | page intersects with [hva_start, hva_end)} =
@@ -2442,6 +2444,8 @@ static __always_inline void kvm_handle_gfn_range(struct kvm *kvm,
 
 	gfn_range.arg.raw = range->arg.raw;
 	gfn_range.may_block = range->may_block;
+	gfn_range.only_private = false;
+	gfn_range.only_shared = false;
 
 	for (i = 0; i < kvm_arch_nr_memslot_as_ids(kvm); i++) {
 		slots = __kvm_memslots(kvm, i);

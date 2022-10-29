0f7e2e90722c518da84746e194cd3ef129b411b7
Isaku Yamahata <isaku.yamahata@intel.com>
2022-10-28 22:15:17 -0700

    KVM: x86/tdp_mmu: Support TDX private mapping for TDP MMU
    Allocate protected page table for private page table, and add hooks to
    operate on protected page table.  This patch adds allocation/free of
    protected page tables and hooks.  When calling hooks to update SPTE entry,
    freeze the entry, call hooks and unfreeze the entry to allow concurrent
    updates on page tables.  Which is the advantage of TDP MMU.  As
    kvm_gfn_shared_mask() returns false always, those hooks aren't called yet
    with this patch.
    
    When the faulting GPA is private, the KVM fault is called private.  When
    resolving private KVM fault, allocate protected page table and call hooks
    to operate on protected page table. On the change of the private PTE entry,
    invoke kvm_x86_ops hook in __handle_changed_spte() to propagate the change
    to protected page table. The following depicts the relationship.
    
      private KVM page fault   |
          |                    |
          V                    |
     private GPA               |     CPU protected EPTP
          |                    |           |
          V                    |           V
     private PT root           |     protected PT root
          |                    |           |
          V                    |           V
       private PT --hook to propagate-->protected PT
          |                    |           |
          \--------------------+------\    |
                               |      |    |
                               |      V    V
                               |    private guest page
                               |
                               |
         non-encrypted memory  |    encrypted memory
                               |
    PT: page table
    
    The existing KVM TDP MMU code uses atomic update of SPTE.  On populating
    the EPT entry, atomically set the entry.  However, it requires TLB
    shootdown to zap SPTE.  To address it, the entry is frozen with the special
    SPTE value that clears the present bit. After the TLB shootdown, the entry
    is set to the eventual value (unfreeze).
    
    For protected page table, hooks are called to update protected page table
    in addition to direct access to the private SPTE. For the zapping case, it
    works to freeze the SPTE. It can call hooks in addition to TLB shootdown.
    For populating the private SPTE entry, there can be a race condition
    without further protection
    
      vcpu 1: populating 2M private SPTE
      vcpu 2: populating 4K private SPTE
      vcpu 2: TDX SEAMCALL to update 4K protected SPTE => error
      vcpu 1: TDX SEAMCALL to update 2M protected SPTE
    
    To avoid the race, the frozen SPTE is utilized.  Instead of atomic update
    of the private entry, freeze the entry, call the hook that update protected
    SPTE, set the entry to the final value.
    
    Support 4K page only at this stage.  2M page support can be done in future
    patches.
    
    Co-developed-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    v14 -> v15:
    - Refined is_private condition check in kvm_tdp_mmu_map().
      Add kvm_gfn_shared_mask() check.
    - catch up for struct kvm_range change

--- arch/x86/kvm/mmu/tdp_mmu.h
+++ arch/x86/kvm/mmu/tdp_mmu.h
@@ -10,7 +10,7 @@
 void kvm_mmu_init_tdp_mmu(struct kvm *kvm);
 void kvm_mmu_uninit_tdp_mmu(struct kvm *kvm);
 
-hpa_t kvm_tdp_mmu_get_vcpu_root_hpa(struct kvm_vcpu *vcpu);
+hpa_t kvm_tdp_mmu_get_vcpu_root_hpa(struct kvm_vcpu *vcpu, bool private);
 
 __must_check static inline bool kvm_tdp_mmu_get_root(struct kvm_mmu_page *root)
 {

e8f609407d5fa6ecb82e103cda250d3f23f53b62
Isaku Yamahata <isaku.yamahata@intel.com>
2022-11-15 04:36:29 -0800

    [REVERTME] KVM: TDX, x86/tdp_mmu: protect TD guest private pages
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -1693,6 +1693,7 @@ static int tdx_sept_drop_private_spte(struct kvm *kvm, gfn_t gfn,
 			       __FILE__, __LINE__, __func__, gfn, level, pfn);
 			return -EIO;
 		}
+		tdx_set_page_present_level(hpa, level);
 		tdx_unpin(kvm, pfn, level);
 		return 0;
 	}
@@ -1710,6 +1711,7 @@ static int tdx_sept_drop_private_spte(struct kvm *kvm, gfn_t gfn,
 		return -EIO;
 	}
 
+	tdx_set_page_present_level(hpa, level);
 	for (i = 0; i < KVM_PAGES_PER_HPAGE(level); i++) {
 		hpa_with_hkid = set_hkid_to_hpa(hpa, (u16)kvm_tdx->hkid);
 		do {

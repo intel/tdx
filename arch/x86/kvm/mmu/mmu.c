18798fdae7627ed1b04c2ff51d4cf9a8fdd0365d
Isaku Yamahata <isaku.yamahata@intel.com>
2022-11-15 04:36:29 -0800

    [REVERTME] KVM: TDX, x86/tdp_mmu: protect TD guest private pages
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -4605,6 +4605,12 @@ static int direct_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault
 	if (r != RET_PF_CONTINUE)
 		return r;
 
+	if (fault->slot && fault->is_private) {
+		int i;
+
+		for (i = 0; i < KVM_PAGES_PER_HPAGE(fault->max_level); i++)
+			kvm_mmu_split_direct_map(pfn_to_page(fault->pfn + i));
+	}
 	r = RET_PF_RETRY;
 	write_lock(&vcpu->kvm->mmu_lock);
 

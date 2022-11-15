867702f832b3900f0d05317ed6b618889713ff75
Isaku Yamahata <isaku.yamahata@intel.com>
2022-11-15 04:15:35 -0800

    [REVERTME] KVM: TDX: protect PAMT
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -33,6 +33,7 @@
 #include <asm/page.h>
 #include <asm/special_insns.h>
 #include <asm/tdx.h>
+#include <asm/set_memory.h>
 #include "tdx.h"
 
 #ifdef CONFIG_SYSFS
@@ -699,6 +700,9 @@ static int tdmr_set_up_pamt(struct tdmr_info *tdmr,
 			nid, &node_online_map);
 	if (!pamt)
 		return -ENOMEM;
+	if (IS_ENABLED(CONFIG_INTEL_TDX_HOST_DEBUG_MEMORY_CORRUPT))
+		set_memory_np((unsigned long)page_to_virt(pamt),
+			      tdmr_pamt_size >> PAGE_SHIFT);
 
 	/*
 	 * Break the contiguous allocation back up into the
@@ -755,14 +759,16 @@ static void tdmr_do_pamt_func(struct tdmr_info *tdmr,
 	(*pamt_func)(pamt_base, pamt_size);
 }
 
-static void free_pamt(unsigned long pamt_base, unsigned long pamt_size)
+static void pamt_free_contig_range(unsigned long base, unsigned long size)
 {
-	free_contig_range(pamt_base >> PAGE_SHIFT, pamt_size >> PAGE_SHIFT);
+	if (IS_ENABLED(CONFIG_INTEL_TDX_HOST_DEBUG_MEMORY_CORRUPT))
+		set_memory_p((unsigned long)__va(base), size >> PAGE_SHIFT);
+	free_contig_range(base >> PAGE_SHIFT, size >> PAGE_SHIFT);
 }
 
 static void tdmr_free_pamt(struct tdmr_info *tdmr)
 {
-	tdmr_do_pamt_func(tdmr, free_pamt);
+	tdmr_do_pamt_func(tdmr, pamt_free_contig_range);
 }
 
 static void tdmrs_free_pamt_all(struct tdmr_info_list *tdmr_list)
@@ -793,6 +799,28 @@ err:
 	return ret;
 }
 
+static void set_tdx_pages_p(unsigned long base, unsigned long size)
+{
+	if (IS_ENABLED(CONFIG_INTEL_TDX_HOST_DEBUG_MEMORY_CORRUPT))
+		set_memory_p((unsigned long)__va(base), size >> PAGE_SHIFT);
+}
+
+static int tdx_pamt_reboot_notifier(struct notifier_block *nb,
+				    unsigned long action,
+				    void *data)
+{
+	int i;
+
+	for (i = 0; i < tdx_tdmr_list.nr_consumed_tdmrs; i++)
+		tdmr_do_pamt_func(tdmr_entry(&tdx_tdmr_list, i), set_tdx_pages_p);
+
+	return NOTIFY_DONE;
+}
+
+static struct notifier_block tdx_pamt_reboot_nb = {
+	.notifier_call = tdx_pamt_reboot_notifier,
+};
+
 /*
  * Convert TDX private pages back to normal by using MOVDIR64B to
  * clear these pages.  Note this function doesn't flush cache of
@@ -802,6 +830,11 @@ static void reset_tdx_pages(unsigned long base, unsigned long size)
 {
 	const void *zero_page = (const void *)page_address(ZERO_PAGE(0));
 	unsigned long phys, end;
+
+	if (system_state != SYSTEM_HALT &&
+	    system_state != SYSTEM_POWER_OFF &&
+	    system_state != SYSTEM_RESTART)
+		set_tdx_pages_p(base, size);
 
 	end = base + size;
 	for (phys = base; phys < end; phys += 64)
@@ -1779,6 +1812,17 @@ static int __init tdx_init(void)
 	tdx_global_keyid = tdx_keyid_start;
 	tdx_guest_keyid_start = tdx_keyid_start + 1;
 	tdx_nr_guest_keyids = nr_tdx_keyids - 1;
+
+	if (boot_cpu_has_bug(X86_BUG_TDX_PW_MCE)) {
+		/*
+		 * Because tdx_memory_shutdown() will access PAMT with movdir64b,
+		 * make the PAMT accesible that requires TLB flush.  TLB
+		 * shootdown requires SMP context. memory_shutdown context
+		 * isn't such context. Do it early with notifier.
+		 */
+		if (IS_ENABLED(CONFIG_INTEL_TDX_HOST_DEBUG_MEMORY_CORRUPT))
+			register_reboot_notifier(&tdx_pamt_reboot_nb);
+	}
 
 	return 0;
 }

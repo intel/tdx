IntelLabs/kafl.linux

selftests: tdx: Test VERIFYREPORT TDX feature
VERIFYREPORT TDCALL is used to verify the validity of REPORTMAC
generated part of TDREPORT. It extracts the MAC ID from the
REPORTMAC and return 0 if the MAC ID matches the current platform.

Generate the TDREPORT and use TDX_CMD_VERIFYREPORT to verify
its correctness.

Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

tools/testing/selftests/tdx/tdx_attest_test.c
@@ -206,4 +206,29 @@ TEST(verify_quote)
 	free(quote_buf);
 }
 
+TEST(verify_reportmac)
+{
+	__u8 reportdata[TDX_REPORTDATA_LEN];
+	struct tdx_verifyreport_req req;
+	struct tdreport tdreport;
+	int devfd;
+
+	devfd = open(devname, O_RDWR | O_SYNC);
+
+	ASSERT_LT(0, devfd);
+
+	/* Get TDREPORT */
+	ASSERT_EQ(0, get_tdreport(devfd, reportdata, &tdreport));
+
+	/* Fill VERIFYREPORT request */
+	req.reportmac	  = (__u64)&tdreport.reportmac;
+	req.rpm_len	  = sizeof(tdreport.reportmac);
+
+	/* Verify reportmac and make sure it is valid */
+	ASSERT_EQ(0, ioctl(devfd, TDX_CMD_VERIFYREPORT, &req));
+
+	ASSERT_EQ(0, close(devfd));
+}
+
+
 TEST_HARNESS_MAIN

intel/tdx

tools/testing/selftests/tdx/tdx_attest_test.c
@@ -209,4 +209,29 @@ TEST(verify_quote)
 	free(quote_buf);
 }
 
+TEST(verify_reportmac)
+{
+	__u8 reportdata[TDX_REPORTDATA_LEN];
+	struct tdx_verifyreport_req req;
+	struct tdreport tdreport;
+	int devfd;
+
+	devfd = open(TDX_GUEST_DEVNAME, O_RDWR | O_SYNC);
+
+	ASSERT_LT(0, devfd);
+
+	/* Get TDREPORT */
+	ASSERT_EQ(0, get_tdreport(devfd, reportdata, &tdreport));
+
+	/* Fill VERIFYREPORT request */
+	req.reportmac	  = (__u64)&tdreport.reportmac;
+	req.rpm_len	  = sizeof(tdreport.reportmac);
+
+	/* Verify reportmac and make sure it is valid */
+	ASSERT_EQ(0, ioctl(devfd, TDX_CMD_VERIFYREPORT, &req));
+
+	ASSERT_EQ(0, close(devfd));
+}
+
+
 TEST_HARNESS_MAIN
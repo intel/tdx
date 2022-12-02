e7449a6566967c8f699d9b06a296439a4a1dc633
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2022-12-02 14:13:39 +0800

    mm: Introduce memfd_restricted system call to create restricted user memory
    Introduce 'memfd_restricted' system call with the ability to create
    memory areas that are restricted from userspace access through ordinary
    MMU operations (e.g. read/write/mmap). The memory content is expected to
    be used through the new in-kernel interface by a third kernel module.
    
    memfd_restricted() is useful for scenarios where a file descriptor(fd)
    can be used as an interface into mm but want to restrict userspace's
    ability on the fd. Initially it is designed to provide protections for
    KVM encrypted guest memory.
    
    Normally KVM uses memfd memory via mmapping the memfd into KVM userspace
    (e.g. QEMU) and then using the mmaped virtual address to setup the
    mapping in the KVM secondary page table (e.g. EPT). With confidential
    computing technologies like Intel TDX, the memfd memory may be encrypted
    with special key for special software domain (e.g. KVM guest) and is not
    expected to be directly accessed by userspace. Precisely, userspace
    access to such encrypted memory may lead to host crash so should be
    prevented.
    
    memfd_restricted() provides semantics required for KVM guest encrypted
    memory support that a fd created with memfd_restricted() is going to be
    used as the source of guest memory in confidential computing environment
    and KVM can directly interact with core-mm without the need to expose
    the memoy content into KVM userspace.
    
    KVM userspace is still in charge of the lifecycle of the fd. It should
    pass the created fd to KVM. KVM uses the new restrictedmem_get_page() to
    obtain the physical memory page and then uses it to populate the KVM
    secondary page table entries.
    
    The userspace restricted memfd can be fallocate-ed or hole-punched
    from userspace. When hole-punched, KVM can get notified through
    invalidate_start/invalidate_end() callbacks, KVM then gets chance to
    remove any mapped entries of the range in the secondary page tables.
    
    Machine check can happen for memory pages in the restricted memfd,
    instead of routing this directly to userspace, we call the error()
    callback that KVM registered. KVM then gets chance to handle it
    correctly.
    
    memfd_restricted() itself is implemented as a shim layer on top of real
    memory file systems (currently tmpfs). Pages in restrictedmem are marked
    as unmovable and unevictable, this is required for current confidential
    usage. But in future this might be changed.
    
    By default memfd_restricted() prevents userspace read, write and mmap.
    By defining new bit in the 'flags', it can be extended to support other
    restricted semantics in the future.
    
    The system call is currently wired up for x86 arch.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>
    Link: https://lore.kernel.org/r/20221202061347.1070246-2-chao.p.peng@linux.intel.com

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -1056,6 +1056,7 @@ asmlinkage long sys_memfd_secret(unsigned int flags);
 asmlinkage long sys_set_mempolicy_home_node(unsigned long start, unsigned long len,
 					    unsigned long home_node,
 					    unsigned long flags);
+asmlinkage long sys_memfd_restricted(unsigned int flags);
 
 /*
  * Architecture-specific system calls

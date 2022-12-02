d610f8300e0fdb721db934e6c24e03f3fd64ebce
Chao Peng <chao.p.peng@linux.intel.com>
2022-12-02 14:13:40 +0800

    KVM: Introduce per-page memory attributes
    In confidential computing usages, whether a page is private or shared is
    necessary information for KVM to perform operations like page fault
    handling, page zapping etc. There are other potential use cases for
    per-page memory attributes, e.g. to make memory read-only (or no-exec,
    or exec-only, etc.) without having to modify memslots.
    
    Introduce two ioctls (advertised by KVM_CAP_MEMORY_ATTRIBUTES) to allow
    userspace to operate on the per-page memory attributes.
      - KVM_SET_MEMORY_ATTRIBUTES to set the per-page memory attributes to
        a guest memory range.
      - KVM_GET_SUPPORTED_MEMORY_ATTRIBUTES to return the KVM supported
        memory attributes.
    
    KVM internally uses xarray to store the per-page memory attributes.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>
    Link: https://lore.kernel.org/all/Y2WB48kD0J4VGynX@google.com/
    Reviewed-by: Fuad Tabba <tabba@google.com>
    Tested-by: Fuad Tabba <tabba@google.com>
    [sean: rename Kconfig to capture "generic" aspect of the code, use slots_lock]
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -1184,6 +1184,7 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_S390_PROTECTED_ASYNC_DISABLE 224
 #define KVM_CAP_DIRTY_LOG_RING_WITH_BITMAP 225
 #define KVM_CAP_PMU_EVENT_MASKED_EVENTS 226
+#define KVM_CAP_MEMORY_ATTRIBUTES 227
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
@@ -2236,5 +2237,18 @@ struct kvm_s390_zpci_op {
 
 /* flags for kvm_s390_zpci_op->u.reg_aen.flags */
 #define KVM_S390_ZPCIOP_REGAEN_HOST    (1 << 0)
+
+/* Available with KVM_CAP_MEMORY_ATTRIBUTES */
+#define KVM_GET_SUPPORTED_MEMORY_ATTRIBUTES    _IOR(KVMIO,  0xd2, __u64)
+#define KVM_SET_MEMORY_ATTRIBUTES              _IOWR(KVMIO,  0xd3, struct kvm_memory_attributes)
+
+struct kvm_memory_attributes {
+	__u64 address;
+	__u64 size;
+	__u64 attributes;
+	__u64 flags;
+};
+
+#define KVM_MEMORY_ATTRIBUTE_PRIVATE           (1ULL << 3)
 
 #endif /* __LINUX_KVM_H */

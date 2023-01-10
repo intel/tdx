86f81d6cb38fe3659d40ba207d4d1e94214b17e0
Peter Gonda <pgonda@google.com>
2023-01-10 09:50:53 -0800

    KVM: selftests: handle protected bits in page tables
    SEV guests rely on an encyption bit which resides within the range that
    current code treats as address bits. Guest code will expect these bits
    to be set appropriately in their page tables, whereas the rest of the
    kvm_util functions will generally expect these bits to not be present.
    Introduce pte_me_mask and struct kvm_vm_arch to allow for arch specific
    address tagging. Currently just adding x86 c_bit and s_bit support for
    SEV and TDX.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Sean Christopherson <seanjc@google.com>
    Cc: Vishal Annapurve <vannapurve@google.com>
    Cc: Ackerley Tng <ackerleytng@google.com>
    cc: Andrew Jones <andrew.jones@linux.dev>
    Originally-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Peter Gonda <pgonda@google.com>
    Change-Id: I2cbe63c2b45d3a86894b86d0adb892100b215717

---
+++ tools/arch/s390/include/asm/kvm_host.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef _TOOLS_LINUX_ASM_S390_KVM_HOST_H
+#define _TOOLS_LINUX_ASM_S390_KVM_HOST_H
+
+struct kvm_vm_arch {};
+
+#endif  // _TOOLS_LINUX_ASM_S390_KVM_HOST_H

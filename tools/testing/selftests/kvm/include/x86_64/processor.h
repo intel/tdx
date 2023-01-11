5e600f89fd0ac1620bee1b0e99228c4e888b5acd
Ackerley Tng <ackerleytng@google.com>
2023-01-11 00:30:04 +0000

    KVM: selftests: Refactor steps in vCPU descriptor table initialization
    Split the vCPU descriptor table initialization process into a few
    steps and expose them:
    
    + Syncing exception handlers into the guest
    
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    
    Tested: see last commit in series
    Effort: kvm/tdx
    Google-Bug-Id: 308646861
    Change-Id: I37b79f85bf7f075c7997b54ef518e07f187ea7ff

--- tools/testing/selftests/kvm/include/x86_64/processor.h
+++ tools/testing/selftests/kvm/include/x86_64/processor.h
@@ -1157,6 +1157,7 @@ struct idt_entry {
 	uint32_t offset2; uint32_t reserved;
 };
 
+void sync_exception_handlers_to_guest(struct kvm_vm *vm);
 void vm_install_exception_handler(struct kvm_vm *vm, int vector,
 			void (*handler)(struct ex_regs *));
 

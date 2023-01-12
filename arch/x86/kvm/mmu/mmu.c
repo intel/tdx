5c09b00f528b954d49a0036410def26a3a71fa25
Isaku Yamahata <isaku.yamahata@intel.com>
2023-01-11 17:59:58 -0800

    KVM: TDX: MTRR: implement get_mt_mask() for TDX
    Because TDX virtualize cpuid[0x1].EDX[MTRR: bit 12] to fixed 1, guest TD
    thinks MTRR is supported.  Although TDX supports only WB for private GPA,
    it's desirable to support MTRR for shared GPA.  As guest access to MTRR
    MSRs causes #VE and KVM/x86 tracks the values of MTRR MSRs, the remining
    part is to implement get_mt_mask method for TDX for shared GPA.
    
    Pass around shared bit from kvm fault handler to get_mt_mask method so that
    it can determine if the gfn is shared or private.  Implement get_mt_mask()
    following vmx case for shared GPA and return WB for private GPA.
    the existing vmx_get_mt_mask() can't be directly used as CPU state(CR0.CD)
    is protected.  GFN passed to kvm_mtrr_check_gfn_range_consistency() should
    include shared bit.
    
    Suggested-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    ---
    Changes from v11 to V12
    - Make common function for VMX and TDX
    - pass around shared bit from KVM fault handler to get_mt_mask method
    - updated commit message

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -4560,7 +4560,7 @@ int kvm_tdp_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 	if (shadow_memtype_mask && kvm_arch_has_noncoherent_dma(vcpu->kvm)) {
 		for ( ; fault->max_level > PG_LEVEL_4K; --fault->max_level) {
 			int page_num = KVM_PAGES_PER_HPAGE(fault->max_level);
-			gfn_t base = fault->gfn & ~(page_num - 1);
+			gfn_t base = (fault->addr >> PAGE_SHIFT) & ~(page_num - 1);
 
 			if (kvm_mtrr_check_gfn_range_consistency(vcpu, base, page_num))
 				break;

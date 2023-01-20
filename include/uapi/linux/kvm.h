de53dadc2b0a62376550d72f313c76533a0050af
Chao Peng <chao.p.peng@linux.intel.com>
2023-01-20 11:22:56 -0800

    KVM: Enable and expose KVM_MEM_PRIVATE
    Register/unregister private memslot to fd-based memory backing store
    restrictedmem and implement the callbacks for restrictedmem_notifier:
      - invalidate_start()/invalidate_end() to zap the existing memory
        mappings in the KVM page table.
      - error() to request KVM_REQ_MEMORY_MCE and later exit to userspace
        with KVM_EXIT_SHUTDOWN.
    
    Expose KVM_MEM_PRIVATE for memslot and KVM_MEMORY_ATTRIBUTE_PRIVATE for
    KVM_GET_SUPPORTED_MEMORY_ATTRIBUTES to userspace but either are
    controlled by kvm_arch_has_private_mem() which should be rewritten by
    architecture code.
    
    Co-developed-by: Yu Zhang <yu.c.zhang@linux.intel.com>
    Signed-off-by: Yu Zhang <yu.c.zhang@linux.intel.com>
    Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>
    Cc: Fuad Tabba <tabba@google.com>
    Co-developed-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -102,7 +102,10 @@ struct kvm_userspace_memory_region2 {
 	__u64 guest_phys_addr;
 	__u64 memory_size;
 	__u64 userspace_addr;
-	__u64 pad[16];
+	__u64 restrictedmem_offset;
+	__u32 restrictedmem_fd;
+	__u32 pad1;
+	__u64 pad2[14];
 };
 
 /*
@@ -112,6 +115,7 @@ struct kvm_userspace_memory_region2 {
  */
 #define KVM_MEM_LOG_DIRTY_PAGES	(1UL << 0)
 #define KVM_MEM_READONLY	(1UL << 1)
+#define KVM_MEM_PRIVATE		(1UL << 2)
 
 /* for KVM_IRQ_LINE */
 struct kvm_irq_level {

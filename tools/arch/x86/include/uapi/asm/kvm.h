763bb2698626fb62f09ece75b471a80b13c50065
Isaku Yamahata <isaku.yamahata@intel.com>
2023-02-16 15:28:20 -0800

    KVM: TDX: initialize VM with TDX specific parameters
    TDX requires additional parameters for TDX VM for confidential execution to
    protect the confidentiality of its memory contents and CPU state from any
    other software, including VMM.  When creating a guest TD VM before creating
    vcpu, the number of vcpu, TSC frequency (the values are the same among
    vcpus, and it can't change.)  CPUIDs which the TDX module emulates.  Guest
    TDs can trust those CPUIDs and sha384 values for measurement.
    
    Add a new subcommand, KVM_TDX_INIT_VM, to pass parameters for the TDX
    guest.  It assigns an encryption key to the TDX guest for memory
    encryption.  TDX encrypts memory per guest basis.  The device model, say
    qemu, passes per-VM parameters for the TDX guest.  The maximum number of
    vcpus, TSC frequency (TDX guest has fixed VM-wide TSC frequency, not per
    vcpu.  The TDX guest can not change it.), attributes (production or debug),
    available extended features (which configure guest XCR0, IA32_XSS MSR),
    CPUIDs, sha384 measurements, etc.
    
    Call this subcommand before creating vcpu and KVM_SET_CPUID2, i.e.  CPUID
    configurations aren't available yet.  So CPUIDs configuration values need
    to be passed in struct kvm_tdx_init_vm.  The device model's responsibility
    to make this CPUID config for KVM_TDX_INIT_VM and KVM_SET_CPUID2.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    v15 -> v16:
    - Removed AMX check as the KVM upstream supports AMX.
    - Added CET flag to guest supported xss
    
    v14 -> v15:
    - add check if the reserved area of init_vm is zero

--- tools/arch/x86/include/uapi/asm/kvm.h
+++ tools/arch/x86/include/uapi/asm/kvm.h
@@ -565,6 +565,7 @@ struct kvm_pmu_event_filter {
 /* Trust Domain eXtension sub-ioctl() commands. */
 enum kvm_tdx_cmd_id {
 	KVM_TDX_CAPABILITIES = 0,
+	KVM_TDX_INIT_VM,
 
 	KVM_TDX_CMD_NR_MAX,
 };
@@ -612,6 +613,38 @@ struct kvm_tdx_capabilities {
 
 	__u32 nr_cpuid_configs;
 	struct kvm_tdx_cpuid_config cpuid_configs[];
+};
+
+struct kvm_tdx_init_vm {
+	__u64 attributes;
+	__u32 max_vcpus;
+	__u32 padding;
+	__u64 mrconfigid[6];    /* sha384 digest */
+	__u64 mrowner[6];       /* sha384 digest */
+	__u64 mrownerconfig[6]; /* sha348 digest */
+	union {
+		/*
+		 * KVM_TDX_INIT_VM is called before vcpu creation, thus before
+		 * KVM_SET_CPUID2.  CPUID configurations needs to be passed.
+		 *
+		 * This configuration supersedes KVM_SET_CPUID{,2}.
+		 * The user space VMM, e.g. qemu, should make them consistent
+		 * with this values.
+		 * sizeof(struct kvm_cpuid_entry2) * KVM_MAX_CPUID_ENTRIES(256)
+		 * = 8KB.
+		 */
+		struct {
+			struct kvm_cpuid2 cpuid;
+			/* 8KB with KVM_MAX_CPUID_ENTRIES. */
+			struct kvm_cpuid_entry2 entries[];
+		};
+		/*
+		 * For future extensibility.
+		 * The size(struct kvm_tdx_init_vm) = 16KB.
+		 * This should be enough given sizeof(TD_PARAMS) = 1024
+		 */
+		__u64 reserved[2028];
+	};
 };
 
 #endif /* _ASM_X86_KVM_H */

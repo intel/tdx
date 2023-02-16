4c3c8eaa885ccc2fc98108c6f852cc02699286a1
Isaku Yamahata <isaku.yamahata@intel.com>
2023-02-16 15:20:12 -0800

    KVM: TDX: create/destroy VM structure
    As the first step to create TDX guest, create/destroy VM struct.  Assign
    TDX private Host Key ID (HKID) to the TDX guest for memory encryption and
    allocate extra pages for the TDX guest. On destruction, free allocated
    pages, and HKID.
    
    Before tearing down private page tables, TDX requires some resources of the
    guest TD to be destroyed (i.e. HKID must have been reclaimed, etc).  Add
    flush_shadow_all_private callback before tearing down private page tables
    for it.
    
    Add vm_free() of kvm_x86_ops hook at the end of kvm_arch_destroy_vm()
    because some per-VM TDX resources, e.g. TDR, need to be freed after other
    TDX resources, e.g. HKID, were freed.
    
    Co-developed-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -137,18 +137,26 @@ void vmx_setup_mce(struct kvm_vcpu *vcpu);
 
 #ifdef CONFIG_INTEL_TDX_HOST
 int __init tdx_hardware_setup(struct kvm_x86_ops *x86_ops);
+void tdx_hardware_unsetup(void);
 bool tdx_is_vm_type_supported(unsigned long type);
 
 int tdx_vm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap);
+int tdx_vm_init(struct kvm *kvm);
+void tdx_mmu_release_hkid(struct kvm *kvm);
+void tdx_vm_free(struct kvm *kvm);
 int tdx_vm_ioctl(struct kvm *kvm, void __user *argp);
 #else
 static inline int tdx_hardware_setup(struct kvm_x86_ops *x86_ops) { return -EOPNOTSUPP; }
+static inline void tdx_hardware_unsetup(void) {}
 static inline bool tdx_is_vm_type_supported(unsigned long type) { return false; }
 
 static inline int tdx_vm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap)
 {
 	return -EINVAL;
 };
+static inline int tdx_vm_init(struct kvm *kvm) { return -EOPNOTSUPP; }
+static inline void tdx_mmu_release_hkid(struct kvm *kvm) {}
+static inline void tdx_vm_free(struct kvm *kvm) {}
 static inline int tdx_vm_ioctl(struct kvm *kvm, void __user *argp) { return -EOPNOTSUPP; }
 #endif
 

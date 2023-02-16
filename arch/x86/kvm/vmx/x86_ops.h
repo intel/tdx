449d6bcaae718197c43fe18f5dae1d6bf584787c
Isaku Yamahata <isaku.yamahata@intel.com>
2023-02-16 15:20:12 -0800

    With fixup: KVM: TDX: create/destroy VM structure
    As the first step to create TDX guest, create/destroy VM struct.  Assign
    TDX private Host Key ID (HKID) to the TDX guest for memory encryption and
    allocate extra pages for the TDX guest. On destruction, free allocated
    pages, and HKID.
    
    Before tearing down private page tables, TDX requires some resources of the
    guest TD to be destroyed (i.e. HKID must have been reclaimed, etc). Do it
    at vm_destroy() kvm_x86_ops hook.
    
    Add vm_free() of kvm_x86_ops hook at the end of kvm_arch_destroy_vm()
    because some per-VM TDX resources, e.g. TDR, need to be freed after other
    TDX resources ( e.g. HKID) were freed.
    
    Co-developed-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Co-developed-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Signed-off-by: Yan Zhao <yan.y.zhao@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    Kai rebase:
     - Move code change to tdx_module_setup() to __tdx_bringup() due to
       initializing is done in post hardware_setup() now and
       tdx_module_setup() is removed.  Remove the code to use API to read
       global metadata but use exported 'struct tdx_sysinfo' pointer.
     - Replace 'tdx_info->nr_tdcs_pages' with a wrapper
       tdx_sysinfo_nr_tdcs_pages() because the 'struct tdx_sysinfo' doesn't
       have nr_tdcs_pages directly.
     - Replace tdx_info->max_vcpus_per_td with the new exported pointer in
       tdx_vm_init().
    
    v1:
     - Fix unnecessary include re-ordering (Chao)
     - Fix the unpaired curly brackets (Chao)
     - Drop the tdx_mng_key_config_lock  (Chao)
     - Drop unnecessary is_hkid_assigned() check (Chao)
     - Use KVM_GENERIC_PRIVATE_MEM and undo the removal of EXPERT (Binbin)
     - Drop the word typically from comments (Binbin)
     - Clarify comments for the need of global tdx_lock mutex (Kai)
     - Add function comments for tdx_clear_page() (Kai)
     - Clarify comments for tdx_clear_page() poisoned page (Kai)
     - Move and update comments for limitations of __tdx_reclaim_page() (Kai)
     - Drop comment related to "rare to contend" (Kai)
     - Drop comment related to TDR and target page (Tony)
     - Make code easier to read with line breaks between paragraphs (Kai)
     - Use cond_resched() retry (Kai)
     - Use for loop for retries (Tony)
     - Use switch to handle errors (Tony)
     - Drop loop for tdh_mng_key_config() (Tony)
     - Rename tdx_reclaim_control_page() td_page_pa to ctrl_page_pa (Kai)
     - Reorganize comments for tdx_reclaim_control_page() (Kai)
     - Use smp_func_do_phymem_cache_wb() naming to indicate SMP (Kai)
     - Use bool resume in smp_func_do_phymem_cache_wb() (Kai)
     - Add comment on retrying to smp_func_do_phymem_cache_wb() (Kai)

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -124,12 +124,18 @@ void vmx_setup_mce(struct kvm_vcpu *vcpu);
 
 #ifdef CONFIG_INTEL_TDX_HOST
 int tdx_vm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap);
+int tdx_vm_init(struct kvm *kvm);
+void tdx_mmu_release_hkid(struct kvm *kvm);
+void tdx_vm_free(struct kvm *kvm);
 int tdx_vm_ioctl(struct kvm *kvm, void __user *argp);
 #else
 static inline int tdx_vm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap)
 {
 	return -EINVAL;
 };
+static inline int tdx_vm_init(struct kvm *kvm) { return -EOPNOTSUPP; }
+static inline void tdx_mmu_release_hkid(struct kvm *kvm) {}
+static inline void tdx_vm_free(struct kvm *kvm) {}
 static inline int tdx_vm_ioctl(struct kvm *kvm, void __user *argp) { return -EOPNOTSUPP; }
 #endif
 

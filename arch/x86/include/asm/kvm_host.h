78bce94c2023afa9d71ba6e2095b465091ebe46e
Isaku Yamahata <isaku.yamahata@intel.com>
2023-02-16 15:20:12 -0800

    KVM: TDX: create/destroy VM structure
    Implement managing the TDX private KeyID to implement, create, destroy
    and free for a TDX guest.
    
    When creating at TDX guest, assign a TDX private KeyID for the TDX guest
    for memory encryption, and allocate pages for the guest. These are used
    for the Trust Domain Root (TDR) and Trust Domain Control Structure (TDCS).
    
    On destruction, free the allocated pages, and the KeyID.
    
    Before tearing down the private page tables, TDX requires the guest TD to
    be destroyed by reclaiming the KeyID. Do it at vm_destroy() kvm_x86_ops
    hook.
    
    Add a call for vm_free() at the end of kvm_arch_destroy_vm() because the
    per-VM TDR needs to be freed after the KeyID.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Co-developed-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Co-developed-by: Yan Zhao <yan.y.zhao@intel.com>
    Signed-off-by: Yan Zhao <yan.y.zhao@intel.com>
    Co-developed-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    ---
    uAPI breakout v1:
     - Fix unnecessary include re-ordering (Chao)
     - Fix the unpaired curly brackets (Chao)
     - Drop the tdx_mng_key_config_lock  (Chao)
     - Drop unnecessary is_hkid_assigned() check (Chao)
     - Use KVM_GENERIC_PRIVATE_MEM and undo the removal of EXPERT (Binbin)
     - Drop the word typically from comments (Binbin)
     - Clarify comments for the need of global tdx_lock mutex (Kai)
     - Add function comments for tdx_clear_page() (Kai)
     - Clarify comments for tdx_clear_page() poisoned page (Kai)
     - Move and update comments for limitations of __tdx_reclaim_page() (Kai)
     - Drop comment related to "rare to contend" (Kai)
     - Drop comment related to TDR and target page (Tony)
     - Make code easier to read with line breaks between paragraphs (Kai)
     - Use cond_resched() retry (Kai)
     - Use for loop for retries (Tony)
     - Use switch to handle errors (Tony)
     - Drop loop for tdh_mng_key_config() (Tony)
     - Rename tdx_reclaim_control_page() td_page_pa to ctrl_page_pa (Kai)
     - Reorganize comments for tdx_reclaim_control_page() (Kai)
     - Use smp_func_do_phymem_cache_wb() naming to indicate SMP (Kai)
     - Use bool resume in smp_func_do_phymem_cache_wb() (Kai)
     - Add comment on retrying to smp_func_do_phymem_cache_wb() (Kai)
     - Move code change to tdx_module_setup() to __tdx_bringup() due to
       initializing is done in post hardware_setup() now and
       tdx_module_setup() is removed.  Remove the code to use API to read
       global metadata but use exported 'struct tdx_sysinfo' pointer.
     - Replace 'tdx_info->nr_tdcs_pages' with a wrapper
       tdx_sysinfo_nr_tdcs_pages() because the 'struct tdx_sysinfo' doesn't
       have nr_tdcs_pages directly.
     - Replace tdx_info->max_vcpus_per_td with the new exported pointer in
       tdx_vm_init().
     - Add comment to tdx_mmu_release_hkid() on KeyID allocated (Kai)
     - Update comments for tdx_mmu_release_hkid() for locking (Kai)
     - Clarify tdx_mmu_release_hkid() comments for freeing HKID (Kai)
     - Use KVM_BUG_ON() for SEAMCALLs in tdx_mmu_release_hkid() (Kai)
     - Use continue for loop in tdx_vm_free() (Kai)
     - Clarify comments in  tdx_vm_free() for reclaiming TDCS (Kai)
     - Use KVM_BUG_ON() for tdx_vm_free()
     - Prettify format with line breaks in tdx_vm_free() (Tony)
     - Prettify formatting for __tdx_td_init() with line breaks (Kai)
     - Simplify comments for __tdx_td_init() locking (Kai)
     - Update patch description (Kai)

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1651,6 +1651,7 @@ struct kvm_x86_ops {
 	int (*vm_enable_cap)(struct kvm *kvm, struct kvm_enable_cap *cap);
 	int (*vm_init)(struct kvm *kvm);
 	void (*vm_destroy)(struct kvm *kvm);
+	void (*vm_free)(struct kvm *kvm);
 
 	/* Create, but do not attach this VCPU */
 	int (*vcpu_precreate)(struct kvm *kvm);

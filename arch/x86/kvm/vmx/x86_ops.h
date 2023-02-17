b16d6861353640ac4bbaec41d02395acfbd5c59a
Isaku Yamahata <isaku.yamahata@intel.com>
2023-02-16 17:03:40 -0800

    KVM: TDX: Allow userspace to configure maximum vCPUs for TDX guests
    TDX has its own mechanism to control the maximum number of vCPUs that
    the TDX guest can use.  When creating a TDX guest, the maximum number of
    vCPUs of the guest needs to be passed to the TDX module as part of the
    measurement of the guest.  Depending on TDX module's version, it may
    also report the maximum vCPUs it can support for all TDX guests.
    
    Because the maximum number of vCPUs is part of the measurement, thus
    part of attestation, it's better to allow the userspace to be able to
    configure it.  E.g. the users may want to precisely control the maximum
    number of vCPUs their precious VMs can use.
    
    The actual control itself must be done via the TDH.MNG.INIT SEAMCALL,
    where the number of maximum cpus is part of the input to the TDX module,
    but KVM needs to support the "per-VM maximum number of vCPUs" and
    reflect that in the KVM_CAP_MAX_VCPUS.
    
    Currently, the KVM x86 always reports KVM_MAX_VCPUS for all VMs but
    doesn't allow to enable KVM_CAP_MAX_VCPUS to configure the number of
    maximum vCPUs on VM-basis.
    
    Add "per-VM maximum number of vCPUs" to KVM x86/TDX to accommodate TDX's
    needs.
    
    Specifically, use KVM's existing KVM_ENABLE_CAP IOCTL() to allow the
    userspace to configure the maximum vCPUs by making KVM x86 support
    enabling the KVM_CAP_MAX_VCPUS cap on VM-basis.
    
    For that, add a new 'kvm_x86_ops::vm_enable_cap()' callback and call
    it from kvm_vm_ioctl_enable_cap() as a placeholder to handle the
    KVM_CAP_MAX_VCPUS for TDX guests (and other KVM_CAP_xx for TDX and/or
    other VMs if needed in the future).
    
    Implement the callback for TDX guest to check whether the maximum vCPUs
    passed from usrspace can be supported by TDX, and if it can, override
    the 'struct kvm::max_vcpus'.  Leave VMX guests and all AMD guests
    unsupported to avoid any side-effect for those VMs.
    
    Accordingly, in the KVM_CHECK_EXTENSION IOCTL(), change to return the
    'struct kvm::max_vcpus' for a given VM for the KVM_CAP_MAX_VCPUS.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    ---
    v20:
    - Drop max_vcpu ops to use kvm.max_vcpus
    - Remove TDX_MAX_VCPUS (Kai)
    - Use type cast (u16) instead of calling memcpy() when reading the
       'max_vcpus_per_td' (Kai)
    - Improve change log and change patch title from "KVM: TDX: Make
      KVM_CAP_MAX_VCPUS backend specific" (Kai)

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -139,11 +139,16 @@ void vmx_setup_mce(struct kvm_vcpu *vcpu);
 int __init tdx_hardware_setup(struct kvm_x86_ops *x86_ops);
 void tdx_hardware_unsetup(void);
 
+int tdx_vm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap);
 int tdx_vm_ioctl(struct kvm *kvm, void __user *argp);
 #else
 static inline int tdx_hardware_setup(struct kvm_x86_ops *x86_ops) { return -EOPNOTSUPP; }
 static inline void tdx_hardware_unsetup(void) {}
 
+static inline int tdx_vm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap)
+{
+	return -EINVAL;
+};
 static inline int tdx_vm_ioctl(struct kvm *kvm, void __user *argp) { return -EOPNOTSUPP; }
 #endif
 

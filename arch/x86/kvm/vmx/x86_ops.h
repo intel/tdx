b2f0e489e98cda3d6bdb61329bea36c8becfea7e
Isaku Yamahata <isaku.yamahata@intel.com>
2023-02-16 17:03:40 -0800

    KVM: x86, tdx: Make KVM_CAP_MAX_VCPUS backend specific
    TDX has its own limitation on the maximum number of vcpus that the guest
    can accommodate.  Allow x86 kvm backend to implement its own KVM_ENABLE_CAP
    handler and implement TDX backend for KVM_CAP_MAX_VCPUS.  user space VMM,
    e.g. qemu, can specify its value instead of KVM_MAX_VCPUS.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/x86_ops.h
+++ arch/x86/kvm/vmx/x86_ops.h
@@ -143,6 +143,7 @@ int tdx_hardware_enable(void);
 bool tdx_is_vm_type_supported(unsigned long type);
 int tdx_dev_ioctl(void __user *argp);
 
+int tdx_vm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap);
 int tdx_vm_ioctl(struct kvm *kvm, void __user *argp);
 #else
 static inline int tdx_hardware_setup(struct kvm_x86_ops *x86_ops) { return -ENOSYS; }
@@ -150,6 +151,7 @@ static inline int tdx_hardware_enable(void) { return -EOPNOTSUPP; }
 static inline bool tdx_is_vm_type_supported(unsigned long type) { return false; }
 static inline int tdx_dev_ioctl(void __user *argp) { return -EOPNOTSUPP; };
 
+static inline int tdx_vm_enable_cap(struct kvm *kvm, struct kvm_enable_cap *cap) { return -EINVAL; };
 static inline int tdx_vm_ioctl(struct kvm *kvm, void __user *argp) { return -EOPNOTSUPP; }
 #endif
 

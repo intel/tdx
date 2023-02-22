f90076c8380200fbac8be916ed9fead8bb584507
Isaku Yamahata <isaku.yamahata@intel.com>
2023-02-21 17:19:45 -0800

    KVM: TDX: TDP MMU TDX support
    Implement hooks of TDP MMU for TDX backend.  TLB flush, TLB shootdown,
    propagating the change private EPT entry to Secure EPT and freeing Secure
    EPT page. TLB flush handles both shared EPT and private EPT.  It flushes
    shared EPT same as VMX.  It also waits for the TDX TLB shootdown.  For the
    hook to free Secure EPT page, unlinks the Secure EPT page from the Secure
    EPT so that the page can be freed to OS.
    
    Propagate the entry change to Secure EPT.  The possible entry changes are
    present -> non-present(zapping) and non-present -> present(population).  On
    population just link the Secure EPT page or the private guest page to the
    Secure EPT by TDX SEAMCALL. Because TDP MMU allows concurrent
    zapping/population, zapping requires synchronous TLB shoot down with the
    frozen EPT entry.  It zaps the secure entry, increments TLB counter, sends
    IPI to remote vcpus to trigger TLB flush, and then unlinks the private
    guest page from the Secure EPT. For simplicity, batched zapping with
    exclude lock is handled as concurrent zapping.  Although it's inefficient,
    it can be optimized in the future.
    
    For MMIO SPTE, the spte value changes as follows.
    initial value (suppress VE bit is set)
    -> Guest issues MMIO and triggers EPT violation
    -> KVM updates SPTE value to MMIO value (suppress VE bit is cleared)
    -> Guest MMIO resumes.  It triggers VE exception in guest TD
    -> Guest VE handler issues TDG.VP.VMCALL<MMIO>
    -> KVM handles MMIO
    -> Guest VE handler resumes its execution after MMIO instruction
    
    TODO: add to commit log about tlb flush range
     - Previous MMU changes removed shared bit from GFN.
     - Existing MMU logic forces generic flush if ops implementation returns an
       error.
     - Other flush_remote_tlbs_range is for hyperv, only in guests, won't
       interfere.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    v20:
    - Dropped KVM_BUG_ON() in vt_flush_tlb_gva(), vt_flush_remote_tlbs(),
      tdx_track(). (Rick)
    - Update for the wrapper functions for SEAMCALLs. (Sean)
    - Add preparation for KVM_TDX_INIT_MEM_REGION to make
      tdx_sept_set_private_spte() callback nop when the guest isn't finalized.
    - use unlikely(err) in  tdx_reclaim_td_page().
    - Use gmem_validate_fault() as part of rebase on kvm-coco-queue
    - Make flush_remote_tlbs() defined for CONFIG_INTEL_TDX_HOST in response
      to upstream commit.
    - Depend on CONFIG_INTEL_TDX_HOST also for tlb flush function
      definitions (Vijay Dhanraj)
    - Remove warning for shadow_mmio_value
    - Fix missing tdx_gmem_private_max_mapping_level() implementation for
      !CONFIG_INTEL_TDX_HOST.
    - Move header definitions from "KVM: TDX: Define TDX architectural
      definitions" (Sean)
    - Drop ugly unions (Sean)
    - Updates from seamcall overhaul (Kai)
    
    v19:
    - Compile fix when CONFIG_HYPERV != y.
      It's due to the following patch.  Catch it up.
      https://lore.kernel.org/all/20231018192325.1893896-1-seanjc@google.com/
    - Add comments on tlb shootdown to explan the sequence.
    - Use gmem_max_level callback, delete tdp_max_page_level.
    
    v18:
    - rename tdx_sept_page_aug() -> tdx_mem_page_aug()
    - checkpatch: space => tab
    
    v15 -> v16:
    - Add the handling of TD_ATTR_SEPT_VE_DISABLE case.
    
    v14 -> v15:
    - Implemented tdx_flush_tlb_current()
    - Removed unnecessary invept in tdx_flush_tlb().  It was carry over
      from the very old code base.

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1673,7 +1673,7 @@ struct kvm_x86_ops {
 
 	void (*flush_tlb_all)(struct kvm_vcpu *vcpu);
 	void (*flush_tlb_current)(struct kvm_vcpu *vcpu);
-#if IS_ENABLED(CONFIG_HYPERV)
+#if IS_ENABLED(CONFIG_HYPERV) || IS_ENABLED(CONFIG_INTEL_TDX_HOST)
 	int  (*flush_remote_tlbs)(struct kvm *kvm);
 	int  (*flush_remote_tlbs_range)(struct kvm *kvm, gfn_t gfn,
 					gfn_t nr_pages);
@@ -1909,7 +1909,7 @@ static inline struct kvm *kvm_arch_alloc_vm(void)
 #define __KVM_HAVE_ARCH_VM_FREE
 void kvm_arch_free_vm(struct kvm *kvm);
 
-#if IS_ENABLED(CONFIG_HYPERV)
+#if IS_ENABLED(CONFIG_HYPERV) || IS_ENABLED(CONFIG_INTEL_TDX_HOST)
 #define __KVM_HAVE_ARCH_FLUSH_REMOTE_TLBS
 static inline int kvm_arch_flush_remote_tlbs(struct kvm *kvm)
 {
@@ -1929,7 +1929,7 @@ static inline int kvm_arch_flush_remote_tlbs_range(struct kvm *kvm, gfn_t gfn,
 
 	return static_call(kvm_x86_flush_remote_tlbs_range)(kvm, gfn, nr_pages);
 }
-#endif /* CONFIG_HYPERV */
+#endif /* CONFIG_HYPERV || CONFIG_INTEL_TDX_HOST*/
 
 enum kvm_intr_type {
 	/* Values are arbitrary, but must be non-zero. */

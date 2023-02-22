63e94960266f43c335e8796a3f0afe5248eb7897
Isaku Yamahata <isaku.yamahata@intel.com>
2023-02-21 17:19:45 -0800

    KVM: TDX: TDP MMU TDX support
    Implement hooks of TDP MMU for TDX backend.  TLB flush, TLB shootdown,
    propagating the change private EPT entry to Secure EPT and freeing Secure
    EPT page. TLB flush handles both shared EPT and private EPT.  It flushes
    shared EPT same as VMX.  It also waits for the TDX TLB shootdown.  For the
    hook to free Secure EPT page, unlinks the Secure EPT page from the Secure
    EPT so that the page can be freed to OS.
    
    Propagate the entry change to Secure EPT.  The possible entry changes are
    present -> non-present(zapping) and non-present -> present(population).  On
    population just link the Secure EPT page or the private guest page to the
    Secure EPT by TDX SEAMCALL. Because TDP MMU allows concurrent
    zapping/population, zapping requires synchronous TLB shoot down with the
    frozen EPT entry.  It zaps the secure entry, increments TLB counter, sends
    IPI to remote vcpus to trigger TLB flush, and then unlinks the private
    guest page from the Secure EPT. For simplicity, batched zapping with
    exclude lock is handled as concurrent zapping.  Although it's inefficient,
    it can be optimized in the future.
    
    For MMIO SPTE, the spte value changes as follows.
    initial value (suppress VE bit is set)
    -> Guest issues MMIO and triggers EPT violation
    -> KVM updates SPTE value to MMIO value (suppress VE bit is cleared)
    -> Guest MMIO resumes.  It triggers VE exception in guest TD
    -> Guest VE handler issues TDG.VP.VMCALL<MMIO>
    -> KVM handles MMIO
    -> Guest VE handler resumes its execution after MMIO instruction
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    
    ---
    Changes v11 to v12
    - removed tlb_remote_flush_with_range mothod.  Instead disable TDX if
      hyper-v support populate tlb_remote_flush and tlb_remote_flush_with_range

--- arch/x86/kvm/vmx/tdx.h
+++ arch/x86/kvm/vmx/tdx.h
@@ -18,6 +18,7 @@ struct kvm_tdx {
 	int hkid;
 
 	bool finalized;
+	atomic_t tdh_mem_track;
 
 	u64 tsc_offset;
 };
@@ -163,6 +164,12 @@ static __always_inline u64 td_tdcs_exec_read64(struct kvm_tdx *kvm_tdx, u32 fiel
 		return 0;
 	}
 	return out.r8;
+}
+
+static __always_inline int pg_level_to_tdx_sept_level(enum pg_level level)
+{
+	WARN_ON_ONCE(level == PG_LEVEL_NONE);
+	return level - 1;
 }
 
 #else

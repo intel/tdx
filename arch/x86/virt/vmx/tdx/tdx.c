1a52ee59bf525b69c89a72078f25354fad7be895
Yang Weijiang <weijiang.yang@intel.com>
2023-03-01 20:24:22 -0500

    [REVERTME] KVM: TDX: Show messages if SEAMCALL failed due to compatibility issue
    With the revert of commits: 9f07ca6fb186/c38eb5bf4375/5fb0a0396fdf,
    TDH.SYS.INIT would fail if host cmdline tsx=off is applied. In this
    case, print informative message after such kind of failure. Also do
    the similar check after TDH.VP.ENTER.
    
    Compatibility issue and informative message:
    
    "tdx: TDX module is outdated. Use v1.0.3 or newer."
    
    Signed-off-by: Yang Weijiang <weijiang.yang@intel.com>

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -263,6 +263,10 @@ static int __always_unused seamcall(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
 				cpu);
 		ret = -EINVAL;
 		break;
+	case TDX_INCORRECT_CPUID_VALUE:
+		pr_err_once("TDX module is outdated. Use v1.0.3 or newer.\n");
+		ret = -ENODEV;
+		break;
 	default:
 		pr_err_once("SEAMCALL failed: CPU %d: leaf %llu, error 0x%llx.\n",
 				cpu, fn, sret);

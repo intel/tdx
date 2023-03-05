f77da6c5023cc3ec83a86421877921f77526d874
Isaku Yamahata <isaku.yamahata@intel.com>
2023-03-04 17:57:46 -0800

    [REVERTME] KVM: TDX: account TDX private pages for debug
    Acount tdx private pages assigned to guest TD.  When TD is destroyed, check
    if the number of private pages assigned to guest TD is zero for debug.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -486,7 +486,7 @@ void tdx_mmu_release_hkid(struct kvm *kvm)
 		;
 }
 
-void tdx_vm_free(struct kvm *kvm)
+static void __tdx_vm_free(struct kvm *kvm)
 {
 	struct kvm_tdx *kvm_tdx = to_kvm_tdx(kvm);
 	u64 err;
@@ -502,8 +502,10 @@ void tdx_vm_free(struct kvm *kvm)
 
 	if (kvm_tdx->tdcs_pa) {
 		for (i = 0; i < tdx_info->nr_tdcs_pages; i++) {
-			if (kvm_tdx->tdcs_pa[i])
-				tdx_reclaim_td_page(kvm_tdx->tdcs_pa[i]);
+			if (!kvm_tdx->tdcs_pa[i])
+				continue;
+			tdx_reclaim_td_page(kvm_tdx->tdcs_pa[i]);
+			tdx_unaccount_ctl_page(kvm);
 		}
 		kfree(kvm_tdx->tdcs_pa);
 		kvm_tdx->tdcs_pa = NULL;
@@ -526,11 +528,33 @@ void tdx_vm_free(struct kvm *kvm)
 	}
 	tdx_clear_page(kvm_tdx->tdr_pa, PAGE_SIZE);
 
+	tdx_unaccount_ctl_page(kvm);
 	free_page((unsigned long)__va(kvm_tdx->tdr_pa));
 	kvm_tdx->tdr_pa = 0;
 
 	kfree(kvm_tdx->cpuid);
 	kvm_tdx->cpuid = NULL;
+}
+
+void tdx_vm_free(struct kvm *kvm)
+{
+	__tdx_vm_free(kvm);
+#ifdef CONFIG_KVM_TDX_ACCOUNT_PRIVATE_PAGES
+	if (WARN_ON_ONCE(atomic64_read(&to_kvm_tdx(kvm)->ctl_pages) ||
+			 tdx_nr_sept_pages(kvm, PG_LEVEL_512G) ||
+			 tdx_nr_sept_pages(kvm, PG_LEVEL_1G) ||
+			 tdx_nr_sept_pages(kvm, PG_LEVEL_2M) ||
+			 tdx_nr_sept_pages(kvm, PG_LEVEL_4K) ||
+			 atomic64_read(&to_kvm_tdx(kvm)->td_pages))) {
+		pr_warn_ratelimited("control %lld sept 512G %lld 1G %lld 2M %lld 4K %lld td %lld pages are left\n",
+				    atomic64_read(&to_kvm_tdx(kvm)->ctl_pages),
+				    tdx_nr_sept_pages(kvm, PG_LEVEL_512G),
+				    tdx_nr_sept_pages(kvm, PG_LEVEL_1G),
+				    tdx_nr_sept_pages(kvm, PG_LEVEL_2M),
+				    tdx_nr_sept_pages(kvm, PG_LEVEL_4K),
+				    atomic64_read(&to_kvm_tdx(kvm)->td_pages));
+	}
+#endif
 }
 
 static int tdx_do_tdh_mng_key_config(void *param)
@@ -813,8 +837,10 @@ void tdx_vcpu_free(struct kvm_vcpu *vcpu)
 
 	if (tdx->tdvpx_pa) {
 		for (i = 0; i < tdx_info->nr_tdvpx_pages; i++) {
-			if (tdx->tdvpx_pa[i])
-				tdx_reclaim_td_page(tdx->tdvpx_pa[i]);
+			if (!tdx->tdvpx_pa[i])
+				continue;
+			tdx_reclaim_td_page(tdx->tdvpx_pa[i]);
+			tdx_unaccount_ctl_page(vcpu->kvm);
 		}
 		kfree(tdx->tdvpx_pa);
 		tdx->tdvpx_pa = NULL;
@@ -822,6 +848,7 @@ void tdx_vcpu_free(struct kvm_vcpu *vcpu)
 	if (tdx->tdvpr_pa) {
 		tdx_reclaim_td_page(tdx->tdvpr_pa);
 		tdx->tdvpr_pa = 0;
+		tdx_unaccount_ctl_page(vcpu->kvm);
 	}
 }
 
@@ -1523,6 +1550,7 @@ static int tdx_sept_page_aug(struct kvm *kvm, gfn_t gfn,
 		return -EIO;
 	}
 
+	tdx_account_td_pages(kvm, level);
 	return 0;
 }
 
@@ -1578,6 +1606,7 @@ static int tdx_sept_page_add(struct kvm *kvm, gfn_t gfn,
 	} else if (measure)
 		tdx_measure_page(kvm_tdx, gpa, KVM_HPAGE_SIZE(level));
 
+	tdx_account_td_pages(kvm, level);
 	return 0;
 
 }
@@ -1635,6 +1664,7 @@ static int tdx_sept_drop_private_spte(struct kvm *kvm, gfn_t gfn,
 		}
 		tdx_set_page_present_level(hpa, level);
 		tdx_unpin(kvm, gfn, pfn, level);
+		tdx_unaccount_td_pages(kvm, level);
 		return 0;
 	}
 
@@ -1672,6 +1702,7 @@ static int tdx_sept_drop_private_spte(struct kvm *kvm, gfn_t gfn,
 		}
 		hpa += PAGE_SIZE;
 	}
+	tdx_unaccount_td_pages(kvm, level);
 	return r;
 }
 
@@ -1715,6 +1746,8 @@ static int tdx_sept_link_private_spt(struct kvm *kvm, gfn_t gfn,
 		return -EIO;
 	}
 
+	/* level is for parent's. */
+	tdx_account_sept_page(kvm, level - 1);
 	return 0;
 }
 
@@ -1737,6 +1770,7 @@ static int tdx_sept_split_private_spt(struct kvm *kvm, gfn_t gfn,
 		return -EIO;
 	}
 
+	tdx_account_sept_page(kvm, level);
 	return 0;
 }
 
@@ -1781,6 +1815,7 @@ static int tdx_sept_merge_private_spt(struct kvm *kvm, gfn_t gfn,
 
 	tdx_set_page_present(__pa(private_spt));
 	tdx_clear_page(__pa(private_spt), PAGE_SIZE);
+	tdx_unaccount_sept_page(kvm, level - 1);
 	return 0;
 }
 
@@ -1944,8 +1979,14 @@ static int tdx_sept_free_private_spt(struct kvm *kvm, gfn_t gfn,
 	 * The HKID assigned to this TD was already freed and cache was
 	 * already flushed. We don't have to flush again.
 	 */
-	if (!is_hkid_assigned(kvm_tdx))
-		return tdx_reclaim_page(__pa(private_spt), PG_LEVEL_4K);
+	if (!is_hkid_assigned(kvm_tdx)) {
+		int r;
+
+		r = tdx_reclaim_page(__pa(private_spt), PG_LEVEL_4K);
+		if (!r)
+			tdx_unaccount_sept_page(kvm, level);
+		return r;
+	}
 
 	/*
 	 * Inefficient. But this is only called for deleting memslot
@@ -1976,6 +2017,7 @@ static int tdx_sept_free_private_spt(struct kvm *kvm, gfn_t gfn,
 		pr_tdx_error(TDH_MEM_SEPT_REMOVE, err, &out);
 		return -EIO;
 	}
+	tdx_unaccount_sept_page(kvm, level);
 
 	err = tdh_phymem_page_wbinvd(set_hkid_to_hpa(__pa(private_spt),
 						     kvm_tdx->hkid));
@@ -2766,6 +2808,7 @@ static int __tdx_td_init(struct kvm *kvm, struct td_params *td_params,
 		goto free_packages;
 	}
 	kvm_tdx->tdr_pa = tdr_pa;
+	tdx_account_ctl_page(kvm);
 
 	for_each_online_cpu(i) {
 		int pkg = topology_physical_package_id(i);
@@ -2802,6 +2845,7 @@ static int __tdx_td_init(struct kvm *kvm, struct td_params *td_params,
 			ret = -EIO;
 			goto teardown;
 		}
+		tdx_account_ctl_page(kvm);
 	}
 
 	err = tdh_mng_init(kvm_tdx->tdr_pa, __pa(td_params), &out);
@@ -3190,6 +3234,7 @@ static int tdx_td_vcpu_init(struct kvm_vcpu *vcpu, u64 vcpu_rcx)
 		goto free_tdvpx;
 	}
 	tdx->tdvpr_pa = tdvpr_pa;
+	tdx_account_ctl_page(vcpu->kvm);
 
 	tdx->tdvpx_pa = tdvpx_pa;
 	for (i = 0; i < tdx_info->nr_tdvpx_pages; i++) {
@@ -3203,6 +3248,7 @@ static int tdx_td_vcpu_init(struct kvm_vcpu *vcpu, u64 vcpu_rcx)
 			/* vcpu_free method frees TDVPX and TDR donated to TDX */
 			return -EIO;
 		}
+		tdx_account_ctl_page(vcpu->kvm);
 	}
 
 	err = tdh_vp_init(tdx->tdvpr_pa, vcpu_rcx);
@@ -3450,6 +3496,8 @@ static int __init tdx_module_setup(void)
 	}
 	tdx_info->no_rbp_mod = no_rbp_mod ? TDX_CONTROL_FLAG_NO_RBP_MOD : 0;
 
+	pr_info("nr_tdcs %d nr_tdvpx %d\n",
+		tdx_info->nr_tdcs_pages, tdx_info->nr_tdvpx_pages);
 	return 0;
 
 error_sys_rd:

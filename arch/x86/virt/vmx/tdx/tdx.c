cdce39b74649cf89ef19ef8cf572d02c47b0a640
Kai Huang <kai.huang@intel.com>
2023-03-07 03:13:49 +1300

    x86/virt/tdx: Add SEAMCALL infrastructure
    TDX introduces a new CPU mode: Secure Arbitration Mode (SEAM).  This
    mode runs only the TDX module itself or other code to load the TDX
    module.
    
    The host kernel communicates with SEAM software via a new SEAMCALL
    instruction.  This is conceptually similar to a guest->host hypercall,
    except it is made from the host to SEAM software instead.  The TDX
    module establishes a new SEAMCALL ABI which allows the host to
    initialize the module and to manage VMs.
    
    Add infrastructure to make SEAMCALLs.  The SEAMCALL ABI is very similar
    to the TDCALL ABI and leverages much TDCALL infrastructure.
    
    SEAMCALL instruction causes #GP when TDX isn't BIOS enabled, and #UD
    when CPU is not in VMX operation.  Currently, only KVM code mocks with
    VMX enabling, and KVM is the only user of TDX.  This implementation
    chooses to make KVM itself responsible for enabling VMX before using
    TDX and let the rest of the kernel stay blissfully unaware of VMX.
    
    The current TDX_MODULE_CALL macro handles neither #GP nor #UD.  The
    kernel would hit Oops if SEAMCALL were mistakenly made w/o enabling VMX
    first.  Architecturally, there is no CPU flag to check whether the CPU
    is in VMX operation.  Also, if a BIOS were buggy, it could still report
    valid TDX private KeyIDs when TDX actually couldn't be enabled.
    
    Extend the TDX_MODULE_CALL macro to handle #UD and #GP to return error
    codes.  Introduce two new TDX error codes for them respectively so the
    caller can distinguish.
    
    Also add a wrapper function of SEAMCALL to convert SEAMCALL error code
    to the kernel error code, and print out SEAMCALL error code to help the
    user to understand what went wrong.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Link: https://lore.kernel.org/r/fdd2f7f9e1ff3d8655c7c70eee06de44095f61fd.1678111292.git.kai.huang@intel.com

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -12,9 +12,11 @@
 #include <linux/init.h>
 #include <linux/errno.h>
 #include <linux/printk.h>
+#include <linux/smp.h>
 #include <asm/msr-index.h>
 #include <asm/msr.h>
 #include <asm/tdx.h>
+#include "tdx.h"
 
 static u32 tdx_global_keyid __ro_after_init;
 static u32 tdx_guest_keyid_start __ro_after_init;
@@ -102,4 +104,58 @@ early_initcall(tdx_init);
 bool platform_tdx_enabled(void)
 {
 	return !!tdx_global_keyid;
+}
+
+/*
+ * Wrapper of __seamcall() to convert SEAMCALL leaf function error code
+ * to kernel error code.  @seamcall_ret and @out contain the SEAMCALL
+ * leaf function return code and the additional output respectively if
+ * not NULL.
+ */
+static int __always_unused seamcall(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
+				    u64 *seamcall_ret,
+				    struct tdx_module_output *out)
+{
+	int cpu, ret = 0;
+	u64 sret;
+
+	/* Need a stable CPU id for printing error message */
+	cpu = get_cpu();
+
+	sret = __seamcall(fn, rcx, rdx, r8, r9, out);
+
+	/* Save SEAMCALL return code if the caller wants it */
+	if (seamcall_ret)
+		*seamcall_ret = sret;
+
+	/* SEAMCALL was successful */
+	if (!sret)
+		goto out;
+
+	switch (sret) {
+	case TDX_SEAMCALL_GP:
+		pr_err_once("[firmware bug]: TDX is not enabled by BIOS.\n");
+		ret = -ENODEV;
+		break;
+	case TDX_SEAMCALL_VMFAILINVALID:
+		pr_err_once("TDX module is not loaded.\n");
+		ret = -ENODEV;
+		break;
+	case TDX_SEAMCALL_UD:
+		pr_err_once("SEAMCALL failed: CPU %d is not in VMX operation.\n",
+				cpu);
+		ret = -EINVAL;
+		break;
+	default:
+		pr_err_once("SEAMCALL failed: CPU %d: leaf %llu, error 0x%llx.\n",
+				cpu, fn, sret);
+		if (out)
+			pr_err_once("additional output: rcx 0x%llx, rdx 0x%llx, r8 0x%llx, r9 0x%llx, r10 0x%llx, r11 0x%llx.\n",
+					out->rcx, out->rdx, out->r8,
+					out->r9, out->r10, out->r11);
+		ret = -EIO;
+	}
+out:
+	put_cpu();
+	return ret;
 }

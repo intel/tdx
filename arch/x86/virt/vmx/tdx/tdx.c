d6b65b861c057e3c6fa811215572244b2ea17d27
Kai Huang <kai.huang@intel.com>
2023-03-07 03:13:58 +1300

    x86/virt/tdx: Configure global KeyID on all packages
    After the list of TDMRs and the global KeyID are configured to the TDX
    module, the kernel needs to configure the key of the global KeyID on all
    packages using TDH.SYS.KEY.CONFIG.
    
    Just use the helper, which conditionally calls function on all online
    cpus, to configure the global KeyID on all packages.  Loop all online
    cpus, keep track which packages have been called and skip all cpus for
    those already called packages.
    
    To keep things simple, this implementation takes no affirmative steps to
    online cpus to make sure there's at least one cpu for each package.  The
    callers (aka. KVM) can ensure success by ensuring that.
    
    Intel hardware doesn't guarantee cache coherency across different
    KeyIDs.  The PAMTs are transitioning from being used by the kernel
    mapping (KeyId 0) to the TDX module's "global KeyID" mapping.
    
    This means that the kernel must flush any dirty KeyID-0 PAMT cachelines
    before the TDX module uses the global KeyID to access the PAMT.
    Otherwise, if those dirty cachelines were written back, they would
    corrupt the TDX module's metadata.  Aside: This corruption would be
    detected by the memory integrity hardware on the next read of the memory
    with the global KeyID.  The result would likely be fatal to the system
    but would not impact TDX security.
    
    Following the TDX module specification, flush cache before configuring
    the global KeyID on all packages.  Given the PAMT size can be large
    (~1/256th of system RAM), just use WBINVD on all CPUs to flush.
    
    Note if any TDH.SYS.KEY.CONFIG fails, the TDX module may already have
    used the global KeyID to write any PAMT.  Therefore, use WBINVD to flush
    cache before freeing the PAMTs back to the kernel.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Reviewed-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Link: https://lore.kernel.org/r/857bc698517a66d45ef85d38efbbfb4e715bb77b.1678111292.git.kai.huang@intel.com

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -1059,6 +1059,55 @@ static int config_tdx_module(struct tdmr_info_list *tdmr_list, u64 global_keyid)
 	return ret;
 }
 
+static int do_global_key_config(void *data)
+{
+	/*
+	 * TDH.SYS.KEY.CONFIG may fail with entropy error (which is a
+	 * recoverable error).  Assume this is exceedingly rare and
+	 * just return error if encountered instead of retrying.
+	 *
+	 * All '0's are just unused parameters.
+	 */
+	return seamcall(TDH_SYS_KEY_CONFIG, 0, 0, 0, 0, NULL, NULL);
+}
+
+/*
+ * Attempt to configure the global KeyID on all physical packages.
+ *
+ * This requires running code on at least one CPU in each package.  If a
+ * package has no online CPUs, that code will not run and TDX module
+ * initialization (TDMR initialization) will fail.
+ *
+ * This code takes no affirmative steps to online CPUs.  Callers (aka.
+ * KVM) can ensure success by ensuring sufficient CPUs are online for
+ * this to succeed.
+ */
+static int config_global_keyid(void)
+{
+	cpumask_var_t packages;
+	int cpu, ret = -EINVAL;
+
+	if (!zalloc_cpumask_var(&packages, GFP_KERNEL))
+		return -ENOMEM;
+
+	for_each_online_cpu(cpu) {
+		if (cpumask_test_and_set_cpu(topology_physical_package_id(cpu),
+					packages))
+			continue;
+
+		/*
+		 * TDH.SYS.KEY.CONFIG cannot run concurrently on
+		 * different cpus, so just do it one by one.
+		 */
+		ret = smp_call_on_cpu(cpu, do_global_key_config, NULL, true);
+		if (ret)
+			break;
+	}
+
+	free_cpumask_var(packages);
+	return ret;
+}
+
 static int init_tdx_module(void)
 {
 	static DECLARE_PADDED_STRUCT(tdsysinfo_struct, tdsysinfo,
@@ -1105,9 +1154,23 @@ static int init_tdx_module(void)
 		goto out_free_pamts;
 
 	/*
+	 * Hardware doesn't guarantee cache coherency across different
+	 * KeyIDs.  The kernel needs to flush PAMT's dirty cachelines
+	 * (associated with KeyID 0) before the TDX module can use the
+	 * global KeyID to access the PAMT.  Given PAMTs are potentially
+	 * large (~1/256th of system RAM), just use WBINVD on all cpus
+	 * to flush the cache.
+	 */
+	wbinvd_on_all_cpus();
+
+	/* Config the key of global KeyID on all packages */
+	ret = config_global_keyid();
+	if (ret)
+		goto out_free_pamts;
+
+	/*
 	 * TODO:
 	 *
-	 *  - Configure the global KeyID on all packages.
 	 *  - Initialize all TDMRs.
 	 *
 	 *  Return error before all steps are done.
@@ -1115,8 +1178,18 @@ static int init_tdx_module(void)
 
 	ret = -EINVAL;
 out_free_pamts:
-	if (ret)
+	if (ret) {
+		/*
+		 * Part of PAMT may already have been initialized by the
+		 * TDX module.  Flush cache before returning PAMT back
+		 * to the kernel.
+		 *
+		 * No need to worry about integrity checks here.  KeyID
+		 * 0 has integrity checking disabled.
+		 */
+		wbinvd_on_all_cpus();
 		tdmrs_free_pamt_all(&tdmr_list);
+	}
 	else
 		pr_info("%lu KBs allocated for PAMT.\n",
 				tdmrs_count_pamt_pages(&tdmr_list) * 4);
@@ -1167,6 +1240,9 @@ static int __tdx_enable(void)
  * This function assumes the caller has: 1) held read lock of CPU hotplug
  * lock to prevent any new cpu from becoming online; 2) done both VMXON
  * and tdx_cpu_enable() on all online cpus.
+ *
+ * This function requires there's at least one online cpu for each CPU
+ * package to succeed.
  *
  * This function can be called in parallel by multiple callers.
  *

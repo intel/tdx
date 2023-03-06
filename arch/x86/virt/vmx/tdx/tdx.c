d5220b1e06f5ae40f564262e1852300ef5678261
Kai Huang <kai.huang@intel.com>
2023-03-07 03:13:50 +1300

    x86/virt/tdx: Add skeleton to enable TDX on demand
    To enable TDX the kernel needs to initialize TDX from two perspectives:
    1) Do a set of SEAMCALLs to initialize the TDX module to make it ready
    to create and run TDX guests; 2) Do the per-cpu initialization SEAMCALL
    on one logical cpu before the kernel wants to make any other SEAMCALLs
    on that cpu (including those involved during module initialization and
    running TDX guests).
    
    The TDX module can be initialized only once in its lifetime.  Instead
    of always initializing it at boot time, this implementation chooses an
    "on demand" approach to initialize TDX until there is a real need (e.g
    when requested by KVM).  This approach has below pros:
    
    1) It avoids consuming the memory that must be allocated by kernel and
    given to the TDX module as metadata (~1/256th of the TDX-usable memory),
    and also saves the CPU cycles of initializing the TDX module (and the
    metadata) when TDX is not used at all.
    
    2) The TDX module design allows it to be updated while the system is
    running.  The update procedure shares quite a few steps with this "on
    demand" initialization mechanism.  The hope is that much of "on demand"
    mechanism can be shared with a future "update" mechanism.  A boot-time
    TDX module implementation would not be able to share much code with the
    update mechanism.
    
    3) Making SEAMCALL requires VMX to be enabled.  Currently, only the KVM
    code mucks with VMX enabling.  If the TDX module were to be initialized
    separately from KVM (like at boot), the boot code would need to be
    taught how to muck with VMX enabling and KVM would need to be taught how
    to cope with that.  Making KVM itself responsible for TDX initialization
    lets the rest of the kernel stay blissfully unaware of VMX.
    
    Similar to module initialization, also make the per-cpu initialization
    "on demand" as it also depends on VMX to be enabled.
    
    Add two functions, tdx_enable() and tdx_cpu_enable(), to enable the TDX
    module and enable TDX on local cpu respectively.  For now tdx_enable()
    is a placeholder.  The TODO list will be pared down as functionality is
    added.
    
    In tdx_enable() use a state machine protected by mutex to make sure the
    initialization will only be done once, as tdx_enable() can be called
    multiple times (i.e. KVM module can be reloaded) and may be called
    concurrently by other kernel components in the future.
    
    The per-cpu initialization on each cpu can only be done once during the
    module's life time.  Use a per-cpu variable to track its status to make
    sure it is only done once in tdx_cpu_enable().
    
    Also, a SEAMCALL to do TDX module global initialization must be done
    once on any logical cpu before any per-cpu initialization SEAMCALL.  Do
    it inside tdx_cpu_enable() too (if hasn't been done).
    
    tdx_enable() can potentially invoke SEAMCALLs on any online cpus.  The
    per-cpu initialization must be done before those SEAMCALLs are invoked
    on some cpu.  To keep things simple, in tdx_cpu_enable(), always do the
    per-cpu initialization regardless of whether the TDX module has been
    initialized or not.  And in tdx_enable(), don't call tdx_cpu_enable()
    but assume the caller has disabled CPU hotplug and done VMXON and
    tdx_cpu_enable() on all online cpus before calling tdx_enable().
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Link: https://lore.kernel.org/r/f150316b975b5ca22c6c4016ffd90db79d657bbf.1678111292.git.kai.huang@intel.com

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -13,6 +13,10 @@
 #include <linux/errno.h>
 #include <linux/printk.h>
 #include <linux/smp.h>
+#include <linux/cpu.h>
+#include <linux/spinlock.h>
+#include <linux/percpu-defs.h>
+#include <linux/mutex.h>
 #include <asm/msr-index.h>
 #include <asm/msr.h>
 #include <asm/tdx.h>
@@ -21,6 +25,18 @@
 static u32 tdx_global_keyid __ro_after_init;
 static u32 tdx_guest_keyid_start __ro_after_init;
 static u32 tdx_nr_guest_keyids __ro_after_init;
+
+static unsigned int tdx_global_init_status;
+static DEFINE_SPINLOCK(tdx_global_init_lock);
+#define TDX_GLOBAL_INIT_DONE	_BITUL(0)
+#define TDX_GLOBAL_INIT_FAILED	_BITUL(1)
+
+static DEFINE_PER_CPU(unsigned int, tdx_lp_init_status);
+#define TDX_LP_INIT_DONE	_BITUL(0)
+#define TDX_LP_INIT_FAILED	_BITUL(1)
+
+static enum tdx_module_status_t tdx_module_status;
+static DEFINE_MUTEX(tdx_module_lock);
 
 /*
  * Use tdx_global_keyid to indicate that TDX is uninitialized.
@@ -159,3 +175,169 @@ out:
 	put_cpu();
 	return ret;
 }
+
+static int try_init_module_global(void)
+{
+	int ret;
+
+	/*
+	 * The TDX module global initialization only needs to be done
+	 * once on any cpu.
+	 */
+	spin_lock(&tdx_global_init_lock);
+
+	if (tdx_global_init_status & TDX_GLOBAL_INIT_DONE) {
+		ret = tdx_global_init_status & TDX_GLOBAL_INIT_FAILED ?
+			-EINVAL : 0;
+		goto out;
+	}
+
+	/* All '0's are just unused parameters. */
+	ret = seamcall(TDH_SYS_INIT, 0, 0, 0, 0, NULL, NULL);
+
+	tdx_global_init_status = TDX_GLOBAL_INIT_DONE;
+	if (ret)
+		tdx_global_init_status |= TDX_GLOBAL_INIT_FAILED;
+out:
+	spin_unlock(&tdx_global_init_lock);
+
+	return ret;
+}
+
+/**
+ * tdx_cpu_enable - Enable TDX on local cpu
+ *
+ * Do one-time TDX module per-cpu initialization SEAMCALL (and TDX module
+ * global initialization SEAMCALL if not done) on local cpu to make this
+ * cpu be ready to run any other SEAMCALLs.
+ *
+ * Note this function must be called when preemption is not possible
+ * (i.e. via SMP call or in per-cpu thread).  It is not IRQ safe either
+ * (i.e. cannot be called in per-cpu thread and via SMP call from remote
+ * cpu simultaneously).
+ *
+ * Return 0 on success, otherwise errors.
+ */
+int tdx_cpu_enable(void)
+{
+	unsigned int lp_status;
+	int ret;
+
+	if (!platform_tdx_enabled())
+		return -EINVAL;
+
+	lp_status = __this_cpu_read(tdx_lp_init_status);
+
+	/* Already done */
+	if (lp_status & TDX_LP_INIT_DONE)
+		return lp_status & TDX_LP_INIT_FAILED ? -EINVAL : 0;
+
+	/*
+	 * The TDX module global initialization is the very first step
+	 * to enable TDX.  Need to do it first (if hasn't been done)
+	 * before doing the per-cpu initialization.
+	 */
+	ret = try_init_module_global();
+
+	/*
+	 * If the module global initialization failed, there's no point
+	 * to do the per-cpu initialization.  Just mark it as done but
+	 * failed.
+	 */
+	if (ret)
+		goto update_status;
+
+	/* All '0's are just unused parameters */
+	ret = seamcall(TDH_SYS_LP_INIT, 0, 0, 0, 0, NULL, NULL);
+
+update_status:
+	lp_status = TDX_LP_INIT_DONE;
+	if (ret)
+		lp_status |= TDX_LP_INIT_FAILED;
+
+	this_cpu_write(tdx_lp_init_status, lp_status);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(tdx_cpu_enable);
+
+static int init_tdx_module(void)
+{
+	/*
+	 * TODO:
+	 *
+	 *  - Get TDX module information and TDX-capable memory regions.
+	 *  - Build the list of TDX-usable memory regions.
+	 *  - Construct a list of "TD Memory Regions" (TDMRs) to cover
+	 *    all TDX-usable memory regions.
+	 *  - Configure the TDMRs and the global KeyID to the TDX module.
+	 *  - Configure the global KeyID on all packages.
+	 *  - Initialize all TDMRs.
+	 *
+	 *  Return error before all steps are done.
+	 */
+	return -EINVAL;
+}
+
+static int __tdx_enable(void)
+{
+	int ret;
+
+	ret = init_tdx_module();
+	if (ret) {
+		pr_err("TDX module initialization failed (%d)\n", ret);
+		tdx_module_status = TDX_MODULE_ERROR;
+		/*
+		 * Just return one universal error code.
+		 * For now the caller cannot recover anyway.
+		 */
+		return -EINVAL;
+	}
+
+	pr_info("TDX module initialized.\n");
+	tdx_module_status = TDX_MODULE_INITIALIZED;
+
+	return 0;
+}
+
+/**
+ * tdx_enable - Enable TDX module to make it ready to run TDX guests
+ *
+ * This function assumes the caller has: 1) held read lock of CPU hotplug
+ * lock to prevent any new cpu from becoming online; 2) done both VMXON
+ * and tdx_cpu_enable() on all online cpus.
+ *
+ * This function can be called in parallel by multiple callers.
+ *
+ * Return 0 if TDX is enabled successfully, otherwise error.
+ */
+int tdx_enable(void)
+{
+	int ret;
+
+	if (!platform_tdx_enabled())
+		return -EINVAL;
+
+	lockdep_assert_cpus_held();
+
+	mutex_lock(&tdx_module_lock);
+
+	switch (tdx_module_status) {
+	case TDX_MODULE_UNKNOWN:
+		ret = __tdx_enable();
+		break;
+	case TDX_MODULE_INITIALIZED:
+		/* Already initialized, great, tell the caller. */
+		ret = 0;
+		break;
+	default:
+		/* Failed to initialize in the previous attempts */
+		ret = -EINVAL;
+		break;
+	}
+
+	mutex_unlock(&tdx_module_lock);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(tdx_enable);

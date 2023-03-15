f22df73e1037c9386bafb788394e790540c3c1a9
Isaku Yamahata <isaku.yamahata@intel.com>
2023-03-15 13:46:31 -0700

    KVM: x86/vmx: initialize loaded_vmcss_on_cpu in vmx_hardware_setup()
    vmx_hardware_disable() accesses loaded_vmcss_on_cpu via
    hardware_disable_all().  To allow hardware_enable/disable_all() before
    kvm_init(), initialize it in vmx_hardware_setup() so that tdx module
    initialization, hardware_setup method, can reference the variable.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -8227,8 +8227,12 @@ __init int vmx_hardware_setup(void)
 {
 	unsigned long host_bndcfgs;
 	struct desc_ptr dt;
+	int cpu;
 	int r;
 
+	/* vmx_hardware_disable() accesses loaded_vmcss_on_cpu. */
+	for_each_possible_cpu(cpu)
+		INIT_LIST_HEAD(&per_cpu(loaded_vmcss_on_cpu, cpu));
 	store_idt(&dt);
 	host_idt_base = dt.address;
 
@@ -8475,11 +8479,8 @@ static int __init vmx_init(void)
 
 	vmx_setup_fb_clear_ctrl();
 
-	for_each_possible_cpu(cpu) {
-		INIT_LIST_HEAD(&per_cpu(loaded_vmcss_on_cpu, cpu));
-
+	for_each_possible_cpu(cpu)
 		pi_init_cpu(cpu);
-	}
 
 #ifdef CONFIG_KEXEC_CORE
 	rcu_assign_pointer(crash_vmclear_loaded_vmcss,

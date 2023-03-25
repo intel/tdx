60ca1b7b16c27d2c2d9fd212bf2a0691c1b43eb8
Isaku Yamahata <isaku.yamahata@intel.com>
2023-03-25 00:22:56 -0700

    [REVERTME] TDX1.4 workaround retry on TDH.MNG.CREATE and TDH.MNG.ADDCX
    From TDX 1.4, TDH.MNG.CREATE and TDH.MNG.ADDCX can return
    TDX_RND_NO_ENTROPY.  Retry it.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -3365,6 +3365,7 @@ static int __tdx_td_init(struct kvm *kvm, struct td_params *td_params)
 	unsigned long *tdcs_pa = NULL;
 	unsigned long tdr_pa = 0;
 	unsigned long va;
+	int retry;
 	int ret, i;
 	u64 err;
 
@@ -3431,8 +3432,16 @@ static int __tdx_td_init(struct kvm *kvm, struct td_params *td_params)
 	 * TDH.PHYMEM.CACHE.WB.
 	 */
 	mutex_lock(&tdx_lock);
-	err = tdh_mng_create(tdr_pa, kvm_tdx->hkid);
+#define TDX_NO_ENTROPY_RETRY_MAX	10
+	retry = 0;
+	do {
+		err = tdh_mng_create(tdr_pa, kvm_tdx->hkid);
+	} while (err == TDX_RND_NO_ENTROPY && retry++ < TDX_NO_ENTROPY_RETRY_MAX);
 	mutex_unlock(&tdx_lock);
+	if (err == TDX_RND_NO_ENTROPY) {
+		ret = -EAGAIN;
+		goto free_packages;
+	}
 	if (WARN_ON_ONCE(err)) {
 		pr_tdx_error(TDH_MNG_CREATE, err, NULL);
 		ret = -EIO;
@@ -3472,7 +3481,16 @@ static int __tdx_td_init(struct kvm *kvm, struct td_params *td_params)
 
 	kvm_tdx->tdcs_pa = tdcs_pa;
 	for (i = 0; i < tdx_info.nr_tdcs_pages; i++) {
-		err = tdh_mng_addcx(kvm_tdx->tdr_pa, tdcs_pa[i]);
+		retry = 0;
+
+		do {
+			err = tdh_mng_addcx(kvm_tdx->tdr_pa, tdcs_pa[i]);
+		} while (err == TDX_RND_NO_ENTROPY && retry++ < TDX_NO_ENTROPY_RETRY_MAX);
+		if (err == TDX_RND_NO_ENTROPY) {
+			/* Here it's hard to allow userspace to retry. */
+			ret = -EBUSY;
+			goto teardown;
+		}
 		if (WARN_ON_ONCE(err)) {
 			pr_tdx_error(TDH_MNG_ADDCX, err, NULL);
 			ret = -EIO;

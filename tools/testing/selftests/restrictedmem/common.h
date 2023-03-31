3e6762343e0ddcb90b2e49af3a341051541d1a3a
Ackerley Tng <ackerleytng@google.com>
2023-03-31 23:50:40 +0000

    selftests: restrictedmem: Check hugepage-ness of shmem file backing restrictedmem fd
    For memfd_restricted() calls without a userspace mount, the backing
    file should be the shmem mount in the kernel, and the size of backing
    pages should be as defined by system-wide shmem configuration.
    
    If a userspace mount is provided, the size of backing pages should be
    as defined in the mount.
    
    Also includes negative tests for invalid inputs, including fds
    representing read-only superblocks/mounts.
    
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Link: https://lore.kernel.org/r/0061b62966d34952fb9f51235d31100df0baf450.1680306489.git.ackerleytng@google.com

---
+++ tools/testing/selftests/restrictedmem/common.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef SELFTESTS_RESTRICTEDMEM_COMMON_H
+#define SELFTESTS_RESTRICTEDMEM_COMMON_H
+
+int memfd_restricted(unsigned int flags, int mount_fd);
+
+#endif  // SELFTESTS_RESTRICTEDMEM_COMMON_H

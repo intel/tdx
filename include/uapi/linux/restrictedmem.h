40c8335f9e7a5477077b5dedf4eee05a9046614d
Ackerley Tng <ackerleytng@google.com>
2023-03-31 23:50:39 +0000

    mm: restrictedmem: Allow userspace to specify mount for memfd_restricted
    By default, the backing shmem file for a restrictedmem fd is created
    on shmem's kernel space mount.
    
    With this patch, an optional tmpfs mount can be specified via an fd,
    which will be used as the mountpoint for backing the shmem file
    associated with a restrictedmem fd.
    
    This will help restrictedmem fds inherit the properties of the
    provided tmpfs mounts, for example, hugepage allocation hints, NUMA
    binding hints, etc.
    
    Permissions for the fd passed to memfd_restricted() is modeled after
    the openat() syscall, since both of these allow creation of a file
    upon a mount/directory.
    
    Permission to reference the mount the fd represents is checked upon fd
    creation by other syscalls (e.g. fsmount(), open(), or open_tree(),
    etc) and any process that can present memfd_restricted() with a valid
    fd is expected to have obtained permission to use the mount
    represented by the fd. This behavior is intended to parallel that of
    the openat() syscall.
    
    memfd_restricted() will check that the tmpfs superblock is
    writable, and that the mount is also writable, before attempting to
    create a restrictedmem file on the mount.
    
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Link: https://lore.kernel.org/r/592ebd9e33a906ba026d56dc68f42d691706f865.1680306489.git.ackerleytng@google.com

---
+++ include/uapi/linux/restrictedmem.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _UAPI_LINUX_RESTRICTEDMEM_H
+#define _UAPI_LINUX_RESTRICTEDMEM_H
+
+/* flags for memfd_restricted */
+#define RMFD_USERMNT		0x0001U
+
+#endif /* _UAPI_LINUX_RESTRICTEDMEM_H */

361dd0d3448d6f774d49e875c8f2974445f426e6
Isaku Yamahata <isaku.yamahata@intel.com>
2023-04-05 11:59:17 -0700

    KVM: TDX: Do TDX specific vcpu initialization
    TD guest vcpu needs TDX specific initialization before running.  Repurpose
    KVM_MEMORY_ENCRYPT_OP to vcpu-scope, add a new sub-command
    KVM_TDX_INIT_VCPU, and implement the callback for it.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    ---
    v20:
    - Support FEATURES0_TOPOLOGY_ENUM
    - Update for the wrapper functions for SEAMCALLs. (Sean)
    - Remove WARN_ON_ONCE() in tdx_vcpu_free().
      WARN_ON_ONCE(vcpu->cpu != -1), WARN_ON_ONCE(tdx->tdvpx_pa),
      WARN_ON_ONCE(tdx->tdvpr_pa)
    - Remove KVM_BUG_ON() in tdx_vcpu_reset().
    - Remove duplicate "tdx->tdvpr_pa=" lines
    - Rename tdvpx to tdcx as it is confusing, follow spec change for same
      reason (Isaku)
    - Updates from seamcall overhaul (Kai)
    
    v18:
    - Use tdh_sys_rd() instead of struct tdsysinfo_struct.
    - Rename tdx_reclaim_td_page() => tdx_reclaim_control_page()
    - Remove the change of tools/arch/x86/include/uapi/asm/kvm.h.

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -6318,6 +6318,12 @@ long kvm_arch_vcpu_ioctl(struct file *filp,
 	case KVM_SET_DEVICE_ATTR:
 		r = kvm_vcpu_ioctl_device_attr(vcpu, ioctl, argp);
 		break;
+	case KVM_MEMORY_ENCRYPT_OP:
+		r = -ENOTTY;
+		if (!kvm_x86_ops.vcpu_mem_enc_ioctl)
+			goto out;
+		r = kvm_x86_ops.vcpu_mem_enc_ioctl(vcpu, argp);
+		break;
 	default:
 		r = -EINVAL;
 	}

17de4f205b7f85556c3405ba0ff7226b883beb12
Isaku Yamahata <isaku.yamahata@intel.com>
2023-05-16 14:37:06 -0700

    [REVERTME] x86/mce, KVM: TDX: Skip MC and let KVM invoke MC handler for mc in TD
    When BIOS eMCA MCE-SMI morphing is enabled, the #MC happens in TD guest is
    morphed to SMI. Then the SMI causes TD exit reason (6) to KVM.  Once
    SEAMCALL(TDENTER) returns from SEAM module, the SMI is delivered and bios
    SMI handler is called. After some handling, the SMI handler delivers #MC to
    OS and returns back to OS. In this case, host kernel #MC handler has no
    idea of whether the #MC is caused by a TD guest. So if the #MC is fatal
    type, it will panic the kernel. We really don't want this.
    
    Add a new fixup type, EX_TYPE_MC, and add fixup #MC right after TD exit to
    other SMI TD exit.  Add callback to check if the #MC is caused by TD exit.
    It's the same as what KVM does for #MC in legacy VM that calling
    kvm_machine_check() with a fake ring 3 context.  Because the MC handle must
    be called on the CPU that received MC, it should be handled after TD exit
    and before enabling preemption.
    
    Note, it's different from the case that TD exit due to #MC. In that case,
    KVM calls kvm_machine_check(), which fakes a ring 3 pt_regs so that
    fatal machine just causes user space VMM to be killed.
    
    abbreviations:
    MCE: Machine Check
    MSMI: Machine Check System Management Interrupt
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/include/asm/asm.h
+++ arch/x86/include/asm/asm.h
@@ -221,5 +221,8 @@ register unsigned long current_stack_pointer asm(_ASM_SP);
 #define _ASM_EXTABLE_FAULT(from, to)				\
 	_ASM_EXTABLE_TYPE(from, to, EX_TYPE_FAULT)
 
+#define _ASM_EXTABLE_TDX_MC(from, to)				\
+	_ASM_EXTABLE_TYPE(from, to, EX_TYPE_TDX_MC)
+
 #endif /* __KERNEL__ */
 #endif /* _ASM_X86_ASM_H */

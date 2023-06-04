347051b70ef2a2bdbd81b85c1b87345088a55ef1
Kai Huang <kai.huang@intel.com>
2023-06-05 02:27:23 +1200

    x86/virt/tdx: Add placeholder to construct TDMRs to cover all TDX memory regions
    After the kernel selects all TDX-usable memory regions, the kernel needs
    to pass those regions to the TDX module via data structure "TD Memory
    Region" (TDMR).
    
    Add a placeholder to construct a list of TDMRs (in multiple steps) to
    cover all TDX-usable memory regions.
    
    === Long Version ===
    
    TDX provides increased levels of memory confidentiality and integrity.
    This requires special hardware support for features like memory
    encryption and storage of memory integrity checksums.  Not all memory
    satisfies these requirements.
    
    As a result, TDX introduced the concept of a "Convertible Memory Region"
    (CMR).  During boot, the firmware builds a list of all of the memory
    ranges which can provide the TDX security guarantees.  The list of these
    ranges is available to the kernel by querying the TDX module.
    
    The TDX architecture needs additional metadata to record things like
    which TD guest "owns" a given page of memory.  This metadata essentially
    serves as the 'struct page' for the TDX module.  The space for this
    metadata is not reserved by the hardware up front and must be allocated
    by the kernel and given to the TDX module.
    
    Since this metadata consumes space, the VMM can choose whether or not to
    allocate it for a given area of convertible memory.  If it chooses not
    to, the memory cannot receive TDX protections and can not be used by TDX
    guests as private memory.
    
    For every memory region that the VMM wants to use as TDX memory, it sets
    up a "TD Memory Region" (TDMR).  Each TDMR represents a physically
    contiguous convertible range and must also have its own physically
    contiguous metadata table, referred to as a Physical Address Metadata
    Table (PAMT), to track status for each page in the TDMR range.
    
    Unlike a CMR, each TDMR requires 1G granularity and alignment.  To
    support physical RAM areas that don't meet those strict requirements,
    each TDMR permits a number of internal "reserved areas" which can be
    placed over memory holes.  If PAMT metadata is placed within a TDMR it
    must be covered by one of these reserved areas.
    
    Let's summarize the concepts:
    
     CMR - Firmware-enumerated physical ranges that support TDX.  CMRs are
           4K aligned.
    TDMR - Physical address range which is chosen by the kernel to support
           TDX.  1G granularity and alignment required.  Each TDMR has
           reserved areas where TDX memory holes and overlapping PAMTs can
           be represented.
    PAMT - Physically contiguous TDX metadata.  One table for each page size
           per TDMR.  Roughly 1/256th of TDMR in size.  256G TDMR = ~1G
           PAMT.
    
    As one step of initializing the TDX module, the kernel configures
    TDX-usable memory regions by passing a list of TDMRs to the TDX module.
    
    Constructing the list of TDMRs consists below steps:
    
    1) Fill out TDMRs to cover all memory regions that the TDX module will
       use for TD memory.
    2) Allocate and set up PAMT for each TDMR.
    3) Designate reserved areas for each TDMR.
    
    Add a placeholder to construct TDMRs to do the above steps.  To keep
    things simple, just allocate enough space to hold maximum number of
    TDMRs up front.
    
    Keep the TDMRs if module initialization is successful.  A later patch
    will need to use them to find PAMTs after module initialization.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Reviewed-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Link: https://lore.kernel.org/r/f9148e67e968d7aed4707b67ea9b1aa761401255.1685887183.git.kai.huang@intel.com

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -24,6 +24,7 @@
 #include <linux/minmax.h>
 #include <linux/sizes.h>
 #include <linux/pfn.h>
+#include <linux/align.h>
 #include <asm/msr-index.h>
 #include <asm/msr.h>
 #include <asm/archrandom.h>
@@ -49,6 +50,8 @@ static DEFINE_MUTEX(tdx_module_lock);
 
 /* All TDX-usable memory regions.  Protected by mem_hotplug_lock. */
 static LIST_HEAD(tdx_memlist);
+
+static struct tdmr_info_list tdx_tdmr_list;
 
 /*
  * Wrapper of __seamcall() to convert SEAMCALL leaf function error code
@@ -329,6 +332,80 @@ err:
 	return ret;
 }
 
+/* Calculate the actual TDMR size */
+static int tdmr_size_single(u16 max_reserved_per_tdmr)
+{
+	int tdmr_sz;
+
+	/*
+	 * The actual size of TDMR depends on the maximum
+	 * number of reserved areas.
+	 */
+	tdmr_sz = sizeof(struct tdmr_info);
+	tdmr_sz += sizeof(struct tdmr_reserved_area) * max_reserved_per_tdmr;
+
+	return ALIGN(tdmr_sz, TDMR_INFO_ALIGNMENT);
+}
+
+static int alloc_tdmr_list(struct tdmr_info_list *tdmr_list,
+			   struct tdsysinfo_struct *sysinfo)
+{
+	size_t tdmr_sz, tdmr_array_sz;
+	void *tdmr_array;
+
+	tdmr_sz = tdmr_size_single(sysinfo->max_reserved_per_tdmr);
+	tdmr_array_sz = tdmr_sz * sysinfo->max_tdmrs;
+
+	/*
+	 * To keep things simple, allocate all TDMRs together.
+	 * The buffer needs to be physically contiguous to make
+	 * sure each TDMR is physically contiguous.
+	 */
+	tdmr_array = alloc_pages_exact(tdmr_array_sz,
+			GFP_KERNEL | __GFP_ZERO);
+	if (!tdmr_array)
+		return -ENOMEM;
+
+	tdmr_list->tdmrs = tdmr_array;
+
+	/*
+	 * Keep the size of TDMR to find the target TDMR
+	 * at a given index in the TDMR list.
+	 */
+	tdmr_list->tdmr_sz = tdmr_sz;
+	tdmr_list->max_tdmrs = sysinfo->max_tdmrs;
+	tdmr_list->nr_consumed_tdmrs = 0;
+
+	return 0;
+}
+
+static void free_tdmr_list(struct tdmr_info_list *tdmr_list)
+{
+	free_pages_exact(tdmr_list->tdmrs,
+			tdmr_list->max_tdmrs * tdmr_list->tdmr_sz);
+}
+
+/*
+ * Construct a list of TDMRs on the preallocated space in @tdmr_list
+ * to cover all TDX memory regions in @tmb_list based on the TDX module
+ * information in @sysinfo.
+ */
+static int construct_tdmrs(struct list_head *tmb_list,
+			   struct tdmr_info_list *tdmr_list,
+			   struct tdsysinfo_struct *sysinfo)
+{
+	/*
+	 * TODO:
+	 *
+	 *  - Fill out TDMRs to cover all TDX memory regions.
+	 *  - Allocate and set up PAMTs for each TDMR.
+	 *  - Designate reserved areas for each TDMR.
+	 *
+	 * Return -EINVAL until constructing TDMRs is done
+	 */
+	return -EINVAL;
+}
+
 static int init_tdx_module(void)
 {
 	static DECLARE_PADDED_STRUCT(tdsysinfo_struct, tdsysinfo,
@@ -358,11 +435,19 @@ static int init_tdx_module(void)
 	if (ret)
 		goto out;
 
+	/* Allocate enough space for constructing TDMRs */
+	ret = alloc_tdmr_list(&tdx_tdmr_list, sysinfo);
+	if (ret)
+		goto out_free_tdx_mem;
+
+	/* Cover all TDX-usable memory regions in TDMRs */
+	ret = construct_tdmrs(&tdx_memlist, &tdx_tdmr_list, sysinfo);
+	if (ret)
+		goto out_free_tdmrs;
+
 	/*
 	 * TODO:
 	 *
-	 *  - Construct a list of "TD Memory Regions" (TDMRs) to cover
-	 *    all TDX-usable memory regions.
 	 *  - Configure the TDMRs and the global KeyID to the TDX module.
 	 *  - Configure the global KeyID on all packages.
 	 *  - Initialize all TDMRs.
@@ -370,6 +455,12 @@ static int init_tdx_module(void)
 	 *  Return error before all steps are done.
 	 */
 	ret = -EINVAL;
+out_free_tdmrs:
+	if (ret)
+		free_tdmr_list(&tdx_tdmr_list);
+out_free_tdx_mem:
+	if (ret)
+		free_tdx_memlist(&tdx_memlist);
 out:
 	/*
 	 * @tdx_memlist is written here and read at memory hotplug time.

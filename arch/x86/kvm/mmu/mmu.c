0fc1289f0775d8be241a66f1e43a20faf11e5073
Michael Roth <michael.roth@amd.com>
2023-06-11 23:25:09 -0500

    KVM: x86: Add gmem hook for initializing private memory
    All gmem pages are expected to be 'private' as defined by a particular
    arch/platform. Platforms like SEV-SNP require additional operations to
    move these pages into a private state, so implement a hook that can be
    used to prepare this memory prior to mapping it into a guest.
    
    In the case of SEV-SNP, whether or not a 2MB page can be mapped via a
    2MB mapping in the guest's nested page table depends on whether or not
    any subpages within the range have already been initialized as private
    in the RMP table, so this hook will also be used by the KVM MMU to clamp
    the maximum mapping size accordingly.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Link: https://lore.kernel.org/r/20230612042559.375660-2-michael.roth@amd.com
    
    ---
    Changes v2 -> v3:
    - Newly added

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -4354,6 +4354,7 @@ static int kvm_faultin_pfn_private(struct kvm_vcpu *vcpu,
 				   struct kvm_page_fault *fault)
 {
 	int max_order, r;
+	u8 max_level;
 
 	if (!kvm_slot_can_be_private(fault->slot))
 		return kvm_do_memory_fault_exit(vcpu, fault);
@@ -4363,8 +4364,15 @@ static int kvm_faultin_pfn_private(struct kvm_vcpu *vcpu,
 	if (r)
 		return r;
 
-	fault->max_level = min(kvm_max_level_for_order(max_order),
-			       fault->max_level);
+	max_level = kvm_max_level_for_order(max_order);
+	r = static_call(kvm_x86_gmem_prepare)(vcpu->kvm, fault->slot, fault->pfn,
+					      fault->gfn, &max_level);
+	if (r) {
+		kvm_release_pfn_clean(fault->pfn);
+		return r;
+	}
+
+	fault->max_level = min(max_level, fault->max_level);
 	fault->map_writable = !(fault->slot->flags & KVM_MEM_READONLY);
 	return RET_PF_CONTINUE;
 }

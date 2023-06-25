1db840705f8f422ce480225d3d0ad5ef593484b2
Kai Huang <kai.huang@intel.com>
2023-06-26 01:50:14 +1200

    x86/kexec(): Reset TDX private memory on platforms with TDX erratum
    The first few generations of TDX hardware have an erratum.  A partial
    write to a TDX private memory cacheline will silently "poison" the
    line.  Subsequent reads will consume the poison and generate a machine
    check.  According to the TDX hardware spec, neither of these things
    should have happened.
    
    == Background ==
    
    Virtually all kernel memory accesses operations happen in full
    cachelines.  In practice, writing a "byte" of memory usually reads a 64
    byte cacheline of memory, modifies it, then writes the whole line back.
    Those operations do not trigger this problem.
    
    This problem is triggered by "partial" writes where a write transaction
    of less than cacheline lands at the memory controller.  The CPU does
    these via non-temporal write instructions (like MOVNTI), or through
    UC/WC memory mappings.  The issue can also be triggered away from the
    CPU by devices doing partial writes via DMA.
    
    == Problem ==
    
    A fast warm reset doesn't reset TDX private memory.  Kexec() can also
    boot into the new kernel directly.  Thus if the old kernel has enabled
    TDX on the platform with this erratum, the new kernel may get unexpected
    machine check.
    
    Note that w/o this erratum any kernel read/write on TDX private memory
    should never cause machine check, thus it's OK for the old kernel to
    leave TDX private pages as is.
    
    == Solution ==
    
    In short, with this erratum, the kernel needs to explicitly convert all
    TDX private pages back to normal to give the new kernel a clean slate
    after kexec().  The BIOS is also expected to disable fast warm reset as
    a workaround to this erratum, thus this implementation doesn't try to
    reset TDX private memory for the reboot case in the kernel but depend on
    the BIOS to enable the workaround.
    
    For now TDX private memory can only be PAMT pages.  It would be ideal to
    cover all types of TDX private memory here (TDX guest private pages and
    Secure-EPT pages are yet to be implemented when TDX gets supported in
    KVM), but there's no existing infrastructure to track TDX private pages.
    It's not feasible to query the TDX module about page type either because
    VMX has already been stopped when KVM receives the reboot notifier.
    
    Another option is to blindly convert all memory pages.  But this may
    bring non-trivial latency to kexec() on large memory systems (especially
    when the number of TDX private pages is small).  Thus even with this
    temporary solution, eventually it's better for the kernel to only reset
    TDX private pages.  Also, it's problematic to convert all memory pages
    because not all pages are mapped as writable in the direct-mapping.  The
    kernel needs to switch to another page table which maps all pages as
    writable (e.g., the identical-mapping table for kexec(), or a new page
    table) to do so, but this looks overkill.
    
    Therefore, rather than doing something dramatic, only reset PAMT pages
    for now.  Do it in machine_kexec() to avoid additional overhead to the
    machine reboot/shutdown as the kernel depends on the BIOS to disable
    fast warm reset as a workaround for the reboot case.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -83,10 +83,12 @@ static inline long tdx_kvm_hypercall(unsigned int nr, unsigned long p1,
 bool platform_tdx_enabled(void);
 int tdx_cpu_enable(void);
 int tdx_enable(void);
+void tdx_reset_memory(void);
 #else	/* !CONFIG_INTEL_TDX_HOST */
 static inline bool platform_tdx_enabled(void) { return false; }
 static inline int tdx_cpu_enable(void) { return -ENODEV; }
 static inline int tdx_enable(void)  { return -ENODEV; }
+static inline void tdx_reset_memory(void) { }
 #endif	/* CONFIG_INTEL_TDX_HOST */
 
 #endif /* !__ASSEMBLY__ */

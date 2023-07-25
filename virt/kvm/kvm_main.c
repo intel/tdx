e89b32f9f5706f5db9e2be0241caf735e131d998
Wei Wang <wei.w.wang@intel.com>
2023-07-25 16:53:10 +0800

    KVM: move KVM_CAP_DEVICE_CTRL to the generic check
    KVM_CAP_DEVICE_CTRL allows userspace to check if the kvm_device
    framework (e.g. KVM_CREATE_DEVICE) is supported by KVM. Move
    KVM_CAP_DEVICE_CTRL to the generic check for the two reasons:
    1) it already supports arch agnostic usages (i.e. KVM_DEV_TYPE_VFIO).
    For example, userspace VFIO implementation may needs to create
    KVM_DEV_TYPE_VFIO on x86, riscv, or arm etc. It is simpler to have it
    checked at the generic code than at each arch's code.
    2) KVM_CREATE_DEVICE has been added to the generic code.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -4885,6 +4885,7 @@ static int kvm_vm_ioctl_check_extension_generic(struct kvm *kvm, long arg)
 #endif
 	case KVM_CAP_BINARY_STATS_FD:
 	case KVM_CAP_SYSTEM_EVENT_DATA:
+	case KVM_CAP_DEVICE_CTRL:
 		return 1;
 	default:
 		break;

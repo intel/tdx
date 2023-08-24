27bda3ddc24307ab7f8a0042ec1424658fb8b230
Wei Wang <wei.w.wang@intel.com>
2023-08-24 15:42:46 +0800

    KVM: TDX: add seamcall TDH_IMPORT_COMMIT
    The TDH_IMPORT_COMMIT seamcall ends the memory pre-copy session and allows
    the memory post-copy session to start if needed (post-copy is
    optional). This is typically invoked after the start token is received
    and imported on the destination.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/vmx/tdx_ops.h
+++ arch/x86/kvm/vmx/tdx_ops.h
@@ -522,4 +522,9 @@ static inline u64 tdh_import_track(hpa_t tdr,
 			    mig_stream_info, 0, NULL);
 }
 
+static inline u64 tdh_import_commit(hpa_t tdr)
+{
+	return tdx_seamcall(TDH_IMPORT_COMMIT, tdr, 0, 0, 0, 0, 0, NULL);
+}
+
 #endif /* __KVM_X86_TDX_OPS_H */

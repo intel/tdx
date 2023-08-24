d83d9609e9b15fde35a02e7a5e919288d2d98c69
Wei Wang <wei.w.wang@intel.com>
2023-08-24 16:29:13 +0800

    KVM: allow private memslots to change flags
    To support guest live migration, userspace can request to write-protect
    the TD private memory by removing the "w" bit. Allow the private memory
    slot to change flags to do write-protection. The old slot is going to
    be replaced by a new slot with the new flag, so unbind the gmem that has
    been bound to the old slot.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -2118,9 +2118,6 @@ int __kvm_set_memory_region(struct kvm *kvm,
 		if ((kvm->nr_memslot_pages + npages) < kvm->nr_memslot_pages)
 			return -EINVAL;
 	} else { /* Modify an existing slot. */
-		/* Private memslots are immutable, they can only be deleted. */
-		if (mem->flags & KVM_MEM_PRIVATE)
-			return -EINVAL;
 		if ((mem->userspace_addr != old->userspace_addr) ||
 		    (npages != old->npages) ||
 		    ((mem->flags ^ old->flags) & KVM_MEM_READONLY))
@@ -2150,6 +2147,11 @@ int __kvm_set_memory_region(struct kvm *kvm,
 	new->flags = mem->flags;
 	new->userspace_addr = mem->userspace_addr;
 	if (mem->flags & KVM_MEM_PRIVATE) {
+		if (change == KVM_MR_FLAGS_ONLY) {
+			kvm_gmem_unbind(old);
+			kvm_memslot_gmem_fput(old);
+		}
+
 		r = kvm_memslot_gmem_fget(new, mem->gmem_fd);
 		if (r)
 			goto out;

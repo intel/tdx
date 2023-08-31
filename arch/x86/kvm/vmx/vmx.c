e07852d0427dca81db2a3a021a4c39a7943c2266
Kai Huang <kai.huang@intel.com>
2023-08-31 10:17:25 +0800

    x86/virt: Add infrastructure to do basic VMXON/VMXOFF in the core kernel
    Intel Trust Domain Extensions (TDX) protects guest VMs from malicious
    host and certain physical attacks.  TDX host kernel support requires to
    do VMXON/VMXOFF outside of KVM.
    
    Add an infrastructure to do basic VMXON/VMXOFF in the core kernel as a
    preparation to support TDX in the host by mimicking KVM's implementation.
    Change KVM to use this basic VMX infrastructure.
    
    Add a new "arch/x86/virt/vmx/vmx.c" to hold generic VMXON/VMXOFF code in
    the core kernel.  Introduce a new Kconfig HAVE_VMX_GENERIC for it.  Make
    KVM_INTEL select it.
    
    Note init_ia32_feat_ctl() only tries to enable VMX when KVM_INTEL is on.
    Change it to try to enable VMX when HAVE_VMX_GENERIC is on.
    
    VMXON requires a 4K aligned "VMXON region" (which can be smaller than
    4K, but never be larger) with its "revision_id" being set to the value
    reported by MSR_IA32_VMX_BASIC on local cpu.
    
    Use a per-cpu variable to cache the value of MSR_IA32_VMX_BASIC, and a
    per-cpu "VMXON region" to do VMXON for each cpu.  Introduce a new CPU
    hotplug callback to prepare the "VMXON region" to make the new cpu ready
    to do VMXON: read the MSR, allocate the VMXON region and set up the
    "revision_id".
    
    Always allow to online cpu even when the preparation of VMXON region
    fails.  Those "non-VMXON-ready" cpus are perfectly usable if there's no
    user of VMX, for instance, when KVM is not loaded.  KVM (as a user of
    VMX) itself further makes sure those "non-VMXON-ready" cpus won't be
    used by KVM: KVM either refuses to load the module when any online cpu
    is not VMXON-ready, or refuses to online any incompatible cpu.
    
    Expose the per-cpu value of MSR_IA32_VMX_BASIC, as KVM needs other
    information encoded in this MSR to do cpu compatibility check across all
    online cpus.
    
    Add two functions cpu_vmxop_{get|put}() to provide a reference-based
    approach to do VMXON/VMXOFF, so that they can be used by multiple users.
    
    Remove relevant implementation in KVM and change to use this basic VMX
    infrastructure.
    
    Note, during reboot, the kernel needs to turn off VMX on all cpus.  This
    basic VMX support doesn't explicitly do that, but depends on the users
    to make sure they don't take any reference of VMX during reboot.  KVM
    already handles this.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Xu Yilun <yilun.xu@intel.com>
    Signed-off-by: Chao Gao <chao.gao@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -475,7 +475,6 @@ noinline void invept_error(unsigned long ext, u64 eptp, gpa_t gpa)
 			ext, eptp, gpa);
 }
 
-static DEFINE_PER_CPU(struct vmcs *, vmxarea);
 DEFINE_PER_CPU(struct vmcs *, current_vmcs);
 /*
  * We maintain a per-CPU linked-list of VMCS loaded on that CPU. This is needed
@@ -748,7 +747,7 @@ static void vmx_emergency_disable(void)
 			    loaded_vmcss_on_cpu_link)
 		vmcs_clear(v->vmcs);
 
-	cpu_vmxoff();
+	cpu_vmxop_put();
 }
 
 static void __loaded_vmcs_clear(void *arg)
@@ -2658,21 +2657,9 @@ static int setup_vmcs_config(struct vmcs_config *vmcs_conf,
 		_vmexit_control &= ~x_ctrl;
 	}
 
-	rdmsrl(MSR_IA32_VMX_BASIC, basic_msr);
-
-	/* IA-32 SDM Vol 3B: VMCS size is never greater than 4kB. */
-	if (vmx_basic_vmcs_size(basic_msr) > PAGE_SIZE)
-		return -EIO;
-
-#ifdef CONFIG_X86_64
-	/* IA-32 SDM Vol 3B: 64-bit CPUs always have VMX_BASIC_MSR[48]==0. */
-	if (basic_msr & VMX_BASIC_64)
-		return -EIO;
-#endif
-
-	/* Require Write-Back (WB) memory type for VMCS accesses. */
-	if (((basic_msr & VMX_BASIC_MEM_TYPE_MASK) >> VMX_BASIC_MEM_TYPE_SHIFT)
-			!= VMX_BASIC_MEM_TYPE_WB)
+	basic_msr = this_cpu_read(vmx_basic);
+	/* A valid MSR_IA32_VMX_BASIC value cannot be 0 */
+	if (!basic_msr)
 		return -EIO;
 
 	rdmsrl(MSR_IA32_VMX_MISC, misc_msr);
@@ -2754,7 +2741,7 @@ void vmxoff_put(int off)
 	WARN_ON_ONCE(off < 0);
 
 	if (off)
-		cpu_vmxoff();
+		cpu_vmxop_put();
 
 	kvm_hardware_enable_unlock();
 	cpus_read_unlock();
@@ -2773,7 +2760,7 @@ int vmxon_get(void)
 	if (cr4_read_shadow() & X86_CR4_VMXE)
 		return 0;
 
-       if (!cpu_vmxon(__pa(this_cpu_read(vmxarea))))
+       if (!cpu_vmxop_get())
 	       return 1;
 
        preempt_enable();
@@ -2786,7 +2773,6 @@ EXPORT_SYMBOL_GPL(vmxon_get);
 int vmx_hardware_enable(void)
 {
 	int cpu = raw_smp_processor_id();
-	u64 phys_addr = __pa(per_cpu(vmxarea, cpu));
 	int r;
 
 	/*
@@ -2796,16 +2782,9 @@ int vmx_hardware_enable(void)
 	if (kvm_is_using_evmcs() && !hv_get_vp_assist_page(cpu))
 		return -EFAULT;
 
-	if (cr4_read_shadow() & X86_CR4_VMXE)
-		return -EBUSY;
-
-	intel_pt_handle_vmx(1);
-
-	r = cpu_vmxon(phys_addr);
-	if (r) {
-		intel_pt_handle_vmx(0);
+	r = cpu_vmxop_get();
+	if (r)
 		return r;
-	}
 
 	if (enable_ept)
 		ept_sync_global();
@@ -2827,10 +2806,8 @@ void vmx_hardware_disable(void)
 {
 	vmclear_local_loaded_vmcss();
 
-	if (cpu_vmxoff())
+	if (cpu_vmxop_put())
 		kvm_spurious_fault();
-
-	intel_pt_handle_vmx(0);
 
 	hv_reset_evmcs();
 }
@@ -2918,34 +2895,6 @@ int alloc_loaded_vmcs(struct loaded_vmcs *loaded_vmcs)
 out_vmcs:
 	free_loaded_vmcs(loaded_vmcs);
 	return -ENOMEM;
-}
-
-static void free_kvm_area(void)
-{
-	int cpu;
-
-	for_each_possible_cpu(cpu) {
-		free_vmcs(per_cpu(vmxarea, cpu));
-		per_cpu(vmxarea, cpu) = NULL;
-	}
-}
-
-static __init int alloc_kvm_area(void)
-{
-	int cpu;
-
-	for_each_possible_cpu(cpu) {
-		struct vmcs *vmcs;
-
-		vmcs = __alloc_vmcs_cpu(cpu, GFP_KERNEL);
-		if (!vmcs) {
-			free_kvm_area();
-			return -ENOMEM;
-		}
-
-		per_cpu(vmxarea, cpu) = vmcs;
-	}
-	return 0;
 }
 
 static void fix_pmode_seg(struct kvm_vcpu *vcpu, int seg,
@@ -8067,8 +8016,6 @@ void vmx_hardware_unsetup(void)
 
 	if (nested)
 		nested_vmx_hardware_unsetup();
-
-	free_kvm_area();
 }
 
 #define VMX_REQUIRED_APICV_INHIBITS				\
@@ -8498,10 +8445,6 @@ __init int vmx_hardware_setup(void)
 	}
 
 	vmx_set_cpu_caps();
-
-	r = alloc_kvm_area();
-	if (r && nested)
-		nested_vmx_hardware_unsetup();
 
 	kvm_set_posted_intr_wakeup_handler(pi_wakeup_handler);
 

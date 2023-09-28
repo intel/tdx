0dd308f33c33199d7928de8c47b47a4f398c5328
Sagi Shahar <sagis@google.com>
2023-09-28 14:57:34 -0700

    Add test for large TDX VM
    Change-Id: Id341715b2eb882b7a0a88157b680108150b0f7b7

--- tools/testing/selftests/kvm/include/x86_64/tdx/tdx_util.h
+++ tools/testing/selftests/kvm/include/x86_64/tdx/tdx_util.h
@@ -11,6 +11,9 @@ struct kvm_vcpu *td_vcpu_add(struct kvm_vm *vm, uint32_t vcpu_id, void *guest_co
 struct kvm_vm *td_create(void);
 void td_initialize(struct kvm_vm *vm, enum vm_mem_backing_src_type src_type,
 		   uint64_t attributes);
+void td_initialize_with_extra_memory(struct kvm_vm *vm,
+				     enum vm_mem_backing_src_type src_type,
+				     uint64_t attributes, uint64_t extra_pages);
 void td_finalize(struct kvm_vm *vm);
 void td_vcpu_run(struct kvm_vcpu *vcpu);
 void handle_memory_conversion(struct kvm_vm *vm, uint64_t gpa, uint64_t size,

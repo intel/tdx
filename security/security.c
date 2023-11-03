53ccdf92fe9f7296212f290d072fde16dffa541c
Isaku Yamahata <isaku.yamahata@intel.com>
2023-11-03 01:27:24 -0700

    security: Export security_inode_init_security_anon()
    KVM guest_memfd() uses security_inode_init_security_anon().
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- security/security.c
+++ security/security.c
@@ -1777,6 +1777,7 @@ int security_inode_init_security_anon(struct inode *inode,
 	return call_int_hook(inode_init_security_anon, 0, inode, name,
 			     context_inode);
 }
+EXPORT_SYMBOL(security_inode_init_security_anon);
 
 #ifdef CONFIG_SECURITY_PATH
 /**

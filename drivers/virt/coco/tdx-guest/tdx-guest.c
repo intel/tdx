2203fdee94ff07f81261eb34978778fdb953e15c
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2023-11-07 06:56:05 +0000

    virt: coco: tdx-guest: Add service quote support
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- drivers/virt/coco/tdx-guest/tdx-guest.c
+++ drivers/virt/coco/tdx-guest/tdx-guest.c
@@ -38,6 +38,13 @@
 #define TDX_RTMR_EXTEND_LEN		48
 #define TDX_RTMR_BUF_LEN		64
 
+/* TDX GetQuote service codes */
+#define SERVICE_QUOTE_REQ_BUF_LEN	SZ_4K
+#define SERVICE_QUOTE_RESP_BUF_LEN	SZ_16K
+
+#define SERVICE_QUOTE_SEND_CMD		0x03
+#define SERVICE_QUOTE_CMD_TIMEOUT	5000
+
 /* struct tdx_quote_buf: Format of Quote request buffer.
  * @version: Quote format version, filled by TD.
  * @status: Status code of Quote request, filled by VMM.
@@ -57,6 +64,55 @@ struct tdx_quote_buf {
 	u8 data[];
 };
 
+/**
+ * struct att_service_req - Attestation Service command buffer
+ * hdr: Command buffer header.
+ * @version: Command version (default is 0).
+ * @cmd: Command ID.
+ * @rsvd: Reserved for future use.
+ * @len: Length of the report data.
+ * @data: Report data used in Quote generation
+ */
+struct att_service_req
+{
+	/* Command buffer header */
+	struct tdx_service_req_hdr hdr;
+
+	/* Command data */
+	u8 version;
+	u8 cmd;
+	u16 rsvd;
+
+	/* Report data */
+	u32 len;
+	u8 data[];
+} __packed;
+
+/**
+ * struct att_service_resp - Attestation Service response buffer
+ * @hdr: Response buffer header.
+ * @version: Command version (default is 0).
+ * @cmd: Command ID.
+ * @status: Response status.
+ * @len: Length of the Quote data
+ * @data: Quote data.
+ */
+struct att_service_resp
+{
+	/* Response buffer header */
+	struct tdx_service_resp_hdr hdr;
+
+	/* Command data */
+	u8 version;
+	u8 cmd;
+	u16 status;
+
+	/* Quote data */
+	u32 len;
+	u8 data[];
+} __packed;
+
+
 /* Quote data buffer */
 static void *quote_data;
 
@@ -68,6 +124,11 @@ static DEFINE_MUTEX(quote_lock);
  * is enough time for QE to respond to any Quote requests.
  */
 static u32 getquote_timeout = 30;
+
+/* TDX service command request/response buffers */
+static void *req_buf, *resp_buf;
+
+static guid_t att_service_guid = GUID_INIT(0xdeadbeef, 0xdead, 0xbeef, 0xde, 0xad, 0xde, 0xad, 0xbe, 0xaf, 0xbe, 0xaf);
 
 static long tdx_get_report0(struct tdx_report_req __user *req)
 {
@@ -104,35 +165,112 @@ out:
 	return ret;
 }
 
-static void free_quote_buf(void *buf)
+static void free_shared_pages(void *addr, size_t len)
 {
-	size_t len = PAGE_ALIGN(GET_QUOTE_BUF_SIZE);
-	unsigned int count = len >> PAGE_SHIFT;
+	size_t aligned_len = PAGE_ALIGN(len);
+	unsigned int count = aligned_len >> PAGE_SHIFT;
 
-	if (set_memory_encrypted((unsigned long)buf, count)) {
+	if (set_memory_encrypted((unsigned long)addr, count)) {
 		pr_err("Failed to restore encryption mask for Quote buffer, leak it\n");
 		return;
 	}
 
-	free_pages_exact(buf, len);
+	free_pages_exact(addr, aligned_len);
+
 }
 
-static void *alloc_quote_buf(void)
+static void *alloc_shared_pages(size_t len)
 {
-	size_t len = PAGE_ALIGN(GET_QUOTE_BUF_SIZE);
-	unsigned int count = len >> PAGE_SHIFT;
+	size_t aligned_len = PAGE_ALIGN(len);
+	unsigned int count = aligned_len >> PAGE_SHIFT;
 	void *addr;
 
-	addr = alloc_pages_exact(len, GFP_KERNEL | __GFP_ZERO);
+	addr = alloc_pages_exact(aligned_len, GFP_KERNEL | __GFP_ZERO);
 	if (!addr)
 		return NULL;
 
 	if (set_memory_decrypted((unsigned long)addr, count)) {
-		free_pages_exact(addr, len);
+		free_pages_exact(addr, aligned_len);
 		return NULL;
 	}
 
 	return addr;
+
+}
+
+static int tdx_service_gen_quote(struct tsm_report *report, u8 *tdreport)
+{
+	struct att_service_resp *resp = resp_buf;
+	struct att_service_req *req = req_buf;
+	int ret;
+	u8 *buf;
+	u64 err;
+
+	/* Check whether the service is supported */
+	ret = tdx_hcall_query_service(req_buf, resp_buf, (u8 *)&att_service_guid);
+	if (ret < 0)
+		return ret;
+
+	/* Initialize request service header */
+	memcpy(req->hdr.guid, &att_service_guid, sizeof(guid_t));
+	req->hdr.buf_len = sizeof(*req) + TDX_REPORT_LEN;
+
+	/* Initialize request command header */
+	req->version = 0;
+	req->cmd = SERVICE_QUOTE_SEND_CMD;
+
+	/* Initialize request command data */
+	req->len = TDX_REPORT_LEN;
+	memcpy(req->data, tdreport, TDX_REPORT_LEN);
+
+	/* Initialize response service header */
+	memcpy(resp->hdr.guid, &att_service_guid, sizeof(guid_t));
+
+	/* Initialize response header */
+	resp->version = 0;
+	resp->cmd = SERVICE_QUOTE_SEND_CMD;
+
+	err = tdx_hcall_service(req_buf, resp_buf, 0, SERVICE_QUOTE_CMD_TIMEOUT);
+	if (err)
+		return -EIO;
+
+	if (resp->status) {
+		pr_err("Quote Service hypercall failed, err:%x\n", resp->status);
+		return -EIO;
+	}
+
+	buf = kvmemdup(resp->data, resp->len, GFP_KERNEL);
+	if (!buf)
+		return -ENOMEM;
+
+	report->outblob = buf;
+	report->outblob_len = resp->len;
+
+	return 0;
+}
+
+static int tdx_quote_service_init(void)
+{
+	req_buf = alloc_shared_pages(SERVICE_QUOTE_REQ_BUF_LEN);
+	if (!req_buf)
+		return -ENOMEM;
+
+	resp_buf = alloc_shared_pages(SERVICE_QUOTE_RESP_BUF_LEN);
+	if (!resp_buf) {
+		free_shared_pages(req_buf, SERVICE_QUOTE_REQ_BUF_LEN);
+		return -ENOMEM;
+	}
+
+	return 0;
+
+}
+
+static void tdx_quote_service_deinit(void)
+{
+	if (req_buf)
+		free_shared_pages(req_buf, SERVICE_QUOTE_REQ_BUF_LEN);
+	if (resp_buf)
+		free_shared_pages(req_buf, SERVICE_QUOTE_RESP_BUF_LEN);
 }
 
 /*
@@ -208,6 +346,13 @@ static int tdx_report_new(struct tsm_report *report, void *data)
 	ret = tdx_mcall_get_report0(reportdata, tdreport);
 	if (ret) {
 		pr_err("GetReport call failed\n");
+		goto done;
+	}
+
+	if (!guid_is_null(&desc->remote_guid)) {
+		ret = tdx_service_gen_quote(data, tdreport);
+		if (ret)
+			pr_err("GetQuote service request failed\n");
 		goto done;
 	}
 
@@ -318,21 +463,30 @@ static int __init tdx_guest_init(void)
 	if (ret)
 		return ret;
 
-	quote_data = alloc_quote_buf();
+	quote_data = alloc_shared_pages(GET_QUOTE_BUF_SIZE);
 	if (!quote_data) {
 		pr_err("Failed to allocate Quote buffer\n");
 		ret = -ENOMEM;
 		goto free_misc;
 	}
 
+	ret = tdx_quote_service_init();
+	if (ret) {
+		pr_err("Failed to allocate service buffers\n");
+		ret = -ENOMEM;
+		goto free_quote;
+	}
+
 	ret = tsm_register(&tdx_tsm_ops, NULL, NULL);
 	if (ret)
-		goto free_quote;
+		goto free_service;
 
 	return 0;
 
+free_service:
+	tdx_quote_service_deinit();
 free_quote:
-	free_quote_buf(quote_data);
+	free_shared_pages(quote_data, GET_QUOTE_BUF_SIZE);
 free_misc:
 	misc_deregister(&tdx_misc_dev);
 
@@ -343,7 +497,8 @@ module_init(tdx_guest_init);
 static void __exit tdx_guest_exit(void)
 {
 	tsm_unregister(&tdx_tsm_ops);
-	free_quote_buf(quote_data);
+	free_shared_pages(quote_data, GET_QUOTE_BUF_SIZE);
+	tdx_quote_service_deinit();
 	misc_deregister(&tdx_misc_dev);
 }
 module_exit(tdx_guest_exit);

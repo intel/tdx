2709c5f962e4d93f7cbfbc213e68f2def48f7008
Erdem Aktas <erdemaktas@google.com>
2023-12-12 12:46:23 -0800

    KVM: selftests: TDX: Add TDX lifecycle test
    Adding a test to verify TDX lifecycle by creating a TD and running a
    dummy TDG.VP.VMCALL <Instruction.IO> inside it.
    
    Signed-off-by: Erdem Aktas <erdemaktas@google.com>
    Signed-off-by: Ryan Afranji <afranji@google.com>
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Co-developed-by: Ackerley Tng <ackerleytng@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Change-Id: Id8ac182384288180668ced250f79d5f54865ef5e

---
+++ tools/testing/selftests/kvm/lib/x86_64/tdx/test_util.c
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "kvm_util.h"
+#include "tdx/tdx.h"
+#include "tdx/test_util.h"
+
+void run_in_new_process(void (*func)(void))
+{
+	if (fork() == 0) {
+		func();
+		exit(0);
+	}
+	wait(NULL);
+}
+
+bool is_tdx_enabled(void)
+{
+	return !!(kvm_check_cap(KVM_CAP_VM_TYPES) & BIT(KVM_X86_TDX_VM));
+}
+
+void tdx_test_success(void)
+{
+	uint64_t code = 0;
+
+	tdg_vp_vmcall_instruction_io(TDX_TEST_SUCCESS_PORT,
+				     TDX_TEST_SUCCESS_SIZE,
+				     TDG_VP_VMCALL_INSTRUCTION_IO_WRITE, &code);
+}

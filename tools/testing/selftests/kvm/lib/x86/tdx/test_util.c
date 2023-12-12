2010cc102a8b70a597d0d8549bd6274c6de8419a
Erdem Aktas <erdemaktas@google.com>
2023-12-12 12:46:23 -0800

    KVM: selftests: TDX: Add TDX lifecycle test
    Adding a test to verify TDX lifecycle by creating a TD and running a
    dummy TDG.VP.VMCALL <Instruction.IO> inside it.
    
    Signed-off-by: Erdem Aktas <erdemaktas@google.com>
    Signed-off-by: Ryan Afranji <afranji@google.com>
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Co-developed-by: Ackerley Tng <ackerleytng@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Co-developed-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    ---
    - Reinette: Squash change from Binbin to define tdcall instruction
      to support selftests on systems with older binutil. No attribution
      for the one line change.
    - Reinette: Drop the "Change-Id:" tag.
    - Reinette: Let run_in_new_process() return failure if the test run by
      it failed its TEST_ASSERT(). Rely on the kselftest framework to track
      how many tests are run and accurately report individual test success.
      Add attribution.
    - Reinette: Remove '/**' kerneldoc marker from comments that do not
      follow kerneldoc syntax.
    - Reinette: fix checkpatch.pl reported "Macro argument reuse 'VCPU' -
      possible side-effects?" by changing the TDX_TEST_ASSERT_SUCCESS()
      macro to a function (tdx_test_assert_success()).
    - Reinette: switch to reverse fir where practical

---
+++ tools/testing/selftests/kvm/lib/x86/tdx/test_util.c
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "kvm_util.h"
+#include "tdx/tdx.h"
+#include "tdx/test_util.h"
+
+int run_in_new_process(void (*func)(void))
+{
+	int wstatus;
+	pid_t ret;
+
+	if (fork() == 0) {
+		func();
+		exit(0);
+	}
+	ret = wait(&wstatus);
+	if (ret == -1)
+		return -1;
+
+	if (WIFEXITED(wstatus) && WEXITSTATUS(wstatus))
+		return -1;
+	else if (WIFSIGNALED(wstatus))
+		return -1;
+
+	return 0;
+}
+
+bool is_tdx_enabled(void)
+{
+	return !!(kvm_check_cap(KVM_CAP_VM_TYPES) & BIT(KVM_X86_TDX_VM));
+}
+
+void tdx_test_success(void)
+{
+	uint64_t code = 0;
+
+	tdg_vp_vmcall_instruction_io(TDX_TEST_SUCCESS_PORT,
+				     TDX_TEST_SUCCESS_SIZE,
+				     TDG_VP_VMCALL_INSTRUCTION_IO_WRITE, &code);
+}
+
+/*
+ * Assert that tdx_test_success() was called in the guest.
+ */
+void tdx_test_assert_success(struct kvm_vcpu *vcpu)
+{
+	TEST_ASSERT((vcpu->run->exit_reason == KVM_EXIT_IO) &&
+		    (vcpu->run->io.port == TDX_TEST_SUCCESS_PORT) &&
+		    (vcpu->run->io.size == TDX_TEST_SUCCESS_SIZE) &&
+		    (vcpu->run->io.direction == TDG_VP_VMCALL_INSTRUCTION_IO_WRITE),
+		    "Unexpected exit values while waiting for test completion: %u (%s) %d %d %d\n",
+		    vcpu->run->exit_reason,
+		    exit_reason_str(vcpu->run->exit_reason),
+		    vcpu->run->io.port, vcpu->run->io.size,
+		    vcpu->run->io.direction);
+}

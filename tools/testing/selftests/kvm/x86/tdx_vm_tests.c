55c7db6b0895d3e8fe11811fcdbc3153a56308f3
Erdem Aktas <erdemaktas@google.com>
2023-12-12 12:46:23 -0800

    KVM: selftests: TDX: Add TDX lifecycle test
    Adding a test to verify TDX lifecycle by creating a TD and running a
    dummy TDG.VP.VMCALL <Instruction.IO> inside it.
    
    Signed-off-by: Erdem Aktas <erdemaktas@google.com>
    Signed-off-by: Ryan Afranji <afranji@google.com>
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Co-developed-by: Ackerley Tng <ackerleytng@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Co-developed-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    ---
    - Reinette: Squash change from Binbin to define tdcall instruction
      to support selftests on systems with older binutil. No attribution
      for the one line change.
    - Reinette: Drop the "Change-Id:" tag.
    - Reinette: Let run_in_new_process() return failure if the test run by
      it failed its TEST_ASSERT(). Rely on the kselftest framework to track
      how many tests are run and accurately report individual test success.
      Add attribution.
    - Reinette: Remove '/**' kerneldoc marker from comments that do not
      follow kerneldoc syntax.
    - Reinette: fix checkpatch.pl reported "Macro argument reuse 'VCPU' -
      possible side-effects?" by changing the TDX_TEST_ASSERT_SUCCESS()
      macro to a function (tdx_test_assert_success()).
    - Reinette: switch to reverse fir where practical
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

---
+++ tools/testing/selftests/kvm/x86/tdx_vm_tests.c
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <signal.h>
+#include "kvm_util.h"
+#include "tdx/tdx_util.h"
+#include "tdx/test_util.h"
+#include "test_util.h"
+
+void guest_code_lifecycle(void)
+{
+	tdx_test_success();
+}
+
+void verify_td_lifecycle(void)
+{
+	struct kvm_vcpu *vcpu;
+	struct kvm_vm *vm;
+
+	vm = td_create();
+	td_initialize(vm, VM_MEM_SRC_ANONYMOUS, 0);
+	vcpu = td_vcpu_add(vm, 0, guest_code_lifecycle);
+	td_finalize(vm);
+
+	printf("Verifying TD lifecycle:\n");
+
+	vcpu_run(vcpu);
+	tdx_test_assert_success(vcpu);
+
+	kvm_vm_free(vm);
+	printf("\t ... PASSED\n");
+}
+
+int main(int argc, char **argv)
+{
+	ksft_print_header();
+
+	if (!is_tdx_enabled())
+		ksft_exit_skip("TDX is not supported by the KVM. Exiting.\n");
+
+	ksft_set_plan(1);
+	ksft_test_result(!run_in_new_process(&verify_td_lifecycle),
+			 "verify_td_lifecycle\n");
+
+	ksft_finished();
+	return 0;
+}

b80d88089499a2dfbe3d2c381cbfef3a4336d4d3
Kai Huang <kai.huang@intel.com>
2023-12-13 13:46:13 +1300

    x86/virt/tdx: Support global metadata read for all element sizes
    The TDX module provides "global metadata fields" for software to query.
    Each metadata field is accessible by a unique "metadata field ID".  TDX
    supports 8/16/32/64 bits metadata element sizes.  The size of each
    metadata field is encoded in its associated metadata field ID.
    
    For now the kernel only reads "TD Memory Region" (TDMR) related global
    metadata fields for module initialization.  All these metadata fields
    are 16-bit, and the kernel only supports reading 16-bit fields.
    
    Future changes will allow for other types of metadata to be read, among
    which there are fields with 64-bit element size.
    
    To resolve this once for all, extend the existing metadata reading code
    to support reading all 8/16/32/64 bits element sizes.
    
    Note, supporting reading of 8-bit and 32-bit element sizes doesn't have
    immediate need, but supporting them doens't make code more complicated.
    In fact, it is more straightforward/clear since reading all sizes can be
    done in a single helper using size-independent logic.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Reviewed-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -271,23 +271,25 @@ static int read_sys_metadata_field(u64 field_id, u64 *data)
 	return 0;
 }
 
-static int read_sys_metadata_field16(u64 field_id,
-				     int offset,
-				     void *stbuf)
+/*
+ * Read one global metadata field and store the data to a location of a
+ * given buffer specified by the offset and size (in bytes).
+ */
+static int stbuf_read_sysmd_field(u64 field_id, void *stbuf, int offset,
+				  int bytes)
 {
-	u16 *st_member = stbuf + offset;
+	void *st_member = stbuf + offset;
 	u64 tmp;
 	int ret;
 
-	if (WARN_ON_ONCE(MD_FIELD_ID_ELE_SIZE_CODE(field_id) !=
-			MD_FIELD_ID_ELE_SIZE_16BIT))
+	if (WARN_ON_ONCE(MD_FIELD_BYTES(field_id) != bytes))
 		return -EINVAL;
 
 	ret = read_sys_metadata_field(field_id, &tmp);
 	if (ret)
 		return ret;
 
-	*st_member = tmp;
+	memcpy(st_member, &tmp, bytes);
 
 	return 0;
 }
@@ -295,11 +297,13 @@ static int read_sys_metadata_field16(u64 field_id,
 struct field_mapping {
 	u64 field_id;
 	int offset;
+	int size;
 };
 
-#define TD_SYSINFO_MAP(_field_id, _struct, _member)	\
-	{ .field_id = MD_FIELD_ID_##_field_id,		\
-	  .offset   = offsetof(_struct, _member) }
+#define TD_SYSINFO_MAP(_field_id, _struct, _member)		\
+	{ .field_id = MD_FIELD_ID_##_field_id,			\
+	  .offset   = offsetof(_struct, _member),		\
+	  .size	    = sizeof(typeof(((_struct *)0)->_member)) }
 
 #define TD_SYSINFO_MAP_TDMR_INFO(_field_id, _member)	\
 	TD_SYSINFO_MAP(_field_id, struct tdx_tdmr_sysinfo, _member)
@@ -320,9 +324,8 @@ static int get_tdx_tdmr_sysinfo(struct tdx_tdmr_sysinfo *tdmr_sysinfo)
 
 	/* Populate 'tdmr_sysinfo' fields using the mapping structure above: */
 	for (i = 0; i < ARRAY_SIZE(fields); i++) {
-		ret = read_sys_metadata_field16(fields[i].field_id,
-						fields[i].offset,
-						tdmr_sysinfo);
+		ret = stbuf_read_sysmd_field(fields[i].field_id, tdmr_sysinfo,
+				fields[i].offset, fields[i].size);
 		if (ret)
 			return ret;
 	}

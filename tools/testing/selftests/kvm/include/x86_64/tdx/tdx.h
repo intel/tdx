f77c397558e756cebab4b8750762f5be3684992c
Sagi Shahar <sagis@google.com>
2024-06-25 01:09:59 -0700

    KVM: selftests: TDX: Add report_fatal_error test
    The test checks report_fatal_error functionality.
    
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Change-Id: I1bf45c42ac23aa81bb7d52f05273786d5832e377
    Signed-off-by: Ryan Afranji <afranji@google.com>
    ---
    Yan: rebased to tdx_kvm_dev-2024-06-21

--- tools/testing/selftests/kvm/include/x86_64/tdx/tdx.h
+++ tools/testing/selftests/kvm/include/x86_64/tdx/tdx.h
@@ -3,10 +3,15 @@
 #define SELFTEST_TDX_TDX_H
 
 #include <stdint.h>
+#include "kvm_util.h"
 
 #define TDG_VP_VMCALL_INSTRUCTION_IO 30
+#define TDG_VP_VMCALL_REPORT_FATAL_ERROR 0x10003
+
+#define TDG_VP_VMCALL_INSTRUCTION_IO 30
+void handle_userspace_tdg_vp_vmcall_exit(struct kvm_vcpu *vcpu);
 
 uint64_t tdg_vp_vmcall_instruction_io(uint64_t port, uint64_t size,
 				      uint64_t write, uint64_t *data);
-
+void tdg_vp_vmcall_report_fatal_error(uint64_t error_code, uint64_t data_gpa);
 #endif // SELFTEST_TDX_TDX_H

fa89c04020171b66352458285da499453f879143
Sagi Shahar <sagis@google.com>
2024-06-25 01:22:37 -0700

    KVM: selftests: TDX: Add basic get_td_vmcall_info test
    The test calls get_td_vmcall_info from the guest and verifies the
    expected returned values.
    
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Change-Id: I6906de835f20c5f48181b6a0734131fa9b62246d
    Signed-off-by: Ryan Afranji <afranji@google.com>
    ---
    Yan: rebased to tdx_kvm_dev-2024-06-21

--- tools/testing/selftests/kvm/include/x86_64/tdx/tdx.h
+++ tools/testing/selftests/kvm/include/x86_64/tdx/tdx.h
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include "kvm_util.h"
 
+#define TDG_VP_VMCALL_GET_TD_VM_CALL_INFO 0x10000
 #define TDG_VP_VMCALL_INSTRUCTION_IO 30
 #define TDG_VP_VMCALL_REPORT_FATAL_ERROR 0x10003
 
@@ -14,4 +15,6 @@ void handle_userspace_tdg_vp_vmcall_exit(struct kvm_vcpu *vcpu);
 uint64_t tdg_vp_vmcall_instruction_io(uint64_t port, uint64_t size,
 				      uint64_t write, uint64_t *data);
 void tdg_vp_vmcall_report_fatal_error(uint64_t error_code, uint64_t data_gpa);
+uint64_t tdg_vp_vmcall_get_td_vmcall_info(uint64_t *r11, uint64_t *r12,
+					uint64_t *r13, uint64_t *r14);
 #endif // SELFTEST_TDX_TDX_H

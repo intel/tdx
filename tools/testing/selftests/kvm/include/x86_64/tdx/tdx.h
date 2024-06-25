655ff3008c34f2035ece5d4c099b6f80f5474b4c
Sagi Shahar <sagis@google.com>
2024-06-25 01:30:15 -0700

    KVM: selftests: TDX: Add TDX MSR read/write tests
    The test verifies reads and writes for MSR registers with different access
    level.
    
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Change-Id: Idb98e4e9ae64f31ab99b80d2e0e01252316d256a
    Signed-off-by: Ryan Afranji <afranji@google.com>
    ---
    Yan: rebased to tdx_kvm_dev-2024-06-21
    
    Reinette: Rebased after earlier change removed
    handle_userspace_tdg_vp_vmcall_exit(). No code change during rebase.

--- tools/testing/selftests/kvm/include/x86_64/tdx/tdx.h
+++ tools/testing/selftests/kvm/include/x86_64/tdx/tdx.h
@@ -10,10 +10,14 @@
 #define TDG_VP_VMCALL_REPORT_FATAL_ERROR 0x10003
 
 #define TDG_VP_VMCALL_INSTRUCTION_IO 30
+#define TDG_VP_VMCALL_INSTRUCTION_RDMSR 31
+#define TDG_VP_VMCALL_INSTRUCTION_WRMSR 32
 
 uint64_t tdg_vp_vmcall_instruction_io(uint64_t port, uint64_t size,
 				      uint64_t write, uint64_t *data);
 void tdg_vp_vmcall_report_fatal_error(uint64_t error_code, uint64_t data_gpa);
 uint64_t tdg_vp_vmcall_get_td_vmcall_info(uint64_t *r11, uint64_t *r12,
 					uint64_t *r13, uint64_t *r14);
+uint64_t tdg_vp_vmcall_instruction_rdmsr(uint64_t index, uint64_t *ret_value);
+uint64_t tdg_vp_vmcall_instruction_wrmsr(uint64_t index, uint64_t value);
 #endif // SELFTEST_TDX_TDX_H

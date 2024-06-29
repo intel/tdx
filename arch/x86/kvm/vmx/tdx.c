3fdce7e5d66091b9101a0af7da8a889937a06a6f
Kai Huang <kai.huang@intel.com>
2024-06-30 00:27:17 +1200

    KVM: VMX: Initialize TDX during KVM module load
    Before KVM can use TDX to create and run TDX guests, TDX needs to be
    initialized from two perspectives: 1) TDX module must be initialized
    properly to a working state; 2) A per-cpu TDX initialization, a.k.a the
    TDH.SYS.LP.INIT SEAMCALL must be done on any logical cpu before it can
    run any other TDX SEAMCALLs.
    
    The TDX host core-kernel provides two functions to do above two
    respectively: tdx_enable() and tdx_cpu_enable().
    
    There are two options in terms of when to initialize TDX: initialize TDX
    at KVM module loading time, or when creating the first TDX guest.
    
    Choose to initialize TDX during KVM module loading time:
    
    Initializing TDX module is both memory and CPU time consuming: 1) the
    kernel needs to allocate a non-trivial size(~1/256) of system memory
    as metadata used by TDX module to track each TDX-usable memory page's
    status; 2) the TDX module needs to initialize this metadata, one entry
    for each TDX-usable memory page.
    
    Also, the kernel uses alloc_contig_pages() to allocate those metadata
    chunks, because they are large and need to be physically contiguous.
    
    alloc_contig_pages() can fail.  If initializing TDX when creating the
    first TDX guest, then there's chance that KVM won't be able to run any
    TDX guests albeit KVM _declares_ to be able to support TDX.
    
    This isn't good to the user.  On the other hand, initializing TDX at KVM
    module loading time can make sure KVM is providing a consistent view of
    whether KVM can support TDX to the user.
    
    Always only try to initialize TDX after VMX has been initialized.  TDX
    is based on VMX, and if VMX fails to initialize then TDX is likely to be
    broken anyway.  Also, in practice, supporting TDX will require part of
    VMX and common x86 infrastructure in working order, so TDX cannot be
    sololy w/o VMX support.
    
    Specifically, initialize TDX after VMX has been initialized and before
    kvm_init() in vt_init().  Don't fail the whole vt_init() if TDX fails to
    initialize, since in this case KVM can still support normal VMX guests.
    
    Because TDX costs additional memory, don't enable TDX by default.  Add a
    new module parameter 'enable_tdx' to allow the user to opt-in.
    
    Register a new TDX-specific cpuhp callback to run tdx_cpu_enable(), and
    call tdx_enable() after that to ensure tdx_cpu_enable() has been done
    for all online CPUs before making the tdx_enable().  Use a dynamic cpuhp
    state for TDX so that KVM's cpuhp callback to enable VMX on new online
    CPU can happen before tdx_cpu_enable().
    
    Note, the name tdx_init() has already been taken by the early boot code.
    Use tdx_bringup() for initializing TDX (and tdx_cleanup() since KVM
    doesn't actually teardown TDX).  They don't match vt_init()/vt_exit(),
    vmx_init()/vmx_exit() etc but it's not end of the world.
    
    Also, once initialized, the TDX module cannot be disabled and enabled
    again w/o the TDX module runtime update, which isn't supported by the
    kernel.  After TDX is enabled, nothing needs to be done when KVM
    disables hardware virtualization, e.g., when offlining CPU, or during
    suspend/resume.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>

---
+++ arch/x86/kvm/vmx/tdx.c
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/cpu.h>
+#include <asm/tdx.h>
+#include "capabilities.h"
+#include "tdx.h"
+
+#undef pr_fmt
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+static bool enable_tdx __ro_after_init;
+module_param_named(tdx, enable_tdx, bool, 0444);
+
+static enum cpuhp_state tdx_cpuhp_state;
+
+static int tdx_online_cpu(unsigned int cpu)
+{
+	unsigned long flags;
+	int r;
+
+	/* Sanity check CPU is already in post-VMXON */
+	WARN_ON_ONCE(!(cr4_read_shadow() & X86_CR4_VMXE));
+
+	/* tdx_cpu_enable() must be called with IRQ disabled */
+	local_irq_save(flags);
+	r = tdx_cpu_enable();
+	local_irq_restore(flags);
+
+	return r;
+}
+
+static void __do_tdx_cleanup(void)
+{
+	/*
+	 * Once TDX module is initialized, it cannot be disabled and
+	 * re-initialized again w/o runtime update (which isn't
+	 * supported by kernel).  In fact the kernel doesn't support
+	 * disable (shut down) TDX module, so only need to remove the
+	 * cpuhp state.
+	 */
+	WARN_ON_ONCE(!tdx_cpuhp_state);
+	cpuhp_remove_state_nocalls(tdx_cpuhp_state);
+	tdx_cpuhp_state = 0;
+}
+
+static int __init __do_tdx_bringup(void)
+{
+	int r;
+
+	/*
+	 * TDX-specific cpuhp callback to call tdx_cpu_enable() on all
+	 * online CPUs before calling tdx_enable(), and on any new
+	 * going-online CPU to make sure it is ready for TDX guest.
+	 */
+	r = cpuhp_setup_state_cpuslocked(CPUHP_AP_ONLINE_DYN,
+					 "kvm/cpu/tdx:online",
+					 tdx_online_cpu, NULL);
+	if (r < 0)
+		return r;
+
+	tdx_cpuhp_state = r;
+
+	/* tdx_enable() must be called with cpus_read_lock() */
+	r = tdx_enable();
+	if (r)
+		__do_tdx_cleanup();
+
+	return r;
+}
+
+static int __init __tdx_bringup(void)
+{
+	int r;
+
+	if (!enable_ept) {
+		pr_err("Cannot enable TDX with EPT disabled.\n");
+		return -EINVAL;
+	}
+
+	/*
+	 * Enabling TDX requires enabling hardware virtualization first,
+	 * as making SEAMCALLs requires CPU being in post-VMXON state.
+	 */
+	r = kvm_enable_virtualization();
+	if (r)
+		return r;
+
+	cpus_read_lock();
+	r = __do_tdx_bringup();
+	cpus_read_unlock();
+
+	if (r)
+		goto tdx_bringup_err;
+
+	/*
+	 * Leave hardware virtualization enabled after TDX is enabled
+	 * successfully.  TDX CPU hotplug depends on this.
+	 */
+	return 0;
+tdx_bringup_err:
+	kvm_disable_virtualization();
+	return r;
+}
+
+void tdx_cleanup(void)
+{
+	if (enable_tdx) {
+		__do_tdx_cleanup();
+		kvm_disable_virtualization();
+	}
+}
+
+void __init tdx_bringup(void)
+{
+	enable_tdx = enable_tdx && !__tdx_bringup();
+}

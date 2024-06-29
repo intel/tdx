4c58fc84519beb115687089021fa20c013eccad8
Kai Huang <kai.huang@intel.com>
2024-06-30 00:27:17 +1200

    KVM: VMX: Initialize TDX during KVM module load
    Before KVM can use TDX to create and run TDX guests, TDX needs to be
    initialized from two perspectives: 1) TDX module must be initialized
    properly to a working state; 2) A per-cpu TDX initialization, a.k.a the
    TDH.SYS.LP.INIT SEAMCALL must be done on any logical cpu before it can
    run any other TDX SEAMCALLs.
    
    The TDX host core-kernel provides two functions to do above two
    respectively: tdx_enable() and tdx_cpu_enable().
    
    There are two options in terms of when to initialize TDX: initialize TDX
    at KVM module loading time, or when creating the first TDX guest.
    
    Choose to initialize TDX during KVM module loading time:
    
    Initializing TDX module is both memory and CPU time consuming: 1) the
    kernel needs to allocate a non-trivial size(~1/256) of system memory
    as metadata used by TDX module to track each TDX-usable memory page's
    status; 2) the TDX module needs to initialize this metadata, one entry
    for each TDX-usable memory page.
    
    Also, the kernel uses alloc_contig_pages() to allocate those metadata
    chunks, because they are large and need to be physically contiguous.
    
    alloc_contig_pages() can fail.  If initializing TDX when creating the
    first TDX guest, then there's chance that KVM won't be able to run any
    TDX guests albeit KVM _declares_ to be able to support TDX.
    
    This isn't good to the user.  On the other hand, initializing TDX at KVM
    module loading time can make sure KVM is providing a consistent view of
    whether KVM can support TDX to the user.
    
    Always only try to initialize TDX after VMX has been initialized.  TDX
    is based on VMX, and if VMX fails to initialize then TDX is likely to be
    broken anyway.  Also, in practice, supporting TDX will require part of
    VMX and common x86 infrastructure in working order, so TDX cannot be
    sololy w/o VMX support.
    
    Specifically, initialize TDX after VMX has been initialized and before
    kvm_init() in vt_init().  Don't fail the whole vt_init() if TDX fails to
    initialize, since in this case KVM can still support normal VMX guests.
    
    Because TDX costs additional memory, don't enable TDX by default.  Add a
    new module parameter 'enable_tdx' to allow the user to opt-in.
    
    Register a new TDX-specific cpuhp callback to run tdx_cpu_enable(), and
    call tdx_enable() after that to ensure tdx_cpu_enable() has been done
    for all online CPUs before making the tdx_enable().  Use a dynamic cpuhp
    state for TDX so that KVM's cpuhp callback to enable VMX on new online
    CPU can happen before tdx_cpu_enable().
    
    Note, the name tdx_init() has already been taken by the early boot code.
    Use tdx_bringup() for initializing TDX (and tdx_cleanup() since KVM
    doesn't actually teardown TDX).  They don't match vt_init()/vt_exit(),
    vmx_init()/vmx_exit() etc but it's not end of the world.
    
    Also, once initialized, the TDX module cannot be disabled and enabled
    again w/o the TDX module runtime update, which isn't supported by the
    kernel.  After TDX is enabled, nothing needs to be done when KVM
    disables hardware virtualization, e.g., when offlining CPU, or during
    suspend/resume.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/kvm/vmx/main.c
+++ arch/x86/kvm/vmx/main.c
@@ -6,6 +6,7 @@
 #include "nested.h"
 #include "pmu.h"
 #include "posted_intr.h"
+#include "tdx.h"
 
 #define VMX_REQUIRED_APICV_INHIBITS				\
 	(BIT(APICV_INHIBIT_REASON_DISABLED) |			\
@@ -168,6 +169,7 @@ struct kvm_x86_init_ops vt_init_ops __initdata = {
 static void vt_exit(void)
 {
 	kvm_exit();
+	tdx_cleanup();
 	vmx_exit();
 }
 module_exit(vt_exit);
@@ -179,6 +181,9 @@ static int __init vt_init(void)
 	r = vmx_init();
 	if (r)
 		return r;
+
+	/* tdx_init() has been taken */
+	tdx_bringup();
 
 	/*
 	 * Common KVM initialization _must_ come last, after this, /dev/kvm is
@@ -192,6 +197,7 @@ static int __init vt_init(void)
 	return 0;
 
 err_kvm_init:
+	tdx_cleanup();
 	vmx_exit();
 	return r;
 }

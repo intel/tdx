06a543a0dcb75213cde872e54f28d1786727ce10
Isaku Yamahata <isaku.yamahata@intel.com>
2024-07-18 14:12:25 -0700

    KVM: x86/tdp_mmu: Propagate attr_filter to MMU notifier callbacks
    Teach the MMU notifier callbacks how to check kvm_gfn_range.process to
    filter which KVM MMU root types to operate on.
    
    The private GPAs are backed by guest memfd. Such memory is not subjected
    to MMU notifier callbacks because it can't be mapped into the host user
    address space. Now kvm_gfn_range conveys info about which root to operate
    on. Enhance the callback to filter the root page table type.
    
    The KVM MMU notifier comes down to two functions.
    kvm_tdp_mmu_unmap_gfn_range() and kvm_tdp_mmu_handle_gfn().
    
    For VM's without a private/shared split in the EPT, all operations
    should target the normal(direct) root.
    
    invalidate_range_start() comes into kvm_tdp_mmu_unmap_gfn_range().
    invalidate_range_end() doesn't come into arch code.
    
    With the switch from for_each_tdp_mmu_root() to
    __for_each_tdp_mmu_root() in kvm_tdp_mmu_handle_gfn(), there are no
    longer any users of for_each_tdp_mmu_root(). Remove it.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Message-ID: <20240718211230.1492011-14-rick.p.edgecombe@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -193,9 +193,6 @@ static struct kvm_mmu_page *tdp_mmu_next_root(struct kvm *kvm,
 		     !tdp_mmu_root_match((_root), (_types)))) {			\
 		} else
 
-#define for_each_tdp_mmu_root(_kvm, _root, _as_id)			\
-	__for_each_tdp_mmu_root(_kvm, _root, _as_id, KVM_ALL_ROOTS)
-
 #define for_each_valid_tdp_mmu_root(_kvm, _root, _as_id)		\
 	__for_each_tdp_mmu_root(_kvm, _root, _as_id, KVM_VALID_ROOTS)
 
@@ -1212,12 +1209,16 @@ retry:
 	return ret;
 }
 
+/* Used by mmu notifier via kvm_unmap_gfn_range() */
 bool kvm_tdp_mmu_unmap_gfn_range(struct kvm *kvm, struct kvm_gfn_range *range,
 				 bool flush)
 {
+	enum kvm_tdp_mmu_root_types types;
 	struct kvm_mmu_page *root;
 
-	__for_each_tdp_mmu_root_yield_safe(kvm, root, range->slot->as_id, KVM_ALL_ROOTS)
+	types = kvm_gfn_range_filter_to_root_types(kvm, range->attr_filter);
+
+	__for_each_tdp_mmu_root_yield_safe(kvm, root, range->slot->as_id, types)
 		flush = tdp_mmu_zap_leafs(kvm, root, range->start, range->end,
 					  range->may_block, flush);
 
@@ -1231,15 +1232,18 @@ static __always_inline bool kvm_tdp_mmu_handle_gfn(struct kvm *kvm,
 						   struct kvm_gfn_range *range,
 						   tdp_handler_t handler)
 {
+	enum kvm_tdp_mmu_root_types types;
 	struct kvm_mmu_page *root;
 	struct tdp_iter iter;
 	bool ret = false;
+
+	types = kvm_gfn_range_filter_to_root_types(kvm, range->attr_filter);
 
 	/*
 	 * Don't support rescheduling, none of the MMU notifiers that funnel
 	 * into this helper allow blocking; it'd be dead, wasteful code.
 	 */
-	for_each_tdp_mmu_root(kvm, root, range->slot->as_id) {
+	__for_each_tdp_mmu_root(kvm, root, range->slot->as_id, types) {
 		rcu_read_lock();
 
 		tdp_root_for_each_leaf_pte(iter, kvm, root, range->start, range->end)

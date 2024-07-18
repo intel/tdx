9f503ca316cc14267a1535f5222c7ba2e9308321
Isaku Yamahata <isaku.yamahata@intel.com>
2024-07-18 14:12:17 -0700

    KVM: x86/mmu: Add an is_mirror member for union kvm_mmu_page_role
    Introduce a "is_mirror" member to the kvm_mmu_page_role union to identify
    SPTEs associated with the mirrored EPT.
    
    The TDX module maintains the private half of the EPT mapped in the TD in
    its protected memory. KVM keeps a copy of the private GPAs in a mirrored
    EPT tree within host memory. This "is_mirror" attribute enables vCPUs to
    find and get the root page of mirrored EPT from the MMU root list for a
    guest TD. This also allows KVM MMU code to detect changes in mirrored EPT
    according to the "is_mirror" mmu page role and propagate the changes to
    the private EPT managed by TDX module.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Message-ID: <20240718211230.1492011-6-rick.p.edgecombe@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/mmu/spte.h
+++ arch/x86/kvm/mmu/spte.h
@@ -267,6 +267,11 @@ static inline struct kvm_mmu_page *root_to_sp(hpa_t root)
 	return spte_to_child_sp(root);
 }
 
+static inline bool is_mirror_sptep(u64 *sptep)
+{
+	return is_mirror_sp(sptep_to_sp(sptep));
+}
+
 static inline bool is_mmio_spte(struct kvm *kvm, u64 spte)
 {
 	return (spte & shadow_mmio_mask) == kvm->arch.shadow_mmio_value &&

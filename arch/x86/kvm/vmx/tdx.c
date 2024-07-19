3d719caef632505fed394ba727636e5e0164c129
Rick Edgecombe <rick.p.edgecombe@intel.com>
2024-07-19 14:59:01 -0700

    With fixup: filter by CPUID
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    ---
    
    Kai rebase:
     - No code change actually, but some code in setup_kvm_tdx_cap() cannot
       be applied by 'git am' (due to previous change) so need to do manually.

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -1083,7 +1083,7 @@ free_tdvpr:
 	return ret;
 }
 
-static int __maybe_unused tdx_get_kvm_supported_cpuid(struct kvm_cpuid2 **cpuid) {
+static int tdx_get_kvm_supported_cpuid(struct kvm_cpuid2 **cpuid) {
 	int r;
 	static const u32 funcs[] = {
 		0, 0x80000000, KVM_CPUID_SIGNATURE,
@@ -1230,8 +1230,10 @@ int tdx_vcpu_ioctl(struct kvm_vcpu *vcpu, void __user *argp)
 static int __init setup_kvm_tdx_caps(void)
 {
 	const struct tdx_sysinfo_td_conf *td_conf = &tdx_sysinfo->td_conf;
+	struct kvm_cpuid_entry2 *cpuid_e;
+	struct kvm_cpuid2 *supported_cpuid;
 	u64 kvm_supported;
-	int i;
+	int i, r;
 
 	kvm_tdx_caps = kzalloc(sizeof(*kvm_tdx_caps) +
 			       sizeof(struct kvm_tdx_cpuid_config) * td_conf->num_cpuid_config + 1,
@@ -1255,6 +1257,10 @@ static int __init setup_kvm_tdx_caps(void)
 		return -EIO;
 	kvm_tdx_caps->supported_xfam = kvm_supported;
 
+	r = tdx_get_kvm_supported_cpuid(&supported_cpuid);
+	if (r)
+		goto out;
+
 	kvm_tdx_caps->num_cpuid_config = td_conf->num_cpuid_config;
 	for (i = 0; i < td_conf->num_cpuid_config; i++)
 	{
@@ -1276,9 +1282,22 @@ static int __init setup_kvm_tdx_caps(void)
 		/* Work around missing support on old TDX modules */
 		if (dest->leaf == 0x80000008)
 			dest->eax |= 0x00ff0000;
+
+		cpuid_e = kvm_find_cpuid_entry2(supported_cpuid->entries, supported_cpuid->nent,
+						dest->leaf, dest->sub_leaf);
+		if (!cpuid_e) {
+			dest->eax = dest->ebx = dest->ecx = dest->edx = 0;
+		} else {
+			dest->eax &= cpuid_e->eax;
+			dest->ebx &= cpuid_e->ebx;
+			dest->ecx &= cpuid_e->ecx;
+			dest->edx &= cpuid_e->edx;
+		}
 	}
 
-	return 0;
+out:
+	kfree(supported_cpuid);
+	return r;
 }
 
 static void free_kvm_tdx_cap(void)

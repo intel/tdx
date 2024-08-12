de00ba7a18cf769a2c567ac3624887d4f6d8d8dd
Xiaoyao Li <xiaoyao.li@intel.com>
2024-08-12 15:48:14 -0700

    KVM: X86: Introduce kvm_get_supported_cpuid_internal()
    TDX module reports a set of configurable CPUIDs. Directly report these
    bits to userspace and allow them to be set is not good nor right. If a
    bit is unknown/unsupported to KVM, it should be reported as unsupported
    thus inconfigurable to userspace.
    
    Introduce and export kvm_get_supported_cpuid_internal() for TDX to get
    the supported CPUID list of KVM. So that TDX can use it to cap the
    configurable CPUID list reported by TDX module.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Message-ID: <20240812224820.34826-20-rick.p.edgecombe@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/cpuid.h
+++ arch/x86/kvm/cpuid.h
@@ -13,6 +13,8 @@ void kvm_set_cpu_caps(void);
 
 void kvm_update_cpuid_runtime(struct kvm_vcpu *vcpu);
 void kvm_update_pv_runtime(struct kvm_vcpu *vcpu);
+int kvm_get_supported_cpuid_internal(struct kvm_cpuid2 *cpuid, const u32 *funcs,
+				     int func_len);
 struct kvm_cpuid_entry2 *kvm_find_cpuid_entry2(struct kvm_cpuid_entry2 *entries,
 					       int nent, u32 function, u64 index);
 struct kvm_cpuid_entry2 *kvm_find_cpuid_entry_index(struct kvm_vcpu *vcpu,

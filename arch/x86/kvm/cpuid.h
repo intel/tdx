7513985abacdc42cf34c5f21646bbb04c769d791
Isaku Yamahata <isaku.yamahata@intel.com>
2024-08-12 15:48:09 -0700

    KVM: TDX: initialize VM with TDX specific parameters
    After the crypto-protection key has been configured, TDX requires a
    VM-scope initialization as a step of creating the TDX guest.  This
    "per-VM" TDX initialization does the global configurations/features that
    the TDX guest can support, such as guest's CPUIDs (emulated by the TDX
    module), the maximum number of vcpus etc.
    
    This "per-VM" TDX initialization must be done before any "vcpu-scope" TDX
    initialization.  To match this better, require the KVM_TDX_INIT_VM IOCTL()
    to be done before KVM creates any vcpus.
    
    Co-developed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Message-ID: <20240812224820.34826-15-rick.p.edgecombe@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/cpuid.h
+++ arch/x86/kvm/cpuid.h
@@ -13,6 +13,8 @@ void kvm_set_cpu_caps(void);
 
 void kvm_update_cpuid_runtime(struct kvm_vcpu *vcpu);
 void kvm_update_pv_runtime(struct kvm_vcpu *vcpu);
+struct kvm_cpuid_entry2 *kvm_find_cpuid_entry2(struct kvm_cpuid_entry2 *entries,
+					       int nent, u32 function, u64 index);
 struct kvm_cpuid_entry2 *kvm_find_cpuid_entry_index(struct kvm_vcpu *vcpu,
 						    u32 function, u32 index);
 struct kvm_cpuid_entry2 *kvm_find_cpuid_entry(struct kvm_vcpu *vcpu,

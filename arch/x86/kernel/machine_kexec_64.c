105d20e1dc1df4fdf584abf8ed4bb949fd4b9eb7
Kai Huang <kai.huang@intel.com>
2024-08-16 23:21:05 +1200

    x86/kexec: do unconditional WBINVD for bare-metal in relocate_kernel()
    Both SME and TDX can leave caches in incoherent state due to memory
    encryption.  During kexec, the caches must be flushed before jumping to
    the second kernel to avoid silent memory corruption to the second kernel.
    
    During kexec, the WBINVD in stop_this_cpu() flushes caches for all
    remote cpus when they are being stopped.  For SME, the WBINVD in
    relocate_kernel() flushes the cache for the last running cpu (which is
    executing the kexec).
    
    Similarly, to support kexec for TDX host, after stopping all remote cpus
    with cache flushed, the kernel needs to flush cache for the last running
    cpu.
    
    Use the existing WBINVD in relocate_kernel() to cover TDX host as well.
    
    However, instead of sprinkling around vendor-specific checks, just do
    unconditional WBINVD to cover both SME and TDX.  Kexec is not a fast path
    so having one additional WBINVD for platforms w/o SME/TDX is acceptable.
    
    But only do WBINVD for bare-metal because TDX guests and SEV-ES/SEV-SNP
    guests will get unexpected (and yet unnecessary) exception (#VE or #VC)
    which the kernel is unable to handle at this stage.
    
    Note, commit 93c1800b3799f ("x86/kexec: Fix bug with call depth
    tracking") introduced a variable 'host_mem_enc_active' to avoid doing
    the cc_platform_has(CC_ATTR_HOST_MEM_ENCRYPT) function call after
    load_segments(), because load_segments() resets GS and making function
    call after that breaks the call depth tracking which uses per-cpu
    variable.
    
    Now with that changed to use !boot_cpu_has(X86_FEATURE_HYPERVISOR) to
    tell whether the kernel runs on bare metal, the bit test can be used
    after load_segments() because the bit test of X86_FEATURE_HYPERVISOR
    always generates inline code instead of function call.
    
    Remove the host_mem_enc_active variable completely and directly use the
    !boot_cpu_has(X86_FEATURE_HYPERVISOR) as the last argument when calling
    relocate_kernel().
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Reviewed-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Dave Young <dyoung@redhat.com>
    Reviewed-by: Tom Lendacky <thomas.lendacky@amd.com>

--- arch/x86/kernel/machine_kexec_64.c
+++ arch/x86/kernel/machine_kexec_64.c
@@ -295,15 +295,8 @@ void machine_kexec_cleanup(struct kimage *image)
 void machine_kexec(struct kimage *image)
 {
 	unsigned long page_list[PAGES_NR];
-	unsigned int host_mem_enc_active;
 	int save_ftrace_enabled;
 	void *control_page;
-
-	/*
-	 * This must be done before load_segments() since if call depth tracking
-	 * is used then GS must be valid to make any function calls.
-	 */
-	host_mem_enc_active = cc_platform_has(CC_ATTR_HOST_MEM_ENCRYPT);
 
 #ifdef CONFIG_KEXEC_JUMP
 	if (image->preserve_context)
@@ -365,7 +358,7 @@ void machine_kexec(struct kimage *image)
 				       (unsigned long)page_list,
 				       image->start,
 				       image->preserve_context,
-				       host_mem_enc_active);
+				       !boot_cpu_has(X86_FEATURE_HYPERVISOR));
 
 #ifdef CONFIG_KEXEC_JUMP
 	if (image->preserve_context)

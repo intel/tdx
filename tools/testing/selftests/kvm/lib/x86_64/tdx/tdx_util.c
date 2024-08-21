777a8d85925313f1bcba5e45d6d6450f519071f6
Rick Edgecombe <rick.p.edgecombe@intel.com>
2024-08-21 10:26:03 -0700

    KVM: selftests: TDX: Remove KVM_CAP_MAX_VCPUS emable cap call
    It is no longer required/supported.
    
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>

--- tools/testing/selftests/kvm/lib/x86_64/tdx/tdx_util.c
+++ tools/testing/selftests/kvm/lib/x86_64/tdx/tdx_util.c
@@ -364,7 +364,6 @@ static void tdx_enable_capabilities(struct kvm_vm *vm)
 		      KVM_X2APIC_API_USE_32BIT_IDS |
 			      KVM_X2APIC_API_DISABLE_BROADCAST_QUIRK);
 	vm_enable_cap(vm, KVM_CAP_SPLIT_IRQCHIP, 24);
-	vm_enable_cap(vm, KVM_CAP_MAX_VCPUS, 512);
 }
 
 static void tdx_configure_memory_encryption(struct kvm_vm *vm)

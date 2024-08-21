87a6d3cdb67ca9080e1988751d5da6108949e031
Isaku Yamahata <isaku.yamahata@intel.com>
2024-08-21 16:17:57 +0800

    KVM: TDX: Set per-VM shadow_mmio_value to 0
    Set per-VM shadow_mmio_value to 0 for TDX.
    
    With enable_mmio_caching on, KVM installs MMIO SPTEs for TDs. To correctly
    configure MMIO SPTEs, TDX requires the per-VM shadow_mmio_value to be set
    to 0. This is necessary to override the default value of the suppress VE
    bit in the SPTE, which is 1, and to ensure value 0 in RWX bits.
    
    For MMIO SPTE, the spte value changes as follows:
    1. initial value (suppress VE bit is set)
    2. Guest issues MMIO and triggers EPT violation
    3. KVM updates SPTE value to MMIO value (suppress VE bit is cleared)
    4. Guest MMIO resumes.  It triggers VE exception in guest TD
    5. Guest VE handler issues TDG.VP.VMCALL<MMIO>
    6. KVM handles MMIO
    7. Guest VE handler resumes its execution after MMIO instruction
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Co-developed-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Co-developed-by: Yan Zhao <yan.y.zhao@intel.com>
    Signed-off-by: Yan Zhao <yan.y.zhao@intel.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    ---
    TDX MMU part 2 v2:
     - Added Paolo's rb.
    
    TDX MMU part 2 v1:
     - Split from the big patch "KVM: TDX: TDP MMU TDX support".
     - Remove warning for shadow_mmio_value

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -5,7 +5,7 @@
 #include "mmu.h"
 #include "x86_ops.h"
 #include "tdx.h"
-
+#include "mmu/spte.h"
 
 #undef pr_fmt
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
@@ -414,6 +414,19 @@ int tdx_vm_init(struct kvm *kvm)
 	struct kvm_tdx *kvm_tdx = to_kvm_tdx(kvm);
 
 	kvm->arch.has_private_mem = true;
+
+	/*
+	 * Because guest TD is protected, VMM can't parse the instruction in TD.
+	 * Instead, guest uses MMIO hypercall.  For unmodified device driver,
+	 * #VE needs to be injected for MMIO and #VE handler in TD converts MMIO
+	 * instruction into MMIO hypercall.
+	 *
+	 * SPTE value for MMIO needs to be setup so that #VE is injected into
+	 * TD instead of triggering EPT MISCONFIG.
+	 * - RWX=0 so that EPT violation is triggered.
+	 * - suppress #VE bit is cleared to inject #VE.
+	 */
+	kvm_mmu_set_mmio_spte_value(kvm, 0);
 
 	/*
 	 * TDX has its own limit of maximum vCPUs it can support for all

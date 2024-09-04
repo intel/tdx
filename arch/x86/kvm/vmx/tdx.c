8e39b0039ecb163dd1f4bfbbc0344e09dcac62f8
Isaku Yamahata <isaku.yamahata@intel.com>
2024-09-03 20:07:43 -0700

    KVM: TDX: Handle TLB tracking for TDX
    Handle TLB tracking for TDX by introducing function tdx_track() for private
    memory TLB tracking and implementing flush_tlb* hooks to flush TLBs for
    shared memory.
    
    Introduce function tdx_track() to do TLB tracking on private memory, which
    basically does two things: calling TDH.MEM.TRACK to increase TD epoch and
    kicking off all vCPUs. The private EPT will then be flushed when each vCPU
    re-enters the TD. This function is unused temporarily in this patch and
    will be called on a page-by-page basis on removal of private guest page in
    a later patch.
    
    In earlier revisions, tdx_track() relied on an atomic counter to coordinate
    the synchronization between the actions of kicking off vCPUs, incrementing
    the TD epoch, and the vCPUs waiting for the incremented TD epoch after
    being kicked off.
    
    However, the core MMU only actually needs to call tdx_track() while
    aleady under a write mmu_lock. So this sychnonization can be made to be
    unneeded. vCPUs are kicked off only after the successful execution of
    TDH.MEM.TRACK, eliminating the need for vCPUs to wait for TDH.MEM.TRACK
    completion after being kicked off. tdx_track() is therefore able to send
    requests KVM_REQ_OUTSIDE_GUEST_MODE rather than KVM_REQ_TLB_FLUSH.
    
    Hooks for flush_remote_tlb and flush_remote_tlbs_range are not necessary
    for TDX, as tdx_track() will handle TLB tracking of private memory on
    page-by-page basis when private guest pages are removed. There is no need
    to invoke tdx_track() again in kvm_flush_remote_tlbs() even after changes
    to the mirrored page table.
    
    For hooks flush_tlb_current and flush_tlb_all, which are invoked during
    kvm_mmu_load() and vcpu load for normal VMs, let VMM to flush all EPTs in
    the two hooks for simplicity, since TDX does not depend on the two
    hooks to notify TDX module to flush private EPT in those cases.
    
    Co-developed-by: Yan Zhao <yan.y.zhao@intel.com>
    Signed-off-by: Yan Zhao <yan.y.zhao@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Message-ID: <20240904030751.117579-14-rick.p.edgecombe@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -6,6 +6,7 @@
 #include "mmu.h"
 #include "tdx.h"
 #include "tdx_ops.h"
+#include "vmx.h"
 #include "mmu/spte.h"
 
 #undef pr_fmt
@@ -480,6 +481,51 @@ void tdx_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event)
 void tdx_load_mmu_pgd(struct kvm_vcpu *vcpu, hpa_t root_hpa, int pgd_level)
 {
 	td_vmcs_write64(to_tdx(vcpu), SHARED_EPT_POINTER, root_hpa);
+}
+
+/*
+ * Ensure shared and private EPTs to be flushed on all vCPUs.
+ * tdh_mem_track() is the only caller that increases TD epoch. An increase in
+ * the TD epoch (e.g., to value "N + 1") is successful only if no vCPUs are
+ * running in guest mode with the value "N - 1".
+ *
+ * A successful execution of tdh_mem_track() ensures that vCPUs can only run in
+ * guest mode with TD epoch value "N" if no TD exit occurs after the TD epoch
+ * being increased to "N + 1".
+ *
+ * Kicking off all vCPUs after that further results in no vCPUs can run in guest
+ * mode with TD epoch value "N", which unblocks the next tdh_mem_track() (e.g.
+ * to increase TD epoch to "N + 2").
+ *
+ * TDX module will flush EPT on the next TD enter and make vCPUs to run in
+ * guest mode with TD epoch value "N + 1".
+ *
+ * kvm_make_all_cpus_request() guarantees all vCPUs are out of guest mode by
+ * waiting empty IPI handler ack_kick().
+ *
+ * No action is required to the vCPUs being kicked off since the kicking off
+ * occurs certainly after TD epoch increment and before the next
+ * tdh_mem_track().
+ */
+static void __always_unused tdx_track(struct kvm *kvm)
+{
+	struct kvm_tdx *kvm_tdx = to_kvm_tdx(kvm);
+	u64 err;
+
+	/* If TD isn't finalized, it's before any vcpu running. */
+	if (unlikely(!is_td_finalized(kvm_tdx)))
+		return;
+
+	lockdep_assert_held_write(&kvm->mmu_lock);
+
+	do {
+		err = tdh_mem_track(kvm_tdx);
+	} while (unlikely((err & TDX_SEAMCALL_STATUS_MASK) == TDX_OPERAND_BUSY));
+
+	if (KVM_BUG_ON(err, kvm))
+		pr_tdx_error(TDH_MEM_TRACK, err);
+
+	kvm_make_all_cpus_request(kvm, KVM_REQ_OUTSIDE_GUEST_MODE);
 }
 
 static int tdx_get_capabilities(struct kvm_tdx_cmd *cmd)
@@ -986,6 +1032,15 @@ out:
 	kfree(td_params);
 
 	return ret;
+}
+
+void tdx_flush_tlb_current(struct kvm_vcpu *vcpu)
+{
+	/*
+	 * flush_tlb_current() is used only the first time for the vcpu to run.
+	 * As it isn't performance critical, keep this function simple.
+	 */
+	ept_sync_global();
 }
 
 int tdx_vm_ioctl(struct kvm *kvm, void __user *argp)

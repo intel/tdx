c9e3ae38d654d179c78ae839b492285586f480da
Ackerley Tng <ackerleytng@google.com>
2024-09-10 23:43:40 +0000

    mm: hugetlb: Expose hugetlb_subpool_{get,put}_pages()
    This will allow hugetlb subpools to be used by guestmem_hugetlb.
    
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Change-Id: I909355935f2ab342e65e7bfdc106bedd1dc177c9

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -120,6 +120,9 @@ struct hugepage_subpool *hugepage_new_subpool(struct hstate *h, long max_hpages,
 					      long min_hpages, bool use_surplus);
 void hugepage_put_subpool(struct hugepage_subpool *spool);
 
+long hugepage_subpool_get_pages(struct hugepage_subpool *spool, long delta);
+long hugepage_subpool_put_pages(struct hugepage_subpool *spool, long delta);
+
 void hugetlb_dup_vma_private(struct vm_area_struct *vma);
 void clear_vma_resv_huge_pages(struct vm_area_struct *vma);
 int move_hugetlb_page_tables(struct vm_area_struct *vma,

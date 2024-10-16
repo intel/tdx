b387988247d3ffb34e1ad206b8d76600666f46cf
Isaku Yamahata <isaku.yamahata@intel.com>
2024-10-15 19:52:07 -0700

    x86/virt/tdx: Add SEAMCALL wrappers for TDX KeyID management
    Intel TDX protects guest VMs from malicious host and certain physical
    attacks. Pre-TDX Intel hardware has support for a memory encryption
    architecture called MK-TME, which repurposes several high bits of
    physical address as "KeyID". TDX ends up with reserving a sub-range of
    MK-TME KeyIDs as "TDX private KeyIDs".
    
    Like MK-TME, these KeyIDs can be associated with an ephemeral key. For TDX
    this association is done by the TDX module. It also has its own tracking
    for which KeyIDs are in use. To do this ephemeral key setup and manipulate
    the TDX module's internal tracking, KVM will use the following SEAMCALLs:
     TDH.MNG.KEY.CONFIG: Mark the KeyID as in use, and initialize its
                         ephemeral key.
     TDH.MNG.KEY.FREEID: Mark the KeyID as not in use.
    
    These SEAMCALLs both operate on TDR structures, which are setup using the
    previously added TDH.MNG.CREATE SEAMCALL. KVM's use of these operations
    will go like:
     - tdx_guest_keyid_alloc()
     - Initialize TD and TDR page with TDH.MNG.CREATE (not yet-added), passing
       KeyID
     - TDH.MNG.KEY.CONFIG to initialize the key
     - TD runs, teardown is started
     - TDH.MNG.KEY.FREEID
     - tdx_guest_keyid_free()
    
    Don't try to combine the tdx_guest_keyid_alloc() and TDH.MNG.KEY.CONFIG
    operations because TDH.MNG.CREATE and some locking need to be done in the
    middle. Don't combine TDH.MNG.KEY.FREEID and tdx_guest_keyid_free() so they
    are symmetrical with the creation path.
    
    So implement tdh_mng_key_config() and tdh_mng_key_freeid() as separate
    functions than tdx_guest_keyid_alloc() and tdx_guest_keyid_free().
    
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Reviewed-by: Binbin Wu <binbin.wu@linux.intel.com>
    Reviewed-by: Yuan Yao <yuan.yao@intel.com>
    ---
    uAPI breakout v2:
     - Change to use 'u64' as function parameter to prepare to move
       SEAMCALL wrappers to arch/x86. (Kai)
     - Split to separate patch
     - Move SEAMCALL wrappers from KVM to x86 core;
     - Move TDH_xx macros from KVM to x86 core;
     - Re-write log
    
    uAPI breakout v1:
     - Make argument to C wrapper function struct kvm_tdx * or
       struct vcpu_tdx * .(Sean)
     - Drop unused helpers (Kai)
     - Fix bisectability issues in headers (Kai)
     - Updates from seamcall overhaul (Kai)
    
    v19:
     - Update the commit message to match the patch by Yuan
     - Use seamcall() and seamcall_ret() by paolo
    
    v18:
     - removed stub functions for __seamcall{,_ret}()
     - Added Reviewed-by Binbin
     - Make tdx_seamcall() use struct tdx_module_args instead of taking
      each inputs.
    
    v16:
     - use struct tdx_module_args instead of struct tdx_module_output
     - Add tdh_mem_sept_rd() for SEPT_VE_DISABLE=1.

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -1562,3 +1562,23 @@ void tdx_guest_keyid_free(unsigned int keyid)
 	ida_free(&tdx_guest_keyid_pool, keyid);
 }
 EXPORT_SYMBOL_GPL(tdx_guest_keyid_free);
+
+u64 tdh_mng_key_config(u64 tdr)
+{
+	struct tdx_module_args args = {
+		.rcx = tdr,
+	};
+
+	return seamcall(TDH_MNG_KEY_CONFIG, &args);
+}
+EXPORT_SYMBOL_GPL(tdh_mng_key_config);
+
+u64 tdh_mng_key_freeid(u64 tdr)
+{
+	struct tdx_module_args args = {
+		.rcx = tdr,
+	};
+
+	return seamcall(TDH_MNG_KEY_FREEID, &args);
+}
+EXPORT_SYMBOL_GPL(tdh_mng_key_freeid);

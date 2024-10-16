9ffc2fe6bac8517dd24b88585c9671e213428f9c
Isaku Yamahata <isaku.yamahata@intel.com>
2024-10-15 20:25:37 -0700

    x86/virt/tdx: Add SEAMCALL wrappers for TDX VM/vCPU field access
    Intel TDX protects guest VMs from malicious host and certain physical
    attacks. The TDX module has TD scoped and vCPU scoped "metadata fields".
    These fields are a bit like VMCS fields, and stored in data structures
    maintained by the TDX module. Export 3 SEAMCALLs for use in reading and
    writing these fields:
    
    Make tdh_mng_rd() use MNG.VP.RD to read the TD scoped metadata.
    
    Make tdh_vp_rd()/tdh_vp_wr() use TDH.VP.RD/WR to read/write the vCPU
    scoped metadata.
    
    KVM will use these by creating inline helpers that target various metadata
    sizes. Export the raw SEAMCALL leaf, to avoid exporting the large number
    of various sized helpers.
    
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Reviewed-by: Binbin Wu <binbin.wu@linux.intel.com>
    Reviewed-by: Yuan Yao <yuan.yao@intel.com>
    ---
    uAPI breakout v2:
     - Change to use 'u64' as function parameter to prepare to move
       SEAMCALL wrappers to arch/x86. (Kai)
     - Split to separate patch
     - Move SEAMCALL wrappers from KVM to x86 core;
     - Move TDH_xx macros from KVM to x86 core;
     - Re-write log
    
    uAPI breakout v1:
     - Make argument to C wrapper function struct kvm_tdx * or
       struct vcpu_tdx * .(Sean)
     - Drop unused helpers (Kai)
     - Fix bisectability issues in headers (Kai)
     - Updates from seamcall overhaul (Kai)
    
    v19:
     - Update the commit message to match the patch by Yuan
     - Use seamcall() and seamcall_ret() by paolo
    
    v18:
     - removed stub functions for __seamcall{,_ret}()
     - Added Reviewed-by Binbin
     - Make tdx_seamcall() use struct tdx_module_args instead of taking
      each inputs.
    
    v16:
     - use struct tdx_module_args instead of struct tdx_module_output
     - Add tdh_mem_sept_rd() for SEPT_VE_DISABLE=1.

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -1621,6 +1621,23 @@ u64 tdh_vp_create(u64 tdr, u64 tdvpr)
 }
 EXPORT_SYMBOL_GPL(tdh_vp_create);
 
+u64 tdh_mng_rd(u64 tdr, u64 field, u64 *data)
+{
+	struct tdx_module_args args = {
+		.rcx = tdr,
+		.rdx = field,
+	};
+	u64 ret;
+
+	ret = seamcall_ret(TDH_MNG_RD, &args);
+
+	/* R8: Content of the field, or 0 in case of error. */
+	*data = args.r8;
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(tdh_mng_rd);
+
 u64 tdh_mng_key_freeid(u64 tdr)
 {
 	struct tdx_module_args args = {
@@ -1657,6 +1674,36 @@ u64 tdh_vp_init(u64 tdvpr, u64 initial_rcx)
 	return seamcall(TDH_VP_INIT, &args);
 }
 EXPORT_SYMBOL_GPL(tdh_vp_init);
+
+u64 tdh_vp_rd(u64 tdvpr, u64 field, u64 *data)
+{
+	struct tdx_module_args args = {
+		.rcx = tdvpr,
+		.rdx = field,
+	};
+	u64 ret;
+
+	ret = seamcall_ret(TDH_VP_RD, &args);
+
+	/* R8: Content of the field, or 0 in case of error. */
+	*data = args.r8;
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(tdh_vp_rd);
+
+u64 tdh_vp_wr(u64 tdvpr, u64 field, u64 data, u64 mask)
+{
+	struct tdx_module_args args = {
+		.rcx = tdvpr,
+		.rdx = field,
+		.r8 = data,
+		.r9 = mask,
+	};
+
+	return seamcall(TDH_VP_WR, &args);
+}
+EXPORT_SYMBOL_GPL(tdh_vp_wr);
 
 u64 tdh_vp_init_apicid(u64 tdvpr, u64 initial_rcx, u32 x2apicid)
 {

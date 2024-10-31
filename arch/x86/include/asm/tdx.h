2d8af16c0a7c6f6d404555317200e7896f69db45
Isaku Yamahata <isaku.yamahata@intel.com>
2024-10-31 17:35:03 +0800

    x86/virt/tdx: Add SEAMCALL wrapper tdh_mem_sept_add() to add SEPT pages
    TDX architecture introduces the concept of private GPA vs shared GPA,
    depending on the GPA.SHARED bit. The TDX module maintains a Secure EPT
    (S-EPT or SEPT) tree per TD for private GPA to HPA translation. Wrap the
    TDH.MEM.SEPT.ADD SEAMCALL with tdh_mem_sept_add() to provide pages to the
    TDX module for building a TD's SEPT tree. (Refer to these pages as SEPT
    pages).
    
    Callers need to allocate and provide a normal page to tdh_mem_sept_add(),
    which then passes the page to the TDX module via the SEAMCALL
    TDH.MEM.SEPT.ADD. The TDX module then installs the page into SEPT tree and
    encrypts this SEPT page with the TD's guest keyID. The kernel cannot use
    the SEPT page until after reclaiming it via TDH.MEM.SEPT.REMOVE or
    TDH.PHYMEM.PAGE.RECLAIM.
    
    Before passing the page to the TDX module, tdh_mem_sept_add() performs a
    CLFLUSH on the page mapped with keyID 0 to ensure that any dirty cache
    lines don't write back later and clobber TD memory or control structures.
    Don't worry about the other MK-TME keyIDs because the kernel doesn't use
    them. The TDX docs specify that this flush is not needed unless the TDX
    module exposes the CLFLUSH_BEFORE_ALLOC feature bit. Do the CLFLUSH
    unconditionally for two reasons: make the solution simpler by having a
    single path that can handle both !CLFLUSH_BEFORE_ALLOC and
    CLFLUSH_BEFORE_ALLOC cases. Avoid wading into any correctness uncertainty
    by going with a conservative solution to start.
    
    Callers should specify "GPA" and "level" for the TDX module to install the
    SEPT page at the specified position in the SEPT. Do not include the root
    page level in "level" since TDH.MEM.SEPT.ADD can only add non-root pages to
    the SEPT. Ensure "level" is between 1 and 3 for a 4-level SEPT or between 1
    and 4 for a 5-level SEPT.
    
    Call tdh_mem_sept_add() during the TD's build time or during the TD's
    runtime. Check for errors from the function return value and retrieve
    extended error info from the function output parameters.
    
    The TDX module has many internal locks. To avoid staying in SEAM mode for
    too long, SEAMCALLs returns a BUSY error code to the kernel instead of
    spinning on the locks. Depending on the specific SEAMCALL, the caller
    may need to handle this error in specific ways (e.g., retry). Therefore,
    return the SEAMCALL error code directly to the caller. Don't attempt to
    handle it in the core kernel.
    
    TDH.MEM.SEPT.ADD effectively manages two internal resources of the TDX
    module: it installs page table pages in the SEPT tree and also updates the
    TDX module's page metadata (PAMT). Don't add a wrapper for the matching
    SEAMCALL for removing a SEPT page (TDH.MEM.SEPT.REMOVE) because KVM, as the
    only in-kernel user, will only tear down the SEPT tree when the TD is being
    torn down. When this happens it can just do other operations that reclaim
    the SEPT pages for the host kernels to use, update the PAMT and let the
    SEPT get trashed.
    
    [Kai: Switched from generic seamcall export]
    [Yan: Re-wrote the changelog]
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Signed-off-by: Yan Zhao <yan.y.zhao@intel.com>
    ---
    TDX MMU part 2 v2:
    - split out TDH.MEM.SEPT.ADD and re-wrote the patch msg. (Yan)
    - dropped TDH.MEM.SEPT.REMOVE. (Yan)
    - split out from original patch "KVM: TDX: Add C wrapper functions for
      SEAMCALLs to the TDX module" and move to x86 core. (Kai)

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -134,6 +134,7 @@ struct tdx_vp {
 };
 
 u64 tdh_mng_addcx(struct tdx_td *td, hpa_t tdcs);
+u64 tdh_mem_sept_add(u64 tdr, u64 gpa, u64 level, u64 hpa, u64 *rcx, u64 *rdx);
 u64 tdh_vp_addcx(struct tdx_vp *vp, hpa_t tdcx);
 u64 tdh_mng_key_config(struct tdx_td *td);
 u64 tdh_mng_create(struct tdx_td *td, hpa_t hkid);

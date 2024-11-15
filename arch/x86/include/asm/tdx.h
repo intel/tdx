34f786697b382750739f8c4e13ebb3da348c307c
Kai Huang <kai.huang@intel.com>
2024-11-15 22:52:41 +1300

    KVM: VMX: Initialize TDX during KVM module load
    Before KVM can use TDX to create and run TDX guests, TDX needs to be
    initialized from two perspectives: 1) TDX module must be initialized
    properly to a working state; 2) A per-cpu TDX initialization, a.k.a the
    TDH.SYS.LP.INIT SEAMCALL must be done on any logical cpu before it can
    run any other TDX SEAMCALLs.
    
    The TDX host core-kernel provides two functions to do the above two
    respectively: tdx_enable() and tdx_cpu_enable().
    
    There are two options in terms of when to initialize TDX: initialize TDX
    at KVM module loading time, or when creating the first TDX guest.
    
    Choose to initialize TDX during KVM module loading time:
    
    Initializing TDX module is both memory and CPU time consuming: 1) the
    kernel needs to allocate a non-trivial size(~1/256) of system memory
    as metadata used by TDX module to track each TDX-usable memory page's
    status; 2) the TDX module needs to initialize this metadata, one entry
    for each TDX-usable memory page.
    
    Also, the kernel uses alloc_contig_pages() to allocate those metadata
    chunks, because they are large and need to be physically contiguous.
    alloc_contig_pages() can fail.  If initializing TDX when creating the
    first TDX guest, then there's chance that KVM won't be able to run any
    TDX guests albeit KVM _declares_ to be able to support TDX.
    
    This isn't good for the user.
    
    On the other hand, initializing TDX at KVM module loading time can make
    sure KVM is providing a consistent view of whether KVM can support TDX
    to the user.
    
    Always only try to initialize TDX after VMX has been initialized.  TDX
    is based on VMX, and if VMX fails to initialize then TDX is likely to be
    broken anyway.  Also, in practice, supporting TDX will require part of
    VMX and common x86 infrastructure in working order, so TDX cannot be
    enabled alone w/o VMX support.
    
    There are two cases that can result in failure to initialize TDX: 1) TDX
    cannot be supported (e.g., because of TDX is not supported or enabled by
    hardware, or module is not loaded, or missing some dependency in KVM's
    configuration); 2) Any unexpected error during TDX bring-up.  For the
    first case only mark TDX is disabled but still allow KVM module to be
    loaded.  For the second case just fail to load the KVM module so that
    the user can be aware.
    
    Because TDX costs additional memory, don't enable TDX by default.  Add a
    new module parameter 'enable_tdx' to allow the user to opt-in.
    
    Note, the name tdx_init() has already been taken by the early boot code.
    Use tdx_bringup() for initializing TDX (and tdx_cleanup() since KVM
    doesn't actually teardown TDX).  They don't match vt_init()/vt_exit(),
    vmx_init()/vmx_exit() etc but it's not end of the world.
    
    Also, once initialized, the TDX module cannot be disabled and enabled
    again w/o the TDX module runtime update, which isn't supported by the
    kernel.  After TDX is enabled, nothing needs to be done when KVM
    disables hardware virtualization, e.g., when offlining CPU, or during
    suspend/resume.  TDX host core-kernel code internally tracks TDX status
    and can handle "multiple enabling" scenario.
    
    Similar to KVM_AMD_SEV, add a new KVM_INTEL_TDX Kconfig to guide KVM TDX
    code.  Make it depend on INTEL_TDX_HOST but not replace INTEL_TDX_HOST
    because in the longer term there's a use case that requires making
    SEAMCALLs w/o KVM as mentioned by Dan [1].
    
    Link: https://lore.kernel.org/6723fc2070a96_60c3294dc@dwillia2-mobl3.amr.corp.intel.com.notmuch/ [1]
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Message-ID: <162f9dee05c729203b9ad6688db1ca2960b4b502.1731664295.git.kai.huang@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -33,6 +33,7 @@
 #ifndef __ASSEMBLY__
 
 #include <uapi/asm/mce.h>
+#include <asm/tdx_global_metadata.h>
 
 /*
  * Used by the #VE exception handler to gather the #VE exception
@@ -116,6 +117,7 @@ static inline u64 sc_retry(sc_func_t func, u64 fn,
 int tdx_cpu_enable(void);
 int tdx_enable(void);
 const char *tdx_dump_mce_info(struct mce *m);
+const struct tdx_sys_info *tdx_get_sysinfo(void);
 
 int tdx_guest_keyid_alloc(void);
 void tdx_guest_keyid_free(unsigned int keyid);
@@ -189,6 +191,7 @@ static inline void tdx_init(void) { }
 static inline int tdx_cpu_enable(void) { return -ENODEV; }
 static inline int tdx_enable(void)  { return -ENODEV; }
 static inline const char *tdx_dump_mce_info(struct mce *m) { return NULL; }
+static inline const struct tdx_sys_info *tdx_get_sysinfo(void) { return NULL; }
 #endif	/* CONFIG_INTEL_TDX_HOST */
 
 #endif /* !__ASSEMBLY__ */

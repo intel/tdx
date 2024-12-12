c6c9de88f00f1cbd97c1c2b007471dae2564a21c
Reinette Chatre <reinette.chatre@intel.com>
2024-12-11 20:01:50 -0800

    amend! KVM: selftests: TDX: Add basic get_td_vmcall_info test
    KVM: selftests: TDX: Add basic get_td_vmcall_info test
    
    The test calls get_td_vmcall_info from the guest and verifies the
    expected returned values.
    
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Signed-off-by: Ryan Afranji <afranji@google.com>
    ---
    - Yan: rebased to tdx_kvm_dev-2024-06-21
    
    - Reinette: Remove "Change-Id:" tag.

--- tools/testing/selftests/kvm/x86_64/tdx_vm_tests.c
+++ tools/testing/selftests/kvm/x86_64/tdx_vm_tests.c
@@ -348,7 +348,7 @@ int main(int argc, char **argv)
 	if (!is_tdx_enabled())
 		ksft_exit_skip("TDX is not supported by the KVM. Exiting.\n");
 
-	ksft_set_plan(4);
+	ksft_set_plan(5);
 	ksft_test_result(!run_in_new_process(&verify_td_lifecycle),
 			 "verify_td_lifecycle\n");
 	ksft_test_result(!run_in_new_process(&verify_report_fatal_error),
@@ -357,7 +357,8 @@ int main(int argc, char **argv)
 			 "verify_td_ioexit\n");
 	ksft_test_result(!run_in_new_process(&verify_td_cpuid),
 			 "verify_td_cpuid\n");
-	run_in_new_process(&verify_get_td_vmcall_info);
+	ksft_test_result(!run_in_new_process(&verify_get_td_vmcall_info),
+			 "verify_get_td_vmcall_info\n");
 
 	ksft_finished();
 	return 0;

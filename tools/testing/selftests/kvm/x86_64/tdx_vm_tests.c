6cc3246d50c4ca323e25bbc7d817576fa4464313
Reinette Chatre <reinette.chatre@intel.com>
2024-12-11 19:55:37 -0800

    amend! KVM: selftests: TDX: Add basic TDX CPUID test
    KVM: selftests: TDX: Add basic TDX CPUID test
    
    The test reads CPUID values from inside a TD VM and compare them
    to expected values.
    
    The test targets CPUID values which are virtualized as "As Configured",
    "As Configured (if Native)", "Calculated", "Fixed" and "Native"
    according to the TDX spec.
    
    Co-developed-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Signed-off-by: Ryan Afranji <afranji@google.com>
    ---
    - Yan: rebased to tdx_kvm_dev-2024-06-21
    
    - Reinette: Squash fix from Isaku to update verify_td_cpuid() fma check.
      First ensure that CPUID entries used in comparison are those the
      vCPU was initialized with after making TDX adjustments and then
      guest cpuid.1.ecx[12] can be checked with value of cpuid passed to
      KVM_TDX_INIT_VM. Give attribution.
    
    - Reinette: Remove "Change-Id:" tag.

--- tools/testing/selftests/kvm/x86_64/tdx_vm_tests.c
+++ tools/testing/selftests/kvm/x86_64/tdx_vm_tests.c
@@ -269,14 +269,15 @@ int main(int argc, char **argv)
 	if (!is_tdx_enabled())
 		ksft_exit_skip("TDX is not supported by the KVM. Exiting.\n");
 
-	ksft_set_plan(3);
+	ksft_set_plan(4);
 	ksft_test_result(!run_in_new_process(&verify_td_lifecycle),
 			 "verify_td_lifecycle\n");
 	ksft_test_result(!run_in_new_process(&verify_report_fatal_error),
 			 "verify_report_fatal_error\n");
 	ksft_test_result(!run_in_new_process(&verify_td_ioexit),
 			 "verify_td_ioexit\n");
-	run_in_new_process(&verify_td_cpuid);
+	ksft_test_result(!run_in_new_process(&verify_td_cpuid),
+			 "verify_td_cpuid\n");
 
 	ksft_finished();
 	return 0;

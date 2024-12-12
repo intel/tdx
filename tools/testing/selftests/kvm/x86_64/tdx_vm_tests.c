019a7f0476ad21dc1511b8fbe73d3553d32a8a55
Reinette Chatre <reinette.chatre@intel.com>
2024-12-11 20:04:46 -0800

    amend! KVM: selftests: TDX: Add TDX IO writes test
    KVM: selftests: TDX: Add TDX IO writes test
    
    The test verifies IO writes of various sizes from the guest to the host.
    
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Signed-off-by: Ryan Afranji <afranji@google.com>
    ---
    - Reinette: Squash fix from Binbin to fix the array member from which the
      error code can be obtained. No attribution for small change.
    
    - Reinette: Remove "Change-Id:" tag.

--- tools/testing/selftests/kvm/x86_64/tdx_vm_tests.c
+++ tools/testing/selftests/kvm/x86_64/tdx_vm_tests.c
@@ -438,7 +438,7 @@ int main(int argc, char **argv)
 	if (!is_tdx_enabled())
 		ksft_exit_skip("TDX is not supported by the KVM. Exiting.\n");
 
-	ksft_set_plan(5);
+	ksft_set_plan(6);
 	ksft_test_result(!run_in_new_process(&verify_td_lifecycle),
 			 "verify_td_lifecycle\n");
 	ksft_test_result(!run_in_new_process(&verify_report_fatal_error),
@@ -449,7 +449,8 @@ int main(int argc, char **argv)
 			 "verify_td_cpuid\n");
 	ksft_test_result(!run_in_new_process(&verify_get_td_vmcall_info),
 			 "verify_get_td_vmcall_info\n");
-	run_in_new_process(&verify_guest_writes);
+	ksft_test_result(!run_in_new_process(&verify_guest_writes),
+			 "verify_guest_writes\n");
 
 	ksft_finished();
 	return 0;

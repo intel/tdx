425a84e30f424863060e171e3bffaa455967129f
Reinette Chatre <reinette.chatre@intel.com>
2025-01-13 15:55:18 -0800

    amend! KVM: selftests: TDX: Add TDX IO writes test
    KVM: selftests: TDX: Add TDX IO writes test
    
    The test verifies IO writes of various sizes from the guest to the host.
    
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Signed-off-by: Ryan Afranji <afranji@google.com>
    ---
    - Reinette: Squash fix from Binbin to fix the array member from which the
      error code can be obtained. No attribution for small change.
    
    - Reinette: Remove "Change-Id:" tag.
    
    - Reinette: checkpatch.pl fixes: alignment should match open parenthesis
    
    - Reinette: switch to reverse fir where practical

--- tools/testing/selftests/kvm/x86/tdx_vm_tests.c
+++ tools/testing/selftests/kvm/x86/tdx_vm_tests.c
@@ -345,9 +345,9 @@ void verify_get_td_vmcall_info(void)
  */
 void guest_io_writes(void)
 {
-	uint64_t byte_1 = 0xAB;
-	uint64_t byte_2 = 0xABCD;
 	uint64_t byte_4 = 0xFFABCDEF;
+	uint64_t byte_2 = 0xABCD;
+	uint64_t byte_1 = 0xAB;
 	uint64_t ret;
 
 	ret = tdg_vp_vmcall_instruction_io(TDX_IO_WRITES_TEST_PORT, 1,
@@ -380,12 +380,11 @@ void guest_io_writes(void)
 
 void verify_guest_writes(void)
 {
-	struct kvm_vm *vm;
 	struct kvm_vcpu *vcpu;
-
-	uint8_t byte_1;
-	uint16_t byte_2;
+	struct kvm_vm *vm;
 	uint32_t byte_4;
+	uint16_t byte_2;
+	uint8_t byte_1;
 
 	vm = td_create();
 	td_initialize(vm, VM_MEM_SRC_ANONYMOUS, 0);

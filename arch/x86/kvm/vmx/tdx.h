3a174261c024cbd84ab61cd30273b1fadb192871
Paolo Bonzini <pbonzini@redhat.com>
2025-01-14 12:34:46 -0500

    KVM: TDX: initialize VM with TDX specific parameters
    After the crypto-protection key has been configured, TDX requires a
    VM-scope initialization as a step of creating the TDX guest.  This
    "per-VM" TDX initialization does the global configurations/features that
    the TDX guest can support, such as guest's CPUIDs (emulated by the TDX
    module), the maximum number of vcpus etc.
    
    This "per-VM" TDX initialization must be done before any "vcpu-scope" TDX
    initialization.  To match this better, require the KVM_TDX_INIT_VM IOCTL()
    to be done before KVM creates any vcpus.
    
    Co-developed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Message-ID: <20241030190039.77971-20-rick.p.edgecombe@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/tdx.h
+++ arch/x86/kvm/vmx/tdx.h
@@ -11,9 +11,23 @@ void tdx_cleanup(void);
 
 extern bool enable_tdx;
 
+/* TDX module hardware states. These follow the TDX module OP_STATEs. */
+enum kvm_tdx_state {
+	TD_STATE_UNINITIALIZED = 0,
+	TD_STATE_INITIALIZED,
+	TD_STATE_RUNNABLE,
+};
+
 struct kvm_tdx {
 	struct kvm kvm;
+
 	int hkid;
+	enum kvm_tdx_state state;
+
+	u64 attributes;
+	u64 xfam;
+
+	u64 tsc_offset;
 
 	struct tdx_td td;
 };
@@ -43,6 +57,17 @@ static __always_inline struct vcpu_tdx *to_tdx(struct kvm_vcpu *vcpu)
 	return container_of(vcpu, struct vcpu_tdx, vcpu);
 }
 
+static __always_inline u64 td_tdcs_exec_read64(struct kvm_tdx *kvm_tdx, u32 field)
+{
+	u64 err, data;
+
+	err = tdh_mng_rd(&kvm_tdx->td, TDCS_EXEC(field), &data);
+	if (unlikely(err)) {
+		pr_err("TDH_MNG_RD[EXEC.0x%x] failed: 0x%llx\n", field, err);
+		return 0;
+	}
+	return data;
+}
 #else
 static inline int tdx_bringup(void) { return 0; }
 static inline void tdx_cleanup(void) {}

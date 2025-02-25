1a62e9e9ee4f51c066fd524cc538d4afe4f26908
Isaku Yamahata <isaku.yamahata@intel.com>
2025-02-25 12:45:13 -0500

    KVM: TDX: create/destroy VM structure
    Implement managing the TDX private KeyID to implement, create, destroy
    and free for a TDX guest.
    
    When creating at TDX guest, assign a TDX private KeyID for the TDX guest
    for memory encryption, and allocate pages for the guest. These are used
    for the Trust Domain Root (TDR) and Trust Domain Control Structure (TDCS).
    
    On destruction, free the allocated pages, and the KeyID.
    
    Before tearing down the private page tables, TDX requires the guest TD to
    be destroyed by reclaiming the KeyID. Do it in the vm_pre_destroy() kvm_x86_ops
    hook. The TDR control structures can be freed in the vm_destroy() hook,
    which runs last.
    
    Co-developed-by: Tony Lindgren <tony.lindgren@linux.intel.com>
    Signed-off-by: Tony Lindgren <tony.lindgren@linux.intel.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Co-developed-by: Kai Huang <kai.huang@intel.com>
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Co-developed-by: Yan Zhao <yan.y.zhao@intel.com>
    Signed-off-by: Yan Zhao <yan.y.zhao@intel.com>
    Co-developed-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    ---
     - Fix build issue in kvm-coco-queue
     - Init ret earlier to fix __tdx_td_init() error handling. (Chao)
     - Standardize -EAGAIN for __tdx_td_init() retry errors (Rick)
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -12843,6 +12843,7 @@ void kvm_arch_pre_destroy_vm(struct kvm *kvm)
 	kvm_free_pit(kvm);
 
 	kvm_mmu_pre_destroy_vm(kvm);
+	static_call_cond(kvm_x86_vm_pre_destroy)(kvm);
 }
 
 void kvm_arch_destroy_vm(struct kvm *kvm)

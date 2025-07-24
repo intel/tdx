e9e821de25de3242d1fc3cc53ecaf227caa5a740
Ackerley Tng <ackerleytng@google.com>
2025-07-24 14:44:09 -0700

    mm: hugetlb: Consolidate gbl_chg interpretation in alloc_hugetlb_folio
    The gbl_chg parameter was passed from alloc_hugetlb_folio() into
    dequeue_hugetlb_folio_vma(), leaking the concept of a "global
    change", (requiring a new page vs. using a reservation) into the
    dequeue helper.
    
    This check belongs in the higher-level allocation
    function. Consolidate the logic for interpreting gbl_chg into
    alloc_hugetlb_folio() by checking for available pages there before
    attempting a dequeue.
    
    This simplifies dequeue_hugetlb_folio_vma() by removing its gbl_chg
    parameter, allowing it to focus solely on dequeuing a folio according
    to the VMA's memory policy.
    
    Change-Id: I322862dee967d92ec26a0df9c348d476f2d6c3f3
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>
    Reviewed-by: James Houghton <jthoughton@google.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -1363,20 +1363,13 @@ static unsigned long available_huge_pages(struct hstate *h)
 
 static struct folio *dequeue_hugetlb_folio_vma(struct hstate *h,
 				struct vm_area_struct *vma,
-				unsigned long address, long gbl_chg)
+				unsigned long address)
 {
 	struct folio *folio = NULL;
 	struct mempolicy *mpol;
 	gfp_t gfp_mask;
 	nodemask_t *nodemask;
 	int nid;
-
-	/*
-	 * gbl_chg==1 means the allocation requires a new page that was not
-	 * reserved before.  Making sure there's at least one free page.
-	 */
-	if (gbl_chg && !available_huge_pages(h))
-		goto err;
 
 	gfp_mask = htlb_alloc_mask(h);
 	nid = huge_node(vma, address, gfp_mask, &mpol, &nodemask);
@@ -1395,9 +1388,6 @@ static struct folio *dequeue_hugetlb_folio_vma(struct hstate *h,
 
 	mpol_cond_put(mpol);
 	return folio;
-
-err:
-	return NULL;
 }
 
 /*
@@ -3017,11 +3007,14 @@ struct folio *alloc_hugetlb_folio(struct vm_area_struct *vma,
 
 	spin_lock_irq(&hugetlb_lock);
 	/*
-	 * glb_chg is passed to indicate whether or not a page must be taken
-	 * from the global free pool (global change).  gbl_chg == 0 indicates
-	 * a reservation exists for the allocation.
+	 * gbl_chg == 0 indicates a reservation exists for the allocation - so
+	 * try dequeuing a page. If there are available_huge_pages(), try using
+	 * them!
 	 */
-	folio = dequeue_hugetlb_folio_vma(h, vma, addr, gbl_chg);
+	folio = NULL;
+	if (!gbl_chg || available_huge_pages(h))
+		folio = dequeue_hugetlb_folio_vma(h, vma, addr);
+
 	if (!folio) {
 		spin_unlock_irq(&hugetlb_lock);
 		folio = alloc_buddy_hugetlb_folio_with_mpol(h, vma, addr);

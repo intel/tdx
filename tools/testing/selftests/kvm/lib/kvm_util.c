9dd9a2555245792ec674bf554145cbf358565631
Sean Christopherson <seanjc@google.com>
2025-10-15 14:42:31 -0700

    KVM: selftests: Check fd/flags provided to mmap() when setting up memslot
    Check that a valid fd provided to mmap() must be accompanied by MAP_SHARED.
    
    With an invalid fd (usually used for anonymous mappings), there are no
    constraints on mmap() flags.
    
    Add this check to make sure that when a guest_memfd is used as region->fd,
    the flag provided to mmap() will include MAP_SHARED.
    
    Change-Id: Ic27b3c58a3b91a35cb165acb03de913b6b2d7e19
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    [Rephrase assertion message.]
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>

--- tools/testing/selftests/kvm/lib/kvm_util.c
+++ tools/testing/selftests/kvm/lib/kvm_util.c
@@ -1062,6 +1062,9 @@ void vm_mem_add(struct kvm_vm *vm, enum vm_mem_backing_src_type src_type,
 					     src_type == VM_MEM_SRC_SHARED_HUGETLB);
 	}
 
+	TEST_ASSERT(region->fd == -1 || backing_src_is_shared(src_type),
+		    "A valid fd provided to mmap() must be accompanied by MAP_SHARED.");
+
 	region->mmap_start = __kvm_mmap(region->mmap_size, PROT_READ | PROT_WRITE,
 					vm_mem_backing_src_alias(src_type)->flag,
 					region->fd, mmap_offset);

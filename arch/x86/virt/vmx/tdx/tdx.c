dc371732895ddcfb55c0dc78ea13d45234785816
Peter Fang <peter.fang@intel.com>
2025-12-07 20:23:31 -0500

    fixup! x86/virt/tdx: Enable Quoting extension
    The Quoting extension generates TDX attestation Quotes in the TDX
    module, without using a discrete Quoting engine. Enable this feature by
    requesting it in TDH.SYS.CONFIG and TDH.SYS.UPDATE.
    
    Signed-off-by: Peter Fang <peter.fang@intel.com>
    Signed-off-by: Xu Yilun <yilun.xu@linux.intel.com>

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -1020,7 +1020,7 @@ static __init int construct_tdmrs(struct list_head *tmb_list,
 }
 
 /* List all kernel supported add-on features0 bits here */
-#define TDX_KERNEL_SUPPORTED_ADDON_FEATURES0	(0)
+#define TDX_KERNEL_SUPPORTED_ADDON_FEATURES0	(TDX_FEATURES0_QUOTE)
 
 static u64 get_tdx_addon_features0(void)
 {

torvalds/linux

commit b47ce1fed42eeb9ac8c07fcda6c795884826723d

x86/cpu: Detect VMX features on Intel, Centaur and Zhaoxin CPUs
Add an entry in struct cpuinfo_x86 to track VMX capabilities and fill
the capabilities during IA32_FEAT_CTL MSR initialization.

Make the VMX capabilities dependent on IA32_FEAT_CTL and
X86_FEATURE_NAMES so as to avoid unnecessary overhead on CPUs that can't
possibly support VMX, or when /proc/cpuinfo is not available.

Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
Signed-off-by: Borislav Petkov <bp@suse.de>
Link: https://lkml.kernel.org/r/20191221044513.21680-11-sean.j.christopherson@intel.com

arch/x86/include/asm/processor.h
@@ -86,6 +86,9 @@ struct cpuinfo_x86 {
 	/* Number of 4K pages in DTLB/ITLB combined(in pages): */
 	int			x86_tlbsize;
 #endif
+#ifdef CONFIG_X86_VMX_FEATURE_NAMES
+	__u32			vmx_capability[NVMXINTS];
+#endif
 	__u8			x86_virt_bits;
 	__u8			x86_phys_bits;
 	/* CPUID returned core id bits: */

arch/x86/Kconfig.cpu
@@ -391,6 +391,10 @@ config IA32_FEAT_CTL
 	def_bool y
 	depends on CPU_SUP_INTEL || CPU_SUP_CENTAUR || CPU_SUP_ZHAOXIN
 
+config X86_VMX_FEATURE_NAMES
+	def_bool y
+	depends on IA32_FEAT_CTL && X86_FEATURE_NAMES
+
 menuconfig PROCESSOR_SELECT
 	bool "Supported processor vendors" if EXPERT
 	---help---

arch/x86/include/asm/vmxfeatures.h
@@ -3,6 +3,11 @@
 #define _ASM_X86_VMXFEATURES_H
 
 /*
+ * Defines VMX CPU feature bits
+ */
+#define NVMXINTS			3 /* N 32-bit words worth of info */
+
+/*
  * Note: If the comment begins with a quoted string, that string is used
  * in /proc/cpuinfo instead of the macro name.  If the string is "",
  * this feature bit is not displayed in /proc/cpuinfo at all.

arch/x86/kernel/cpu/common.c
@@ -1449,6 +1449,9 @@ static void identify_cpu(struct cpuinfo_x86 *c)
 #endif
 	c->x86_cache_alignment = c->x86_clflush_size;
 	memset(&c->x86_capability, 0, sizeof(c->x86_capability));
+#ifdef CONFIG_X86_VMX_FEATURE_NAMES
+	memset(&c->vmx_capability, 0, sizeof(c->vmx_capability));
+#endif
 
 	generic_identify(c);
 

arch/x86/kernel/cpu/feat_ctl.c
@@ -4,9 +4,79 @@
 #include <asm/cpufeature.h>
 #include <asm/msr-index.h>
 #include <asm/processor.h>
+#include <asm/vmx.h>
 
 #undef pr_fmt
 #define pr_fmt(fmt)	"x86/cpu: " fmt
+
+#ifdef CONFIG_X86_VMX_FEATURE_NAMES
+enum vmx_feature_leafs {
+	MISC_FEATURES = 0,
+	PRIMARY_CTLS,
+	SECONDARY_CTLS,
+	NR_VMX_FEATURE_WORDS,
+};
+
+#define VMX_F(x) BIT(VMX_FEATURE_##x & 0x1f)
+
+static void init_vmx_capabilities(struct cpuinfo_x86 *c)
+{
+	u32 supported, funcs, ept, vpid, ign;
+
+	BUILD_BUG_ON(NVMXINTS != NR_VMX_FEATURE_WORDS);
+
+	/*
+	 * The high bits contain the allowed-1 settings, i.e. features that can
+	 * be turned on.  The low bits contain the allowed-0 settings, i.e.
+	 * features that can be turned off.  Ignore the allowed-0 settings,
+	 * if a feature can be turned on then it's supported.
+	 *
+	 * Use raw rdmsr() for primary processor controls and pin controls MSRs
+	 * as they exist on any CPU that supports VMX, i.e. we want the WARN if
+	 * the RDMSR faults.
+	 */
+	rdmsr(MSR_IA32_VMX_PROCBASED_CTLS, ign, supported);
+	c->vmx_capability[PRIMARY_CTLS] = supported;
+
+	rdmsr_safe(MSR_IA32_VMX_PROCBASED_CTLS2, &ign, &supported);
+	c->vmx_capability[SECONDARY_CTLS] = supported;
+
+	rdmsr(MSR_IA32_VMX_PINBASED_CTLS, ign, supported);
+	rdmsr_safe(MSR_IA32_VMX_VMFUNC, &ign, &funcs);
+
+	/*
+	 * Except for EPT+VPID, which enumerates support for both in a single
+	 * MSR, low for EPT, high for VPID.
+	 */
+	rdmsr_safe(MSR_IA32_VMX_EPT_VPID_CAP, &ept, &vpid);
+
+	/* Pin, EPT, VPID and VM-Func are merged into a single word. */
+	WARN_ON_ONCE(supported >> 16);
+	WARN_ON_ONCE(funcs >> 4);
+	c->vmx_capability[MISC_FEATURES] = (supported & 0xffff) |
+					   ((vpid & 0x1) << 16) |
+					   ((funcs & 0xf) << 28);
+
+	/* EPT bits are full on scattered and must be manually handled. */
+	if (ept & VMX_EPT_EXECUTE_ONLY_BIT)
+		c->vmx_capability[MISC_FEATURES] |= VMX_F(EPT_EXECUTE_ONLY);
+	if (ept & VMX_EPT_AD_BIT)
+		c->vmx_capability[MISC_FEATURES] |= VMX_F(EPT_AD);
+	if (ept & VMX_EPT_1GB_PAGE_BIT)
+		c->vmx_capability[MISC_FEATURES] |= VMX_F(EPT_1GB);
+
+	/* Synthetic APIC features that are aggregates of multiple features. */
+	if ((c->vmx_capability[PRIMARY_CTLS] & VMX_F(VIRTUAL_TPR)) &&
+	    (c->vmx_capability[SECONDARY_CTLS] & VMX_F(VIRT_APIC_ACCESSES)))
+		c->vmx_capability[MISC_FEATURES] |= VMX_F(FLEXPRIORITY);
+
+	if ((c->vmx_capability[PRIMARY_CTLS] & VMX_F(VIRTUAL_TPR)) &&
+	    (c->vmx_capability[SECONDARY_CTLS] & VMX_F(APIC_REGISTER_VIRT)) &&
+	    (c->vmx_capability[SECONDARY_CTLS] & VMX_F(VIRT_INTR_DELIVERY)) &&
+	    (c->vmx_capability[MISC_FEATURES] & VMX_F(POSTED_INTR)))
+		c->vmx_capability[MISC_FEATURES] |= VMX_F(APICV);
+}
+#endif /* CONFIG_X86_VMX_FEATURE_NAMES */
 
 void init_ia32_feat_ctl(struct cpuinfo_x86 *c)
 {
@@ -50,5 +120,9 @@ update_caps:
 		pr_err_once("VMX (%s TXT) disabled by BIOS\n",
 			    tboot ? "inside" : "outside");
 		clear_cpu_cap(c, X86_FEATURE_VMX);
+	} else {
+#ifdef CONFIG_X86_VMX_FEATURE_NAMES
+		init_vmx_capabilities(c);
+#endif
 	}
 }

intel/tdx

commit 133c714f9a91af7e4216cd8e99e5492bbd100632

x86/cpu: Align the vmx_capability array to size of unsigned long
A WARNING splat was triggered during system boot with a kernel built
with CONFIG_DEBUG_ATOMIC=y and CONFIG_DEBUG_ATOMIC_LARGEST_ALIGN=y on
one Intel platform:

  systemd[1]: DMI BIOS Extension table does not indicate virtualization.
  ------------[ cut here ]------------
  (unsigned long)v & mask
  WARNING: ./include/linux/instrumented.h:67 at show_cpuinfo+0x4e9/0x620, CPU#133: systemd/1
  ...
  RIP: 0010:show_cpuinfo+0x4e9/0x620
  ...
  Call Trace:
   <TASK>
   seq_read_iter+0x130/0x4b0
   ? rw_verify_area+0x15b/0x200
   vfs_read+0x224/0x350
   ksys_read+0x61/0xd0
   do_syscall_64+0x12c/0x1510

show_cpuinfo() calls test_bit() for each bit in the cpuinfo_x86's
vmx_capability array to print the feature name of that bit in
/proc/cpuinfo if that bit is set.

The vmx_capability array is type __u32 thus it is aligned to 4 bytes.
But test_bit(), which is an atomic bit operation, requires the array to
be aligned to size of unsigned long, causing the above WARNING splat.

The alignment requirement of the atomic operation is to make sure on x86
an atomic instruction can never access data crossing cacheline boundary,
which can cause severe performance degradation.  If the kernel has split
lock detection enabled, this kind access can cause #AC split lock
exception and crash the kernel.

The cpuinfo_x86's x86_capability array once was only aligned to __u32 as
well.  The commit db8c33f8b5be ("x86/cpu: Align the x86_capability array
to size of unsigned long") changed the alignment for x86_capability as a
preparation to support split lock detection in the kernel.

Follow it and do similar treatment for the vmx_capability array.

Note:

- No real split lock exception was triggered but only the WARNING splat
  (the kernel enabled the split lock detection to "warn" mode).

- The test_bit() in show_cpuinfo() is the only atomic operation user of
  the vmx_capability array.  It's arguable whether it can be changed to
  non-atomic operation but there's no __test_bit().  Also changing the
  alignment to size of unsigned long addresses the split lock concern
  once for all.

- The vmx_capability array itself and the test_bit() in show_cpuinfo()
  were introduced in two different commits, but they were both merged
  after split lock detection was merged in commit 6650cdd9a8cc
  ("x86/split_lock: Enable split lock detection by kernel"), hence the
  'Fixes' tags.

Fixes: b47ce1fed42e ("x86/cpu: Detect VMX features on Intel, Centaur and Zhaoxin CPUs")

arch/x86/include/asm/processor.h
@@ -150,7 +150,11 @@ struct cpuinfo_x86 {
 	int			x86_tlbsize;
 #endif
 #ifdef CONFIG_X86_VMX_FEATURE_NAMES
-	__u32			vmx_capability[NVMXINTS];
+	/* See the comment of 'x86_capability_alignment' below */
+	union {
+		__u32		vmx_capability[NVMXINTS];
+		unsigned long	vmx_capability_alignment;
+	};
 #endif
 	__u8			x86_virt_bits;
 	__u8			x86_phys_bits;
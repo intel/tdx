e38709d89be927f1a57b12b35c33cc80ab269868
Xu Yilun <yilun.xu@linux.intel.com>
2026-03-26 01:11:11 -0400

    x86/virt/tdx: Detect if the extensions initialization is required
    Some add-on features require TDX module extensions. The TDX module
    provides a metadata field "ext_required" to indicate this requirement.
    
    Add the first step of TDX module extensions initialization by detecting
    if the extensions are required:
    
      1. Check if the extensions are supported via TDX_FEATURES0_EXT. If
         not, ext_required is not readable.
      2. Check if any TDX feature needs the extensions via ext_required.
    
    Skip the extensions initialization when it is not required.
    
    Currently all metadata fields are read at the very beginning of TDX
    module initialization. However, ext_required is only valid after the
    add-on feature configuration, so it cannot use the existing metadata
    reading method.
    
    Add a dedicated metadata reading interface for the extensions, call it
    after add-on feature configuration.
    
    Signed-off-by: Xu Yilun <yilun.xu@linux.intel.com>
    Reviewed-by: Tony Lindgren <tony.lindgren@linux.intel.com>
    ---
    v1:
     - Include struct tdx_sys_info_ext in struct tdx_sys_info.

--- arch/x86/include/asm/tdx_global_metadata.h
+++ arch/x86/include/asm/tdx_global_metadata.h
@@ -44,12 +44,17 @@ struct tdx_sys_info_handoff {
 	u16 module_hv;
 };
 
+struct tdx_sys_info_ext {
+	bool ext_required;
+};
+
 struct tdx_sys_info {
 	struct tdx_sys_info_version version;
 	struct tdx_sys_info_features features;
 	struct tdx_sys_info_tdmr tdmr;
 	struct tdx_sys_info_td_ctrl td_ctrl;
 	struct tdx_sys_info_td_conf td_conf;
+	struct tdx_sys_info_ext ext;
 };
 
 #endif

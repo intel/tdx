5693f53a35de68118f449e09e05b3375c4cb001b
Sean Christopherson <seanjc@google.com>
2026-04-23 14:37:03 +0800

    KVM: TDX: Drop kvm_x86_ops.remove_external_spte()
    Drop kvm_x86_ops.remove_external_spte(), and instead handle the removal of
    leaf SPTEs in the S-EPT (a.k.a. external page table) in
    kvm_x86_ops.set_external_spte(). This will also allow extending
    tdx_sept_set_private_spte() to support splitting a huge S-EPT entry without
    needing yet another kvm_x86_ops hook.
    
    Now all changes for removing leaf mirror SPTEs are propagated through
    kvm_x86_ops.set_external_spte().
    - When removing leaf mirror SPTEs under shared mmu_lock (though currently
      no path can trigger this scenario and TDX does not support this
      scenario), tdx_sept_remove_private_spte() may produce a warning due to
      lockdep_assert_held_write() or may return -EIO and trigger TDX_BUG_ON()
      due to concurrent BLOCK, TRACK, REMOVE.
    - When removing leaf mirror SPTEs under exclusive mmu_lock, all errors are
      unexpected. If any error occurs in this scenario,
      tdx_sept_remove_private_spte() will return -EIO and trigger KVM_BUG_ON().
      A redundant KVM_BUG_ON() call will also be triggered in TDP MMU core in
      handle_changed_spte(), which is benign (the WARN will fire if and only if
      the VM isn't already bugged).
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>
    Signed-off-by: Yan Zhao <yan.y.zhao@intel.com>
    ---
    MMU_refactors v2:
    - Added expected lock and valid scenarios in function comment of
      tdx_sept_set_private_spte(). (Yan/Rick)
    - Updated patch title (was "Handle removal of leaf SPTEs in
      .set_private_spte()"), since atomic zaps of leaf SPTEs are already
      handled in kvm_x86_ops.set_external_spte() before this patch. (Yan).

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1899,9 +1899,6 @@ struct kvm_x86_ops {
 	int (*free_external_spt)(struct kvm *kvm, gfn_t gfn, enum pg_level level,
 				 void *external_spt);
 
-	/* Update external page table from spte getting removed, and flush TLB. */
-	void (*remove_external_spte)(struct kvm *kvm, gfn_t gfn, enum pg_level level,
-				     u64 mirror_spte);
 
 	bool (*has_wbinvd_exit)(void);
 
